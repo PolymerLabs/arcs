e53daa88be4379fd65419e5eae643d1bdb9908fb
Douglas Stockwell <dstockwell@chromium.org>
2017-03-13 13:49:22 +1100

    Move data-layer components to separate files

--- runtime/data-layer.js
+++ runtime/data-layer.js
@@ -10,97 +10,14 @@
 "use strict";
 
 const assert = require('assert');
+const View = require('./view.js');
+const Identifier = require('./identifier.js');
+const Symbols = require('./symbols.js');
+const Entity = require('./entity.js');
+const Type = require('./type.js');
+const Relation = require('./relation.js');
 
-// TODO: This won't be needed once data is transferred between contexts.
-function cloneData(data) {
-  return JSON.parse(JSON.stringify(data));
-}
-
-function restore(entry, scope) {
-  assert(scope);
-  let {id, data} = entry;
-  var entity = Entity.fromLiteral(id, cloneData(data));
-  // TODO: Relation magic should happen elsewhere, and be better.
-  if (scope.typeFor(entity).isRelation) {
-    let ids = data.map(literal => Identifier.fromLiteral(literal, scope));
-    let entities = ids.map(id => scope._viewFor(id.type).get(id));
-    assert(!entities.includes(undefined));
-    entity = new Relation(...entities);
-    entity.entities = entities;
-    entity[identifier] = id;
-  }
-  return entity;
-}
-
-function emptyRegistrationSlot() {};
-
-class View {
-  constructor(type, scope) {
-    assert(scope);
-    this.type = type;
-    this._scope = scope;
-    this.data = [];
-    this.observers = [];
-  }
-
-  *iterator(start, end) {
-    while (start < end) {
-      yield restore(this.data[start++], this._scope);
-    }
-  }
-
-  get(id) {
-    for (let entry of this.data) {
-      if (JSON.stringify(entry.id.toLiteral()) == JSON.stringify(id.toLiteral())) {
-        return restore(entry, this._scope);
-      }
-    }
-  }
-
-  checkpoint() {
-    if (this._checkpoint == undefined)
-      this._checkpoint = this.data.length;
-  }
-
-  revert() {
-    if (this._checkpoint == undefined)
-      return;
-    this.data.splice(this._checkpoint);
-    this._checkpoint = undefined;
-  }
-
-  register(observer) {
-    var rid = this.observers.length;
-    this.observers.push(observer);
-    if (this.data.length > 0)
-      observer(this.iterator(0, this.data.length));
-    return rid;
-  }
-
-  unregister(rid) {
-    for (var i = rid + 1; i < this.observers.length; i++) {
-      if (this.observers[i] !== emptyRegistrationSlot) {
-        this.observers[rid] = emptyRegistrationSlot;
-        return;
-      }
-    }
-    this.observers.splice(rid);
-  }
-
-  store(entity) {
-    console.log("storing", entity, entity[identifier]);
-    let id = entity[identifier];
-    let data = cloneData(entity.toLiteral());
-    this.data.push({
-      id,
-      data: data,
-    });
-    for (var observer of this.observers) {
-      let clone = Entity.fromLiteral(id, cloneData(data));
-      observer([clone][Symbol.iterator]());
-    }
-  }
-}
+let identifier = Symbols.identifier;
 
 class Scope {
   constructor() {
@@ -137,7 +54,7 @@ class Scope {
       return this.typeFor(classOrInstance.constructor);
     }
     if (!this._types.has(classOrInstance)) {
-      let key = classOrInstance.key || this.nextType++;
+      let key = classOrInstance.key || this._nextType++;
       this._types.set(classOrInstance, new Type(key, this));
     }
     return this._types.get(classOrInstance);
@@ -161,76 +78,9 @@ class Scope {
       if (entity instanceof Relation) {
         entity.entities.forEach(entity => this._viewFor(this.typeFor(entity)).store(entity));
       }
+      console.log(entity, this.typeFor(entity));
       this._viewFor(this.typeFor(entity)).store(entity);
     }
-  }
-}
-
-class Type {
-  constructor(key, scope) {
-    assert(scope);
-    let normalized = JSON.stringify(key);
-    let type = scope._types.get(normalized);
-    if (type) {
-      return type;
-    }
-    this.key = key;
-    scope._types.set(normalized, this);
-  }
-  get isRelation() {
-    return Array.isArray(this.key);
-  }
-  toLiteral() {
-    return this.key;
-  }
-  static fromLiteral(literal, scope) {
-    assert(scope);
-    return new Type(literal, scope);
-  }
-}
-
-// TODO: relation identifier should incorporate key/value identifiers
-class Identifier {
-  constructor(view, type, key) {
-    this.view = type;
-    this.type = type;
-    this.key = key;
-  }
-  toLiteral() {
-    return [this.view, this.type.toLiteral(), this.key];
-  }
-  static fromLiteral(data, scope) {
-    assert(scope);
-    let [view, literalType, key] = data;
-    return new Identifier(view, Type.fromLiteral(literalType, scope), key);
-  }
-}
-
-let identifier = Symbol('id');
-class Entity {
-  constructor() {
-    this[identifier] = undefined;
-  }
-  get data() {
-    return undefined;
-  }
-  // TODO: clean up internal glue
-  identify(view, scope) {
-    assert(scope);
-    if (this[identifier]) {
-      // assert view correct?
-      return;
-    }
-    this[identifier] = scope._newIdentifier(view, scope.typeFor(this));
-  }
-  toLiteral() {
-    return this.data;
-  }
-  static fromLiteral(id, literal) {
-    // TODO: restore as the appropriate type from type registry?
-    let entity = new BasicEntity(literal);
-    entity[identifier] = id;
-    return entity;
   }
 }
 
@@ -250,21 +100,6 @@ function testEntityClass(type) {
       return type;
     }
   };
-}
-
-// TODO: Should relations normalized by another layer, or here?
-class Relation extends Entity {
-  constructor(...entities) {
-    super();
-    this.entities = entities;
-  }
-  get data() {
-    return this.entities.map(entity => entity[identifier].toLiteral());
-  }
-  static typeFor(relation, scope) {
-    assert(scope);
-    return new Type(relation.entities.map(entity => scope.typeFor(entity)), scope);
-  }
 }
 
 Object.assign(exports, {
---
+++ runtime/identifier.js
@@ -0,0 +1,30 @@
+// @license
+// Copyright (c) 2017 Google Inc. All rights reserved.
+// This code may only be used under the BSD style license found at
+// http://polymer.github.io/LICENSE.txt
+// Code distributed by Google as part of this project is also
+// subject to an additional IP rights grant found at
+// http://polymer.github.io/PATENTS.txt
+'use strict';
+
+const assert = require('assert');
+const Type = require('./type.js');
+
+// TODO: relation identifier should incorporate key/value identifiers
+class Identifier {
+  constructor(view, type, key) {
+    this.view = type;
+    this.type = type;
+    this.key = key;
+  }
+  toLiteral() {
+    return [this.view, this.type.toLiteral(), this.key];
+  }
+  static fromLiteral(data, scope) {
+    assert(scope);
+    let [view, literalType, key] = data;
+    return new Identifier(view, Type.fromLiteral(literalType, scope), key);
+  }
+}
+
+module.exports = Identifier;
