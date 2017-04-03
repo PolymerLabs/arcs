b5c35b9d0e46ba89b0a957a3d9c4add1f79f4aff
Douglas Stockwell <dstockwell@chromium.org>
2017-04-03 12:06:08 +1000

    Sketch PEC API

---
+++ runtime/particle-execution-context.js
@@ -0,0 +1,38 @@
+// @license
+// Copyright (c) 2017 Google Inc. All rights reserved.
+// This code may only be used under the BSD style license found at
+// http://polymer.github.io/LICENSE.txt
+// Code distributed by Google as part of this project is also
+// subject to an additional IP rights grant found at
+// http://polymer.github.io/PATENTS.txt
+"use strict";
+
+class ParticleExecutionContext {
+  constructor() {
+  }
+
+  // Instantiates `particle` in this context, connecting `views` to the particle's inputs and outputs.
+  // `mutateCallback` will be called each time the particle mutates a view or entity.
+  // Returns an identifier to refer to the particle (in `dispatch`).
+  instantiate(particle, views, mutateCallback) {
+    // views => [[name, viewId]]
+    // returns particleId
+    throw 'unimplemented';
+  }
+
+  // Dispatches an event to the particle identified by `particleId` for the view or entity identified
+  // by `entityId` concerning `eventDetails. The `complete` flag indicates whether there are any further
+  // events to be dispatched to the same particle.
+  dispatch(particleId, entityId, eventDetails, complete) {
+    throw 'unimplemented';
+  }
+
+  // Returns a promise which resolves when the PEC becomes idle (no known input processing).
+  get idle() {
+    throw 'unimplemented';
+  }
+
+  // TODO: Where did relevance go?
+}
+
+module.exports = ParticleExecutionContext;
