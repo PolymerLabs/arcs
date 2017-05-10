06ca60b82e12b065c096eae99e9dcddc6c3f3f18
Scott J Miles <sjmiles@google.com>
2017-05-10 15:55:27 -0700

    Refactoring to move output context from SlotManager into Slot, support polymorphic Slot types (i.e. DomSlot, MockDomSlot)

---
+++ runtime/slot.js
@@ -0,0 +1,69 @@
+/**
+ * @license
+ * Copyright (c) 2017 Google Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+"use strict";
+
+const assert = require('assert');
+
+class Slot {
+  constructor(slotid) {
+    this._slotid = slotid;
+    this._dom = null;
+    this._exposedView = null;
+    this._particleSpec = null;
+    this._pendingRequestsHandlers = [];
+  }
+  get slotid() { 
+    return this._slotid; 
+  }
+  get particleSpec() { 
+    return this._particleSpec; 
+  }
+  associateWithParticle(particleSpec) {
+    assert(!this._particleSpec, "Particle spec already set, cannot associate slot");
+    assert(!this._exposedView ||
+           this._particleSpec.renderMap.get(this._slotid) == this._exposedView,
+           "Cannot associate particle-spec with an unmatching view.");
+    this._particleSpec = particleSpec;
+  }
+  disassociateParticle() {
+    assert(this._particleSpec, "Particle spec is not set, cannot disassociate slot");
+    this._particleSpec = null;
+  }
+  isAssociated() {
+    return !!this._particleSpec;
+  }
+  addPendingRequest(handler) {
+    this._pendingRequestsHandlers.push(handler);
+  }
+  providePendingSlot() {
+    assert(!this.isAssociated(), "Cannot provide associated slot.");
+    let handler = this._pendingRequestsHandlers.shift();
+    if (handler) {
+      handler(this);
+    }
+  }
+  /*
+    abstract methods
+  */
+  initialize(context, exposedView) {
+  }
+  isInitialized() {
+  }
+  uninitialize() {
+  }
+  get content() {
+  }
+  render(content, eventHandler) {
+  }
+  derender() {
+  }
+}
+
+module.exports = Slot;
