b70af31c9602d5232ddd504efb4668cc2686958a
Noe Lutz <noelutz@google.com>
2017-06-15 14:12:42 -0700

    fix bugs. add tests

---
+++ runtime/test/view-test.js
@@ -0,0 +1,44 @@
+/**
+ * @license
+ * Copyright (c) 2017 Google Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+ "use strict";
+
+var runtime = require("../runtime.js");
+var Arc = require("../arc.js");
+let assert = require('chai').assert;
+let particles = require('./test-particles.js');
+const SlotComposer = require('../slot-composer.js');
+
+let view = require('../view.js');
+let viewlet = require('../viewlet.js');
+
+let loader = new (require('../loader'));
+loader.registerParticle(particles.TestParticle);
+const slotComposer = new SlotComposer({});
+const Bar = loader.loadEntity("Bar");
+
+describe('View', function() {
+
+  it('clear singleton view', async () => {
+    let arc = new Arc({loader, slotComposer});
+    let barView = arc.createView(Bar.type);
+    barView.set(new Bar({value: 'a Bar'}));
+    barView.clear();
+    assert.equal(barView.get(), undefined);
+  });
+  
+  it('remove entry from view', async () => {
+    let arc = new Arc({loader, slotComposer});
+    let barView = arc.createView(Bar.type.viewOf());
+    let bar = new Bar({id: 0, value: 'a Bar'});
+    barView.store(bar);
+    barView.remove(bar.id);
+    assert.equal(barView.toList().length, 0);
+  });
+});
