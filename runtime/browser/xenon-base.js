50e673c5319bcc5ff3c8ea11b3f1bf6703d2bebe
Scott J Miles <sjmiles@google.com>
2017-06-19 11:52:48 -0700

    Implement `_setProps`

--- runtime/browser/xenon-base.js
+++ runtime/browser/xenon-base.js
@@ -1,17 +1,28 @@
+let nob = () => Object.create(null);
+
 let XenonStaterMixin = Base => class extends Base {
   constructor() {
     super();
-    //this._pendingProps = Object.create(null);
-    this._props = this._getInitialProps() || Object.create(null);
-    //this._lastProps = Object.create(null);
-    this._state = this._getInitialState() || Object.create(null);
-    this._lastState = Object.create(null);
+    this._pendingProps = nob();
+    this._props = this._getInitialProps() || nob();
+    this._lastProps = nob();
+    this._state = this._getInitialState() || nob();
+    this._lastState = nob();
     //this.__configureAccessors();
     //this.__lazyAcquireProps();
   }
   _getInitialProps() {
   }
   _getInitialState() {
+  }
+  _setProps(props) {
+    // TODO(sjmiles): should this be a replace instead of a merge?
+    Object.assign(this._pendingProps, props);
+    this._invalidateProps();
+  }
+  _invalidateProps() {
+    this._propsInvalid = true;
+    this._invalidate();
   }
   _setState(state) {
     Object.assign(this._state, state);
@@ -32,19 +43,13 @@ let XenonStaterMixin = Base => class extends Base {
   _validate() {
     // try..catch to ensure we nullify `validator` before return
     try {
-      //console.warn(this.localName + (this.id ? '#' + this.id : '') + ': validated');
-      //Object.assign(this._props, this._pendingProps);
-      // TODO(sjmiles): React doesn't call `_willReceiveProps` if `setProps` wasn't called
-      // (https://github.com/facebook/react/issues/3610#issuecomment-90632210)
-      // ... have to find another way to update state on state.
-      //if (this._propsInvalid) {
+      // TODO(sjmiles): should this be a replace instead of a merge?
+      Object.assign(this._props, this._pendingProps);
+      if (this._propsInvalid) {
         // TODO(sjmiles): should/can have different timing from rendering?
-        //this._willReceiveProps(this._props, this._state);
-        //this._propsInvalid = false;
-      //}
-      // TODO(sjmiles): invented to provide a place to update state on state,
-      // as discussed above ... does it break the world
-      // this._willReceiveState(this._props, this._state);
+        this._willReceiveProps(this._props, this._state);
+        this._propsInvalid = false;
+      }
       //if (this._shouldUpdate(this._lastProps, this._lastState, this._props, this._state)) {
         // TODO(sjmiles): consider throttling render to rAF
         //this._doMount();
@@ -58,13 +63,13 @@ let XenonStaterMixin = Base => class extends Base {
     this._validator = null;
     // save the old props and state
     // TODO(sjmiles): don't need to create these for default _shouldUpdate
-    //this._lastProps = Xenon.mix(null, this._props);
-    //this._lastState = Object.assign(Object.create(null), this._state);
+    //this._lastProps = Object.assign(nob(), this._props);
+    //this._lastState = Object.assign(nob(), this._state);
     this._didUpdate(this._props, this._state);
   }
-  /*
   _willReceiveProps(props, state) {
   }
+  /*
   _willReceiveState(props, state) {
   }
   _shouldUpdate(oldProps, oldState, props, state) {
