8f766d79f745b1727f8793133f138a5c9f19fe87
shans <shane.stephens@gmail.com>
2017-06-21 16:02:27 +1000

    [WIP] flesh out part of new-recipe, add a (legacy) builder for it
    I'm going to experiment a bit with some resolvers next.

--- runtime/new-recipe.js
+++ runtime/new-recipe.js
@@ -15,30 +15,106 @@ class Connection extends Edge {
 }
 
 class Particle extends Node {
-  get id() {} // Not resolved until we have an ID.
-  get name() {}
-  get tags() {}
-  get providedSlots() {} // Slot*
-  get consumedSlots() {} // SlotConnection*
-  get parameters() {} // {parameter -> ViewConnection}
+  constructor(recipe, name) {
+    assert(recipe);
+    this._recipe = recipe;
+    this._id = undefined;
+    this._name = name;
+    this._tags = [];
+    this._providedSlots = [];
+    this._consumedSlots = [];
+    this._connections = {};
+    this._unnamedConnections = [];
+  }
+  get id() { return this._id; } // Not resolved until we have an ID.
+  get name() { return this._name; }
+  get tags() { return this._tags; }
+  get providedSlots() { return this._providedSlots; } // Slot*
+  get consumedSlots() { return this._consumedSlots; } // SlotConnection*
+  get connections() { return this._connections; } // {parameter -> ViewConnection}
+
+  addUnnamedConnection() {
+    var connection = new ViewConnection(undefined, this);
+    this._unnamedConnections.push(connection);
+    return connection;
+  }
+
+  addConnectionName(name) {
+    this._connections[name] = new ViewConnection(name, this);
+    return this._connections[name];
+  }
+
+  nameConnection(connection, name) {
+    var idx = this._unnamedConnections.indexOf(connection);
+    assert(idx >= 0);
+    connection._name = name;
+    this._connections[name] = connection;
+    this._unnamedConnections.splice(idx, 1);
+  }
+
+  isResolved() {
+    return this._unnamedConnections.length == 0;
+    // and probably some other stuff too.
+  }
 }
 
 class View extends Node {
+  constructor(recipe) {
+    assert(recipe);
+    this._recipe = recipe;
+    this._id = undefined;
+    this._tags = [];
+    this._type = undefined;
+    this._create = false;
+    this._connections = [];
+  }
   // a resolved View has either an id or create=true
-  get tags() {} // only tags owned by the view
-  get type() {} // nullable
-  get id() {}
-  get create() {}
-  get connections() {} // ViewConnection*
+  get tags() { return this._tags; } // only tags owned by the view
+  get type() { return this._type; } // nullable
+  get id() { return this._id; }
+  get create() { return this._create; }
+  get connections() { return this._connections } // ViewConnection*
+
+  isResolved() {
+    return this._id !== undefined || this._create == true;
+  }
 }
 
 class ViewConnection extends Connection {
-  get name() {} // Parameter name?
-  get tags() {}
-  get type() {}
-  get direction() {} // in/out
-  get view() {} // View?
-  get particle() {} // never null
+  constructor(name, particle) {
+    assert(particle);
+    this._recipe = particle._recipe;
+    this._name = name;
+    this._tags = [];
+    this._type = undefined;
+    this._direction = undefined;
+    this._particle = particle;
+  }
+
+  get name() { return this._name; } // Parameter name?
+  get tags() { return this._tags; }
+  get type() { return this._type; }
+  get direction() { return this._direction; } // in/out
+  get view() { return this._view; } // View?
+  get particle() { return this._particle; } // never null
+
+  hasDefinedType() {
+    return this._type !== undefined && this._direction !== undefined;
+  }
+
+  connectToView(view) {
+    assert(view._recipe == this._recipe);
+    if (this._type !== undefined) {
+      if (view._type == undefined)
+        view._type = this._type;
+      else 
+        assert(this_type == view._type);
+    } else if (view._type !== undefined) {
+        this._type = view._type;
+    }
+    this._view = view;
+    this._view.connections.push(this);
+  }
 }
 
 class SlotConnection extends Connection {
@@ -58,9 +134,36 @@ class Slot extends Node {
 }
 
 class Recipe {
-  get particles() {} // Particle*
-  get views() {} // View*
+  constructor() {
+    this._particles = [];
+    this._views = [];
+  }
+
+  newParticle(name) {
+    var particle = new Particle(this, name);
+    this._particles.push(particle);
+    return particle;
+  }
+
+  newView() {
+    var view = new View(this);
+    this._views.push(view);
+    return view;
+
+  }
+  get particles() { return this._particles; } // Particle*
+  get views() { return this._views; } // View*
   get slots() {} // Slot*
+  
   get slotConnections() {} // SlotConnection*
-  get viewConnections() {} // ViewConnection*
+  
+  get viewConnections() {
+    var viewConnections = [];
+    this._particles.forEach(particle => {
+      viewConnection.push(...Object.entries(particle.connections));
+      viweConnection.push(...particle._unnamedConnections);
+    });
+  }
 }
+
+module.exports = Recipe;
