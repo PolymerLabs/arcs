d04e372718288cb5c2da6d8ab567b4cf74bf9186
Douglas Stockwell <dstockwell@chromium.org>
2017-06-30 12:36:13 +1000

    Parse and resolve local names in recipes

--- runtime/test/manifest-test.js
+++ runtime/test/manifest-test.js
@@ -11,6 +11,13 @@
 let Manifest = require('../manifest.js');
 let assert = require('chai').assert;
 
+function assertRecipeParses(input, result) {
+  // Strip common leading whitespace.
+  //result = result.replace(new Regex(`()^|\n)${result.match(/^ */)[0]}`), '$1'),
+  let target = Manifest.parse(result).recipes[0].toString();
+  assert.deepEqual(Manifest.parse(input).recipes[0].toString(), target);
+}
+
 describe('manifest', function() {
   it('can parse a manifest containing a recipe', () => {
     let manifest = Manifest.parse(`
@@ -18,7 +25,7 @@ describe('manifest', function() {
         map #someView
         SomeParticle
           someParam -> #tag`);
-    let recipe = manifest.recipes.SomeRecipe;
+    let recipe = manifest.recipes[0];
     assert(recipe);
     assert.equal(recipe.particles.length, 1);
     assert.equal(recipe.views.length, 1);
@@ -32,7 +39,7 @@ describe('manifest', function() {
           x -> P2
         P2
           y -> P1.y`);
-    let recipe = manifest.recipes.Connected;
+    let recipe = manifest.recipes[0];
     assert(recipe);
     assert.equal(recipe.views.length, 2);
     assert.equal(recipe.viewConnections.length, 4);
@@ -44,15 +51,34 @@ describe('manifest', function() {
           x -> P2.x
         P2
           x -> P1.x`);
-    let recipe = manifest.recipes.Bidirectional;
+    let recipe = manifest.recipes[0];
     assert(recipe);
     assert.equal(recipe.views.length, 1);
     assert.equal(recipe.viewConnections.length, 2);
-    assert.equal(recipe.toString(), `recipe undefined
+    assert.equal(recipe.toString(), `recipe
   map as view0
   P1 as particle0
     x -> view0
   P2 as particle1
     x -> view0`);
+  });
+  it('supports recipes with local names', () => {
+    assertRecipeParses(
+      `recipe
+        map #things as thingView
+        P1 as p1
+          x -> thingView
+        P2
+          x -> thingView
+          y -> p1.y`,
+      `recipe
+        map #things as thingView
+        map as view0
+        P1 as p1
+          x -> thingView
+          y = view0
+        P2 as particle0
+          x -> thingView
+          y -> view0`);
   });
 });
