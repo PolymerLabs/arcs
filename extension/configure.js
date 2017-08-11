e1c05c7be163a6614bdf3001fc12dad6b43456a9
Matt Small <mbsmall@google.com>
2017-08-11 15:43:58 -0700

    insert scott's playground demo into the extension Displays the simple bank account workflow, and disables (for now) retrieving tab information from across devices.

---
+++ extension/configure.js
@@ -0,0 +1,47 @@
+/**
+ * @license
+ * Copyright (c) 2017 Google Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+window.urls = {
+  // "../entities/Account": "entities/Account",
+  "../entities/Account": "https://sjmiles.github.io/arcs-playground/entities/Account",
+  "../particles/ShowAccounts": "https://sjmiles.github.io/arcs-playground/particles/ShowAccounts"
+};
+
+window.db = {
+  entities: [
+    'Account'
+  ],
+  views: {
+    'accounts': 'Account'
+  },
+  model: {
+    Account: [
+      {
+        name: "Fred's Bank",
+        balance: "20.30"
+      },
+      {
+        name: "Savings and Koan",
+        balance: "9876.54"
+      }
+    ]
+  }
+};
+
+window.recipes = [
+{
+  particles: [{
+    name: "ShowAccounts",
+    constrain: {
+      "list": "list"
+    }
+  }]
+}
+];
