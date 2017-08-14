cbfb8a75552966930cf3e2415d07acc35d44ed46
Douglas Stockwell <dstockwell@chromium.org>
2017-08-14 15:12:00 +1000

    Add a view representation for manifests

---
+++ runtime/manifest-view.js
@@ -0,0 +1,49 @@
+// @license
+// Copyright (c) 2017 Google Inc. All rights reserved.
+// This code may only be used under the BSD style license found at
+// http://polymer.github.io/LICENSE.txt
+// Code distributed by Google as part of this project is also
+// subject to an additional IP rights grant found at
+// http://polymer.github.io/PATENTS.txt
+'use strict';
+
+class ManifestView {
+  constructor() {
+    this._version = null;
+    this._id = null;
+    this._entities = [];
+    this._type = null; // not optional!
+    this.localName = null;
+  }
+  get version() {
+    return this._version;
+  }
+  set version(version) {
+    this._version = version;
+  }
+  get id() {
+    return this._id;
+  }
+  set id(id) {
+    this._id = id;
+  }
+  get entities() {
+    return this._entities;
+  }
+  addEntity(entity) {
+    // This should be in the format used by Viewlet.
+    assert(entity.id && entity.rawData);
+    this._entities.push(entity);
+  }
+  get type() {
+    return this._type;
+  }
+  set type(type) {
+    this._type = type;
+  }
+  isValid() {
+    return !!this._type;
+    // TODO: id implies version required
+    // TODO: duplicate entities? entities have id/rawData?
+  }
+}
