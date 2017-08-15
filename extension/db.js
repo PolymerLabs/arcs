e3c900e9cff227abda0bf9f67546da089025d04f
Matt Small <mbsmall@google.com>
2017-08-15 15:52:32 -0700

    upgrade to cdn v0.01 Functionality is largely unchanged.

---
+++ extension/db.js
@@ -0,0 +1,34 @@
+/**
+ * @license
+ * Copyright (c) 2017 Google Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+window.db = {
+  model: {
+    Url: [
+      {
+        name: "url1",
+        balance: "20.30"
+      },
+      {
+        name: "url2",
+        balance: "9876.54"
+      }
+    ],
+    Product: [
+      {
+        name: "Minecraft Book",
+        category: "Books",
+        seller: "denile.com",
+        price: "$14.50",
+        shipDays: 7,
+        image: "assets/products/book.png"
+      }
+    ]
+  }
+};
