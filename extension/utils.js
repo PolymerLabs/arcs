91eadbc5558d73d9f4147f828125c0991085680f
Matt Small <mbsmall@google.com>
2017-08-17 12:23:57 -0700

    misc fixes

--- extension/utils.js
+++ extension/utils.js
@@ -1,4 +1,12 @@
-// ad-hoc (for now) utilities
+// Copyright (c) 2017 Google Inc. All rights reserved.
+// This code may only be used under the BSD style license found at
+// http://polymer.github.io/LICENSE.txt
+// Code distributed by Google as part of this project is also
+// subject to an additional IP rights grant found at
+// http://polymer.github.io/PATENTS.txt
+
+// ad-hoc (for now) utilities from sjmiles
+// should these go into arcslib.js?
 let utils = {
   createUrlMap: cdnRoot => {
     return {
