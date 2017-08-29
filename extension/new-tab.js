794def6758ce53f81ed6ea4d12f9b01840749e05
Matt Small <mbsmall@google.com>
2017-08-29 11:34:59 -0700

    upgrade to CDN v0.03 - push logic out of new-tab.html (avoid constant security policy updates),   although unfortunately this makes the diff really hard to read - reference CDN at https://polymerlabs.github.io (instead of   https://sjmiles.github.io)

--- extension/new-tab.js
+++ extension/new-tab.js
@@ -9,6 +9,46 @@
 // TODO(smalls) - there should be a better system of unique ids
 var faux_gid = 2000;
 
+function instantiate_arcs(doc) {
+  let template = doc.document.querySelector('template').content;
+  doc.document.body.appendChild(doc.document.importNode(template, true));
+  //
+  let app = async function(urlMap, manifestPath, container, db) {
+    // create a system loader
+    // TODO(sjmiles): `pecFactory` can create loader objects (via worker-entry*.js) for the innerPEC,
+    // but we have to create one by hand for manifest loading
+    let loader = new Arcs.BrowserLoader(urlMap);
+    // load manifest
+    let manifest = await Arcs.Manifest.load(manifestPath, loader);
+    // TODO(sjmiles): hack in ability to utilize imported recipes
+    utils.collapseRecipes(manifest);
+    console.log(manifest);
+    // renderer
+    let slotComposer = new Arcs.SlotComposer(container, /* affordance */ "dom");
+    // an Arc!
+    let arc = Arcs.utils.createArc({id: 'demo', urlMap, slotComposer, context: manifest});
+    // load our dynamic data
+    await loadBrowsingData(arc, manifest);
+    // generate suggestions
+    Arcs.utils.suggest(arc, doc.document.querySelector('suggestions-element'));
+  };
+  //
+  let go = async ({db, urls}) => {
+    // create default URL map
+    let root = `https://polymerlabs.github.io/arcs-cdn/v0.03`;
+    let urlMap = utils.createUrlMap(root);
+
+    // we have an additional artifact that we need to load dynamically
+    urlMap['worker-entry-cdn.js'] = `${root}/worker-entry-cdn.js`;
+    // customize map
+    urls && Object.assign(urlMap, urls);
+    // start application
+    app(urlMap, './new-tab.manifest', window['particle-container'], db);
+  };
+  //
+  go(window);
+}
+
 /**
  * Load the current browsing data from all non-https tabs on all devices.
  */
