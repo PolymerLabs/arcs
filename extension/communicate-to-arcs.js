7c6dd4fee64710ec3577131cb1067328c8731a7b
Matt Small <mbsmall@google.com>
2017-09-26 09:41:47 -0700

    popup activation sends *all* schema.org (inc WebPage) up to firebase

--- extension/communicate-to-arcs.js
+++ extension/communicate-to-arcs.js
@@ -5,7 +5,16 @@
 // subject to an additional IP rights grant found at
 // http://polymer.github.io/PATENTS.txt
 
+
 function getSessionId() {
   // TODO(smalls) - use a user id instead of chrome.runtime.id.
   return chrome.runtime.id;
+}
+
+function populateIframe(doc) {
+
+  let iframe = doc.getElementById('arcs-if');
+  let cdnRoot = 'https://polymerlabs.github.io/arcs-cdn/dev/';
+
+  iframe.src = cdnRoot+"/app/?manifest=arcs-extension.manifest&amkey="+getSessionId();
 }
