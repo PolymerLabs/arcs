7bbbcf3b9cc6f770b868262c57d44ea7f5fa286e
shans <shane.stephens@gmail.com>
2017-10-13 19:09:18 +1100

    experimental implementation of loadRecipe
    NOTE THAT in this implementation a loader is imported directly
    into outer-PEC.js, which is bad.

--- runtime/test/particle-api-test.js
+++ runtime/test/particle-api-test.js
@@ -79,4 +79,94 @@ describe('particle-api', function() {
     assert(newView.name == "a view");
     await util.assertSingletonIs(newView, Result, "success");
   });
+
+  it.only('can load a recipe', async () => {
+    let registry = {};
+    let loader = new class extends Loader {
+      loadResource(path) {
+        return {
+          manifest: `
+            schema Result
+              normative
+                Text value
+
+            particle P in 'a.js'
+              P(out Result result)
+
+            recipe
+              use 'test:1' as view0
+              P
+                result -> view0
+
+          `,
+          'a.js': `
+            "use strict";
+
+            defineParticle(({Particle}) => {
+              return class P extends Particle {
+                async setViews(views) {
+                  let arc = await this.constructInnerArc();
+                  var resultView = views.get('result');
+                  let inView = await arc.createView(resultView.type, "in view");
+                  let outView = await arc.createView(resultView.type, "out view");
+                  try {
+                    await arc.loadRecipe(\`
+                      import '../particles/test/test-particles.manifest'
+                      recipe
+                        use '\${inView.id}' as v1
+                        use '\${outView.id}' as v2
+                        PassThrough
+                          a <- v1
+                          b -> v2
+
+                    \`);
+                    inView.set(new resultView.entityClass({value: 'success'}));
+                    resultView.set(new resultView.entityClass({value: 'done'}));
+                  } catch (e) {
+                    resultView.set(new resultView.entityClass({value: e}));
+                  }
+                }
+              }
+            });
+          `,
+          '../particles/test/pass-through.js': `
+            "use strict";
+
+            defineParticle(({Particle}) => {
+              return class PassThrough extends Particle {
+                setViews(views) {
+                  views.get('a').get().then(result => {
+                    views.get('b').set(result);
+                  });
+                }
+              }
+          });
+          `
+        }[path];
+      }
+      path(fileName) {
+        return fileName;
+      }
+      join(_, file) {
+        return file;
+      }
+    };
+    let manifest = await Manifest.load('manifest', loader, {registry});
+    var pecFactory = function(id) {
+      var channel = new MessageChannel();
+      new InnerPec(channel.port1, `${id}:inner`, loader);
+      return channel.port2;
+    };
+    let arc = new Arc({id:'test', pecFactory});
+    let Result = manifest.findSchemaByName('Result').entityClass();
+    let resultView = arc.createView(Result.type);
+    let recipe = manifest.recipes[0];
+    recipe.normalize();
+    arc.instantiate(recipe);
+
+    await util.assertSingletonWillChangeTo(resultView, Result, "done");
+    let newView = arc.findViewsByType(Result.type)[2];
+    assert(newView.name == "out view");
+    await util.assertSingletonWillChangeTo(newView, Result, "success");
+  });
 });
