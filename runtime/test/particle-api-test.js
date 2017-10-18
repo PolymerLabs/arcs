1cfed42c29761665d741adee94ef2e3943b37800
Maria Mandlis <mmandlis@chromium.org>
2017-10-18 11:19:26 -0700

    Add test for view  multiplexing in inner arc

--- runtime/test/particle-api-test.js
+++ runtime/test/particle-api-test.js
@@ -175,4 +175,120 @@ describe('particle-api', function() {
     assert(newView.name == "out view");
     await util.assertSingletonWillChangeTo(newView, Result, "success");
   });
+
+  it('multiplexing', async () => {
+    let registry = {};
+    let loader = new class extends Loader {
+      loadResource(path) {
+        return {
+          manifest: `
+            schema Result
+              normative
+                Text value
+
+            particle P in 'a.js'
+              P(in [Result] inputs, inout [Result] results)
+
+            recipe
+              use 'test:1' as view0
+              use 'test:2' as view1
+              P
+                inputs <- view0
+                results = view1
+
+          `,
+          'a.js': `
+            "use strict";
+
+            defineParticle(({Particle}) => {
+              return class P extends Particle {
+                async setViews(views) {
+                  let arc = await this.constructInnerArc();
+                  var inputsView = views.get('inputs');
+                  var inputsList = await inputsView.toList();
+                  var resultsView = views.get('results');
+                  for (let input of inputsList) {
+                    let inView = await arc.createView(resultsView.type.primitiveType(), "in view");
+                    let outView = await arc.createView(resultsView.type.primitiveType(), "out view");
+                    try {
+                      let done = await arc.loadRecipe(\`
+                        schema Result
+                          normative
+                            Text value
+
+                        particle PassThrough in 'pass-through.js'
+                          PassThrough(in Result a, out Result b)
+
+                        recipe
+                          use '\${inView.id}' as v1
+                          use '\${outView.id}' as v2
+                          PassThrough
+                            a <- v1
+                            b -> v2
+
+                      \`);
+                      inView.set(input);
+                      let res = resultsView.store(new resultsView.entityClass({value: 'done'}));
+
+                      // outView.on('change', () => outView.get().then(result => {
+                      //   console.log('????? outview changed = ', result);
+                      // }), {});
+                    } catch (e) {
+                      resultsView.store(new resultsView.entityClass({value: e}));
+                    }
+                  }
+                }
+              }
+            });
+          `,
+          'pass-through.js': `
+            "use strict";
+
+            defineParticle(({Particle}) => {
+              return class PassThrough extends Particle {
+                setViews(views) {
+                  views.get('a').get().then(result => {
+                    var bView = views.get('b');
+                    bView.set(new bView.entityClass({value:result.value.toUpperCase()}));
+                  });
+                }
+              }
+          });
+          `
+        }[path];
+      }
+      path(fileName) {
+        return fileName;
+      }
+      join(_, file) {
+        return file;
+      }
+    };
+    let manifest = await Manifest.load('manifest', loader, {registry});
+    var pecFactory = function(id) {
+      var channel = new MessageChannel();
+      new InnerPec(channel.port1, `${id}:inner`, loader);
+      return channel.port2;
+    };
+    let arc = new Arc({id:'test', pecFactory, loader});
+    let Result = manifest.findSchemaByName('Result').entityClass();
+    let inputsView = arc.createView(Result.type.viewOf());
+    inputsView.store({id: 1, rawData: {value: 'hello'} });
+    inputsView.store({id: 2, rawData: {value: 'world'} });
+    let resultsView = arc.createView(Result.type.viewOf());
+    let recipe = manifest.recipes[0];
+    recipe.normalize();
+    arc.instantiate(recipe);
+
+    await util.assertViewWillChangeTo(resultsView, Result, "value", ["done", "done"]);
+    // TODO: how do i listen to inner arc's outView view-changes?
+    // await util.assertViewWillChangeTo(resultsView, Result, "value", ["HELLO", "WORLD"]);
+    let newView = arc.findViewsByType(Result.type)[1];
+    assert(newView.name == "out view", `Unexpected newView name: ${newView.name}`);
+
+    util.assertSingletonIs(newView, Result, "HELLO");
+    newView = arc.findViewsByType(Result.type)[3];
+    assert(newView.name == "out view", `Unexpected newView name: ${newView.name}`);
+    await util.assertSingletonWillChangeTo(newView, Result, "WORLD");
+  });
 });
