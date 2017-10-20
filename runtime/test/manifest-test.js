43869bd3053785236bc9f65f0225ddf128e5218b
Douglas Stockwell <dstockwell@chromium.org>
2017-10-20 11:08:12 +1100

    Better error message when top level identifier is not capitalized
    Fixes https://github.com/PolymerLabs/arcs/issues/403

--- runtime/test/manifest-test.js
+++ runtime/test/manifest-test.js
@@ -565,7 +565,7 @@ describe('manifest', function() {
       assert(false);
     } catch (e) {
       assert.deepEqual(e.message, `Parse error in 'bad-file' line 1.
-Expected " ", "#", "\\n", "\\r", [ ] or [A-Z] but "?" found.
+Expected " ", "#", "\\n", "\\r", [ ], [A-Z] or [a-z] but "?" found.
   recipe ?
          ^`);
     }
