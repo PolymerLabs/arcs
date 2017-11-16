6effb121b4de9c8367e69d2e2516e216580efc7a
shans <shane.stephens@gmail.com>
2017-11-17 09:25:09 +1100

    unskip test

--- runtime/test/particle-shape-loading-with-slots-test.js
+++ runtime/test/particle-shape-loading-with-slots-test.js
@@ -113,7 +113,7 @@ describe('particle-shape-loading-with-slots', function() {
     assert.deepEqual([{value: 'foo1'}, {value: 'foo2'}], slot._content.model);
   });
 
-  it.skip('multiplex recipe with slots (init context later)', async () => {
+  it('multiplex recipe with slots (init context later)', async () => {
     // This test is different from the one above because it initializes the transformation particle context
     // after the hosted particles are also instantiated.
     // This verifies a different start-render call in slot-composer.
