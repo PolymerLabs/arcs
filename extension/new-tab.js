79f4becde89e10e150b37f6ff70ce4ecaaa82d4b
Walter Korman <wkorman@chromium.org>
2017-11-17 15:16:32 -0800

    Auto-format extension code with prettier.

--- extension/new-tab.js
+++ extension/new-tab.js
@@ -9,8 +9,9 @@
  * Load schema.org entities from all available tabs.
  */
 async function loadEntitiesFromTabs() {
-
-  let devices = await new Promise((resolve) => chrome.sessions.getDevices(null, resolve));
+  let devices = await new Promise(resolve =>
+    chrome.sessions.getDevices(null, resolve)
+  );
   let tabs = [];
   for (let device of devices) {
     for (let session of device.sessions) {
@@ -24,23 +25,25 @@ async function loadEntitiesFromTabs() {
           id: tab.sessionId,
           url: tab.url,
           title: tab.title,
-          favIconUrl: tab.favIconUrl,
+          favIconUrl: tab.favIconUrl
         });
       }
     }
   }
-  let currentTabs = await new Promise((resolve) => chrome.tabs.query({}, resolve));
+  let currentTabs = await new Promise(resolve =>
+    chrome.tabs.query({}, resolve)
+  );
   for (let tab of currentTabs) {
     if (!/^https?/.test(tab.url)) {
       continue;
     }
     tabs.push({
-      device: 'local',
+      device: "local",
       group: `local:${tab.windowId}`,
       url: tab.url,
       title: tab.title,
       id: tab.id,
-      local: true,
+      local: true
     });
   }
   tabs.sort((a, b) => {
@@ -63,6 +66,7 @@ async function loadEntitiesFromTabs() {
     groupTabMap.get(tab.group).push(tab);
   }
 
-  return [].concat(...await Promise.all(tabs.map(tab => tabEntityMap.get(tab))));
+  return [].concat(
+    ...(await Promise.all(tabs.map(tab => tabEntityMap.get(tab))))
+  );
 }
-
