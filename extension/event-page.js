61beb0c7b9779701895c1ced3f6cb7c127190385
Walter Korman <wkorman@chromium.org>
2017-11-28 14:58:06 -0800

    Add extension test infra with a simple initial page extractor test.

--- extension/event-page.js
+++ extension/event-page.js
@@ -6,30 +6,28 @@
 // http://polymer.github.io/PATENTS.txt
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  console.log('event page received message ' + request.method, request);
-  if (request.method == 'storePageEntities') {
+  console.log("event page received message " + request.method, request);
+  if (request.method == "storePageEntities") {
     let key = request.url;
     chrome.storage.local.set({ [request.url]: request.results }, () => {
       if (chrome.runtime.lastError) {
-        console.log('ERROR failed to store: ' + chrome.runtime.lastError);
+        console.log("ERROR failed to store: " + chrome.runtime.lastError);
       }
     });
 
     // TODO(smalls) remove this, it's only for debugging
     chrome.storage.local.get(null, result => {
-      console.log('after store completed, storage contains', result);
+      console.log("after store completed, storage contains", result);
     });
-  } else if (request.method == 'loadAllEntities') {
+  } else if (request.method == "loadAllEntities") {
     chrome.storage.local.get(null, result => {
       if (chrome.runtime.lastError) {
-        console.log('ERROR retrieving storage: ' + chrome.runtime.lastError);
+        console.log("ERROR retrieving storage: " + chrome.runtime.lastError);
         return;
       }
 
       sendResponse(result);
     });
     return true;
-  } else {
-    return;
   }
 });
