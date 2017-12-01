abdaac2deb861e7153c5b43c0e297c24a60ab03c
Douglas Stockwell <dstockwell@chromium.org>
2017-12-01 12:09:53 +1100

    Add copyright headers

--- runtime/recipe/digest-web.js
+++ runtime/recipe/digest-web.js
@@ -1,3 +1,10 @@
+// Copyright (c) 2017 Google Inc. All rights reserved.
+// This code may only be used under the BSD style license found at
+// http://polymer.github.io/LICENSE.txt
+// Code distributed by Google as part of this project is also
+// subject to an additional IP rights grant found at
+// http://polymer.github.io/PATENTS.txt
+
 export default async function(str) {
   let buffer = new TextEncoder('utf-8').encode(str);
   let digest = await crypto.subtle.digest('SHA-1', buffer)
