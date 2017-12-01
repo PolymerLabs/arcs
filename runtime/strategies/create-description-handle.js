aa4e34db8c62735fe46d1efd884392cd4e5db6e6
Douglas Stockwell <dstockwell@chromium.org>
2017-12-01 11:55:15 +1100

    Fixes after rebase

--- runtime/strategies/create-description-handle.js
+++ runtime/strategies/create-description-handle.js
@@ -5,12 +5,12 @@
 // subject to an additional IP rights grant found at
 // http://polymer.github.io/PATENTS.txt
 
-var assert = require('assert'); // remove this!
-let {Strategy} = require('../../strategizer/strategizer.js');
-let Recipe = require('../recipe/recipe.js');
-let RecipeWalker = require('../recipe/walker.js');
+import assert from '../../platform/assert-web.js';
+import {Strategy} from '../../strategizer/strategizer.js';
+import Recipe from '../recipe/recipe.js';
+import RecipeWalker from '../recipe/walker.js';
 
-module.exports = class CreateDescriptionHandle extends Strategy {
+export default class CreateDescriptionHandle extends Strategy {
   async generate(strategizer) {
     var results = Recipe.over(this.getResults(strategizer), new class extends RecipeWalker {
       onViewConnection(recipe, viewConnection) {
