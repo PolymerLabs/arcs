21a9d321d2bc62cee09e44dac56b4ef28f89d6e2
Matt Small <mbsmall@google.com>
2017-12-01 11:41:40 -0800

    clean up comments and logs

--- extension/event-page.js
+++ extension/event-page.js
@@ -25,29 +25,7 @@ function _prepareResults(results) {
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log('event page received message ' + request.method, request);
-  /*if (request.method == 'storePageEntities') {
-    let key = request.url;
-    chrome.storage.local.set({ [request.url]: request.results }, () => {
-      if (chrome.runtime.lastError) {
-        console.log('ERROR failed to store: ' + chrome.runtime.lastError);
-      }
-    });
-
-    // TODO(smalls) remove this, it's only for debugging
-    chrome.storage.local.get(null, result => {
-      console.log('after store completed, storage contains', result);
-    });
-  } else */ if (request.method == 'loadAllEntities') {
-    /*
-    chrome.storage.local.get(null, result => {
-      if (chrome.runtime.lastError) {
-        console.log('ERROR retrieving storage: ' + chrome.runtime.lastError);
-        return;
-      }
-
-      sendResponse(result);
-    });
-    */
+  if (request.method == 'loadAllEntities') {
     loadEntitiesFromTabs().then(results => {
       console.log('got result from all tabs', results);
       sendResponse(_prepareResults(results));
@@ -60,34 +38,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 /**
  * Load schema.org entities from all available tabs.
  * Previous versions of this method (in new-tab.js) queried across devices,
- * but I've removed that for simplicity in this version.
+ * but I've removed that for simplicity.
  */
 async function loadEntitiesFromTabs() {
-  /*
-  let devices = await new Promise(resolve =>
-    chrome.sessions.getDevices(null, resolve)
-  );
-  */
   let tabs = [];
-  /*
-  for (let device of devices) {
-    for (let session of device.sessions) {
-      for (let tab of session.window.tabs) {
-        if (!/^https?/.test(tab.url)) {
-          continue;
-        }
-        tabs.push({
-          device: device.deviceName,
-          group: session.window.sessionId,
-          id: tab.sessionId,
-          url: tab.url,
-          title: tab.title,
-          favIconUrl: tab.favIconUrl
-        });
-      }
-    }
-  }
-  */
   let currentTabs = await new Promise(resolve =>
     chrome.tabs.query({}, resolve)
   );
@@ -107,10 +61,6 @@ async function loadEntitiesFromTabs() {
   tabs.sort((a, b) => {
     return a.group.localeCompare(b.group);
   });
-  /*
-  let groupElement = null;
-  let lastGroup = null;
-  */
 
   // Trigger entity extraction.
   let tabEntityMap = new Map();
@@ -119,26 +69,11 @@ async function loadEntitiesFromTabs() {
   }
 
   return Promise.all(tabs.map(tab => tabEntityMap.get(tab)));
-
-  /*
-  let groupTabMap = new Map();
-  for (let tab of tabs) {
-    if (!groupTabMap.has(tab.group)) {
-      groupTabMap.set(tab.group, []);
-    }
-    groupTabMap.get(tab.group).push(tab);
-  }
-
-  return [].concat(
-    ...(await Promise.all(tabs.map(tab => tabEntityMap.get(tab))))
-  );
-  */
 }
 
 async function loadEntitiesFromTab(tab) {
   return new Promise(resolve => 
     chrome.tabs.sendMessage(tab.id, {'method': 'loadEntities'}, result => {
-      console.log('hey!! got result', result);
       resolve({tab: tab, result: result});
     })
   );
