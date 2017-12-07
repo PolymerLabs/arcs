ec7d3a03349048d460530554308190892b99eb50
Douglas Stockwell <dstockwell@chromium.org>
2017-12-07 13:35:18 +1100

    Add a runner for executing runtime tests in a browser

---
+++ runtime/test/chai-web.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2017 Google Inc. All rights reserved.
+// This code may only be used under the BSD style license found at
+// http://polymer.github.io/LICENSE.txt
+// Code distributed by Google as part of this project is also
+// subject to an additional IP rights grant found at
+// http://polymer.github.io/PATENTS.txt
+
+import '../../node_modules/chai/chai.js';
+export default window.chai;
+export const assert = window.chai.assert;
