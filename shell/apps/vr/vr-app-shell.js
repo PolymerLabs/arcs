deef91447c2e107718cc9a55e63814e466550131
Matt Small <mbsmall@google.com>
2018-02-16 09:16:44 -0800

    begin upgrade of extension from v0.2.1 to current The page (shell/apps/chrome-extension/index.html) loads without errors, but isn't functional yet - move code around to be in line with modern practices

--- shell/apps/vr/vr-app-shell.js
+++ shell/apps/vr/vr-app-shell.js
@@ -1,3 +1,10 @@
+// Copyright (c) 2017 Google Inc. All rights reserved.
+// This code may only be used under the BSD style license found at
+// http://polymer.github.io/LICENSE.txt
+// Code distributed by Google as part of this project is also
+// subject to an additional IP rights grant found at
+// http://polymer.github.io/PATENTS.txt
+
 import ArcsUtils from '../../app-shell/lib/arcs-utils.js';
 import AppShell from '../../app-shell/app-shell.js';
 
