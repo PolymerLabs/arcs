1a493594872dcc70ff4af3b8f0de8001acb9b853
Walter Korman <wkorman@chromium.org>
2018-02-25 15:09:37 -0800

    Initial framing for Blob support.
    Add parsing support for 'Bytes' in Schema definitions. Rough
    framing for Bytes per design, and Blob in platform.
    
    No real logic yet other than parsing.

---
+++ platform/blob-node.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2018 Google Inc. All rights reserved.
+// This code may only be used under the BSD style license found at
+// http://polymer.github.io/LICENSE.txt
+// Code distributed by Google as part of this project is also
+// subject to an additional IP rights grant found at
+// http://polymer.github.io/PATENTS.txt
+'use strict';
+
+// TODO(wkorman): Implement further perhaps via
+// https://nodejs.org/api/buffer.html
+export default class Blob {
+  constructor(values, options) {
+    this._values = values;
+    this._options = options;
+  }
+};
