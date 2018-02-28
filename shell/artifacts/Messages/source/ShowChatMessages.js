26a91f5969a52fa008842eef04b94ba8d8be6156
Scott J Miles <sjmiles@google.com>
2018-02-27 21:06:45 -0800

    subid's are passed as properties now, not attributes

--- shell/artifacts/Messages/source/ShowChatMessages.js
+++ shell/artifacts/Messages/source/ShowChatMessages.js
@@ -70,7 +70,7 @@ defineParticle(({DomParticle, resolver}) => {
     <div message isme$="{{isme}}">
       <div name><span avatar><b>{{name}}</b><img src="{{src}}" title="{{name}}" alt="{{name}}"></span><i>{{blurb}}</i></div>
       <div content iscustom$="{{iscustom}}">{{content}}</div>
-      <div slotid="custom_message" subid$="{{subId}}"></div>
+      <div slotid="custom_message" subid="{{subId}}"></div>
     </div>
   </template>
   <div list>{{messages}}</div>
