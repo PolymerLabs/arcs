4b35b6ad7f49ac8e2bfc26272e9b2f78a318af39
Maria Mandlis <mmandlis@chromium.org>
2018-03-05 10:56:13 -0800

    fix schema serialization (fixes demo-flow-test), and fix recipe-test

--- runtime/test/recipe-test.js
+++ runtime/test/recipe-test.js
@@ -30,13 +30,13 @@ describe('recipe', function() {
             s2 -> view2
     `);
     let recipe = manifest.recipes[0];
-    recipe.views[0]._mappedType = recipe.particles[0].connections['s2'].type;
+    recipe.handles[0]._mappedType = recipe.particles[0].connections['s2'].type;
     let options = {errors: new Map()};
 
     recipe.normalize(options);
 
     assert.equal(4, options.errors.size);
-    recipe.views.forEach(handle => assert.isTrue(options.errors.has(handle)));
+    recipe.handles.forEach(handle => assert.isTrue(options.errors.has(handle)));
     options.errors.has(recipe.slots[1]);
   });
 });
