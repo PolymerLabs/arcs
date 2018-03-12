e7f395edef7be78515ee8e56f99acf95a412a84d
Scott J Miles <sjmiles@google.com>
2018-03-12 13:06:46 -0700

    finish up first draft

--- shell/components/firebase-upload.js
+++ shell/components/firebase-upload.js
@@ -4,9 +4,18 @@ import Firebase from './firebase.js';
 const {firebase, storage} = Firebase;
 
 const template = `
-
-  <input type="file" on-change="_onFilesChanged" multiple="{{multiple}}">
-
+  <style>
+    ::slotted(*) {
+      cursor: pointer;
+    }
+    input[type="file"] {
+      display: none;
+    }
+  </style>
+  <label>
+    <slot></slot>
+    <input type="file" accept="{{accept}}" multiple="{{multiple}}" on-change="_onFilesChanged">
+  </label>
 `;
 
 class FirebaseUpload extends Xen.Base {
@@ -14,20 +23,20 @@ class FirebaseUpload extends Xen.Base {
     return template;
   }
   static get observedAttributes() {
-    return ['multiple'];
+    return ['multiple', 'accept'];
   }
-  _render({multiple}) {
+  _render({accept, multiple}) {
     return {
+      accept,
       multiple
     };
   }
   _onFilesChanged(e) {
     const input = e.currentTarget;
-    //console.log(input.value, input.files);
     const file = input.files[0];
-    const rando = Math.floor((Math.random()+1)*1e8);
-    const path = `files/${rando}`;
-    console.log(file.name, 'as', path);
+    const random = Math.floor((Math.random()+1)*1e8);
+    const path = `files/${random}`;
+    console.log('uploading', file.name, 'as', path);
     this._uploadFile(file, path);
   }
   _uploadFile(file, uploadPath) {
@@ -35,17 +44,19 @@ class FirebaseUpload extends Xen.Base {
     const next = () => {
     };
     const error = error => {
-      console.error('Error uploading: ' + error);
+      console.error('Error uploading', error);
     };
-    const complete = snapshot => {
-      console.log('Uploaded a blob or file!');
-      imageRef.getDownloadURL().then(function(url) {
-        console.log(url);
-      }).catch(function(error) {
-        console.error('Error getting download url: ' + error);
-      });
-    };
+    const complete = () => this._uploadComplete(imageRef);
     imageRef.put(file).on(firebase.storage.TaskEvent.STATE_CHANGED, {next, error, complete});
+  }
+  async _uploadComplete(ref) {
+    try {
+      const url = await ref.getDownloadURL();
+      this.value = url;
+      this._fire('upload');
+    } catch (error) {
+      console.error('Error getting download url', error);
+    };
   }
 }
 customElements.define('firebase-upload', FirebaseUpload);
