137b3492e271da38bd0270345796d2636b6b1e30
Scott J Miles <sjmiles@google.com>
2018-03-12 20:55:45 -0700

    move config processing from shell-ui to app-shell

--- shell/app-shell/app-shell.js
+++ shell/app-shell/app-shell.js
@@ -1,14 +1,19 @@
 // elements
 import './elements/shell-ui.js';
+import './elements/arc-config.js';
+// components
 import '../components/suggestion-element.js';
-
 // code
 import Xen from '../components/xen/xen.js';
+// globals
+/* global shellPath */
 
 // templates
 const template = Xen.html`
 
-  <shell-ui>
+  <arc-config rootpath="{{shellPath}}" on-config="_onData"></arc-config>
+
+  <shell-ui config="{{config}}">
     <div slotid="toproot"></div>
     <div slotid="root"></div>
     <div slotid="modal"></div>
@@ -17,35 +22,65 @@ const template = Xen.html`
 
 `;
 
+const log = Xen.logFactory('AppShell', '#6660ac');
+
+const launcherKey = 'launcher';
+const profileKey = 'profile';
+
 class AppShell extends Xen.Base {
-  static get observedAttributes() {
-    return [];
-  }
   get host() {
     return this;
   }
   get template() {
     return template;
   }
-  _getInitialState() {
-    return {};
+  getInitialState() {
+    return {
+      launcherSoloPath: '../web/artifacts/launcher.manifest',
+      profileSoloPath: '../web/artifacts/profile.manifest'
+    };
   }
-  _update(props, state) {
-    super._update(props, state);
+  _update(props, state, oldProps, oldState) {
+    const {config} = state;
+    if (config && config !== oldState.config) {
+      this._consumeConfig(state, config);
+    }
   }
-  _render({}, {}) {
-    const render = {};
+  _consumeConfig(state, config) {
+    let configkey = config.key || '';
+    if (!config.key) {
+      config.key = launcherKey;
+    }
+    if (config.key === launcherKey) {
+      config.soloPath = state.launcherSoloPath;
+      config.launcher = true;
+      configkey = '';
+      state.description = 'Launcher';
+    }
+    if (config.key === profileKey) {
+      config.soloPath = state.profileSoloPath;
+      config.profiler = true;
+      configkey = '*';
+    }
+    this._setState({
+      configkey
+    });
+  }
+  _render({}, {config}) {
+    const render = {
+      config,
+      shellPath
+    };
     return render;
   }
   _onData(e, data) {
-    const property = e.type.replace(/-/g, '');
+    const property = e.type;
     if (this._setIfDirty({[property]: data})) {
-      AppShell.log(property, data);
+      log(property, data);
     }
   }
 }
 
-AppShell.log = Xen.Base.logFactory('AppShell', '#6660ac');
 customElements.define('app-shell', AppShell);
 
 export default AppShell;
