c47022f56a49a6b24563c1794cc2d410fc2f3e19
shans <shane.stephens@gmail.com>
2018-03-14 17:45:34 +1100

    1) Add a strategy that resolves handles when there's only one choice. 2) Stop view mapper strategies from resolving when there's only one choice. 3) Add some very simple metrics to the strategy explorer.
    Splitting just view resolution like this (deterministic vs. non-deterministic) reduces
    the total number of generated recipes (on current front-end environment) from 190 to 108.
    Most savings are early on in the generation.

--- runtime/recipe/walker-base.js
+++ runtime/recipe/walker-base.js
@@ -9,6 +9,31 @@ import {Strategizer} from '../../strategizer/strategizer.js';
 import Recipe from './recipe.js';
 import assert from '../../platform/assert-web.js';
 
+/**
+ * Walkers traverse an object, calling methods based on the
+ * features encountered on that object. For example, a RecipeWalker
+ * takes a list of recipes and calls methods when:
+ *  - a new recipe is encountered
+ *  - a handle is found inside a recipe
+ *  - a particle is found inside a recipe
+ *  - etc..
+ * 
+ * Each of these methods can return a list of updates:
+ *   [(recipe, encountered_thing) => new_recipe]
+ *
+ * The walker then does something with the updates depending on the
+ * tactic selected.
+ * 
+ * If the tactic is "Permuted", then an output will be generated
+ * for every combination of 1 element drawn from each update list.
+ * For example, if 3 methods return [a,b], [c,d,e], and [f] respectively
+ * then "Permuted" will cause 6 outputs to be generated: [acf, adf, aef, bcf, bdf, bef]
+ * 
+ * If the tactic is "Independent", an output will be generated for each
+ * update, regardless of the list the update is in. For example,
+ * if 3 methods return [a,b], [c,d,e], and [f] respectively,
+ * then "Independent" will cause 6 outputs to be generated: [a,b,c,d,e,f]
+ */
 class WalkerBase extends Strategizer.Walker {
   constructor(tactic) {
     super();
