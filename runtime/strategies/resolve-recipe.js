2e34020e5c8b44d66f80bcc0c2bafa6d4a0c0ce5
shans <shane.stephens@gmail.com>
2018-03-16 14:28:10 +1100

    Review comments + tests

--- runtime/strategies/resolve-recipe.js
+++ runtime/strategies/resolve-recipe.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Google Inc. All rights reserved.
+// Copyright (c) 2018 Google Inc. All rights reserved.
 // This code may only be used under the BSD style license found at
 // http://polymer.github.io/LICENSE.txt
 // Code distributed by Google as part of this project is also
@@ -10,6 +10,7 @@ import RecipeWalker from '../recipe/walker.js';
 import Recipe from '../recipe/recipe.js';
 import RecipeUtil from '../recipe/recipe-util.js';
 import assert from '../../platform/assert-web.js';
+import MapSlots from './map-slots.js';
 
 export default class ResolveRecipe extends Strategy {
   constructor(arc) {
@@ -64,9 +65,9 @@ export default class ResolveRecipe extends Strategy {
         
         let selectedSlots = [];
         if (!slotConnection.targetSlot)
-          selectedSlots = this._findSlotCandidates(slotConnection, recipe.slots);
+          selectedSlots = MapSlots._findSlotCandidates(slotConnection, recipe.slots);
         
-        selectedSlots = selectedSlots.concat(this._findSlotCandidates(slotConnection, arc.pec.slotComposer.getAvailableSlots()));
+        selectedSlots = selectedSlots.concat(MapSlots._findSlotCandidates(slotConnection, arc.pec.slotComposer.getAvailableSlots()));
       
         if (selectedSlots.length !== 1)
           return;
@@ -96,44 +97,9 @@ export default class ResolveRecipe extends Strategy {
         };
       }
 
+      // TODO: move to SlotConnection.
       _slotIsConnected(slotConnection) {
         return slotConnection.targetSlot && ((!!slotConnection.targetSlot.sourceConnection) || (!!slotConnection.targetSlot.id));
-      }
-
-      _findSlotCandidates(slotConnection, slots) {
-        return slots.filter(slot => {
-          if (slotConnection.slotSpec.isSet !== slot.getProvidedSlotSpec().isSet)
-            return false;
-          if (!this._tagsMatch(slotConnection, slot)) {
-            if (slotConnection.name !== slot.name)
-              return false;
-          }
-          if (!this._handlesMatch(slotConnection, slot))
-            return false;
-          return true;
-        });
-      }
-
-      // TODO: replace with generic tag matcher
-      _tagsMatch(slotConnection, slot) {
-        let consumeConnTags = slotConnection.slotSpec.tags || [];
-        let slotTags = new Set([].concat(slot.tags, slot.getProvidedSlotSpec().tags || []));
-        // Consume connection tags aren't empty and intersection with the slot isn't empty.
-        return consumeConnTags.length > 0 && consumeConnTags.filter(t => slotTags.has(t)).length > 0;
-      }
-      
-      // TODO: should we move some of this logic to the recipe? Or type matching?
-      _handlesMatch(slotConnection, slot) {
-        let consumingParticle = slotConnection.particle;
-        let providingSlotHandles = slot.handleConnections.map(connection => connection.handle);
-        providingSlotHandles = providingSlotHandles.filter(a => a !== undefined);
-        if (providingSlotHandles.length == 0)
-          return true;
-        
-        return Object.values(consumingParticle.connections).find(handleConn => {
-          return providingSlotHandles.includes(handleConn.handle) ||
-            (handleConn.handle && handleConn.handle.id && providingSlotHandles.map(sh => sh.id).includes(handleConn.handle.id));
-        });
       }
 
     }(RecipeWalker.Permuted), this);
