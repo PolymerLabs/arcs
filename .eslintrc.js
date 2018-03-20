e33c67e7f65c6c7de282459ab4def87a4814a14a
Douglas Stockwell <dstockwell@chromium.org>
2018-03-20 16:55:20 +1100

    Turn off no-undef, particle imports make this hard

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     "new-cap": "off",
     "arrow-parens": "off", // puts parens around single arg arrow functions
     "no-useless-escape": "off", // eg. [\.] in a regex, there are just too many to fix by hand atm.
+    "no-undef": "off", // Particles use importScripts+globals, need to move to JS modules to fix
 
     // Things we don't care about.
     "require-jsdoc": "off",
