27c8b5d6f8b97c29af93caff867c531a0a0931bd
shans <shane.stephens@gmail.com>
2018-04-04 09:15:00 +1000

    move assert

--- runtime/storage/firebase-storage.js
+++ runtime/storage/firebase-storage.js
@@ -110,7 +110,7 @@ export default class FirebaseStorage {
       if (!shouldExist) {
         return {version: 0};
       }
-     assert(data);     
+      assert(data);     
       return data;
     });
 
