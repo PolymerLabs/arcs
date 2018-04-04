11a505afe0082744a6c4bb5155bb45e58f6e4a44
Scott J Miles <sjmiles@google.com>
2018-04-04 16:40:54 -0700

    more tests

--- shell/app-shell-prototype/test/test-cloud-arc.js
+++ shell/app-shell-prototype/test/test-cloud-arc.js
@@ -1,23 +1,71 @@
+import db from './mock-db.js';
 import '../elements/cloud-data/cloud-arc.js';
 
-const nop = () => {};
+const assert = chai.assert;
+const expect = chai.expect;
 
-window.db = {
-  child: () => window.db,
-  ref: () => window.db,
-  on: nop,
-  once: nop,
-  set: nop,
-  store: nop,
-  push: nop
-};
+const mockdb = Object.assign({}, db);
 
-const cloudArc = document.createElement('cloud-arc');
+let cloudArc;
 
 describe('CloudArc', function() {
-  describe('#canBeTested', function() {
-    it('can be tested', function() {
-      assert(cloudArc);
+  beforeEach(() => {
+    cloudArc = document.createElement('cloud-arc');
+    // TODO(sjmiles): attempt to initialize elements without
+    // actually atttaching them to DOM
+    cloudArc._mount();
+    Object.assign(db, mockdb);
+  });
+  it('can be tested', () => {
+    assert(cloudArc);
+  });
+  it(`given key='*', generates a new meta arc`, function(done) {
+    db.push = ({metadata}) => {
+      expect(metadata).to.have.property('description');
+      expect(metadata).to.have.property('color');
+      expect(metadata).to.have.property('bg');
+      expect(metadata).to.have.property('externalManifest');
+      console.log(metadata);
+      return {key: 'KEY'};
+    };
+    const listener = cloudArc.addEventListener('key', e => {
+      cloudArc.removeEventListener('key', listener);
+      assert.equal(e.detail, 'KEY');
+      done();
     });
+    cloudArc.key = '*';
+  });
+  it(`given a key, watches arcs/[key]/metadata and arcs/[key]/serialized`, done => {
+    let count = 0;
+    let path = '';
+    db.child = _path => {
+      path = _path;
+      return db;
+    };
+    const paths = ['arcs/KEY/metadata', 'arcs/KEY/serialized'];
+    let error;
+    db.on = (name, callback) => {
+      try {
+        assert.equal(paths[count++], path);
+        if (count == paths.length && !error) {
+          done();
+        }
+      } catch (x) {
+        done(error = x);
+      }
+    };
+    cloudArc.key = 'KEY';
+  });
+  it(`given metadata, updates db as needed`, function(done) {
+    db.update = metadata => {
+      try {
+        assert.deepEqual(metadata, cloudArc.metadata);
+        done();
+      } catch (x) {
+        done(x);
+      }
+    };
+    cloudArc.key = 'key';
+    cloudArc.metadata = {fake: true};
   });
 });
