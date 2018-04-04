07f0b66bd74ff0567675d40334ed16b414b15ebb
Scott J Miles <sjmiles@google.com>
2018-04-04 16:46:17 -0700

    default log level should be 2

--- shell/apps/common/config.js
+++ shell/apps/common/config.js
@@ -1,6 +1,6 @@
 // establish Debug level before using logFactory
 import Xen from '../../components/xen/xen.js';
-Xen.Debug.level = ((new URL(document.location)).searchParams.has('log')) ? 3 : 0;
+Xen.Debug.level = ((new URL(document.location)).searchParams.has('log')) ? 2 : 0;
 
 // common shell resources are here
 window.shellPath = '../..';
