ddc84aa46aab605df9d9ff1d1217e99be7c2b311
Bernhard A. M. Seefeld <seefeld@gmail.com>
2018-04-05 21:52:45 -0700

    in progress, texts not reliably appearing yet

---
+++ shell/artifacts/Messages/Vr/source/GitHubPRs.js
@@ -0,0 +1,74 @@
+// @license
+// Copyright (c) 2017 Google Inc. All rights reserved.
+// This code may only be used under the BSD style license found at
+// http://polymer.github.io/LICENSE.txt
+// Code distributed by Google as part of this project is also
+// subject to an additional IP rights grant found at
+// http://polymer.github.io/PATENTS.txt
+
+"use strict";
+
+defineParticle(({ DomParticle, html, log }) => {
+
+  const template = html`
+    <aframe-html id={{subId}} anchor="center" height="1" width="2" position="0 1 0" html="{{pr}}"></aframe-html>
+  `.trim();
+
+  let githubIds = {
+    'Scott': 'sjmiles',
+    'Shane': 'shans',
+    'Doug': 'dstockwell',
+    'Berni': 'bseefeld',
+    'Noe': 'shaper',
+    'Mike': 'smalls'
+  };
+
+  const service = `https://xenonjs.com/services/http/php/github.php`;
+
+  return class extends DomParticle {
+    get template() {
+      return template;
+    }
+    render({ participants }, { prs, issues }) {
+      if (!prs) {
+        this.fetchPRs();
+      }
+      if (!issues) {
+        //this.fetchIssues();
+      }
+      return {
+        items: Object.keys(githubIds).map(name => {
+          const githubAccount = githubIds[name];
+          console.log("looking for PRs for", name, githubAccount);
+
+          const last = prs && prs.find(pr => pr.user.login === githubAccount);
+          if (!last) return {};
+
+          const needReview = last.requested_reviewers;
+          const title = last.title.replace(';', ',');
+
+          const reviewers = last.requested_reviewers ? `• reviews requested from ${last.requested_reviewers.map(r => r.login)}` : '';
+
+          const content = html`
+            <div style="background: #F8F8F8; color: #333; font-size: 24px; padding: 32px; border-radius: 16px;">
+              <div style="font-size: 1.3em; margin-bottom: 16px;">${last.title}</div>
+              <div style="color: gray;">#${last.number} ${reviewers}</div>
+            </div>
+          `;
+
+          console.log('rendering PR', last.number, content);
+          return {
+            subId: name,
+            pr: content
+          }
+        })
+      };
+    }
+    async fetchPRs() {
+      const response = await fetch(`${service}/repos/PolymerLabs/arcs/pulls`);
+      const prs = await response.json();
+      this.setState({ prs });
+    }
+  };
+
+});
