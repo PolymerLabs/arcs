996474941fd97bdc416dad1a71cd46233b0f1772
Walter Korman <wkorman@chromium.org>
2018-04-10 13:34:07 -0700

    Report progress during image upload.

--- shell/components/firebase-upload.js
+++ shell/components/firebase-upload.js
@@ -1,5 +1,5 @@
-import Xen from './xen/xen.js';
 import Firebase from './firebase.js';
+import Xen from './xen/xen.js';
 
 const {firebase, storage} = Firebase;
 
@@ -18,6 +18,10 @@ const template = `
   </label>
 `;
 
+const preamble = ['FirebaseUpload', '#ff69b4'];
+const log = Xen.logFactory(...preamble);
+const logError = Xen.logFactory(...preamble, 'error');
+
 class FirebaseUpload extends Xen.Base {
   get template() {
     return template;
@@ -26,28 +30,33 @@ class FirebaseUpload extends Xen.Base {
     return ['multiple', 'accept'];
   }
   _render({accept, multiple}) {
-    return {
-      accept,
-      multiple
-    };
+    return {accept, multiple};
   }
   _onFilesChanged(e) {
+    // TODO(wkorman): Handle multiple files if present.
     const input = e.currentTarget;
     const file = input.files[0];
-    const random = Math.floor((Math.random()+1)*1e8);
+    const random = Math.floor((Math.random() + 1) * 1e8);
     const path = `files/${random}`;
-    console.log('uploading', file.name, 'as', path);
+    log(`Uploading file [name=${file.name}, size=${
+        Math.floor(file.size / 1024)} KB, path=${path}].`);
     this._uploadFile(file, path);
   }
   _uploadFile(file, uploadPath) {
     const imageRef = storage.ref().child(uploadPath);
-    const next = () => {
+    const next = snapshot => {
+      const percentProgress = (snapshot.bytesTransferred / snapshot.totalBytes);
+      log(`Upload progress [percent=${
+          Math.floor(percentProgress * 100.0)}%, state=${snapshot.state}].`);
+      // TODO(wkorman): Report progress update to client.
     };
     const error = error => {
-      console.error('Error uploading', error);
+      logError('Error uploading', error);
+      // TODO(wkorman): Report error to client.
     };
     const complete = () => this._uploadComplete(imageRef);
-    imageRef.put(file).on(firebase.storage.TaskEvent.STATE_CHANGED, {next, error, complete});
+    imageRef.put(file).on(
+        firebase.storage.TaskEvent.STATE_CHANGED, {next, error, complete});
   }
   async _uploadComplete(ref) {
     try {
@@ -55,7 +64,7 @@ class FirebaseUpload extends Xen.Base {
       this.value = url;
       this._fire('upload');
     } catch (error) {
-      console.error('Error getting download url', error);
+      logError('Error getting download url', error);
     }
   }
 }
