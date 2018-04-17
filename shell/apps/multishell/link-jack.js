e5704a2d9e168755ac7623b4a00a684d5e142876
Scott J Miles <sjmiles@google.com>
2018-04-17 09:58:21 -0700

    dynamic instances in multishell

--- shell/apps/multishell/link-jack.js
+++ shell/apps/multishell/link-jack.js
@@ -1,12 +1,34 @@
 
-const jackLinks = (window, cb) => {
-  window.addEventListener('click', e => {
-    const anchor = e.path.find(el => el.localName === 'a');
-    if (anchor) {
-      e.preventDefault();
-      cb(anchor);
-    }
-  });
+const jackLinks = (target, cb) => {
+  setTimeout(() =>
+    target.addEventListener('click', e => {
+      const anchor = e.path.find(el => el.localName === 'a');
+      if (anchor) {
+        e.preventDefault();
+        cb(e, anchor);
+      }
+    })
+  , 2000);
 };
+
+// const jackLinks = (target, cb) => {
+//   console.log(target.readyState);
+//   const ready = {interactive: 1, complete: 1};
+//   if (!ready[target.readyState]) {
+//     target.addEventListener('readystatechange', ready => reallyJackLinks(target, cb));
+//   } else {
+//     reallyJackLinks(target, cb);
+//   }
+// };
+
+// const reallyJackLinks = (target, cb) => {
+//   target.addEventListener('click', e => {
+//     const anchor = e.path.find(el => el.localName === 'a');
+//     if (anchor) {
+//       e.preventDefault();
+//       cb(e, anchor);
+//     }
+//   });
+// };
 
 export default jackLinks;
