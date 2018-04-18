28471c37daf392c7c1e4e74f19842e985eec31c2
Scott J Miles <sjmiles@google.com>
2018-04-17 17:26:26 -0700

    use composedPath() for Safari compatibility

--- shell/app-shell-prototype/lib/link-jack.js
+++ shell/app-shell-prototype/lib/link-jack.js
@@ -1,6 +1,6 @@
 const jackLinks = (window, cb) => {
   window.addEventListener('click', e => {
-    const anchor = e.path.find(el => el.localName === 'a');
+    const anchor = e.composedPath().find(el => el.localName === 'a');
     if (anchor) {
       e.preventDefault();
       cb(anchor);
