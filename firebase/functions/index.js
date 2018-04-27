088331342d094ba0866f659afe36c4b0a2c11f89
Matt Small <mbsmall@google.com>
2018-04-27 10:33:22 -0700

    license & format

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,3 +1,11 @@
+// @license
+// Copyright (c) 2017 Google Inc. All rights reserved.
+// This code may only be used under the BSD style license found at
+// http://polymer.github.io/LICENSE.txt
+// Code distributed by Google as part of this project is also
+// subject to an additional IP rights grant found at
+// http://polymer.github.io/PATENTS.txt
+
 const functions = require('firebase-functions');
 const request = require('request-promise-native');
 
@@ -6,30 +14,35 @@ exports.places = functions.https.onRequest((req, res) => {
   const {location, radius, type} = req.query;
   const placesKey = functions.config().places.key;
 
-  const serviceUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
+  const serviceUrl =
+      'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
 
   return request({
-    uri: `${serviceUrl}?location=${location}&radius=${radius}&type=${type}&key=${placesKey}`,
-    method: 'GET',
-    resolveWithFullResponse: true
-  }).then(({statusCode, body}) => {
-    res.status(statusCode).send(body);
-  });
+           uri: `${serviceUrl}?location=${location}&radius=${radius}&type=${
+               type}&key=${placesKey}`,
+           method: 'GET',
+           resolveWithFullResponse: true
+         })
+      .then(({statusCode, body}) => {
+        res.status(statusCode).send(body);
+      });
 });
 
 /** Proxy for the Place Photos API */
 exports.placePhotos = functions.https.onRequest((req, res) => {
   const placesKey = functions.config().places.key;
   const query = Object.assign({'key': placesKey}, req.query);
-  const queryString = Object.entries(query).map(elem => elem.join('=')).join('&');
+  const queryString =
+      Object.entries(query).map(elem => elem.join('=')).join('&');
 
   const serviceUrl = 'https://maps.googleapis.com/maps/api/place/photo';
 
   return request({
-    uri: `${serviceUrl}?${queryString}`,
-    method: 'GET',
-    resolveWithFullResponse: true
-  }).then(({statusCode, body}) => {
-    res.status(statusCode).send(body);
-  });
+           uri: `${serviceUrl}?${queryString}`,
+           method: 'GET',
+           resolveWithFullResponse: true
+         })
+      .then(({statusCode, body}) => {
+        res.status(statusCode).send(body);
+      });
 });
