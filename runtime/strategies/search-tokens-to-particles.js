6977f7080309611428861534e906fd8ac4717aaf
Maria Mandlis <mmandlis@chromium.org>
2018-05-02 15:58:48 -0700

    SearchTokensToParticles strategy to also find recipe by name (#1244)

--- runtime/strategies/search-tokens-to-particles.js
+++ runtime/strategies/search-tokens-to-particles.js
@@ -14,7 +14,8 @@ export default class SearchTokensToParticles extends Strategy {
   constructor(arc) {
     super();
     // TODO: Recipes. Handles?
-    this._byToken = {};
+    this._particleByToken = {};
+    this._recipeByToken = {};
     for (let particle of arc.context.particles) {
       let name = particle.name.toLowerCase();
       this._addParticle(name, particle);
@@ -24,23 +25,34 @@ export default class SearchTokensToParticles extends Strategy {
         this._addParticle(verb, particle);
       }
     }
+    for (let recipe of arc.context.recipes) {
+      if (recipe.name) {
+        this._addRecipe(recipe.name.toLowerCase(), recipe);
+      }
+    }
 
-    let findParticles = token => this._byToken[token] || [];
+    let findParticles = token => this._particleByToken[token] || [];
+    let findRecipes = token => this._recipeByToken[token] || [];
     class Walker extends RecipeWalker {
       onRecipe(recipe) {
         if (!recipe.search || !recipe.search.unresolvedTokens.length) {
           return;
         }
 
-        let specsByToken = {};
+        let byToken = {};
         for (let token of recipe.search.unresolvedTokens) {
           for (let spec of findParticles(token)) {
             // TODO: Skip particles that are already in the active recipe?
-            specsByToken[token] = specsByToken[token] || [];
-            specsByToken[token].push(spec);
+            byToken[token] = byToken[token] || [];
+            byToken[token].push({spec});
+          }
+          for (let innerRecipe of findRecipes(token)) {
+            // TODO: Skip recipes with particles that are already in the active recipe?
+            byToken[token] = byToken[token] || [];
+            byToken[token].push({innerRecipe});
           }
         }
-        let resolvedTokens = Object.keys(specsByToken);
+        let resolvedTokens = Object.keys(byToken);
         if (resolvedTokens.length == 0) {
           return;
         }
@@ -50,14 +62,19 @@ export default class SearchTokensToParticles extends Strategy {
           sets.reduce((acc, set) =>
             flatten(acc.map(x => set.map(y => [...x, y]))),
             [[]]);
-        let possibleCombinations = product(...Object.values(specsByToken).map(v => flatten(v)));
+        let possibleCombinations = product(...Object.values(byToken).map(v => flatten(v)));
 
         return possibleCombinations.map(combination => {
           return recipe => {
             resolvedTokens.forEach(token => recipe.search.resolveToken(token));
-            combination.forEach(spec => {
-              let particle = recipe.newParticle(spec.name);
-              particle.spec = spec;
+            combination.forEach(({spec, innerRecipe}) => {
+              if (spec) {
+                let particle = recipe.newParticle(spec.name);
+                particle.spec = spec;
+              } else {
+                assert(innerRecipe);
+                innerRecipe.mergeInto(recipe);
+              }
             });
             return resolvedTokens.size;
           };
@@ -79,8 +96,12 @@ export default class SearchTokensToParticles extends Strategy {
   }
 
   _addParticle(token, particle) {
-    this._byToken[token] = this._byToken[token] || [];
-    this._byToken[token].push(particle);
+    this._particleByToken[token] = this._particleByToken[token] || [];
+    this._particleByToken[token].push(particle);
+  }
+  _addRecipe(token, recipe) {
+    this._recipeByToken[token] = this._recipeByToken[token] || [];
+    this._recipeByToken[token].push(recipe);
   }
   async generate(inputParams) {
     return Recipe.over(this.getResults(inputParams), this.walker, this);
