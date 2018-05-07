58c50aa38b605bc00059aeea2c62a95f1ed81d38
Matt Small <mbsmall@google.com>
2018-05-07 09:38:32 -0700

    add screenshots to aid debugging note that this avenue of exploration isn't going to pan out - headless chrome doesn't run extensions and is unlikely to (https://bugs.chromium.org/p/chromium/issues/detail?id=604324). However, if anything changes there or if we pursue a non-headless option there may be useful code in this branch.

--- shell/test/wdio.conf.js
+++ shell/test/wdio.conf.js
@@ -145,6 +145,12 @@ exports.config = {
   //     webdriverrtc: {},
   //     browserevent: {}
   // },
+
+  plugins: {
+    'wdio-screenshot': {}
+  },
+
+
   //
   // Test runner services
   // Services take over a specific job you don't want to take care of. They enhance
