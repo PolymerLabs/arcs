11efe00116dda6265e5e01421c3b9dad238bf2df
shans <shane.stephens@gmail.com>
2018-05-07 12:33:39 +1000

    lint

--- runtime/test/test-environment-test.js
+++ runtime/test/test-environment-test.js
@@ -1,16 +1,16 @@
-import { registerSystemExceptionHandler } from '../arc-exceptions.js';
+import {registerSystemExceptionHandler} from '../arc-exceptions.js';
 
 let exceptions = [];
 
 beforeEach(() => registerSystemExceptionHandler((exception, name, particle) => exceptions.push({exception, name, particle})));
 
-afterEach(function () {
+afterEach(function() {
   if (exceptions.length > 0) {
-    exceptions.forEach(({exception, name, particle}) => { 
+    for (let {exception, name, particle} of exceptions) {
       let error = new Error(`${exception.name} when invoking system function ${name} on behalf of ${particle}`); 
       error.stack = exception.stack;
-      this.test.error(error);;
-    });
+      this.test.error(error); // eslint-disable-line no-invalid-this
+    }
     exceptions = [];
   }
-})
+});
