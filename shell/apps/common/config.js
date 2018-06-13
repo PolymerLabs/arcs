8e1d75f1db3857ea1c766774c60ccc27aa6a2184
Scott J. Miles <sjmiles@google.com>
2018-06-13 12:01:15 -0700

    move service-worker handler into common code, make it opt-in (#1475)

--- shell/apps/common/config.js
+++ shell/apps/common/config.js
@@ -1,8 +1,10 @@
 const defaultDebugLevel = 0;
 
+const params = (new URL(document.location)).searchParams;
+
 // must establish Debug level before using logFactory
 import Xen from '../../components/xen/xen.js';
-Xen.Debug.level = ((new URL(document.location)).searchParams.has('log')) ? 2 : defaultDebugLevel;
+Xen.Debug.level = params.has('log') ? 2 : defaultDebugLevel;
 
 // some globals configured here for the convenience of providing in this simple config file.
 // TODO(sjmiles): at least collate globals into a namespace, or provide as a module
@@ -17,6 +19,18 @@ window.shellUrls = {};
 // TOOD(sjmiles): allow persistent configurable redirects for developer environments, maybe via `localstorage`?
 if (document.location.pathname.includes('projects')) {
   window.shellUrls['https://sjmiles.github.io/'] = `${window.shellPath}/../../`;
+}
+
+// optional service worker
+const useSw = params.has('sw');
+if (useSw) {
+  navigator.serviceWorker.register('../../sw.js');
+} else {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
 }
 
 // default manifest!
