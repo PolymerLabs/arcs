5bce2656bc69eefa5acbf5ec432cc0dd5b408f91
Bernhard A. M. Seefeld <seefeld@gmail.com>
2018-06-12 17:41:53 -0700

    simplify flushing: fetching the shell clears the cache

--- shell/sw.js
+++ shell/sw.js
@@ -1,20 +1,12 @@
-// simple cache
-// clears if receiving fetch for ../flush.me
-// must be in shell/ so it can cache shell/artifacts/, etc.
+// simple cache, flushing it on reload (if loaded via shell/apps/...)
 
-let cacheVersion = 1;
-
-self.addEventListener('install', function(event) {
-  console.log('cache: install');
-});
+const cacheName = 'arcs';
 
 self.addEventListener('fetch', function(event) {
-  const cacheName = 'cache' + cacheVersion;
-  if (event.request.url.match(/\/flush\.me$/)) {
+  if (event.request.url.match(/\/apps\/[a-z]+\/(index.html|\?|$)/)) {
     event.waitUntil(caches.delete(cacheName));
-    event.respondWith(new Response(`cache $cacheName flushed.`));
-    cacheVersion++;
-    console.log('cache: flushed, and version incremented to', cacheVersion);
+    event.respondWith(fetch(event.request));
+    console.log('cache: flushed');
   } else event.respondWith(
     caches.match(event.request).then(function(resp) {
       return resp || fetch(event.request).then(function(response) {
