ea7995dc0f00815e27879cacd55897d3dd99133b
Matt Small <mbsmall@google.com>
2018-06-18 15:33:01 -0700

    license notices

--- asylo/host/storage/bin/start-couchdb.sh
+++ asylo/host/storage/bin/start-couchdb.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
+# This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+# The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+# The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+# Code distributed by Google as part of the polymer project is also
+# subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+
+
 KEY=/opt/storage/key
 IMAGE=/opt/storage/db.img
 SECURE_MOUNT_POINT=/opt/secure
