fdb2135acb0a887a93272ab358f02704cf932416
Matt Small <mbsmall@google.com>
2018-06-26 13:03:45 -0700

    potential lint fixes

--- shell/apps/couchdb-test/couch.js
+++ shell/apps/couchdb-test/couch.js
@@ -17,12 +17,12 @@ nano.db.destroy('alice', function() {
     // specify the database we are going to use
     let alice = nano.use('alice');
     // and insert a document in it
-    alice.insert({ happy: true }, 'rabbit', function(err, body, header) {
+    alice.insert({happy: true}, 'rabbit', function(err, body, header) {
       if (err) {
         console.log('[alice.insert] ', err.message);
         return;
       }
-      console.log('you have inserted the rabbit.')
+      console.log('you have inserted the rabbit.');
       console.log(body);
     });
   });
