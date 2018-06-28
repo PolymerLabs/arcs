663f6ecd48c4c7afd6b95d4cb4a4b7c843418773
Scott J Miles <sjmiles@google.com>
2018-06-28 13:25:21 -0700

    import cx-button

--- shell/apps/common/whitelisted.js
+++ shell/apps/common/whitelisted.js
@@ -1,11 +1,14 @@
 // components for particle use
 import '../../components/corellia-xen/cx-input.js';
 import '../../components/corellia-xen/cx-tabs.js';
-import '../../components/good-map.js';
+import '../../components/corellia-xen/cx-button.js';
+
 import '../../components/video-controller.js';
 import '../../components/firebase-upload.js';
 import '../../components/mic-input.js';
 import '../../components/model-input.js';
 
-// components for particle use: deprecated
+import '../../components/good-map.js';
+
+// deprecated
 import '../../components/x-list.js';
