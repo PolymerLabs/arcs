c14f0487f29ceab88f01317795a97fa0a9e4416f
Scott J Miles <sjmiles@google.com>
2018-07-09 12:49:16 -0700

    code cleanup

--- shell/artifacts/Common/source/ShowCollection.js
+++ shell/artifacts/Common/source/ShowCollection.js
@@ -9,87 +9,84 @@
 'use strict';
 
 defineParticle(({DomParticle, html, resolver}) => {
-  let host = `show-collection`;
-
-  let styles = html`
+  const host = `show-collection`;
+  const styles = html`
   <style>
-  [${host}] {
-    padding: 16px;
-    background-color: white;
-    max-width: 400px;
-    margin: 0 auto;
-  }
-  [${host}] > [head] {
-    display: flex;
-    align-items: center;
-    padding: 8px 0;
-    color: #aaaaaa;
-    // font-weight: bold;
-  }
-  [${host}] > [items] [item] {
-    // padding: 0 0 32px 0;
-    background-color: white;
-    border-bottom: 1px solid #eeeeee;
-  }
-  [${host}] > [items] [item]:last-child {
-    border: none;
-  }
-  [${host}] div[slotid="annotation"] {
-    font-size: 0.7em;
-  }
-  [${host}] [empty] {
-    color: #aaaaaa;
-    font-size: 14px;
-    font-style: italic;
-    padding: 10px 0;
-  }
-  [${host}] [items] p {
-    margin: 0;
-  }
+    [${host}] {
+      padding: 16px;
+      background-color: white;
+      max-width: 400px;
+      margin: 0 auto;
+    }
+    [${host}] > [head] {
+      display: flex;
+      align-items: center;
+      padding: 4px 0;
+      /*color: #aaaaaa;*/
+      font-size: 1.2em;
+    }
+    [${host}] > [items] {
+      background-color: white;
+    }
+    [${host}] > [items] > [item] {
+      /*
+      padding: 0 0 32px 0;
+      box-shadow: 0 0 2px rgba(0,0,0,0.2);
+      */
+      padding: 16px 0;
+      border-top: 1px solid #eeeeee;
+    }
+    [${host}] > [items] > [item]:last-child {
+      border-bottom: 1px solid #eeeeee;
+    }
+    [${host}] div[slotid="annotation"] {
+      font-size: 0.7em;
+    }
+    [${host}] [empty] {
+      color: #aaaaaa;
+      font-size: 14px;
+      font-style: italic;
+      padding: 10px 0;
+    }
+    [${host}] > [items] p {
+      margin: 0;
+    }
   </style>
   `;
 
-  let template = html`
+  const template = html`
+
 <div ${host}>
-  ${styles}
-  
+${styles}
   <div head>
-    <span>Your shortlist</span>
+    <span>List</span>
   </div>
-
   <div slotid="preamble"></div>
-
   <div empty hidden="{{hasItems}}">List is empty</div>
-
   <div items>{{items}}</div>
   <template items>
-    <div>
-      <div slotid="item" subid="{{id}}" style="background: white;box-shadow: 0 0 2px rgba(0,0,0,.2); padding: 16px; "></div>
+    <div item>
+      <div slotid="item" subid="{{id}}"></div>
       <div slotid="annotation" subid="{{id}}"></div>
     </div>
   </template>
-
   <div slotid="action"></div>
-
   <div slotid="postamble"></div>
 </div>
+
   `;
 
   return class extends DomParticle {
     get template() {
       return template;
     }
-    render(props) {
-      let {collection} = props;
+    render({collection}) {
+      collection = collection || [];
       return {
-        hasItems: collection && collection.length > 0,
+        hasItems: collection.length > 0,
         items: {
           $template: 'items',
-          models: collection ? collection.map(item => {
-            return {
-              id: item.id
-            };
-          }) : []
+          models: collection.map(({id}) => ({id}))
         }
       };
     }
