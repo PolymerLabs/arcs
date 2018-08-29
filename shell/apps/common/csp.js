5c726dbd772c3a432e83ad880e4fbb74acc1b80d
Scott J Miles <sjmiles@google.com>
2018-08-29 12:05:39 -0700

    allow more stream types from sjmiles.github.io

--- shell/apps/common/csp.js
+++ shell/apps/common/csp.js
@@ -29,39 +29,37 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
         wss://*.firebase.io wss://*.firebaseio.com
         https://*.firebaseio.com
         https://*.firebase.io
-        https://xenonjs.com
         https://raw.githubusercontent.com/shaper/
+        https://sjmiles.github.io
+        https://xenonjs.com
         https://*.tvmaze.com
         https://media.w3.org
-        Xttps://bost.ocks.org
-        Xttps://noelutz.github.io
         ;
     font-src
-      'self'
+        'self'
         https://fonts.googleapis.com
         https://fonts.gstatic.com
+        https://raw.githubusercontent.com/shaper/
         https://sjmiles.github.io
         ;
     img-src
         'self'
-        https://xenonjs.com
         https://raw.githubusercontent.com/shaper/
+        https://sjmiles.github.io
+        https://xenonjs.com
         https://*.tvmaze.com
         https://media.w3.org
-        Xttps://bost.ocks.org
-        Xttps://noelutz.github.io
         data:
         ;
-    style-src 'self'
+    style-src
+        'self'
         'unsafe-inline'
         https://fonts.googleapis.com
         https://fonts.gstatic.com
+        https://raw.githubusercontent.com/shaper/
         https://sjmiles.github.io
         https://xenonjs.com
-        https://raw.githubusercontent.com/shaper/
         https://media.w3.org
-        Xttps://bost.ocks.org
-        Xttps://noelutz.github.io
         ;
   `
   // (optional) compress whitespace
