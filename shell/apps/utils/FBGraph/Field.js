90aa0099ab3d196cdbbe421499d71da0954fceec
Paul Lindner <plindner@google.com>
2018-09-11 11:54:41 -0700

    Fix firebase imports for FBGraph

--- shell/apps/utils/FBGraph/Field.js
+++ shell/apps/utils/FBGraph/Field.js
@@ -1,4 +1,8 @@
 import {FbGraph} from '../../../components/FbGraph/FbGraph.js';
+
+// arcs runtime
+import '../../../build/ArcsLib.js';
+// firebase config requires arcs runtime
 import Firebase from '../../common/firebase-config.js';
 
 const {Field} = FbGraph(Firebase.db);
