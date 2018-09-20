79c5e6033ccdac8e1abb448e15c7ae9d2366c1b8
Ray Cromwell <cromwellian@gmail.com>
2018-09-19 17:47:16 -0700

    Initial prototype of Arcs Cloud Deployment Management System (#1904)
    * Initial prototype of Arcs Cloud Deployment Management System
    
    * Fix review nits, add ondisk pouchdb

---
+++ server/src/deployment/cloud.ts
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Copyright (c) 2018 Google Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+import {GCPCloud} from "./gcp/gcp";
+import {ContainerManager} from "./containers";
+import {DiskManager} from "./disks";
+
+
+/**
+ * Top level interface representing a Cloud service provider capable
+ * of provisioning storage and node deployments.
+ */
+export interface Cloud {
+    containers():ContainerManager;
+    disks():DiskManager;
+}
+
+/**
+ * Offers implementations for various cloud providers, currently limited
+ * to GCP.
+ */
+export class CloudManager {
+    static forGCP():Cloud {
+        return new GCPCloud();
+    }
+}
