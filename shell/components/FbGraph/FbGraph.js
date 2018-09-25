218cd3e7431ab5211ecdc405315bf969c77999c7
Scott J Miles <sjmiles@google.com>
2018-09-24 20:04:04 -0700

    remove unnecessary `window`

--- shell/components/FbGraph/FbGraph.js
+++ shell/components/FbGraph/FbGraph.js
@@ -12,10 +12,10 @@ const changeDebounceMs = 16;
 
 const debounce = (key, action, delay) => {
   if (key) {
-    window.clearTimeout(key);
+    clearTimeout(key);
   }
   if (action && delay) {
-    return window.setTimeout(action, delay);
+    return setTimeout(action, delay);
   }
 };
 
