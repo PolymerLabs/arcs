fdccfbab09f080414aaa19f7f4317fe74ecfd045
Maria Mandlis <mmandlis@chromium.org>
2018-09-28 19:22:03 -0700

    remove blank line and add comment.

--- shell/app-shell/elements/arc-host.js
+++ shell/app-shell/elements/arc-host.js
@@ -152,7 +152,8 @@ class ArcHost extends Xen.Debug(Xen.Base, log) {
     }
     // TODO(sjmiles): temporarily elide search info, it seems to choke the deserializer
     serialization = serialization.replace(/search `[^`]*`/, '').replace(/tokens \/\/ `[^`]*`/, '');
-
+    // Tearing down the existing arc, so that the DOM gets cleared before initializing
+    // slot composer.
     this._teardownArc(state.arc);
     //
     // generate new slotComposer
