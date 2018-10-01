635ec464f814766757fd42b47ccfe1c44a2d18b0
Maria Mandlis <mmandlis@chromium.org>
2018-10-01 13:04:24 -0700

    change inline suggestion color to yellow

--- shell/app-shell/elements/shell-ui/suggestion-element.js
+++ shell/app-shell/elements/shell-ui/suggestion-element.js
@@ -51,24 +51,29 @@ const template = html`
     }
     @-webkit-keyframes glowing {
       0% {
-        background: rgba(3,169,244,0.05);
+        // background: rgba(3,169,244,0.05);
+        background: rgba(255,242,204,0.05);
         // background: rgba(3,169,244,0);
         box-shadow: 0 0 20px rgba(3, 169, 244, 0);
       }
       25% {
-        background: rgba(3,169,244,0.2);
+        //background: rgba(3,169,244,0.2);
+        background: rgba(255,242,204,0.5);
         box-shadow: 0 0 20px rgba(3, 169, 244, 0.2);
       }
       50% {
-        background: rgba(3,169,244,.3);
+        // background: rgba(3,169,244,.3);
+        background: rgba(255,242,204,1.0);
         box-shadow: 0 0 20px rgba(3, 169, 244, .3);
       }
       75% {
-        background: rgba(3,169,244,0.2);
+        // background: rgba(3,169,244,0.2);
+        background: rgba(255,242,204,1.0);
         box-shadow: 0 0 20px rgba(3, 169, 244, 0.2);
       }
       100% {
-        background: rgba(3,169,244,0.05);
+        // background: rgba(3,169,244,0.05);
+        background: rgba(255,242,204,0.8);
         box-shadow: 0 0 20px rgba(3, 169, 244, 0.2);
       }
     }
