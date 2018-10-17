40f86ffe5ab9a48f0d572986a3e435491bf31a66
Scott J Miles <sjmiles@google.com>
2018-10-16 17:24:25 -0700

    create arc+dom+composer encapsulation (web-arc), factor live launcher (web-launcher)

--- shell_0_6_0/web-shell/elements/web-shell.js
+++ shell_0_6_0/web-shell/elements/web-shell.js
@@ -10,7 +10,6 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
 import {linkJack} from '../../components/link-jack.js';
 import {generateId} from '../../components/generate-id.js';
-import {SlotComposer} from '../../../runtime/slot-composer.js';
 import {SlotDomConsumer} from '../../../runtime/ts-build/slot-dom-consumer.js';
 import {ArcsEnvWeb} from '../../lib/web/arcs-env-web.js';
 import {Xen} from '../../lib/xen.js';
@@ -18,6 +17,7 @@ import './web-config.js';
 import './web-arc.js';
 import './user-context.js';
 import './ui/web-shell-ui.js';
+import './web-launcher.js';
 
 // disable flushing template cache on dispose
 SlotDomConsumer.multitenant = true;
@@ -33,18 +33,28 @@ const template = Xen.Template.html`
       padding: 4px;
       margin-bottom: 8px;
     }
+    web-launcher[hidden] {
+      display: none;
+    }
+    web-arc[hidden] {
+      display: none;
+    }
   </style>
-
+  <!-- manage configuration (read and persist) -->
   <web-config userid="{{userid}}" arckey="{{arckey}}" on-config="onState"></web-config>
+  <!-- context bootstrap -->
+  <web-arc env="{{env}}" storage="volatile://context" config="{{contextConfig}}" context="{{context}}"></web-arc>
+  <!-- context feed -->
   <user-context env="{{env}}" storage="{{storage}}" userid="{{userid}}" context="{{context}}" arcstore="{{store}}"></user-context>
-
-  <web-arc env="{{env}}" storage="{{storage}}" composer="{{launcherComposer}}" config="{{launcherConfig}}" on-arc="onLauncherArc" on-recipe="onState"></web-arc>
-  <web-arc env="{{env}}" storage="{{storage}}" composer="{{arcComposer}}" config="{{arcConfig}}" context="{{context}}" on-arc="onState"></web-arc>
-  <web-arc env="{{env}}" storage="volatile://context" composer="{{launcherComposer}}" config="{{contextConfig}}" context="{{contextContext}}" on-arc="onContextArc"></web-arc>
-
+  <!-- ui chrome -->
   <web-shell-ui arc="{{arc}}" context="{{context}}">
-    <slot></slot>
-    <slot slot="suggestions" name="suggestions"></slot>
+    <!-- launcher -->
+    <web-arc hidden="{{hideLauncher}}" Xstyle="display: none;" env="{{env}}" storage="{{storage}}" config="{{launcherConfig}}" on-arc="onLauncherArc" Xon-recipe="onState"></web-arc>
+    <!-- <web-launcher hidden="{{hideLauncher}}" env="{{env}}" storage="{{storage}}" info="{{info}}"></web-launcher> -->
+    <!-- other arcs -->
+    <web-arc hidden="{{hideArc}}" env="{{env}}" storage="{{storage}}" config="{{arcConfig}}" context="{{context}}" on-arc="onState" manifest="{{manifest}}"></web-arc>
+    <!-- suggestions -->
+    <div slot="suggestions" slotid="suggestions"></div>
   </web-shell-ui>
 `;
 
@@ -76,19 +86,24 @@ export class WebShell extends Xen.Debug(Xen.Async, log) {
       this.waitForStore(10);
     }
     if (state.env && state.arckey && state.context) {
-      if (state.arcConfig && state.arcConfig.id === state.arckey) {
-        //this.showHideLauncher(false);
-      } else {
-        this.openArc(state.arckey);
+      if (!state.arcConfig || state.arcConfig.id !== state.arckey) {
+        this.state = {
+          arc: null,
+          arckey: state.arckey,
+          arcConfig: {
+            id: state.arckey
+          }
+        };
       }
     }
     if (!state.launcherConfig && state.env && state.context) {
       // spin up launcher arc
       this.spawnLauncher();
     }
-    this.showHideLauncher(!state.arckey);
+    this.state = {hideLauncher: Boolean(state.arckey)};
   }
   render(props, state) {
+    state.hideArc = !state.hideLauncher;
     return [props, state];
   }
   async updateEnv({root}, state) {
@@ -98,14 +113,6 @@ export class WebShell extends Xen.Debug(Xen.Async, log) {
     state.env = new ArcsEnvWeb(root);
     // map in 0_6_0 artifacts
     state.env.pathMap['https://$artifacts/'] = `${state.env.rootPath}/artifacts_0_6_0/`;
-    // marshal nodes from light-dom
-    state.launcherNodes = document.body.querySelector('[launcherNodes]');
-    state.arcNodes = document.body.querySelector('[arcNodes]');
-    // configure composers
-    // TODO(sjmiles): composers own DOM, probably should be custom elements
-    state.launcherComposer = new SlotComposer({affordance: 'dom', rootContainer: state.launcherNodes});
-    state.arcComposer = new SlotComposer({affordance: 'dom', rootContainer: state.arcNodes});
-    //state.contextComposer = new SlotComposer({affordance: 'dom', rootContainer: state.launcherNodes});
     // spin up context arc
     await this.spawnContext();
   }
@@ -124,7 +131,7 @@ export class WebShell extends Xen.Debug(Xen.Async, log) {
     if (launcherArc && !store) {
       if (launcherArc._stores.length) {
         const store = launcherArc._stores.pop();
-        store.on('change', info => this.launcherStoreChange(info), this);
+        store.on('change', info => this.state = {info}, this);
         this.state = {store};
       } else {
         setTimeout(() => this.waitForStore(), pollInterval);
@@ -132,12 +139,12 @@ export class WebShell extends Xen.Debug(Xen.Async, log) {
     }
   }
   async spawnContext() {
-    const contextContext = await this.state.env.parse(`
+    const context = await this.state.env.parse(`
   import 'https://$artifacts/canonical.manifest'
   import 'https://$artifacts/Profile/Sharing.recipe'
     `);
     this.state = {
-      contextContext,
+      context,
       contextConfig: {
         id: `${this.state.userid}-context`
       }
@@ -151,6 +158,32 @@ export class WebShell extends Xen.Debug(Xen.Async, log) {
       }
     };
   }
+  spawnSuggestions() {
+    const suggestions = [
+      `Arcs/Login.recipe`,
+      `Music/Playlist.recipe`,
+      `Profile/BasicProfile.recipe`,
+      `Restaurants/Restaurants.recipes`,
+      `Reservations/Reservations.recipes`
+    ];
+    const slot = this.host.querySelector(`[slotid="suggestions"]`);
+    if (slot) {
+      suggestions.forEach(suggestion => {
+        slot.appendChild(Object.assign(document.createElement(`suggestion-element`), {
+          suggestion,
+          innerText: suggestion.split('/').pop().split('.').shift()
+        }))
+        .addEventListener('plan-choose', () => this.applySuggestion(suggestion));
+      });
+    }
+  }
+  applySuggestion(suggestion) {
+    if (this.state.arc) {
+      this.state = {manifest: `import 'https://$artifacts/${suggestion}'`};
+    } else {
+      this.spawnArc(suggestion);
+    }
+  }
   spawnArc(recipe) {
     const luid = generateId();
     const id = `${this.state.userid}-${luid}`;
@@ -160,7 +193,8 @@ export class WebShell extends Xen.Debug(Xen.Async, log) {
       arcConfig: {
         id,
         manifest: `import 'https://$artifacts/${recipe}'`
-      }
+      },
+      manifest: null
     };
     const color = ['purple', 'blue', 'green', 'orange', 'brown'][Math.floor(Math.random()*5)];
     this.recordArcMeta({
@@ -181,103 +215,11 @@ export class WebShell extends Xen.Debug(Xen.Async, log) {
       await store.store(metaEntity, [String(Math.random())]);
     }
   }
-  openArc(id) {
-    this.state = {
-      arckey: id,
-      arc: null,
-      arcConfig: {id}
-    };
-  }
-  showHideLauncher(show) {
-    const {launcherNodes, arcNodes} = this.state;
-    if (launcherNodes && arcNodes) {
-      launcherNodes.hidden = !show;
-      arcNodes.hidden = show;
-    }
-  }
-  spawnSuggestions() {
-    const suggestions = [
-      `Arcs/Login.recipe`,
-      `Music/Playlist.recipe`,
-      `Profile/BasicProfile.recipe`,
-      `Profile/Geolocate.recipe`
-    ];
-    const slot = document.querySelector(`[slotid="suggestions"]`);
-    if (slot) {
-      suggestions.forEach(suggestion => {
-        const elt = slot.appendChild(
-          Object.assign(document.createElement(`suggestion-element`), {
-            recipe: suggestion,
-            innerText: suggestion.split('.').shift()
-          }
-        ));
-        elt.addEventListener('plan-choose', () => {
-          this.spawnArc(suggestion);
-        });
-      });
-    }
-  }
   onLauncherArc(e, launcherArc) {
     this.state = {launcherArc};
   }
-  async onContextArc(e, contextArc) {
-    this.state = {context: contextArc.context};
-  }
-  onSpawnClick(e) {
-    const recipe = e.target.getAttribute('recipe');
-    this.spawnArc(recipe);
-  }
   onLauncherClick() {
     this.state = {arckey: ''};
-  }
-  launcherStoreChange(info) {
-    log(info);
-    info.add && info.add.forEach(({value: {rawData}}) => {
-      if (!rawData.deleted) {
-        this.createArcBox(rawData);
-      }
-    });
-  }
-  createArcBox({key, href, description}) {
-    this.boxes = this.boxes || {};
-    const oldarc = this.boxes[key];
-    if (oldarc) {
-      oldarc.dispose();
-      oldarc.remove();
-      oldarc.node.remove();
-      this.boxes[key] = null;
-    }
-    //
-    const html = Xen.html`
-<div slotid="toproot"></div>
-<div slotid="root"></div>
-<div slotid="modal"></div>
-    `;
-    const preview = document.querySelector('[preview]');
-    preview.style.cssText = 'text-align: center';
-    const wrapper = document.createElement('a');
-    wrapper.href = href;
-    wrapper.style.cssText = `display: inline-block; border: 1px solid silver; width: 192px; height: 200px; overflow: hidden; margin: 8px; text-align: left;`;
-    const label = document.createElement('div');
-    label.style.cssText = `background-color: whitesmoke; height: 32px; white-space: nowrap; font-size: 12px; padding: 4px;`;
-    label.innerHTML = description;
-    wrapper.appendChild(label);
-    const box = document.createElement('div');
-    box.style.cssText = `transform-origin: top left; transform: scale(0.3); width: 640px; height: 560px; pointer-events: none;`;
-    box.innerHTML = html;
-    wrapper.appendChild(box);
-    preview.appendChild(wrapper);
-    const composer = new SlotComposer({affordance: 'dom', rootContainer: box});
-    const webarc = Object.assign(document.createElement('web-arc'), {
-      env: this.state.env,
-      storage: this.props.storage,
-      composer: composer,
-      config: {id: key}
-    });
-    webarc.style.cssText = 'display: none;';
-    this.appendChild(webarc);
-    this.boxes[key] = webarc;
-    webarc.node = wrapper;
   }
 }
 
