5f0589fd2d8bf029b1266a1d1ef722e65ba793fc
Scott J Miles <sjmiles@google.com>
2018-10-17 20:10:23 -0700

    give single-shell a few more resources so we can taste more recipes

--- shell_0_6_0/single-shell/index.js
+++ shell_0_6_0/single-shell/index.js
@@ -1,6 +1,12 @@
 export const App = async (env, composer, manifestPath) => {
-  const context = await env.parse(`import 'https://$artifacts/canonical.manifest'`);
+  const context = await env.parse(`
+import 'https://$artifacts/canonical.manifest'
+import 'https://$artifacts/Profile/Sharing.recipe'
+  `);
   console.log(`context [${context.id}]`);
+
+  await installSystemUser({userid: 'gomer', context});
+  console.log('installed SYSTEM_user');
 
   const manifest = await env.parse(`import 'https://$artifacts/${manifestPath || 'Arcs/Login.recipe'}'`);
   console.log(`manifest [${manifest.id}]`);
@@ -19,3 +25,16 @@ export const App = async (env, composer, manifestPath) => {
 
   return arc;
 };
+
+async function installSystemUser({userid, context}) {
+  const store = await context.findStoreById('SYSTEM_user');
+  if (store) {
+    const user = {
+      id: store.generateID(),
+      rawData: {
+        id: userid,
+      }
+    };
+    store.set(user);
+  }
+}
