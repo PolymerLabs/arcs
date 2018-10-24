b1bee5d3c2c45bac9b6dadc93f10369ffce095f3
Paul Lindner <lindner@inuus.com>
2018-10-24 11:53:16 -0700

    remove extraneous 'use strict' from converted typescript files (#2137)

--- runtime/ts/arc.ts
+++ runtime/ts/arc.ts
@@ -7,7 +7,6 @@
  * subject to an additional IP rights grant found at
  * http://polymer.github.io/PATENTS.txt
  */
-'use strict';
 
 import {assert} from '../../platform/assert-web.js';
 import {Type} from './type.js';
