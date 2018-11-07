933aeaffd317c7525ff9a9d613f67137c6047dc2
Maria Mandlis <mmandlis@chromium.org>
2018-11-07 10:05:20 -0800

    clean up some TODOs description (post ts conversion) (#2197)

--- runtime/ts/description-dom-formatter.ts
+++ runtime/ts/description-dom-formatter.ts
@@ -41,9 +41,7 @@ export class DescriptionDomFormatter extends DescriptionFormatter {
       let {template, model} = this._retrieveTemplateAndModel(particleDesc, suggestionByParticleDesc.size, options || {});
 
       const success = await Promise.all(Object.keys(model).map(async tokenKey => {
-        // TODO(mmandlis): this cast is invalid, as _initSubTokens can sometimes
-        // return a single element
-        const tokens = this._initSubTokens(model[tokenKey], particleDesc) as {}[];
+        const tokens = this._initSubTokens(model[tokenKey], particleDesc);
         
         return (await Promise.all(tokens.map(async token => {
           const tokenValue = await this.tokenToString(token);
