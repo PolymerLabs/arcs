d0727460f0f0e230b2bbea435e3f02b1b5d0d753
Maria Mandlis <mmandlis@chromium.org>
2018-11-14 11:51:45 -0800

    maintain `contextual` status of planning results and trigger search based planning (#2234)
    * maintain `contextual` status of planning results and trigger search based planning
    
    * remove duplicate assignment
    
    * use correct replanning options on subsequent requests (fixes tests)

--- runtime/ts/plan/plan-producer.ts
+++ runtime/ts/plan/plan-producer.ts
@@ -10,6 +10,7 @@
 
 import {assert} from '../../../platform/assert-web.js';
 import {Arc} from '../arc';
+import {InitSearch} from '../../strategies/init-search.js';
 import {now} from '../../../platform/date-web.js';
 import {Planner} from '../planner.js';
 import {PlanningResult} from './planning-result.js';
@@ -25,16 +26,25 @@ export class PlanProducer {
   planner: Planner|null = null;
   speculator: Speculator;
   needReplan: boolean;
+  replanOptions: {};
   _isPlanning: boolean;
   stateChangedCallbacks: ((isPlanning: boolean) => void)[] = [];
+  search: string;
+  searchStore: StorageProviderBase;
+  searchStoreCallback: ({}) => void;
 
-  constructor(arc: Arc, store: StorageProviderBase) {
+  constructor(arc: Arc, store: StorageProviderBase, searchStore: StorageProviderBase) {
     assert(arc, 'arc cannot be null');
     assert(store, 'store cannot be null');
     this.arc = arc;
     this.result = new PlanningResult(arc);
     this.store = store;
     this.speculator = new Speculator();
+    this.searchStore = searchStore;
+    if (this.searchStore) {
+      this.searchStoreCallback = () => this.onSearchChanged();
+      this.searchStore.on('change', this.searchStoreCallback, this);
+    }
   }
 
   get isPlanning() { return this._isPlanning; }
@@ -50,12 +60,63 @@ export class PlanProducer {
     this.stateChangedCallbacks.push(callback);
   }
 
+  async onSearchChanged() {
+    const values = await this.searchStore['get']() || [];
+    const value = values.find(value => value.arc === this.arcKey);
+    if (!value) {
+      return;
+    }
+    if (value.search === this.search) {
+      return;
+    }
+    this.search = value.search;
+    if (!this.search) {
+      // search string turned empty, no need to replan, going back to contextual plans.
+      return;
+    }
+    if (this.search === '*') { // Search for ALL (including non-contextual) plans.
+      if (this.result.contextual) {
+        this.producePlans({contextual: false});
+      }
+    } else { // Search by search term.
+      const options = {
+        cancelOngoingPlanning: this.result.plans.length > 0,
+        search: this.search
+      };
+      if (this.result.contextual) {
+        // If we're searching but currently only have contextual plans,
+        // we need get non-contextual plans as well.
+        Object.assign(options, {contextual: false});
+      } else {
+        // If search changed and we already how all plans (i.e. including
+        // non-contextual ones) then it's enough to initialize with InitSearch
+        // with a new search phrase.
+        Object.assign(options, {
+          strategies: [InitSearch].concat(Planner.ResolutionStrategies),
+          append: true
+        });
+      }
+
+      this.producePlans(options);
+    }
+  }
+
+  get arcKey(): string {
+    // TODO: this is a duplicate method of one in planificator.ts, refactor?
+    return this.arc.storageKey.substring(this.arc.storageKey.lastIndexOf('/') + 1);
+  }
+
+  dispose() {
+    this.searchStore.off('change', this.searchStoreCallback);
+  }
+
   async producePlans(options = {}) {
     if (options['cancelOngoingPlanning'] && this.isPlanning) {
       this._cancelPlanning();
     }
 
     this.needReplan = true;
+    this.replanOptions = options;
     if (this.isPlanning) {
       return;
     }
@@ -68,15 +129,15 @@ export class PlanProducer {
     while (this.needReplan) {
       this.needReplan = false;
       generations = [];
-      plans = await this.runPlanner(options, generations);
+      plans = await this.runPlanner(this.replanOptions, generations);
     }
     time = ((now() - time) / 1000).toFixed(2);
 
     // Plans are null, if planning was cancelled.
     if (plans) {
-      console.log(`Produced ${plans.length}${options['append'] ? ' additional' : ''} plans [elapsed=${time}s].`);
+      console.log(`Produced ${plans.length}${this.replanOptions['append'] ? ' additional' : ''} plans [elapsed=${time}s].`);
       this.isPlanning = false;
-      await this._updateResult({plans, generations}, options);
+      await this._updateResult({plans, generations}, this.replanOptions);
     }
   }
 
@@ -85,8 +146,11 @@ export class PlanProducer {
     assert(!this.planner, 'Planner must be null');
     this.planner = new Planner();
     this.planner.init(this.arc, {
-      strategies: options['strategies']
-      // TODO: add `search` and `contextual` params.
+      strategies: options['strategies'],
+      strategyArgs: {
+        contextual: options['contextual'],
+        search: options['search']
+      }
     });
 
     plans = await this.planner.suggest(options['timeout'] || defaultTimeoutMs, generations, this.speculator);
@@ -110,11 +174,12 @@ export class PlanProducer {
 
   private async _updateResult({plans, generations}, options) {
     if (options.append) {
+      assert(!options['contextual'], `Cannot append to contextual options`);
       if (!this.result.append({plans, generations})) {
         return;
       }
     } else {
-      if (!this.result.set({plans, generations})) {
+      if (!this.result.set({plans, generations, contextual: options['contextual']})) {
         return;
       }
     }
