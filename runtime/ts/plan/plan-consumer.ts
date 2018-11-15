ad7baf88c5291c1da8b34e56a28ba0640ae0530d
Maria Mandlis <mmandlis@chromium.org>
2018-11-15 10:11:57 -0800

    Only show truly contextual suggestions when search is empty (#2252)
    and use log factory in plan-producer.

--- runtime/ts/plan/plan-consumer.ts
+++ runtime/ts/plan/plan-consumer.ts
@@ -96,7 +96,7 @@ export class PlanConsumer {
       });
       const usesRemoteNonRootSlots = suggestion['plan'].slots.find(slot => {
         return !slot.name.includes('root') && !slot.tags.includes('root') &&
-               slot.id && !slot.id.includes('root');
+               slot.id && !slot.id.includes('root') && this.arc.pec.slotComposer.findContextById(slot.id);
       });
       const onlyUsesNonRootSlots =
           !suggestion['plan'].slots.find(s => s.name.includes('root') || s.tags.includes('root'));
