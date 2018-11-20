8a784666bed81af86f509fa4528f5f8301aac09f
Ray Cromwell <cromwellian@gmail.com>
2018-11-19 16:29:22 -0800

    Add a Web ESM rollup config for frontend apps (e.g. provisioning app)
    Polymer 3.0 only understands ESM modules, so we need to rollup a
    bundled output that has all CJS dependencies bundled inside, except
    it should not contain any non-web dependencies (e.g. node APIs).
    
    To do this, we use the Typescript plugin to compile direct from TS,
    export .d.ts files, and and use the commonjs() resolver plugin.
    
    We still face the problem that the Polymer-CLI build tools won't
    pick up the 'browser' dependency in package.json, so that you must
    directly import 'dist/indexweb.esm.js' for now.

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,10 @@ import ignore from 'rollup-plugin-ignore';
 
 import pkg from './package.json';
 import path from 'path';
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
 
-export default {
+export default [{
   input: ['runtime/ts-build/runtime.js', 'runtime/ts-build/keymgmt/manager.js', 'shell/apps/remote-planning/interface.js'],
   output: [
     {
@@ -30,4 +32,31 @@ export default {
     resolve({jsnext: true, modulesOnly: true}),
     multiEntry()
   ]
-};
+},
+  {
+    input: ['runtime/ts/keymgmt/manager.ts'],
+    output: [
+      {
+        file: pkg.browser,
+        format: 'esm',
+      }
+    ],
+    plugins: [
+      typescript(
+        {
+          tsconfig: 'tsconfig.json'
+        }),
+      ignore(['whatwg-fetch']),
+      {
+        resolveId: (importee, importer) => {
+          if (importee.includes('-node.js')) {
+            return path.resolve(path.dirname(importer), importee.replace('-node.js', '-web.js'));
+          }
+          // if nothing is returned, we fall back to default resolution
+        }
+      }, 
+      resolve(),
+      commonjs(),
+      multiEntry()
+    ]
+  }];
