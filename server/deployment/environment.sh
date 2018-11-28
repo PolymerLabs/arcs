507c9cdd4696344aa32a2e064566ac270bedc7b2
Ray Cromwell <cromwellian@gmail.com>
2018-11-28 13:27:19 -0800

    fix hardcoded arcs-project project-id in cloud setup

--- server/deployment/environment.sh
+++ server/deployment/environment.sh
@@ -1,8 +1,8 @@
 
-export PROJECT_ID=arcs-project
+export PROJECT_ID=arcs-project-223901
 export COMPUTE_ZONE=us-central1-a
 export CLUSTER_NAME=personal-cloud
-export BUILD_LABEL=gcr.io/arcs-project/deployment:latest
+export BUILD_LABEL=gcr.io/${PROJECT_ID}/deployment:latest
 export SERVICE_ACCOUNT=arcs-sa@${PROJECT_ID}.iam.gserviceaccount.com
 export DNS_ZONE=skarabrae.org
 export DNS_ZONE_NAME=skarabrae
