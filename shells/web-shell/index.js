5cba9ec0b77a2829920975c36335deefba1003d9
Scott J Miles <sjmiles@google.com>
2018-11-30 17:15:04 -0800

    remove unused import

--- shells/web-shell/index.js
+++ shells/web-shell/index.js
@@ -1,7 +1,8 @@
+// default to semi-verbose logging
 import {Xen} from '../lib/xen.js';
 Xen.Debug.level = 2;
 
-import {Const} from '../configuration/constants.js';
+// configure root path
 Object.assign(document.querySelector('web-shell'), {
   root: '../..' // path to arcs/
 });
