8c054fa462262e811216e704b3c6158455affeee
Ray Cromwell <cromwellian@gmail.com>
2018-12-04 16:15:31 -0800

    fix hardcoded arcs-project project-id in cloud setup (#2334)

--- server/deployment/setup_helm.sh
+++ server/deployment/setup_helm.sh
@@ -6,6 +6,7 @@ source ./configure_helm_permissions.sh
 echo "Initialing Helm"
 helm init --wait  --service-account tiller 
 helm repo update
+helm dep build ./arcs
 
 # Needed by external-dns
 kubectl create clusterrolebinding default-admin --clusterrole cluster-admin --serviceaccount=default:default
@@ -18,10 +19,10 @@ gcloud iam service-accounts create prod-clouddns-svc-acct-secret \
   --project=${PROJECT_ID}
 
 gcloud iam service-accounts keys create ./prod-clouddns-service-account.json \
---iam-account=prod-clouddns-svc-acct-secret@arcs-project.iam.gserviceaccount.com \
+--iam-account=prod-clouddns-svc-acct-secret@${PROJECT_ID}.iam.gserviceaccount.com \
 --project=${PROJECT_ID}
 
-gcloud projects add-iam-policy-binding arcs-project \
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
 --member=serviceAccount:prod-clouddns-svc-acct-secret@${PROJECT_ID}.iam.gserviceaccount.com \
 --role=roles/dns.admin
 
@@ -31,7 +32,7 @@ kubectl create secret generic prod-clouddns-svc-acct-secret \
 helm install ./arcs \
   --set domain=${DNS_ZONE} \
   --set arcs=${SERVICE_ACCOUNT} \
-  --set project=${PROJECT_ID}
-
+  --set project=${PROJECT_ID} \
+  --set image.repository=gcr.io/${PROJECT_ID}/deployment
 
 
