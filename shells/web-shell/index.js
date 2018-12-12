d3d01133e820ab0a09762830820c9b3d0b732d4a
Scott J Miles <sjmiles@google.com>
2018-12-12 00:19:30 -0800

    broken attempt to fix log-web, must revisit

--- shells/web-shell/index.js
+++ shells/web-shell/index.js
@@ -1,7 +1,10 @@
 import {Xen} from '../lib/xen.js';
-
 const params = (new URL(document.location)).searchParams;
-Xen.Debug.level = params.get('logLevel') || (params.has('log') ? 2 : Xen.Debug.level);
+const logLevel = params.get('logLevel') || (params.has('log') ? 2 : Xen.Debug.level);
+Xen.Debug.level = logLevel;
+
+import {Xen as XD} from '../env/arcs.js';
+XD.Debug.level = logLevel;
 
 // configure root path
 Object.assign(document.querySelector('web-shell'), {
--- src/platform/log-web.js
+++ src/platform/log-web.js
@@ -9,6 +9,6 @@ import {Debug, logFactory as _logFactory} from '../../modalities/dom/components/
 
 const _nopFactory = () => () => {};
 
-// TODO(sjmiles): problem with timing Debug.level or duplicate modules?
-export const logFactory = (...args) => Debug.level < 1 ? _nopFactory() : _logFactory(...args);
-//export const logFactory = _logFactory;
+// TODO(sjmiles): problems with timing Debug.level and duplicate modules
+//export const logFactory = (...args) => Debug.level < 1 ? _nopFactory() : _logFactory(...args);
+export const logFactory = _logFactory;
