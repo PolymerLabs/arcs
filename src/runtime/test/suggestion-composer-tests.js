8db056b02ad466f5d17003ce585717a20efb6204
Maria Mandlis <mmandlis@chromium.org>
2019-01-04 13:02:37 -0800

    suggestion-composer test workaround (#2532)
    * workaround in suggestion-composer test to bypass inner arc strategizing
    
    * address comment

--- src/runtime/test/suggestion-composer-tests.js
+++ src/runtime/test/suggestion-composer-tests.js
@@ -72,9 +72,7 @@ describe('suggestion composer', function() {
     assert.isEmpty(suggestionComposer._suggestConsumers);
   });
 
-  // #2523: This test is building outer arc suggestions connecting to the inner arc particles.
-  // We don't support this, so this test is skipped for now.
-  it.skip('suggestion set-slots', async () => {
+  it('suggestion set-slots', async () => {
     const slotComposer = new FakeSlotComposer();
     const helper = await TestHelper.createAndPlan({
       manifestFilename: './src/runtime/test/artifacts/suggestions/Cakes.recipes',
@@ -86,15 +84,20 @@ describe('suggestion composer', function() {
     assert.isEmpty(suggestionComposer._suggestConsumers);
 
     await helper.acceptSuggestion({particles: ['List', 'CakeMuxer']});
-    await helper.makePlans();
-    assert.lengthOf(helper.suggestions, 1);
+    await helper.makePlans({includeInnerArcs: true});
+    assert.lengthOf(helper.suggestions.filter(s => s.descriptionText === 'Light candles on Tiramisu cake.'), 1);
     await suggestionComposer.setSuggestions(helper.suggestions);
     assert.lengthOf(suggestionComposer._suggestConsumers, 1);
     const suggestConsumer = suggestionComposer._suggestConsumers[0];
     await suggestConsumer._setContentPromise;
     assert.isTrue(suggestConsumer._content.template.includes('Light candles on Tiramisu cake'));
 
-    await helper.acceptSuggestion({particles: ['LightCandles']});
+    // TODO(mmandlis): Better support in test-helper for instantiating suggestions in inner arcs.
+    // Instantiate inner arc's suggestion.
+    const innerSuggestion = helper.findSuggestionByParticleNames(['LightCandles'])[0];
+    const innerArc = helper.arc.innerArcs[0];
+    await innerSuggestion.instantiate(innerArc);
+    await helper.idle();
 
     await helper.makePlans();
     assert.isEmpty(helper.suggestions);
--- src/runtime/testing/test-helper.ts
+++ src/runtime/testing/test-helper.ts
@@ -35,6 +35,7 @@ type TestHelperOptions = {
 type TestHelperPlanOptions = TestHelperOptions & {
   expectedNumPlans?: number;
   expectedSuggestions?;
+  includeInnerArcs?: boolean;
   verify?;
 };
 
@@ -126,6 +127,13 @@ export class TestHelper {
     const planner = new Planner();
     planner.init(this.arc, {strategyArgs: {recipeIndex: this.recipeIndex}});
     this.suggestions = await planner.suggest();
+    if (options && options.includeInnerArcs) {
+      for (const innerArc of this.arc.innerArcs) {
+        const innerPlanner = new Planner();
+        innerPlanner.init(innerArc, {strategyArgs: {recipeIndex: this.recipeIndex}});
+        this.suggestions = this.suggestions.concat(await innerPlanner.suggest());
+      }
+    }
     if (options) {
       if (options.expectedNumPlans) {
         assert.lengthOf(this.suggestions, options.expectedNumPlans);
