9e5992c3216d740ce6784d7046cbe6262b1b9d02
Scott J Miles <sjmiles@google.com>
2019-01-08 18:30:04 -0800

    enable log-web logging in all cases (stopgap)

--- src/platform/log-web.js
+++ src/platform/log-web.js
@@ -5,8 +5,9 @@
 // subject to an additional IP rights grant found at
 // http://polymer.github.io/PATENTS.txt
 
-import {Debug, logFactory as _logFactory} from '../../modalities/dom/components/xen/xen-debug.js';
+//import {Debug, logFactory as _logFactory} from '../../modalities/dom/components/xen/xen-debug.js';
+//const factory = /*Debug.Level < 1 ? () => () => {} :*/ _logFactory;
 
-const factory = /*Debug.Level < 1 ? () => () => {} :*/ _logFactory;
+const factory = (preamble, color, log='log') => console[log].bind(console, `%c${preamble}`, `background: ${color}; color: white; padding: 1px 6px 2px 7px; border-radius: 6px;`);
 
 export const logFactory = (...args) => factory(...args);
