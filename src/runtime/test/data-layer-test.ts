2dba3a4f691d335bfe9702363bca7aac15fd141e
Paul Lindner <lindner@inuus.com>
2019-01-25 10:37:28 -0800

    convert data-layer-test to typescript (#2640)

--- src/runtime/test/data-layer-test.ts
+++ src/runtime/test/data-layer-test.ts
@@ -11,40 +11,33 @@
 import {assert} from '../../platform/chai-web.js';
 import {Arc} from '../arc.js';
 import {FakeSlotComposer} from '../testing/fake-slot-composer.js';
+import {Loader} from '../loader.js';
 import {Schema} from '../schema.js';
 import {EntityType} from '../type.js';
 import {handleFor} from '../handle.js';
+import {CollectionStorageProvider} from '../storage/storage-provider-base.js';
 
-describe('entity', async function() {
+describe('entity', async () => {
   it('can be created, stored, and restored', async () => {
     const schema = new Schema(['TestSchema'], {value: 'Text'});
 
-    const arc = new Arc({slotComposer: new FakeSlotComposer(), id: 'test'});
+    const arc = new Arc({slotComposer: new FakeSlotComposer(), id: 'test', context: null, loader: new Loader()});
     const entity = new (schema.entityClass())({value: 'hello world'});
     assert.isDefined(entity);
-    const storage = await arc.createStore(new EntityType(schema).collectionOf());
+
+    const collectionType = new EntityType(schema).collectionOf();
+    
+    // Get around incompatible types for handleFor()
+    let storage;
+    storage = await arc.createStore(collectionType);
     const handle = handleFor(storage);
     await handle.store(entity);
 
-    const list = await arc.findStoresByType(entity.constructor.type.collectionOf())[0].toList();
+    const collection = arc.findStoresByType(collectionType)[0] as CollectionStorageProvider;
+    const list = await collection.toList();
     const clone = list[0];
     assert.isDefined(clone);
     assert.deepEqual(clone.rawData, {value: 'hello world'});
     assert.notEqual(entity, clone);
-  });
-});
-
-describe.skip('relation', function() {
-  it('can be created, stored, and restored', function() {
-    let Relation;
-    let BasicEntity;
-    const arc = new Arc({});
-    const relation = new Relation(new BasicEntity('thing1'), new BasicEntity('thing2'));
-    assert.isDefined(relation);
-    arc.commit([relation]);
-    const clone = arc.findStoresByType(relation.constructor.type.collectionOf())[0].toList()[0];
-    assert.isDefined(clone);
-    assert.equal(clone.entities[0].data, 'thing1');
-    assert.notEqual(relation, clone);
   });
 });
