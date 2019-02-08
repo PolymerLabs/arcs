701df510fe6b7a40720301172af9d8da5f0bd2fd
Paul Lindner <lindner@inuus.com>
2019-02-08 13:22:24 -0800

    Introduce a testing class to verify the number of StorageProvider callbacks (#2678)
    - Still needs to be used in more tests
    - Can be extended to verify that specific callbacks were called
    - Eventually convert to ts-mockito when it's compatible with our environment

--- src/runtime/manual_tests/firebase-tests.ts
+++ src/runtime/manual_tests/firebase-tests.ts
@@ -12,12 +12,14 @@ import {StorageProviderFactory} from '../storage/storage-provider-factory.js';
 import {Arc} from '../arc.js';
 import {Loader} from '../loader.js';
 import {Manifest} from '../manifest.js';
+import {Id} from '../id.js';
 import {EntityType, ReferenceType} from '../type.js';
 import {assert} from '../../platform/chai-web.js';
 import {resetStorageForTesting} from '../storage/firebase-storage.js';
 import {BigCollectionStorageProvider, CollectionStorageProvider, VariableStorageProvider} from '../storage/storage-provider-base.js';
 import {StubLoader} from '../testing/stub-loader.js';
 import {TestHelper} from '../testing/test-helper.js';
+import {CallbackTracker} from '../testing/callback-tracker.js';
 
 // Console is https://firebase.corp.google.com/project/arcs-storage-test/database/arcs-storage-test/data/firebase-storage-test
 const testUrl = 'firebase://arcs-storage-test.firebaseio.com/AIzaSyBLqThan3QCOICj0JZ-nEwk27H4gmnADP8/firebase-storage-test';
@@ -50,7 +52,7 @@ describe('firebase', function() {
 
   let storageInstances = [];
 
-  function createStorage(id) {
+  function createStorage(id: Id): StorageProviderFactory {
     const storage = new StorageProviderFactory(id);
     storageInstances.push(storage);
     return storage;
@@ -72,10 +74,14 @@ describe('firebase', function() {
       const barType = new EntityType(manifest.schemas.Bar);
       const value = 'Hi there' + Math.random();
       const variable = await storage.construct('test0', barType, newStoreKey('variable')) as VariableStorageProvider;
+      const callbackTracker = new CallbackTracker(variable, 1);
+
       await variable.set({id: 'test0:test', value});
       const result = await variable.get();
       assert.equal(value, result.value);
+      callbackTracker.verify();
     });
+
     it('resolves concurrent set', async () => {
       const manifest = await Manifest.parse(`
         schema Bar
@@ -86,12 +92,18 @@ describe('firebase', function() {
       const barType = new EntityType(manifest.schemas.Bar);
       const key = newStoreKey('variable');
       const var1 = await storage.construct('test0', barType, key) as VariableStorageProvider;
+      const var1Callbacks = new CallbackTracker(var1, 2);
       const var2 = await storage.connect('test0', barType, key) as VariableStorageProvider;
+      const var2Callbacks = new CallbackTracker(var2, 2);
+
       var1.set({id: 'id1', value: 'value1'});
       var2.set({id: 'id2', value: 'value2'});
       await synchronized(var1, var2);
       assert.deepEqual(await var1.get(), await var2.get());
+      var1Callbacks.verify();
+      var2Callbacks.verify();
     });
+
     it('enables referenceMode by default', async () => {
       const manifest = await Manifest.parse(`
         schema Bar
@@ -154,6 +166,7 @@ describe('firebase', function() {
       result = await collection.toList();
       assert.deepEqual(result, [{id: 'id0', value: value1}, {id: 'id1', value: value2}]);
     });
+
     it('resolves concurrent add of same id', async () => {
       const manifest = await Manifest.parse(`
         schema Bar
@@ -170,6 +183,7 @@ describe('firebase', function() {
       await synchronized(collection1, collection2);
       assert.deepEqual(await collection1.toList(), await collection2.toList());
     });
+
     it('resolves concurrent add/remove of same id', async () => {
       const manifest = await Manifest.parse(`
         schema Bar
@@ -189,6 +203,7 @@ describe('firebase', function() {
       assert.isEmpty(await collection1.toList());
       assert.isEmpty(await collection2.toList());
     });
+
     it('resolves concurrent add of different id', async () => {
       const manifest = await Manifest.parse(`
         schema Bar
@@ -245,6 +260,7 @@ describe('firebase', function() {
       const key1 = newStoreKey('colPtr');
   
       const collection1 = await storage.construct('test0', new ReferenceType(barType).collectionOf(), key1) as CollectionStorageProvider;
+      const callbackTracker = new CallbackTracker(collection1, 4);
   
       await collection1.store({id: 'id1', storageKey: 'value1'}, ['key1']);
       await collection1.store({id: 'id2', storageKey: 'value2'}, ['key2']);
@@ -256,7 +272,9 @@ describe('firebase', function() {
 
       assert.isFalse(collection1.referenceMode);
       assert.isNull(collection1.backingStore);
+      callbackTracker.verify();
     }); 
+
     it('supports removeMultiple', async () => {
       const manifest = await Manifest.parse(`
         schema Bar
@@ -267,12 +285,14 @@ describe('firebase', function() {
       const barType = new EntityType(manifest.schemas.Bar);
       const key = newStoreKey('collection');
       const collection = await storage.construct('test1', barType.collectionOf(), key) as CollectionStorageProvider;
+      const collectionCallbacks = new CallbackTracker(collection, 6);
       await collection.store({id: 'id1', value: 'value'}, ['key1']);
       await collection.store({id: 'id2', value: 'value'}, ['key2']);
       await collection.removeMultiple([
         {id: 'id1', keys: ['key1']}, {id: 'id2', keys: ['key2']}
       ]);
       assert.isEmpty(await collection.toList());
+      collectionCallbacks.verify();
     });
   });
 
@@ -650,7 +670,7 @@ describe('firebase', function() {
       const varStore2 = arc2.findStoreById(varStore.id) as VariableStorageProvider;
       const colStore2 = arc2.findStoreById(colStore.id) as CollectionStorageProvider;
       const bigStore2 = arc2.findStoreById(bigStore.id) as BigCollectionStorageProvider; 
-
+      
       // New storage providers should have been created.
       assert.notStrictEqual(varStore2, varStore);
       assert.notStrictEqual(colStore2, colStore);
---
+++ src/runtime/testing/callback-tracker.ts
@@ -0,0 +1,49 @@
+/**
+ * @license
+ * Copyright (c) 2017 Google Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+import {assert} from '../../platform/chai-web.js';
+import {StorageProviderBase} from '../storage/storage-provider-base.js';
+
+/**
+ * Simple class to verify callbacks used in the Arcs storage APIs.
+ *
+ * Usage:
+ * ```
+ *   const varCallbacks = new CallbackTracker(var1, 6);
+ *   .... // do work
+ *   varCallbacks.verify();
+ * ```
+ */
+// TODO(lindner): make this more generic when we have a mocking toolkit available
+export class CallbackTracker {
+  // tslint:disable-next-line: no-any
+  events: {[index: string]: any}[] = [];
+
+  constructor(storageProvider: StorageProviderBase, public expectedEvents = 0) {
+    storageProvider.on('change', (val) => this.changeEvent(val), {});
+  }
+
+  // called for each change event
+  // tslint:disable-next-line: no-any
+  public changeEvent(c: {[index: string]: any}): void {
+    this.events.push(c);
+  }
+
+  /**
+   * Tests that the number of expected callbacks are executed.
+   * If the DEBUG environment variable is set always display accumulated events
+   */
+  public verify() {
+    if (process.env['DEBUG'] === 'true' || this.events.length !== this.expectedEvents) {
+      console.log('Callback events:', JSON.stringify(this.events, null, ' '));
+    }
+    assert.lengthOf(this.events, this.expectedEvents, 'Mismatched number of callbacks');
+  }
+}
