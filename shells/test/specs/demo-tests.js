9cdfca5e86c0da46e1ffa8c00a1360d5af5e1a9c
Paul Lindner <lindner@inuus.com>
2019-02-12 17:08:34 -0800

    Run webdriver tests for pouch and firebase (#2685)

--- shells/test/specs/demo-tests.js
+++ shells/test/specs/demo-tests.js
@@ -18,51 +18,53 @@ const searchFor = text => keys('input[search]', text);
 const receiveEntity = async entity =>
   browser.execute(json => window.ShellApi.receiveEntity(json), JSON.stringify(entity));
 
-describe('pipes', function() {
-  it('searches', async function() {
-    await openNewArc(this.test.fullTitle());
-    // TODO(sjmiles): wait for context to prepare, need a signal instead
-    await browser.pause(seconds(5));
-    await receiveEntity({type: 'search', query: 'restaurants'});
-    //await waitFor(findRestaurants);
+['pouchdb', 'firebase'].forEach(async (storage) => {
+  describe('pipes ' + storage, () => {
+    it('searches', async function() {
+      await openNewArc(this.test.fullTitle(), undefined, storage);
+      // TODO(sjmiles): wait for context to prepare, need a signal instead
+      await browser.pause(seconds(5));
+      await receiveEntity({type: 'search', query: 'restaurants'});
+      //await waitFor(findRestaurants);
+    });
+    it('receives', async function() {
+      await openNewArc(this.test.fullTitle(), undefined, storage);
+      const bodyguardIsOn = `[title^="Bodyguard is on BBC One"]`;
+      // TODO(sjmiles): wait for context to prepare, need a signal instead
+      await browser.pause(seconds(5));
+      await receiveEntity({type: 'tv_show', name: 'bodyguard'});
+      await waitFor(bodyguardIsOn);
+    });
   });
-  it('receives', async function() {
-    await openNewArc(this.test.fullTitle());
-    const bodyguardIsOn = `[title^="Bodyguard is on BBC One"]`;
-    // TODO(sjmiles): wait for context to prepare, need a signal instead
-    await browser.pause(seconds(5));
-    await receiveEntity({type: 'tv_show', name: 'bodyguard'});
-    await waitFor(bodyguardIsOn);
-  });
-});
 
-describe('demo', function() {
-  it('restaurants', async function() {
-    await openNewArc(this.test.fullTitle());
-    const search = `restaurants`;
-    const findRestaurants = `[title^="Find restaurants"]`;
-    const restaurantItem = `#webtest-title`;
-    const reservation = `[title*="ou are "]`;
-    const calendarAction = `[particle-host="Calendar::action"]`;
-    await searchFor(search);
-    await click(findRestaurants);
-    // TODO(sjmiles): rendering tiles takes forever to stabilize
-    //await browser.pause(seconds(10));
-    //await click(restaurantItem);
-    await click(reservation);
-    await waitFor(calendarAction);
-  });
-  it('gifts', async function() {
-    await openNewArc(this.test.fullTitle());
-    const products = `products`;
-    const createList = `[title^="Create shopping list"]`;
-    const buyGifts = `[title^="Buy gifts"]`;
-    const checkManufacturer = `[title^="Check manufacturer"]`;
-    const interests = `[title^="Find out"]`;
-    await searchFor(products);
-    await click(createList);
-    await click(buyGifts);
-    await click(checkManufacturer);
-    await click(interests);
+  describe('demo ' + storage, () => {
+    it('restaurants', async function() {
+      await openNewArc(this.test.fullTitle(), undefined, storage);
+      const search = `restaurants`;
+      const findRestaurants = `[title^="Find restaurants"]`;
+      const restaurantItem = `#webtest-title`;
+      const reservation = `[title*="ou are "]`;
+      const calendarAction = `[particle-host="Calendar::action"]`;
+      await searchFor(search);
+      await click(findRestaurants);
+      // TODO(sjmiles): rendering tiles takes forever to stabilize
+      //await browser.pause(seconds(10));
+      //await click(restaurantItem);
+      await click(reservation);
+      await waitFor(calendarAction);
+    });
+    it('gifts', async function() {
+      await openNewArc(this.test.fullTitle(), undefined, storage);
+      const products = `products`;
+      const createList = `[title^="Create shopping list"]`;
+      const buyGifts = `[title^="Buy gifts"]`;
+      const checkManufacturer = `[title^="Check manufacturer"]`;
+      const interests = `[title^="Find out"]`;
+      await searchFor(products);
+      await click(createList);
+      await click(buyGifts);
+      await click(checkManufacturer);
+      await click(interests);
+    });
   });
 });
