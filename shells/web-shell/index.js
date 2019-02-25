c383aa85588694102f146389420d34475fb66b76
Scott J Miles <sjmiles@google.com>
2019-02-25 12:25:42 -0800

    restore static web-shell decl

--- shells/web-shell/index.js
+++ shells/web-shell/index.js
@@ -1,5 +1,3 @@
-import {Xen} from '../lib/xen.js';
-import {DevtoolsConnection} from '../../build/runtime/debug/devtools-connection.js';
 // optional firebase support
 import '../lib/build/firebase.js';
 import '../../build/runtime/storage/firebase/firebase-provider.js';
@@ -9,20 +7,25 @@ import '../../build/runtime/storage/pouchdb/pouchdb-provider.js';
 // whitelist components
 import '../configuration/whitelisted.js';
 
+import {Xen} from '../lib/xen.js';
 const params = (new URL(document.location)).searchParams;
 const logLevel = params.get('logLevel') || (params.has('log') ? 2 : Xen.Debug.level);
 window.debugLevel = Xen.Debug.level = logLevel;
 
+import {DevtoolsConnection} from '../../build/runtime/debug/devtools-connection.js';
 (async () => {
   if (params.has('remote-explore-key')) {
     // Wait for the remote Arcs Explorer to connect before starting the Shell.
     DevtoolsConnection.ensure();
     await DevtoolsConnection.onceConnected;
   }
-
+  // Shell blocks until root is provided
+  document.querySelector('web-shell').root = '../../';
+  /*
   document.querySelector('body').appendChild(document.createElement('web-shell'));
   // configure root path
   Object.assign(document.querySelector('web-shell'), {
     root: '../..' // path to arcs/
   });
+  */
 })();
