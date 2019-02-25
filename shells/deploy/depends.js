350e3c47c0b87e4e8721ad60b4e33a661768d7f2
Scott J Miles <sjmiles@google.com>
2019-02-25 13:15:54 -0800

    cleanup

--- shells/deploy/depends.js
+++
@@ -1,5 +0,0 @@
-import '../lib/build/pouchdb.js';
-import '../lib/build/firebase.js';
-import './sourcemap.js';
-import '../configuration/whitelisted.js';
-import '../web-shell/elements/web-shell.js';
