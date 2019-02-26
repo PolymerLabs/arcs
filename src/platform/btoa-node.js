6dc2de747f5ccc13d1914439ce619c06c5ed0f33
Scott J Miles <sjmiles@google.com>
2019-02-25 17:27:03 -0800

    address reviewer comments

--- src/platform/btoa-node.js
+++ src/platform/btoa-node.js
@@ -1,9 +1,21 @@
-// Copyright (c) 2017 Google Inc. All rights reserved.
-// This code may only be used under the BSD style license found at
-// http://polymer.github.io/LICENSE.txt
-// Code distributed by Google as part of this project is also
-// subject to an additional IP rights grant found at
-// http://polymer.github.io/PATENTS.txt
+/**
+* @license
+* Copyright (c) 2019 Google Inc. All rights reserved.
+* This code may only be used under the BSD style license found at
+* http://polymer.github.io/LICENSE.txt
+* Code distributed by Google as part of this project is also
+* subject to an additional IP rights grant found at
+* http://polymer.github.io/PATENTS.txt
+*/
 
-import btoa from 'btoa';
+function btoa(str) {
+  let buffer;
+  if (str instanceof Buffer) {
+    buffer = str;
+  } else {
+    buffer = Buffer.from(str.toString(), 'binary');
+  }
+  return buffer.toString('base64');
+}
+
 export {btoa};
