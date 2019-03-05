294197a0e5050ccf78d669f890e64d24da2fcf63
Raul Vera <rvera@google.com>
2019-03-05 17:19:43 +1100

    Define initial stubs for dataflow analysis tool

---
+++ src/dataflow/analysis/flowconfig.ts
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * Copyright (c) 2019 Google Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+/**
+ * A FlowConfig is a set of assertions used to configure a FlowChecker,
+ * along with any assertion-related metadata. The language for specifying
+ * the assertions is parsed in the constructor, or, if we choose to have
+ * a FlowAssertion class, each assertion is parsed in the FlowAssertion
+ * constructor.
+ */
+export class FlowConfig {
+  public assertions: string[]; // parsed assertions usable by FlowChecker
+
+  constructor(input : string[]) { // input is raw set of assertions, e.g. contents of a file
+    this.assertions = input; // Just a dummy without parsing for now.
+  }
+}
