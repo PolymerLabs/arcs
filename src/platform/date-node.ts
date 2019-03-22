bbfe91dfb26a75d56f99ca7ceab003af20312b57
Raul Vera <rvera@google.com>
2019-03-22 14:54:39 +1100

    Migrate all low-hanging fruit in src/platform from js to ts.
    Low-hanging fruit here means that changing just the name works. :-)

--- src/platform/date-node.ts
+++ src/platform/date-node.ts
@@ -5,7 +5,7 @@
 // subject to an additional IP rights grant found at
 // http://polymer.github.io/PATENTS.txt
 
-export function now() {
+export function now() : number {
   const time = process.hrtime();
   return time[0] * 1000 + time[1] / 1000000;
 }
