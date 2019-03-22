5c1b6558341bcc9431d16f11699d4a323b5312dc
Scott J Miles <sjmiles@google.com>
2019-03-22 11:11:37 -0700

    remove particle exclusion affordance

--- src/tests/particles/particles-test.ts
+++ src/tests/particles/particles-test.ts
@@ -3,18 +3,12 @@ import { Manifest } from '../../runtime/manifest.js';
 import glob from 'glob';
 import { Loader } from '../../runtime/loader.js';
 
-// Skip these files, they do not compile successfully.
-const FILES_TO_SKIP = new Set([
-  // this house is clean
-]);
-
 /** Tests that all .schema, .recipe(s) and .manifest files in the particles folder compile successfully. */
 describe('Particle definitions', () => {
   const loader = new Loader();
   const filenames = glob.sync('particles/**/*.{manifest,schema,recipe,recipes}');
 
   filenames
-    .filter(filename => !FILES_TO_SKIP.has(filename))
     .forEach(filename => {
       const contents = fs.readFileSync(filename, 'utf8');
 
