2338a3725e9c79d741e9dcbba12d61242af3e341
Scott J. Miles <sjmiles@google.com>
2019-03-22 11:23:24 -0700

    Fix bad manifests and remove parsing test exclusions (#2785)
    * fix some non-parsing recipes
    
    * more manifest fixups
    
    * remove particle exclusion affordance

--- src/tests/particles/particles-test.ts
+++ src/tests/particles/particles-test.ts
@@ -3,24 +3,12 @@ import { Manifest } from '../../runtime/manifest.js';
 import glob from 'glob';
 import { Loader } from '../../runtime/loader.js';
 
-// Skip these files, they do not compile successfully.
-// TODO: Figure out why.
-const FILES_TO_SKIP = new Set([
-  'particles/Demo/PipeDemo.recipe',
-  'particles/Demo/RestaurantsDemo.recipes',
-  'particles/Music/ArtistPipe.recipes',
-  'particles/TVMaze/TVMazePipe.recipes',
-  'particles/TVMaze/TVMazePipe.recipes',
-  'particles/Words/ShowSingleStats.manifest',
-]);
-
 /** Tests that all .schema, .recipe(s) and .manifest files in the particles folder compile successfully. */
 describe('Particle definitions', () => {
   const loader = new Loader();
   const filenames = glob.sync('particles/**/*.{manifest,schema,recipe,recipes}');
 
   filenames
-    .filter(filename => !FILES_TO_SKIP.has(filename))
     .forEach(filename => {
       const contents = fs.readFileSync(filename, 'utf8');
 
