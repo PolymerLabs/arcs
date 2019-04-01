b1af2b120a452259d595994e9d08a617b8674bcf
Cameron Silvestrini <csilvestrini@google.com>
2019-04-01 16:23:44 +1100

    Expanded comments in .d.ts files

--- src/platform/pouchdb-web.d.ts
+++ src/platform/pouchdb-web.d.ts
@@ -1,4 +1,5 @@
-// Export PouchDB type definitions from Node.
+// Export PouchDB type definitions from Node. This module is not actually used in the web deployment, but it's useful to import the Node
+// definitions to provide type information in an editor environment.
 import PouchDB from 'pouchdb';
 import PouchDbMemory from 'pouchdb-adapter-memory';
 import PouchDbDebug from 'pouchdb-debug';
