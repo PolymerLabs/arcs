a19ff0829708530d4b4a97842d311f67b5b53cfd
Scott J Miles <sjmiles@google.com>
2019-04-01 20:43:01 -0700

    begin making config declarable

--- shells/web-shell/elements/web-config.js
+++ shells/web-shell/elements/web-config.js
@@ -12,11 +12,45 @@ import {Const} from '../../configuration/constants.js';
 
 const log = Xen.logFactory('WebConfig', '#60ac66');
 
+const configOptions = {
+  storage: {
+    aliases: ['storageKey'],
+    default: Const.DEFAULT.storageKey,
+    map: {
+      'firebase': Const.DEFAULT.firebaseStorageKey,
+      'pouchdb': Const.DEFAULT.pouchdbStorageKey,
+      'pouch': Const.DEFAULT.pouchdbStorageKey,
+      'volatile': Const.DEFAULT.volatileStorageKey,
+      'default': Const.DEFAULT.storageKey
+    },
+    localStorageKey: Const.LOCALSTORAGE.storage,
+    //stripFromURL: false,
+    //persistToURL: false
+  },
+  userid: {
+    aliases: ['user'],
+    localStorageKey: Const.LOCALSTORAGE.user
+  },
+  arckey: {
+    aliases: ['arc']
+  },
+  search: {
+  },
+  plannerStorage: {
+    localStorageKey: Const.LOCALSTORAGE.plannerStorage,
+  },
+  plannerNoDebug: {
+  },
+  plannerOnlyConsumer: {
+  }
+};
+
 export class WebConfig extends Xen.Debug(Xen.Async, log) {
   static get observedAttributes() {
     return ['userid', 'arckey'];
   }
-  _update({userid, arckey}, state, oldProps) {
+  update({userid, arckey}, state, oldProps) {
+    this.processConfig(configOptions);
     if (!state.config) {
       const config = this.basicConfig();
       this.updateUserConfig(config);
@@ -34,7 +68,7 @@ export class WebConfig extends Xen.Debug(Xen.Async, log) {
       this.setUrlParam('arc', arckey);
     }
     // TODO(sjmiles): persisting search term is confusing in practice, avoid for now
-    this.setUrlParam('search', null);
+    //this.setUrlParam('search', null);
     // if (search && search !== oldProps.search) {
     //   ArcUtils.setUrlParam('search', search);
     // }
@@ -42,11 +76,6 @@ export class WebConfig extends Xen.Debug(Xen.Async, log) {
   basicConfig() {
     const params = (new URL(document.location)).searchParams;
     return {
-      //modality: 'dom',
-      //root: params.get('root') || window.arcsPath,
-      //manifestPath: params.get('manifest'),
-      //solo: params.get('solo'),
-      //defaultManifest: window.defaultManifest,
       storage: params.get('storage') || localStorage.getItem(Const.LOCALSTORAGE.storage),
       userid: params.get('user') || localStorage.getItem(Const.LOCALSTORAGE.user),
       arckey: params.get('arc'),
@@ -54,31 +83,59 @@ export class WebConfig extends Xen.Debug(Xen.Async, log) {
       plannerStorage: params.get('plannerStorage') || localStorage.getItem(Const.LOCALSTORAGE.plannerStorage),
       plannerDebug: !params.has('plannerNoDebug'),
       plannerOnlyConsumer: params.has('plannerOnlyConsumer'),
-      //urls: window.shellUrls || {},
-      //useStorage: !params.has('legacy') && !params.has('legacy-store'),
-      //useSerialization: !params.has('legacy')
     };
   }
   updateStorageConfig(config) {
     if (config.storage === 'firebase') {
-      config.storage = Const.defaultFirebaseStorageKey;
+      config.storage = Const.DEFAULT.firebaseStorageKey;
     }
     if (config.storage === 'pouchdb') {
-      config.storage = Const.defaultPouchdbStorageKey;
+      config.storage = Const.DEFAULT.pouchdbStorageKey;
+    }
+    if (config.storage === 'volatile') {
+      config.storage = Const.DEFAULT.volatileStorageKey;
     }
     if (!config.storage || config.storage === 'default') {
-      config.storage = Const.defaultStorageKey;
+      config.storage = Const.DEFAULT.storageKey;
     }
     localStorage.setItem(Const.LOCALSTORAGE.storage, config.storage);
     if (!config.plannerStorage || config.plannerStorage === 'default') {
-      config.plannerStorage = Const.defaultPlannerStorageKey;
+      config.plannerStorage = Const.DEFAULT.plannerStorageKey;
     }
     localStorage.setItem(Const.LOCALSTORAGE.plannerStorage, config.plannerStorage);
     return config;
   }
   updateUserConfig(config) {
     if (!config.userid) {
-      config.userid = Const.defaultUserId;
+      config.userid = Const.DEFAULT.userId;
+    }
+  }
+  processConfig(options) {
+    const config = {};
+    const params = (new URL(document.location)).searchParams;
+    Object.keys(options).forEach(key => config[key] = this.processConfigOption(params, key, options[key]));
+    log(config);
+  }
+  processConfigOption(params, name, option) {
+    let names = [name];
+    if (option.aliases) {
+      names = names.concat(option.aliases);
+    }
+    const param = names.find(name => params.has(name));
+    //
+    let value;
+    if (param) {
+      // use user-supplied value if available
+      value = params.get(param);
+    } else {
+      // use local storage value if available
+      if (option.localStorageKey) {
+        value = localStorage.getItem(option.localStorageKey);
+      }
+      // otherwise use default value
+      if (!value) {
+        value = option.default;
+      }
     }
   }
   setUrlParam(name, value) {
