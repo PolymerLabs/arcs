8ae3edebab8d4eae0d97699bfd06e3fd07019195
Scott J Miles <sjmiles@google.com>
2019-04-04 17:38:45 -0700

    make cx-tab-slider work if WebAnimation API isn't supported (Safari, boo)

--- modalities/dom/components/elements/corellia-xen/cx-tab-slider.js
+++ modalities/dom/components/elements/corellia-xen/cx-tab-slider.js
@@ -49,68 +49,68 @@ class CorelliaXenTabSlider extends Xen.Base {
     }
   }
   async _move(fromTab, toTab) {
+    await this._animatedMove(fromTab, toTab);
+    this._fire('tab-slider-done');
+  }
+  async _animatedMove(fromTab, toTab) {
     if (typeof this.animate !== 'function') return;
     if (fromTab === undefined || toTab === undefined) return;
     this.setAttribute('animating', '');
-    if (!this.offsetParent) {
-      this.removeAttribute('animating');
-      return;
+    if (this.offsetParent) {
+      Object.assign(this.style, {
+        top: '0px',
+        right: '0px',
+        bottom: '0px',
+        left: '0px',
+      });
+      const originBox = this.offsetParent.getBoundingClientRect();
+      const fromBox = fromTab.getBoundingClientRect();
+      const toBox = toTab.getBoundingClientRect();
+      const easing = 'ease-in-out';
+      const duration = 50;
+      const fill = 'both';
+      const edgeDelay = 50;
+      const animations = [
+        this.animate([
+            {top: `${fromBox.top - originBox.top}px`},
+            {top: `${toBox.top - originBox.top}px`},
+          ], {
+            delay: (fromBox.top < toBox.top && fromBox.bottom < toBox.bottom) ? edgeDelay : 0,
+            duration,
+            easing,
+            fill,
+          }),
+        this.animate([
+            {right: `${originBox.right - fromBox.right}px`},
+            {right: `${originBox.right - toBox.right}px`},
+          ], {
+            delay: (fromBox.left > toBox.left && fromBox.right > toBox.right) ? edgeDelay : 0,
+            duration,
+            easing,
+            fill,
+          }),
+        this.animate([
+            {bottom: `${originBox.bottom - fromBox.bottom}px`},
+            {bottom: `${originBox.bottom - toBox.bottom}px`},
+          ], {
+            delay: (fromBox.top > toBox.top && fromBox.bottom > toBox.bottom) ? edgeDelay : 0,
+            duration,
+            easing,
+            fill,
+          }),
+        this.animate([
+            {left: `${fromBox.left - originBox.left}px`},
+            {left: `${toBox.left - originBox.left}px`},
+          ], {
+            delay: (fromBox.left < toBox.left && fromBox.right < toBox.right) ? edgeDelay : 0,
+            duration,
+            easing,
+            fill,
+          }),
+      ];
+      await Promise.all(animations.map(a => new Promise(resolve => a.onfinish = resolve)));
     }
-    Object.assign(this.style, {
-      top: '0px',
-      right: '0px',
-      bottom: '0px',
-      left: '0px',
-    });
-    const originBox = this.offsetParent.getBoundingClientRect();
-    const fromBox = fromTab.getBoundingClientRect();
-    const toBox = toTab.getBoundingClientRect();
-    const easing = 'ease-in-out';
-    const duration = 50;
-    const fill = 'both';
-    const edgeDelay = 50;
-    const animations = [
-      this.animate([
-          {top: `${fromBox.top - originBox.top}px`},
-          {top: `${toBox.top - originBox.top}px`},
-        ], {
-          delay: (fromBox.top < toBox.top && fromBox.bottom < toBox.bottom) ? edgeDelay : 0,
-          duration,
-          easing,
-          fill,
-        }),
-      this.animate([
-          {right: `${originBox.right - fromBox.right}px`},
-          {right: `${originBox.right - toBox.right}px`},
-        ], {
-          delay: (fromBox.left > toBox.left && fromBox.right > toBox.right) ? edgeDelay : 0,
-          duration,
-          easing,
-          fill,
-        }),
-      this.animate([
-          {bottom: `${originBox.bottom - fromBox.bottom}px`},
-          {bottom: `${originBox.bottom - toBox.bottom}px`},
-        ], {
-          delay: (fromBox.top > toBox.top && fromBox.bottom > toBox.bottom) ? edgeDelay : 0,
-          duration,
-          easing,
-          fill,
-        }),
-      this.animate([
-          {left: `${fromBox.left - originBox.left}px`},
-          {left: `${toBox.left - originBox.left}px`},
-        ], {
-          delay: (fromBox.left < toBox.left && fromBox.right < toBox.right) ? edgeDelay : 0,
-          duration,
-          easing,
-          fill,
-        }),
-    ];
-    await Promise.all(animations.map(a => new Promise(resolve => a.onfinish = resolve)));
-    //await Promise.all(animations.map(a => a.finished));
     this.removeAttribute('animating');
-    this._fire('tab-slider-done');
   }
 }
 customElements.define('cx-tab-slider', CorelliaXenTabSlider);
