ae52dcd214c53045329444ab55aa52f04bea7cd0
Paul Lindner <plindner@google.com>
2019-04-05 11:08:01 -0700

    Tighten up CRDT Typescript Types

--- src/runtime/test/storage/crdt-collection-model-test.ts
+++ src/runtime/test/storage/crdt-collection-model-test.ts
@@ -6,43 +6,45 @@
 // http://polymer.github.io/PATENTS.txt
 
 import {assert} from '../../../platform/chai-web.js';
-import { CrdtCollectionModel } from '../../storage/crdt-collection-model.js';
+import { CrdtCollectionModel, ModelValue } from '../../storage/crdt-collection-model.js';
 
 describe('crdt-collection-model', () => {
   it('can add values', async () => {
+    const value: ModelValue = {id: 'value'};
     const model = new CrdtCollectionModel();
-    const effective = model.add('id', 'value', ['key1', 'key2']);
+    const effective = model.add('id', value, ['key1', 'key2']);
     assert.isTrue(effective);
     assert.equal(model.size, 1);
-    assert.equal(model.getValue('id'), 'value' as unknown);
+    assert.equal(model.getValue('id'), value);
     assert.sameMembers(model.getKeys('id'), ['key1', 'key2']);
   });
   it('can remove values', async () => {
     const model = new CrdtCollectionModel();
-    model.add('id', 'value', ['key1', 'key2']);
+    model.add('id', {id: 'value'}, ['key1', 'key2']);
     const effective = model.remove('id', ['key1', 'key2']);
     assert.isTrue(effective);
     assert.equal(model.size, 0);
   });
   it('treats add with different keys as idempotent', async () => {
+    const value: ModelValue = {id: 'value'};
     const model = new CrdtCollectionModel();
-    model.add('id', 'value', ['key1']);
-    const effective = model.add('id', 'value', ['key2']);
+    model.add('id', value, ['key1']);
+    const effective = model.add('id', {id: 'value'}, ['key2']);
     assert.isFalse(effective);
     assert.equal(model.size, 1);
-    assert.equal(model.getValue('id'), 'value' as unknown);
+    assert.equal(model.getValue('id'), value);
     assert.sameMembers(model.getKeys('id'), ['key1', 'key2']);
   });
   it('treats remove as idempotent', async () => {
     const model = new CrdtCollectionModel();
-    model.add('id', 'value', ['key1', 'key2']);
+    model.add('id', {id: 'value'}, ['key1', 'key2']);
     model.remove('id', ['key1', 'key2']);
     const effective = model.remove('id', ['key1', 'key2']);
     assert.isFalse(effective);
   });
   it('doesnt treat value as removed until all keys are removed', async () => {
     const model = new CrdtCollectionModel();
-    model.add('id', 'value', ['key1', 'key2']);
+    model.add('id', {id: 'value'}, ['key1', 'key2']);
     let effective = model.remove('id', ['key1']);
     assert.isFalse(effective);
     assert.equal(model.size, 1);
@@ -52,25 +54,27 @@ describe('crdt-collection-model', () => {
     assert.equal(model.size, 0);
   });
   it('allows a value to be updated', async () => {
+    const value2: ModelValue = {id: 'value2'};
+
     const model = new CrdtCollectionModel();
-    model.add('id', 'value', ['key1', 'key2']);
-    const effective = model.add('id', 'value2', ['key3']);
+    model.add('id', {id: 'value'}, ['key1', 'key2']);
+    const effective = model.add('id', value2, ['key3']);
     assert.isTrue(effective);
-    assert.equal(model.getValue('id'), 'value2' as unknown);
+    assert.equal(model.getValue('id'), value2);
   });
   it('does not allow a value to be updated unless new keys are added', async () => {
     const model = new CrdtCollectionModel();
-    model.add('id', 'value', ['key1', 'key2']);
-    assert.throws(() => model.add('id', 'value2', ['key1']), /cannot add without new keys/);
+    model.add('id', {id: 'value'}, ['key1', 'key2']);
+    assert.throws(() => model.add('id', {id: 'value2'}, ['key1']), /cannot add without new keys/);
   });
   it('does not allow a value to be added without keys', async () => {
     const model = new CrdtCollectionModel();
-    assert.throws(() => model.add('id', 'value', []), /add requires a list of keys/);
+    assert.throws(() => model.add('id', {id: 'value'}, []), /add requires a list of keys/);
   });
   it('allows keys to be initialized empty', async () => {
     const model = new CrdtCollectionModel([
-      { id: 'nokeys', 'value': 'value' },
-      { id: 'keys', 'value': 'value', keys: ['key1'] },
+      { id: 'nokeys', value: {id: 'value'}, keys: [] },
+      { id: 'keys', value: {id: 'value'}, keys: ['key1'] },
     ]);
     assert.equal(model.size, 2);
     assert.isEmpty(model.getKeys('nokeys'));
