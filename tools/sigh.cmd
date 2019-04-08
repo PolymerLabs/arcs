3438c972db5c375b808dd1cab588c8559bafe1a5
mykmartin <34528845+mykmartin@users.noreply.github.com>
2019-04-08 14:48:57 +1000

    Convert sigh to TypeScript (#2847)
    * Convert sigh to TypeScript
    
    * Avoid recompiling sigh.ts using a simple timestamp check.
    
    * Temporary logging for debugging travis failure
    
    * Debugging step 2: rename sigh source and output
    
    * Restoring previous behaviour, but will leave a printout when building sigh

--- tools/sigh.cmd
+++ tools/sigh.cmd
@@ -1,1 +1,10 @@
-CALL node %~d0%~sp0\sigh.js %*
+@echo off
+
+for %%f in ("tools/sigh.ts") do set ts_time=%%~tf
+for %%f in ("build/sigh.js") do set js_time=%%~tf
+if %ts_time:~0,10% gtr %js_time:~0,10% do (
+  node_modules\.bin\tsc -p tools
+)
+if %errorlevel% == 0 (
+  call node %~d0%~sp0\..\build\sigh.js %*
+)
