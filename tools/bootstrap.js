7e6d6d578d62d2acbc3e12ee3bbc98acd09bf7b8
mykmartin <34528845+mykmartin@users.noreply.github.com>
2019-04-09 11:38:31 +1000

    sigh: use a cross-platform bootstrap.js pre-compile step (#2868)
    * sigh: use a cross-platform bootstrap.js pre-compile step
    
    * Reduced bootstrap file size

---
+++ tools/bootstrap.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2019 Google Inc. All rights reserved.
+// This code may only be used under the BSD style license found at
+// http://polymer.github.io/LICENSE.txt
+// Code distributed by Google as part of this project is also
+// subject to an additional IP rights grant found at
+// http://polymer.github.io/PATENTS.txt
+
+const fs = require('fs');
+const path = require('path');
+const child_process = require('child_process');
+
+function spawn(cmd, ...args) {
+  const res = child_process.spawnSync(path.normalize(cmd), args, {shell: true,  stdio: 'inherit'});
+  return res.status === 0 && !res.error;
+}
+
+const src = 'tools/sigh.ts';
+const dst = 'build/sigh.js';
+
+if (!fs.existsSync(dst) || fs.statSync(dst).mtimeMs < fs.statSync(src).mtimeMs) {
+  console.log('Building sigh');
+  if (!spawn('node_modules/.bin/tsc', '-p', 'tools')) {
+    process.exit(1);
+  }
+}
+
+process.exit(!spawn('node', 'build/sigh.js', ...process.argv.slice(2)));
--- tools/sigh.cmd
+++ tools/sigh.cmd
@@ -1,10 +1,1 @@
-@echo off
-
-for %%f in ("tools/sigh.ts") do set ts_time=%%~tf
-for %%f in ("build/sigh.js") do set js_time=%%~tf
-if %ts_time:~0,10% gtr %js_time:~0,10% do (
-  node_modules\.bin\tsc -p tools
-)
-if %errorlevel% == 0 (
-  call node %~d0%~sp0\..\build\sigh.js %*
-)
+CALL node %~d0%~sp0\bootstrap.js %*
