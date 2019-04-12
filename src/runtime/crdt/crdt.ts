3c827c75f4a99bba08dbf26a15a4965ab7d59a95
shans <shane.stephens@gmail.com>
2019-04-12 15:17:05 +1000

    Documentation

--- src/runtime/crdt/crdt.ts
+++ src/runtime/crdt/crdt.ts
@@ -17,16 +17,23 @@ export interface CRDTOperation {}
 export interface CRDTData {}
 export interface CRDTConsumerType {}
 
+// A CRDT model is parameterized by:
+//  - the operations that can be applied
+//  - the internal data representation of the model
+//  - the external (particle-facing) data representation of the model
+// These type parameters are wrapped up into a single CRDTTypeRecord interface
+// that can be extended as a unit by CRDT implementation. This allows users
+// of CRDT classes to be generic on a single type parameter. 
+//
+// Note that this interface and its subclasses are intended never to be
+// implemented; instead they are a convenient way of associating the set of types
+// required in a CRDT implementation together.
 export interface CRDTTypeRecord {
   data: CRDTData;
   operation: CRDTOperation;
   consumerType: CRDTConsumerType;
 }
 
-// A CRDT model is parameterized by:
-//  - the operations that can be applied
-//  - the internal data representation of the model
-//  - the external (particle-facing) data representation of the model
 // A CRDT model can:
 //  - merge with other models. This produces a 2-sided delta 
 //    (change from this model to merged model, change from other model to merged model).
@@ -41,6 +48,8 @@ export interface CRDTTypeRecord {
 // This is a serious error and will result in merge throwing a CRDTError.
 export interface CRDTModel<T extends CRDTTypeRecord> {
   merge(other: CRDTModel<T>): {modelChange: CRDTChange<T>, otherChange: CRDTChange<T>};
+  // note that the object-access syntax here & below is in fact a type-level action; op is constrained to 
+  // be of the type of the operation field in T, which extends CRDTTypeRecord.
   applyOperation(op: T['operation']): boolean;
   getData(): T['data'];
   getParticleView(): T['consumerType'];
