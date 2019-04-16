74770365f73cf26c4670e264c88135ff12311810
Scott J Miles <sjmiles@google.com>
2019-04-16 13:39:24 -0700

    fix another path

--- shells/lib/components/ram-slot-composer.js
+++ shells/lib/components/ram-slot-composer.js
@@ -7,8 +7,9 @@
  * subject to an additional IP rights grant found at
  * http://polymer.github.io/PATENTS.txt
  */
-import {ModalityHandler} from '../../build/runtime/modality-handler.js';
-import {SlotComposer} from '../../build/runtime/slot-composer.js';
+
+import {ModalityHandler} from '../../../build/runtime/modality-handler.js';
+import {SlotComposer} from '../../../build/runtime/slot-composer.js';
 
 export class RamSlotComposer extends SlotComposer {
   constructor(options = {}) {
