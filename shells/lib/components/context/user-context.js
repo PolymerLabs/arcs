3cc87ff1d6dd612cc3a34e014e3840ceaf678b7f
Scott J Miles <sjmiles@google.com>
2019-04-16 16:02:30 -0700

    don't block on context if launcher-arc doesn't exist

--- shells/lib/components/context/user-context.js
+++ shells/lib/components/context/user-context.js
@@ -40,8 +40,11 @@ export class UserContext {
       this.observer = ContextObserver(context, store);
       // wait for ready connection
       await this.observer.ready;
-      // resolve context ready
-      this._resolveReady();
+    } else {
+      console.warn('UserContext: no launcher arc, will look again in 5s');
+      setTimeout(() => this.connect(context, storageKey), 5000);
     }
+    // resolve context ready
+    this._resolveReady();
   }
 }
