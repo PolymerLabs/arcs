95e7579b55f87889eed8747c890e7e594f876330
Alex Rosengarten <alxrsngrtn@google.com>
2019-05-01 15:37:14 +1000

    - also added a MlModel schema

---
+++ particles/Processing/particles/source/ImageStyler.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2019 Google Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+'use strict';
+
+defineParticle(({DomParticle, html, log}) => {
+
+  const tmpl = html`
+  <image-styler imgUrl="{{url}}" modelUrl="{{model}}" on-results="{{onResults}}"></image-styler>
+  <div style="padding: 16px">
+    <img src="{{newImg}}">
+  </div>
+  `;
+
+  return class extends DomParticle {
+    get template() {
+      return tmpl;
+    }
+
+    render({image, model}, state) {
+      return {
+        status: state.status || (image ? 'classifying' : 'idle'),
+        url: image ? image.url : '',
+        model: model ? model.url : '',
+        newImg: state.newImg || '',
+      };
+    }
+
+    onResults({data: {value}}) {
+      this.setState({
+        status: 'done',
+        newImg: value.src,
+      });
+    }
+  };
+});
