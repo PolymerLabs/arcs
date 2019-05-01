3f3e0a2289fea2ef85655ae4ff64436249b04ecc
Josh Pratt <jopra@chromium.org>
2019-05-01 16:07:54 +1000

    Possibly working TicTacToe MoveApplier

--- particles/TicTacToe/source/MoveApplier.js
+++ particles/TicTacToe/source/MoveApplier.js
@@ -15,30 +15,58 @@ defineParticle(({Particle}) => {
    setHandles(handles) {
      this.handles = handles;
    }
-   async onHandleUpdate(handle, update) {
+
+   async onHandleSync(handle, model) {
+      this.change(handle);
+   }
+
+   async onHandleUpdate(handle) {
+      this.change(handle);
+   }
+
+  async change(handle) {
      const board = await this.handles.get('board').get();
-     const resultHandle = this.handles.get('result');
+     const boardResultHandle = this.handles.get('boardResult');
+     const messageResultHandle = this.handles.get('messageResult');
 
       const nextMove = await handle.get();
+
+     if (!nextMove) {
+       messageResultHandle.set(new messageResultHandle.entityClass({msg: `Invalid nextMove ${nextMove}`}));
+       return;
+     }
+
+     if (!board) {
+       messageResultHandle.set(new messageResultHandle.entityClass({msg: `Invalid board ${board}`}));
+       return;
+     }
+
      if (handle.name === 'nextMove') {
+       const state = await this.handles.get('state').get();
+       if (!state) {
+         messageResultHandle.set(new messageResultHandle.entityClass({msg: `Invalid state ${state}`}));
+         return;
+       }
+
        // Valid player
-       if (nextMove.player !== 1 || nextMove.player !== 2) {
-         resultHandle.set(new resultHandle.entityClass(
-             `Invalid player ID: ${nextMove.player}`));
+       if (nextMove.player !== 1 && nextMove.player !== 2) {
+         messageResultHandle.set(
+           new messageResultHandle.entityClass(
+             {msg: `Invalid player ID: ${nextMove.player}`}));
          return;
        }
        // Correct player
-       const state = await this.handles.get('state').get();
-       if (nextMove.player !== state.player) {
-         resultHandle.set(new resultHandle.entityClass(`Incorrect player ID: ${
-             nextMove.player} expected ${state.player}`));
+       if ((nextMove.player === 1 && state.state !== 0)
+         || (nextMove.player === 2 && state.state !== 1)) {
+         messageResultHandle.set(new messageResultHandle.entityClass({msg: `Incorrect player ID: ${
+             nextMove.player} with state ${state.state}`}));
          return;
        }
        // Range checks
        if (nextMove.x < 0 || nextMove.x > 2 || nextMove.y < 0 ||
            nextMove.y > 2) {
-         resultHandle.set(new resultHandle.entityClass(
-             `Invalid location: ${nextMove.x}, ${nextMove.y}`));
+         messageResultHandle.set(new messageResultHandle.entityClass(
+           {msg: `Invalid location: ${nextMove.x}, ${nextMove.y}`}));
          return;
        }
        // Empty board location
@@ -47,18 +75,18 @@ defineParticle(({Particle}) => {
          [board.p10, board.p11, board.p12],
          [board.p20, board.p21, board.p22]
        ];
-       if (boardMatrix[nextMove.y][nextMove.x] == 0) {
-         resultHandle.set(new resultHandle.entityClass(
-             `Occupied location: ${nextMove.x}, ${nextMove.y}`));
+       if (boardMatrix[nextMove.y][nextMove.x] !== 0) {
+         messageResultHandle.set(new messageResultHandle.entityClass(
+           {msg: `Occupied location: x:${nextMove.x}, y:${nextMove.y} by:${boardMatrix[nextMove.y][nextMove.x]}`}));
          return;
        }
+       boardMatrix[nextMove.y][nextMove.x] = nextMove.player;
+       boardResultHandle.set(new boardResultHandle.entityClass({
+         p00: boardMatrix[0][0], p01: boardMatrix[1][0], p02: boardMatrix[2][0],
+         p10: boardMatrix[0][1], p11: boardMatrix[1][1], p12: boardMatrix[2][1],
+         p20: boardMatrix[0][2], p21: boardMatrix[1][2], p22: boardMatrix[2][2]
+       }));
      }
-     boardMatrix[nextMove.y][nextMove.x] = state.player;
-     resultHandle.set(new resultHandle.entityClass({
-       p00: boardMatrix[0][0], p01: boardMatrix[1][0], p02: boardMatrix[2][0],
-       p10: boardMatrix[0][1], p11: boardMatrix[1][1], p12: boardMatrix[2][1],
-       p20: boardMatrix[0][2], p21: boardMatrix[1][2], p22: boardMatrix[2][2]
-     }));
    }
  };
 });
