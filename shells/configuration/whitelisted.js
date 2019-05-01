299f5679283728ac096604d941001adfd8f15960
Scott J Miles <sjmiles@google.com>
2019-04-30 18:33:54 -0700

    image processing skeleton

--- shells/configuration/whitelisted.js
+++ shells/configuration/whitelisted.js
@@ -9,6 +9,7 @@ import '../../modalities/dom/components/elements/geo-location.js';
 import '../../modalities/dom/components/elements/model-input.js';
 import '../../modalities/dom/components/elements/model-img.js';
 import '../../modalities/dom/components/elements/dom-repeater.js';
+import '../../modalities/dom/components/elements/processing/image-processor.js';
 
 // requires app-level firebase configuration
 import '../lib/database/firebase-upload.js';
