dd5c660a5ef6db5af7d410744dc2099f4ef097a9
Josh Pratt <jopra@chromium.org>
2019-05-06 13:41:22 +1000

    Ignore directories generated by tests

--- tools/sigh.ts
+++ tools/sigh.ts
@@ -623,7 +623,7 @@ function watch(args: string[]): boolean {
   }
   const command = args.shift() || 'webpack';
   const watcher = chokidar.watch('.', {
-    ignored: new RegExp(`(node_modules|build/|.git|${eslintCache})`),
+    ignored: new RegExp(`(node_modules|build/|.git|user-test/|test-output/|${eslintCache})`),
     persistent: true
   });
   let timeout = null;
