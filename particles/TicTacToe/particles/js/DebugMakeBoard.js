a62d416a707340d020b98cb6d5fb0001a9cc9c3f
Paul Lindner <plindner@google.com>
2019-05-06 11:03:25 -0700

    Fix TicTacToe lint

--- particles/TicTacToe/particles/js/DebugMakeBoard.js
+++ particles/TicTacToe/particles/js/DebugMakeBoard.js
@@ -4,11 +4,11 @@ defineParticle(({Particle, html, log}) => {
 
   return class extends Particle {
     async setHandles(handles) {
-      let boardHandle = handles.get('board');
-      let boardEntity = new boardHandle.entityClass({p00: 0, p01: 1, p02: 2, p10: 1, p11: 2, p12: 0, p20: 2, p21: 0, p22: 1});
+      const boardHandle = handles.get('board');
+      const boardEntity = new boardHandle.entityClass({p00: 0, p01: 1, p02: 2, p10: 1, p11: 2, p12: 0, p20: 2, p21: 0, p22: 1});
       console.log(boardHandle);
       await boardHandle.set(boardEntity);
       console.log('woo');
     }
-  }
+  };
 });
