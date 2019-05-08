f9f1263bf9713c7969a5c70435bc0f0a026f0ec9
Walter Korman <wkorman@chromium.org>
2019-05-08 14:28:04 -0700

    Comment out verbose logging.

--- particles/TicTacToe/particles/js/DebugMakeBoard.js
+++ particles/TicTacToe/particles/js/DebugMakeBoard.js
@@ -6,9 +6,9 @@ defineParticle(({Particle, html, log}) => {
     async setHandles(handles) {
       const boardHandle = handles.get('board');
       const boardEntity = new boardHandle.entityClass({p00: 0, p01: 1, p02: 2, p10: 1, p11: 2, p12: 0, p20: 2, p21: 0, p22: 1});
-      console.log(boardHandle);
+      // console.log(boardHandle);
       await boardHandle.set(boardEntity);
-      console.log('woo');
+      // console.log('woo');
     }
   };
 });
