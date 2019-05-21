7adfbbabe4d926dde58a0c3e2166daeca4d3d871
mykmartin <34528845+mykmartin@users.noreply.github.com>
2019-05-21 17:00:43 +1000

    Add basic rendering and events to the wasm particle (#3042)
    * Add basic rendering to the wasm particle
    
    * Add basic event forwarding to wasm particle

--- src/runtime/wasm.ts
+++ src/runtime/wasm.ts
@@ -133,10 +133,12 @@ export class WasmParticle extends Particle {
   private wasm: WebAssembly.ResultObject;
   // tslint:disable-next-line: no-any
   private exports: any;
-  
+
   private innerParticle: WasmAddress;
   private handleMap = new Map<Handle, WasmAddress>();
   private revHandleMap = new Map<WasmAddress, Handle>();
+  private slotProxies = new Set<string>();
+
   private converters = new Map<Schema, EntityProtoConverter>();
 
   async initialize(buffer: ArrayBuffer) {
@@ -148,7 +150,7 @@ export class WasmParticle extends Particle {
       // Memory setup
       memory: this.memory,
       __memory_base: 1024,
-      table: new WebAssembly.Table({initial: 35, maximum: 35, element: 'anyfunc'}),
+      table: new WebAssembly.Table({initial: 43, maximum: 43, element: 'anyfunc'}),
       __table_base: 0,
       DYNAMICTOP_PTR: 4096,
 
@@ -168,8 +170,8 @@ export class WasmParticle extends Particle {
       _handleSet: async (wasmHandle, num) => setVariable(this.revHandleMap.get(wasmHandle), num),
 
       // Logging functions
-      _console: i => console.log(`<${this.spec.name}> ${this.readString(i)}`),
-      _consoleN: (i, n) => console.log(`<${this.spec.name}> ${this.readString(i)} ${n}`),
+      __console: (line, strp) => console.log(`[${this.spec.name}:${line}] ${this.readString(strp)}`),
+      __consoleN: (line, strp, num) => console.log(`[${this.spec.name}:${line}] ${this.readString(strp)} ${num}`),
     };
     this.wasm = await WebAssembly.instantiate(buffer, {env});
     this.exports = this.wasm.instance.exports;
@@ -201,7 +203,7 @@ export class WasmParticle extends Particle {
       this.converters.set(model.schema, converter);
     }
     const buf = converter.encode(model);
-    
+
     // Encode and send the protobuf... but for now just stuff 'num' in as the first byte to be extracted wasm-side.
     buf[0] = model.num & 0xff;
 
@@ -213,21 +215,39 @@ export class WasmParticle extends Particle {
   // tslint:disable-next-line: no-any
   async onHandleUpdate(handle: Handle, update: {data?: any, oldData?: any, added?: any, removed?: any, originator?: any}) {}
   async onHandleDesync(handle: Handle) {}
-  renderSlot(slotName: string, contentTypes: string[]) {}
+
+  renderSlot(slotName: string, contentTypes: string[]) {
+    const slot = this.slotProxiesByName.get(slotName);
+    if (!slot) return;
+
+    // We allocate space for the name and the particle allocates space for the content; we free them both.
+    const sp = this.storeString(slotName);
+    const cp = this.exports._renderSlot(this.innerParticle, sp);
+    contentTypes.forEach(ct => slot.requestedContentTypes.add(ct));
+    slot.render({template: this.readString(cp), model: {}, templateName: 'default'});
+    this.exports._free(sp);
+    this.exports._free(cp);
+  }
+
   renderHostedSlot(slotName: string, hostedSlotId: string, content: string) {}
-  fireEvent(slotName: string, event: {}) {}
+
+  fireEvent(slotName: string, event) {
+    const sp = this.storeString(slotName);
+    const hp = this.storeString(event.handler);
+    this.exports._fireEvent(this.innerParticle, sp, hp);
+    this.exports._free(sp);
+    this.exports._free(hp);
+  }
 
   // Allocates memory in the wasm container.
-  private storeBuffer(buf: Uint8Array) {
+  private storeBuffer(buf: Uint8Array): WasmAddress {
     const p = this.exports._malloc(buf.length);
-    for (let i = 0; i < buf.length; i++) {
-      this.heap[p + i] = buf[i];
-    }
+    this.heap.set(buf, p);
     return p;
   }
 
   // Allocates memory in the wasm container.
-  private storeString(str: string) {
+  private storeString(str: string): WasmAddress {
     const p = this.exports._malloc(str.length + 1);
     for (let i = 0; i < str.length; i++) {
       this.heap[p + i] = str.charCodeAt(i);
@@ -236,7 +256,8 @@ export class WasmParticle extends Particle {
     return p;
   }
 
-  private readString(idx: number) {
+  // Currently only supports ASCII.
+  private readString(idx: number): string {
     let str = '';
     while (idx < this.heap.length && this.heap[idx] !== 0) {
       str += String.fromCharCode(this.heap[idx++]);
--- src/wasm/working.cc
+++ src/wasm/working.cc
@@ -4,10 +4,12 @@
 #include <string>
 #include <map>
 
-EM_JS(void, console, (const char* msg), {})
-EM_JS(void, consoleN, (const char* msg, int num), {})
+EM_JS(void, _console, (int line, const char* msg), {})
+EM_JS(void, _consoleN, (int line, const char* msg, int num), {})
 
-#define MKSTR(text, args)  (std::string(text) + args).c_str()
+#define console(msg)        _console(__LINE__, msg)
+#define consoleN(msg, num)  _consoleN(__LINE__, msg, num)
+#define mkstr(text, args)   (std::string(text) + args).c_str()
 
 
 class Handle;
@@ -44,14 +46,14 @@ public:
   virtual ~Particle() {}
 
   Handle* newHandle(const char* name) {
-    console(MKSTR("newHandle: ", name));
+    console(mkstr("newHandle: ", name));
     Handle* h = new Handle(name);
     handles_[name] = h;
     return h;
   }
 
   void sync(Handle* handle, void* buffer, int size) {
-    console(MKSTR("sync: ", handle->name()));
+    console(mkstr("sync: ", handle->name()));
     consoleN("  buffer size:", size);
 
     // Will eventually decode protobuf; just extract 'num' for now.
@@ -61,9 +63,20 @@ public:
     onHandleSync(handle);
   }
 
+  void* render(const char* slotName) {
+    console(mkstr("render: ", slotName));
+    std::string content = renderSlot(slotName);
+    unsigned char* buf = (unsigned char*)malloc(content.size() + 1);
+    memcpy(buf, content.c_str(), content.size());
+    buf[content.size()] = 0;
+    return buf;
+  }
+
   // Equivalent to setHandles() in the JS Particle.
   virtual void init() {}
   virtual void onHandleSync(Handle* handle) {}
+  virtual std::string renderSlot(std::string_view slotName) { return ""; }
+  virtual void fireEvent(std::string_view slotName, std::string_view handler) {}
 
   std::map<std::string_view, Handle*> handles_;
 };
@@ -78,12 +91,23 @@ public:
   }
 
   void onHandleSync(Handle* handle) override {
-    console(MKSTR("onHandleSync: ", handle->name()));
+    console(mkstr("onHandleSync: ", handle->name()));
     if (strcmp(handle->name(), "data") == 0) {
       int newValue = handle->get() * 2;
       consoleN("writing res =", newValue);
       handles_["res"]->set(newValue);
     }
+  }
+
+  std::string renderSlot(std::string_view slotName) override {
+    std::string content = "<div on-click=\"me\"><i>Hello from <span on-click=\"THE_WASM\">wasm!</span> This slot is '";
+    content += slotName;
+    content += "'</i></div>";
+    return content;
+  }
+
+  void fireEvent(std::string_view slotName, std::string_view handler) override {
+    console(mkstr("You clicked ", std::string(handler) + "!"));
   }
 };
 
@@ -110,6 +134,19 @@ void initParticle(Particle* particle) {
 EMSCRIPTEN_KEEPALIVE
 void syncHandle(Particle* particle, Handle* handle, void* buffer, int size) {
   particle->sync(handle, buffer, size);
+}
+
+// Does not take ownership of slotName; external caller frees.
+// Caller also takes ownership of returned allocated buffer.
+EMSCRIPTEN_KEEPALIVE
+void* renderSlot(Particle* particle, const char* slotName) {
+  return particle->render(slotName);
+}
+
+// Does not take ownership of slotName or handler; external caller frees.
+EMSCRIPTEN_KEEPALIVE
+void fireEvent(Particle* particle, const char* slotName, const char* handler) {
+  particle->fireEvent(slotName, handler);
 }
 
 }
