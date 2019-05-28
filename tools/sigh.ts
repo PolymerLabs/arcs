1e308286fd6da5a59b11b623d029993076f4c734
Paul Lindner <plindner@google.com>
2019-05-28 14:10:05 -0700

    Add license checks for Arcs
    - Introduce js-green-licenses at version 0.5.0
    - Add a stub for test:licenses in package.json
    - Whitelist WTFPL license usages for now
    
    Addresses #1724

--- tools/sigh.ts
+++ tools/sigh.ts
@@ -80,6 +80,7 @@ const steps: {[index: string]: ((args?: string[]) => boolean)[]} = {
   bundle: [build, bundle],
   schema2proto: [build, schema2proto],
   schema2pkg: [build, schema2pkg],
+  licenses: [build],
   default: [check, peg, railroad, build, runTests, webpack, webpackTools, lint, tslint],
 };
 
@@ -430,6 +431,14 @@ function lint(args: string[]): boolean {
   }
 
   return report.errorCount === 0;
+}
+
+function licenses(): boolean {
+  const result = saneSpawnWithOutput('npm', ['run', 'test:licenses']);
+  if (result.stdout) {
+    console.log(result.stdout);
+  }
+  return result.success;
 }
 
 function webpack(): boolean {
