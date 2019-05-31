2beacd95106b35a29b93f538025f0cf290095681
Scott J Miles <sjmiles@google.com>
2019-05-31 00:28:13 -0700

    Revert 'clean ups'

--- src/platform/pec-industry-web.ts
+++ src/platform/pec-industry-web.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2019 Google Inc. All rights reserved.
+ * Copyright 2019 Google LLC.
  * This code may only be used under the BSD style license found at
  * http://polymer.github.io/LICENSE.txt
  * Code distributed by Google as part of this project is also
@@ -10,24 +10,31 @@
 
 const WORKER_PATH = `https://$build/worker.js`;
 
+let workerUrl;
+let workerBlobUrl;
+
 const pecIndustry = loader => {
   // worker paths are relative to worker location, remap urls from there to here
   const remap = _expandUrls(loader._urlMap);
   // get real path from meta path
   const workerUrl = loader._resolve(WORKER_PATH);
-  // provision (cached) Blob url (async, same workerBlobUrl is captured in both closures)
+  // provision (cached) Blob url (async)
   let workerBlobUrl;
   loader.provisionObjectUrl(workerUrl).then((url: string) => workerBlobUrl = url);
-  // return pecFactory
   return id => {
     if (!workerBlobUrl) {
-      console.warn('workerBlob not available, falling back to network URL');
+      console.warn('wokerBlob not available, falling back to network URL');
     }
     const worker = new Worker(workerBlobUrl || workerUrl);
     const channel = new MessageChannel();
     worker.postMessage({id: `${id}:inner`, base: remap}, [channel.port1]);
     return channel.port2;
   };
+};
+
+const provisionWorkersUrls = loader => {
+  workerUrl = loader._resolve(WORKER_PATH);
+  loader.provisionObjectUrl(workerUrl).then((url: string) => workerBlobUrl = url);
 };
 
 const _expandUrls = urlMap => {
