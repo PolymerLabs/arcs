d97ed389500b7f4bc766be16692013d2130a16c6
Scott J Miles <sjmiles@google.com>
2019-05-31 15:24:01 -0700

    fix license headers

--- shells/pipes-shell/api/push-api.js
+++ shells/pipes-shell/api/push-api.js
@@ -1,11 +1,13 @@
 /**
- * Copyright (c) 2019 Google Inc. All rights reserved.
+ * @license
+ * Copyright 2019 Google LLC.
  * This code may only be used under the BSD style license found at
  * http://polymer.github.io/LICENSE.txt
  * Code distributed by Google as part of this project is also
  * subject to an additional IP rights grant found at
  * http://polymer.github.io/PATENTS.txt
  */
+
 
 import {generateId} from '../../../modalities/dom/components/generate-id.js';
 import {logFactory} from '../../../build/platform/log-web.js';
--- shells/pipes-shell/dispatcher.js
+++ shells/pipes-shell/dispatcher.js
@@ -1,11 +1,13 @@
 /**
- * Copyright (c) 2019 Google Inc. All rights reserved.
+ * @license
+ * Copyright 2019 Google LLC.
  * This code may only be used under the BSD style license found at
  * http://polymer.github.io/LICENSE.txt
  * Code distributed by Google as part of this project is also
  * subject to an additional IP rights grant found at
  * http://polymer.github.io/PATENTS.txt
  */
+
 export const dispatcher = {
   dispatch(msg, ...args) {
     if (!msg.message) {
