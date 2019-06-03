ed9515ea5d5cff1b6c65521c467161a940c7eca4
shans <shane.stephens@gmail.com>
2019-06-03 10:28:29 +1000

    Short timeout to observe appVeyor output

--- src/runtime/storageNG/tests/store-sequence-test.ts
+++ src/runtime/storageNG/tests/store-sequence-test.ts
@@ -131,7 +131,10 @@ describe('Store Flow', async () => {
   });
 
   // Tests 3 operation updates happening synchronously with 2 model updates from the driver
-  it.skip('applies 3 operations and 2 models simultaneously', async () => {
+  it.only('applies 3 operations and 2 models simultaneously', async function() {    
+
+    this.timeout(20);
+
     const sequenceTest = new SequenceTest<ActiveStore<CRDTCountTypeRecord>>();
     sequenceTest.setTestConstructor(() => {
       DriverFactory.clearRegistrationsForTesting();
