6b5bc4afbca64f44496f20eedfbe1c27d9ea2889
Scott J Miles <sjmiles@google.com>
2019-06-03 14:06:11 -0700

    wip

--- shells/configuration/constants.js
+++ shells/configuration/constants.js
@@ -11,6 +11,7 @@ const version = '0_7_0';
 const firebase = `firebase://arcs-storage.firebaseio.com/AIzaSyBme42moeI-2k8WgXh-6YK_wYyjEXo4Oz8/${version}`;
 const pouchdb = `pouchdb://local/arcs/${version}`;
 const volatile = 'volatile://';
+const prefix = `arcs-${version}`;
 
 export const Const = {
   version,
@@ -25,10 +26,10 @@ export const Const = {
     launcherId: 'arc-launcher'
   },
   LOCALSTORAGE: {
-    user: `${version}-user`,
-    storage: `${version}-storage`,
-    plannerStorage: `${version}-plannerStorage`,
-    userHistory: `${version}-userHistory`
+    user: `${prefix}-user`,
+    storage: `${prefix}-storage`,
+    plannerStorage: `${prefix}-plannerStorage`,
+    userHistory: `${prefix}-userHistory`
   },
   SHARE: {
     private: 1,
