dc8fc1eba38da87f8d8eb67cb489e98dc995247a
Alex Rosengarten <alxrsngrtn@google.com>
2019-06-05 15:06:34 -0700

    breaking up classifying and labeling

---
+++ particles/Processing/particles/js/Classifier.js
@@ -0,0 +1,31 @@
+/**
+ * @license
+ * Copyright (c) 2019 Google Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+defineParticle(({DomParticle, log}) => {
+
+  const handleName = 'logits';
+
+  return class extends DomParticle {
+    willReceiveProps({tensor, model}, state) {
+      if (model && tensor) {
+        this.inference(model, tensor);
+      }
+    }
+
+    async inference(model, tensor) {
+      log('Classifying...');
+      const yHat = await this.service({call: 'graph-model.predict', model, inputs: tensor});
+      log('Classified.');
+
+      await this.clearHandle(handleName);
+      this.appendRawDataToHandle(handleName, {ref: yHat});
+    }
+  };
+});
