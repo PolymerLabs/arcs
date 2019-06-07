55682b4b21d302916ba9915cf4c93bbba27a4ca0
Alex Rosengarten <alxrsngrtn@google.com>
2019-06-07 10:26:33 -0700

    added license to new file

--- src/services/mobilenet.ts
+++ src/services/mobilenet.ts
@@ -1,3 +1,12 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
 import {dynamicScript} from '../platform/dynamic-script-web.js';
 
 const modelUrl = 'https://cdn.jsdelivr.net/npm/@tensorflow-models/mobilenet@1.0.0';
@@ -7,4 +16,4 @@ export const requireMobilenet = async () => {
     await dynamicScript(modelUrl);
   }
   return window['mobilenet'];
-}
+};
