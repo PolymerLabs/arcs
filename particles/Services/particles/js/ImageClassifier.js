40c862940244da0b6292ab81867112809ca67163
Alex Rosengarten <alxrsngrtn@google.com>
2019-06-07 10:37:16 -0700

    using centralized tf services - also: added alt text for img

--- particles/Services/particles/js/ImageClassifier.js
+++ particles/Services/particles/js/ImageClassifier.js
@@ -15,7 +15,7 @@ defineParticle(({DomParticle, log, html, resolver}) => {
   const template_ = html`
 <div>
   <h2>Classification with a generic image classifier</h2>
-  <img style="max-width: 240px;" src="{{imageUrl}}"><br>
+  <img style="max-width: 240px;" src="{{imageUrl}}" alt="Image to classify"><br>
   <div id="progress-bar">{{progress}}</div>
   <div>
     <div>Label: </span><span>{{label}}</div>
@@ -64,8 +64,8 @@ defineParticle(({DomParticle, log, html, resolver}) => {
 
     async loadModel(modelUrl, options) {
       log('Loading Model...');
-      const model = await this.service({call: 'graph-model.load', modelUrl, options});
-      await this.service({call: 'graph-model.warmUp', model});
+      const model = await this.service({call: 'tf.loadGraphModel', modelUrl, options});
+      await this.service({call: 'tf.warmUp', model});
       log('Model Loaded');
       this.setState({model});
 
@@ -80,17 +80,17 @@ defineParticle(({DomParticle, log, html, resolver}) => {
       }
 
       log('Preprocessing...');
-      const imgReference = await this.service({call: 'tf-image.imageToTensor', imageUrl});
-      const normalized = await this.service({call: 'preprocess.normalize', input: imgReference, range: [0, 255]});
-      const interpolated = await this.service({call: 'tf-image.resizeBilinear', images: normalized, size: [224, 224], alignCorners: true});
-      const resized = await this.service({call: 'preprocess.reshape', input: interpolated, newShape: [-1, 224, 224, 3]});
+      const imgReference = await this.service({call: 'tf.imageToTensor', imageUrl});
+      const normalized = await this.service({call: 'tf.normalize', input: imgReference, range: [0, 255]});
+      const interpolated = await this.service({call: 'tf.resizeBilinear', images: normalized, size: [224, 224], alignCorners: true});
+      const resized = await this.service({call: 'tf.reshape', input: interpolated, newShape: [-1, 224, 224, 3]});
 
       log('Classifying Model...');
-      const yHat = await this.service({call: 'graph-model.predict', model: this.state.model, inputs: resized});
+      const yHat = await this.service({call: 'tf.predict', model: this.state.model, inputs: resized});
 
       log('Classified. Interpreting results...');
       await this.getModelLabels();
-      const predictions = await this.service({call: 'postprocess.getTopKClasses', yHat, labels: this.state.labels, topK: 5});
+      const predictions = await this.service({call: 'tf.getTopKClasses', yHat, labels: this.state.labels, topK: 5});
       log(predictions);
 
       this.setState({response: predictions[0]});
