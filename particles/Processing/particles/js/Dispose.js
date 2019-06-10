59e9e10e57f555ac91299d01825fb2ebe51f96d2
Alex Rosengarten <alxrsngrtn@google.com>
2019-06-10 11:49:39 -0700

    added dispose particle

---
+++ particles/Processing/particles/js/Dispose.js
@@ -0,0 +1,27 @@
+/**
+ * @license
+ * Copyright (c) 2019 Google Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+defineParticle(({DomParticle, log}) => {
+
+  return class extends DomParticle {
+    willReceiveProps({resource}) {
+      if (resource) {
+        this.cleanup(resource);
+      }
+    }
+
+    async cleanup(resource) {
+      const ref = typeof resource !== 'number' && resource.ref ? resource.ref : resource;
+      log('Disposing...');
+      await this.service({call: 'tf.dispose', reference: ref});
+      log('Disposed.');
+    }
+  };
+});
