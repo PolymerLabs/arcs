1df0736e5b67411a31bca590ae0007ff245f986f
Maria Mandlis <mmandlis@chromium.org>
2019-06-18 14:41:32 -0700

    address comments: use List instead of []

--- src/javaharness/java/arcs/api/Schema.java
+++ src/javaharness/java/arcs/api/Schema.java
@@ -1,6 +1,7 @@
 package arcs.api;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Schema {
     enum FieldKind {
@@ -23,15 +24,15 @@ public class Schema {
         }
     }
     public static class UnionField extends Field {
-        public final Field types[];
-        public UnionField(Field types[]) {
+        public final List<Field> types;
+        public UnionField(List<Field> types) {
             super(FieldKind.UNION);
             this.types = types;
         }
     }
     public static class TupleField extends Field {
-        public final Field types[];
-        public TupleField(Field types[]) {
+        public final List<Field> types;
+        public TupleField(List<Field> types) {
             super(FieldKind.TUPLE);
             this.types = types;
         }
@@ -43,9 +44,9 @@ public class Schema {
             return new PrimitiveField(json.getString("type"));
         } else {
             PortableJson types = json.getObject("types");
-            Field fields[] = new Field[types.getLength()];
+            List<Field> fields = new ArrayList<Field>(types.getLength());
             for (int i = 0; i < types.getLength(); ++i) {
-                fields[i] = fieldFromJson(types.getObject(i));
+                fields.add(fieldFromJson(types.getObject(i)));
             }
             if (kind.equals("schema-union")) {
                 return new UnionField(fields);
@@ -56,24 +57,24 @@ public class Schema {
         throw new AssertionError("Unsupported schema field kind " + kind);
     }
 
-    public final String names[];
-    public final Field fields[];
-    public Schema(String names[], Field fields[]) {
+    public final List<String> names;
+    public final List<Field> fields;
+    public Schema(List<String> names, List<Field> fields) {
         this.names = names;
         this.fields = fields;
     }
 
     public static Schema fromJson(PortableJson json) {
         PortableJson namesJson = json.getObject("names");
-        String names[] = new String[namesJson.getLength()];
+        List<String> names = new ArrayList<String>(namesJson.getLength());
         for (int i = 0; i < namesJson.getLength(); ++i) {
-            names[i] = namesJson.getString(i);
+            names.add(namesJson.getString(i));
         }
         PortableJson fieldsJson = json.getObject("fields");
-        ArrayList<Field> fields = new ArrayList<Field>();
+        List<Field> fields = new ArrayList<Field>();
         fieldsJson.forEach(fieldName -> {
             fields.add(Schema.fieldFromJson(fieldsJson.getObject(fieldName)));
         });
-        return new Schema(names, fields.toArray(new Field[fields.size()]));
+        return new Schema(names, fields);
     }
 }
