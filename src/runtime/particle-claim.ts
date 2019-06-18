e8aca9e2c41cfbf83ce356e92e570973f715be1f
Cameron Silvestrini <csilvestrini@google.com>
2019-06-18 13:08:58 +1000

    Renames the claim/check AST nodes.
    These names are now a bit shorter, and should help make it easier to
    disambiguate between the AST class names and the "real" classes that
    we'll add shortly.
    
    Also moved the ClaimType enum to its own file, so that it can be shared
    between the AST and real classes (real classes will be added to that
    same file, particle-claim.ts)

---
+++ src/runtime/particle-claim.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+/** The different types of trust claims that particles can make. */
+export enum ClaimType {
+  IsTag = 'is-tag',
+  DerivesFrom = 'derives-from',
+}
