98425ac873f8198b9998a7fe25cafd5118ed28ed
mykmartin <34528845+mykmartin@users.noreply.github.com>
2019-06-24 00:09:16 +0100

    Move wasm build script a bit closer to production usage (#3213)
    * Move wasm build script a bit closer to production usage
    
    * Typo

--- src/wasm/cpp/build.sh
+++ src/wasm/cpp/build.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
 
-set -e
+fail() {
+  echo "$1" >&2
+  exit 1
+}
+
+which em++ >/dev/null || fail "em++ not found; do you need to source emsdk_env.sh?"
+
+# Version check
+read A B C <<<$(em++ --version | head -1 | sed 's/^[^0-9]*\([0-9.]\+\).*/\1/' | tr '.' ' ')
+V=$(( A * 10**8 + B * 10**4 + C ))
+(( V >= 100380034 )) || fail "emscripten must be at least version 1.38.34"
 
 invoke() {
   # export EMCC_DEBUG=1 for debug info
   em++ -s "EXPORTED_FUNCTIONS=['_malloc', '_free']" -s EMIT_EMSCRIPTEN_METADATA \
-       -std=c++17 -O3 test-particle.cc -o $1
+       -std=c++17 -O3 test-particle.cc -o $1 || fail "em++ failed"
 }
 
-if [[ $1 = -g ]]; then
-  # Generate the glue code (useful as a development reference).
-  invoke glue.js
-  rm glue.wasm
-  which js-beautify >/dev/null && js-beautify -r glue.js
-  exit
-fi
-
+echo "Building output.wasm"
 invoke output.wasm
 
-if which wasm2wat >/dev/null; then
-  wasm2wat output.wasm -o output.wat
-  grep "^  (import" output.wat
+# For debugging: generate the .wat file and glue JS code as well as the standard build.
+# It is highly recommended that you 'npm install -g js-beautify' to make the glue code readable;
+# we want to build it with the same optimisations enabled as for the standard build, but that
+# means the resulting JS code is minified and therefore unreadable.
+if [[ $1 = --debug ]]; then
+  WWCMD=$(which wasm2wat)
+  if [[ $WWCMD = "" ]]; then
+    # wabt tools should be part of the emsdk installation
+    WWCMD="$EMSDK"/fastcomp/bin/wasm2wat
+    [[ -x $WWCMD ]] || WWCMD=""
+  fi
+  if [[ $WWCMD != "" ]]; then
+    echo "Generating output.wat"
+    "$WWCMD" output.wasm -o output.wat
+  else
+    echo "wasm2wat command not found; skipping generation of output.wat"
+  fi
+
+  echo "Generating glue.js"
+  invoke glue.js
+  rm glue.wasm
+  { which js-beautify && js-beautify -r glue.js; } >/dev/null
 fi
