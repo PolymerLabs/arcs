b50bdc70b51e4e900204ec20501105b5832d7d7c
Paul Lindner <plindner@google.com>
2019-07-02 11:52:21 -0700

    Move unused Mapper type

--- src/common/base/mapper.ts
+++ src/common/base/mapper.ts
@@ -8,23 +8,5 @@
  * http://polymer.github.io/PATENTS.txt
  */
 
-/**
- * Higher Order Types
- */
-
-// Higher Order Function Types
-
-/** A function type that returns a value of type `T` */
-export type Producer<T> = () => T;
-
-/** A function that takes a value of type `T` as input. */
-export type Consumer<T> = (input: T) => void;
-
-/** A function that just runs; it takes no values and returns nothing. */
-export type Runnable = () => void;
-
 /** A function that maps an input to an output. */
 export type Mapper<I, O> = (input: I) => O;
-
-// Higher Order Data Types
-
