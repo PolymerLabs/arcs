f83ba9df5612a075189317918cf0d91978816dd9
Paul Lindner <plindner@google.com>
2019-07-05 08:40:20 -0700

    migrate hot.js usage

--- src/runtime/crdt/crdt-entity.ts
+++ src/runtime/crdt/crdt-entity.ts
@@ -11,7 +11,7 @@
 import {ChangeType, CRDTChange, CRDTModel, CRDTTypeRecord, VersionMap} from './crdt.js';
 import {Referenceable, CRDTCollectionTypeRecord, CRDTCollection, CollectionOpTypes} from './crdt-collection.js';
 import {CRDTSingletonTypeRecord, CRDTSingleton, SingletonOpTypes} from './crdt-singleton.js';
-import {Dictionary} from '../hot.js';
+import {Dictionary} from '../../common/collect/dictionary.js';
 
 // Identified extends the concept of Referenceable to a Dictionary
 type Identified = Dictionary<Referenceable>;
