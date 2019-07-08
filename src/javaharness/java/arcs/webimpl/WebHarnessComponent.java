a113d2a4174c421e743be67357e11bcace6531ce
Maria Mandlis <mmandlis@chromium.org>
2019-07-08 08:25:42 -0700

    provides @singleton for suggest listeners (#3295)

--- src/javaharness/java/arcs/webimpl/WebHarnessComponent.java
+++ src/javaharness/java/arcs/webimpl/WebHarnessComponent.java
@@ -3,7 +3,9 @@ package arcs.webimpl;
 import arcs.api.ArcsEnvironment;
 import arcs.nativeparticles.NativeParticlesModule;
 import dagger.Component;
+import javax.inject.Singleton;
 
+@Singleton
 @Component(modules = {WebHarnessModule.class, NativeParticlesModule.class})
 public interface WebHarnessComponent {
   ArcsEnvironment getArcsEnvironment();
