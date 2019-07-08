c63dd8c13e1f9636acb7d0c775da16256801064d
Maria Mandlis <mmandlis@chromium.org>
2019-07-08 15:37:40 -0700

    add comment to ShellApi and DeviceClient interfaces

--- src/javaharness/java/arcs/api/DeviceClient.java
+++ src/javaharness/java/arcs/api/DeviceClient.java
@@ -4,5 +4,6 @@ package arcs.api;
  * Exposed to JS so that Javascript can call Java.
  */
 public interface DeviceClient {
+    // Receives a message from Arcs JS shell.
     void receive(String json);
 }
--- src/javaharness/java/arcs/api/ShellApi.java
+++ src/javaharness/java/arcs/api/ShellApi.java
@@ -1,5 +1,9 @@
 package arcs.api;
 
+/**
+ * An API for Java to call Javascript (shell).
+ */
 public interface ShellApi {
+    // Sends a message to Arcs JS shell.
     String receive(String json);
 }
