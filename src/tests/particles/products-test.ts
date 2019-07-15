a9b2eb3adb366c72b4b55ff3958cbbea2a923285
Maria Mandlis <mmandlis@chromium.org>
2019-07-15 12:37:49 -0700

    get rid of test planner helper in products-test (#3133)
    * get rid of test planner helper in products-test
    
    * address comment (long awaited)
    
    * fix lint and merge

--- src/tests/particles/products-test.ts
+++ src/tests/particles/products-test.ts
@@ -9,29 +9,51 @@
  */
 
 import {assert} from '../../platform/chai-web.js';
-import {PlanningTestHelper} from '../../planning/testing/arcs-planning-testing.js';
+import {Arc} from '../../runtime/arc.js';
+import {IdGenerator} from '../../runtime/id.js';
+import {Loader} from '../../runtime/loader.js';
+import {Manifest} from '../../runtime/manifest.js';
+import {Runtime} from '../../runtime/runtime.js';
+import {MockSlotComposer} from '../../runtime/testing/mock-slot-composer.js';
+import {FakeSlotComposer} from '../../runtime/testing/fake-slot-composer.js';
 
 describe('products test', () => {
   const manifestFilename = './src/tests/particles/artifacts/products-test.recipes';
 
-  const verifyFilteredBook = async (handle) => {
-    const list = await handle.toList();
+  const verifyFilteredBook = async (arc) => {
+    const booksHandle = arc.activeRecipe.handleConnections.find(hc => hc.isOutput).handle;
+    const store = arc.findStoreById(booksHandle.id);
+    const list = await store.toList();
     assert.lengthOf(list, 1);
     assert.strictEqual('Harry Potter', list[0].rawData.name);
   };
 
   it('filters', async () => {
-    const helper = await PlanningTestHelper.createAndPlan({manifestFilename});
-
-    await helper.acceptSuggestion({particles: ['ProductFilter']});
-
-    await helper.verifyData('ProductFilter', 'results', verifyFilteredBook);
+    const loader = new Loader();
+    const runtime = new Runtime(loader, FakeSlotComposer, await Manifest.load(manifestFilename, loader));
+    const arc = runtime.newArc('demo', 'volatile://');
+    const recipe = arc.context.recipes.find(r => r.name === 'FilterBooks');
+    assert.isTrue(recipe.normalize() && recipe.isResolved());
+    await arc.instantiate(recipe);
+    await arc.idle;
+    await verifyFilteredBook(arc);
   });
 
   it('filters and displays', async () => {
-    const helper = await PlanningTestHelper.createAndPlan({manifestFilename});
+    const loader = new Loader();
+    const slotComposer = new MockSlotComposer({strict: false});
+    const id = IdGenerator.newSession().newArcId('demo');
+    const arc = new Arc({
+      id,
+      storageKey: `volatile://${id.toString()}`,
+      loader,
+      slotComposer,
+      context: await Manifest.load(manifestFilename, loader)
+    });
+    const recipe = arc.context.recipes.find(r => r.name === 'FilterAndDisplayBooks');
+    assert.isTrue(recipe.normalize() && recipe.isResolved());
 
-    helper.slotComposer
+    slotComposer
         .newExpectations()
           .expectRenderSlot('List', 'root', {contentTypes: ['template']})
           .expectRenderSlot('List', 'root', {contentTypes: ['model'], verify: (content) => {
@@ -42,10 +64,10 @@ describe('products test', () => {
               // TODO: reaching directly into data objects like this is super dodgy and we should
               // fix. It's particularly bad here as there's no guarantee that the backingStore
               // exists - should await ensureBackingStore() before accessing it.
-              const reference = helper.arc._stores[0]._model.getValue(content.model.items.models[0].id);
-              const store = helper.arc._stores[0].backingStore;
-              assert.strictEqual(store.storageKey, reference.storageKey);
-              assert.strictEqual('Harry Potter', store._model.getValue(reference.id).rawData.name);
+              const reference = arc._stores[0]['_model'].getValue(content.model.items.models[0].id);
+              const store = arc._stores[0].backingStore;
+              assert.equal(store.storageKey, reference.storageKey);
+              assert.equal('Harry Potter', store['_model'].getValue(reference.id).rawData.name);
             }
             return verified;
           }})
@@ -55,9 +77,8 @@ describe('products test', () => {
                 && 1 === content.model.items.length
                 && 'Harry Potter' === content.model.items[0].name;
           }});
-
-    await helper.acceptSuggestion({particles: ['ItemMultiplexer', 'List', 'ProductFilter']});
-
-    await helper.verifyData('ProductFilter', 'results', verifyFilteredBook);
+    await arc.instantiate(recipe);
+    await arc.idle;
+    await verifyFilteredBook(arc);
   });
 });
