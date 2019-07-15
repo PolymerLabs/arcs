6f13951d11cd740595d49fce3fdf69e8a1130e46
Paul Lindner <plindner@google.com>
2019-07-15 11:06:08 -0700

    Add Instant and Timeunit classes (#3320)
    Addresses #3319

---
+++ src/common/time/instant.ts
@@ -0,0 +1,147 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+import {TimeUnit} from './timeunit.js';
+
+/**
+ * An Instant represents a specific instance of universal time with a
+ * defined precision.  It is loosely based on the same class in the
+ * TC39 temporal specification, with added resolution/precision data.
+ *
+ * @see https://github.com/tc39/proposal-temporal/blob/master/spec/instant.html
+ *
+ * TODO implement comparable?
+ * TODO implement other utility methods.
+ * TODO make sure Instant works with time arithmetic.
+ */
+export class Instant {
+  private readonly secs: number;
+  private readonly nanos: number;
+  private readonly resolutionValue: TimeUnit;
+
+  private constructor(secs: number, nanos, resolution: TimeUnit) {
+    if (nanos > 1000000 || nanos < 0 || secs < 0) {
+      throw new Error('input values out of range secs=' + secs + ' nanos=' + nanos);
+    }
+    this.secs = secs;
+    this.nanos = nanos;
+    this.resolutionValue = resolution;
+  }
+
+  // TODO epochNanoseconds and epochMicroseconds require bigint
+
+  /**
+   * Returns the rounded number of milliseconds from the Epoch for this Instant..
+   */
+  get epochMilliseconds(): number {
+    return this.secs * 1000 + Math.round(this.nanos / 1000000);
+  }
+
+  /**
+   * Returns the rounded number of seconds from the Epoch for this Instant.
+   */
+  get epochSeconds(): number {
+    return this.nanos > 500000 ? this.secs + 1 : this.secs;
+  }
+
+  /**
+   * Provides the resolution of this Instant based on the inputs
+   */
+  get resolution(): TimeUnit {
+    return this.resolutionValue;
+  }
+
+  /**
+   * Creates a new Instant from the given milliseconds.  The returned
+   * value uses TimeUnit.MILLIS as the resolution.
+   */
+  static fromEpochMilliseconds(epochMillis: number): Instant {
+    // TODO check for negative numbers.
+    const secs = Math.floor(epochMillis/1000);
+    const nanos = (epochMillis % 1000) * 1000;
+    return new Instant(secs, nanos, TimeUnit.MILLIS);
+  }
+
+
+  /**
+   * Creates a new Instant from the given seconds.  The returned
+   * value uses TimeUnit.SECONDS as the resolution.
+   */
+  static fromEpochSeconds(epochSeconds: number): Instant {
+    return new Instant(epochSeconds, 0, TimeUnit.SECONDS);
+  }
+
+  // Provides constants for parsing differnet input lengths
+  // TODO add millis and nanos resolutions
+  private static inputLenParseData: ReadonlyMap<number, {timeunit: TimeUnit, suffix: string}> = new Map(
+    [[4,  {timeunit: TimeUnit.YEARS, suffix: '-01-01'}],
+     [7,  {timeunit: TimeUnit.MONTHS, suffix: '-01'}],
+     [10, {timeunit: TimeUnit.DAYS, suffix: ''}],
+     [13, {timeunit: TimeUnit.HOURS, suffix: ':00:00+00:00'}],
+     [16, {timeunit: TimeUnit.MINUTES, suffix: ':00+00:00'}],
+     [19, {timeunit: TimeUnit.SECONDS, suffix: '+00:00'}]]);
+
+  // String to truncate based on the TimeUnit
+  private static toStringTruncation: ReadonlyMap<TimeUnit, number> = new Map(
+    [[TimeUnit.YEARS, 4],
+     [TimeUnit.MONTHS, 7],
+     [TimeUnit.DAYS, 10],
+     [TimeUnit.HOURS, 13],
+     [TimeUnit.MINUTES, 16],
+     [TimeUnit.SECONDS, 19]]);
+
+
+  /**
+   * Parses a UTC date string in ISO 8601 format and generates an
+   * Instant.  The TimeUnit resolution is derived from the amount of
+   * information presented.  For example if `2018-01-18` is used then
+   * `TimeUnit.DAYS` is the resolution.
+   */
+  static fromString(instantToParse: string): Instant {
+    if (instantToParse.includes('+')) {
+      throw new Error('time zone information not supported, only UTC');
+    }
+
+    if (instantToParse.length === 0) {
+      throw new Error('requires a string to parse');
+    }
+
+    // Determine the TimeUnit based on the length of the string
+    const inputLen = instantToParse.length;
+
+    if (inputLen < 4) {
+      throw new Error('Decades and centuries not supported');
+    }
+
+    // Find procesing parameters based on the input length
+    const result = Instant.inputLenParseData.get(instantToParse.length);
+
+    if (!result) {
+      throw new Error('invalid instant');
+    }
+    const {timeunit, suffix} = result;
+
+    // TODO Parse using a more robust implementation
+    const epochMillis = Date.parse(instantToParse + suffix);
+    const secs = Math.floor(epochMillis/1000);
+    const nanos = (epochMillis % 1000) * 1000;
+    return new Instant(secs, nanos, timeunit);
+  }
+
+  public toString(): string {
+    const val = new Date(this.epochMilliseconds).toISOString();
+
+    // Use resolution to truncate output.
+    const truncate = Instant.toStringTruncation.get(this.resolution);
+    if (!truncate) {
+      throw new Error('unimplemented resolution');
+    }
+    return val.substr(0, truncate);
+  }
+}
---
+++ src/common/time/timeunit.ts
@@ -0,0 +1,68 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+
+
+export class TimeUnit {
+
+  private constructor(
+    /** a value to return for toString() */
+    private readonly name: string,
+
+    /** The seconds portion of the duration */
+    public readonly durationSec: number,
+
+    /** The nanoseconds portion of the duration */
+    public readonly durationNS: number,
+
+    /**
+     * If the timeunit is approximate.  Note that leap seconds are
+     * not used, so this pertains to dates only..
+     */
+    public readonly estimated = false) {
+  }
+
+  /**  Unit that represents the concept of a nanosecond. */
+  static readonly NANOS: TimeUnit = new TimeUnit('NANOS', 0, 1);
+
+  /** Unit that represents the concept of a microsecond. */
+  static readonly MICROS: TimeUnit = new TimeUnit('MICROS', 0, 1000);
+
+  /** Unit that represents the concept of a millisecond. */
+  static readonly MILLIS: TimeUnit = new TimeUnit('MILLIS', 0, 1000000);
+
+  /** Unit that represents the concept of a second. */
+  static readonly SECONDS: TimeUnit = new TimeUnit('SECONDS', 1, 0);
+
+  /** Unit that represents the concept of a minute. */
+  static readonly MINUTES: TimeUnit = new TimeUnit('MINUTES', 60, 0);
+
+  /** Unit that represents the concept of an hour. */
+  static readonly HOURS: TimeUnit = new TimeUnit('HOURS', 3600, 0);
+
+  /** Unit that represents the concept of a week. */
+  static readonly DAYS: TimeUnit = new TimeUnit('DAYS', 86400, 0);
+
+  /** Unit that represents the concept of a week. */
+  static readonly WEEKS: TimeUnit = new TimeUnit('WEEKS', 604800, 0);
+
+  /** Unit that represents the concept of a month, 30 days, approximate. */
+  static readonly MONTHS: TimeUnit = new TimeUnit('MONTHS', 2592000, 0, true);
+
+  /** Unit that represents the concept of a year, 365 days, approximate */
+  static readonly YEARS: TimeUnit = new TimeUnit('YEARS', 31536000, 0, true);
+
+  /** An uppercase string matching the name of the constant. ex MILLIS */
+  public toString() {
+    return this.name;
+  }
+
+  // TODO DECADES, CENTURIES, MILLENIA, ERAS
+}
