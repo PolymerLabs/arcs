53fa9c274988b655b574617151237ff186dcb6be
Scott J. Miles <sjmiles@google.com>
2019-07-16 16:57:45 -0700

    fix screenshot path

--- shells/tests/wdio.conf.js
+++ shells/tests/wdio.conf.js
@@ -114,7 +114,7 @@ exports.config = {
   bail: 0,
   //
   // Saves a screenshot to a given path if a command fails.
-  screenshotPath: `./${root}/test/errorShots/`,
+  screenshotPath: `./${root}/tests/errorShots/`,
   //
   // Set a base URL in order to shorten url command calls. If your `url` parameter starts
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
