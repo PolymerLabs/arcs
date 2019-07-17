97969fe216f3432b69afa20a68ddf779b7ed0995
Cameron Silvestrini <csilvestrini@users.noreply.github.com>
2019-07-17 13:38:52 +1000

    Implemented new dataflow algorithm inspired by Hindley-Milner algo. (#3339)
    * Implemented new dataflow algorithm inspired by Hindley-Milner algo.
    
    Does not yet handle cycles.
    
    All existing unit tests are passing. However, I had to remove the
    "failed for path: P1.output -> P2.input" part of the error messages,
    since it wasn't obvious how to support that with the new algorithm.
    
    * Review comments:

--- src/dataflow/analysis/analysis.ts
+++ src/dataflow/analysis/analysis.ts
@@ -10,67 +10,257 @@
 
 import {FlowGraph} from './flow-graph.js';
 import {assert} from '../../platform/assert-web.js';
-import {BackwardsPath, allInputPaths} from './graph-traversal.js';
-import {Edge, Flow} from './graph-internals.js';
+import {Edge, FlowModifier, FlowSet, FlowModifierSet} from './graph-internals.js';
 
 /** Result from validating an entire graph. */
 export class ValidationResult {
-  failures: string[] = [];
+  failures: Set<string> = new Set();
 
   get isValid() {
-    return this.failures.length === 0;
+    return this.failures.size === 0;
   }
 }
 
 /** Returns true if all checks in the graph pass. */
 export function validateGraph(graph: FlowGraph): ValidationResult {
-  const finalResult = new ValidationResult();
-  for (const edge of graph.edges) {
-    if (edge.check) {
-      const result = validateSingleEdge(edge);
-      result.failures.forEach(f => finalResult.failures.push(f));
-    }
-  }
-  return finalResult;
-}
-
-/** 
- * Validates a single check (on the given edge). We define validation as
- * every path ending at the edge must pass the check on that edge. 
- * Returns true if the check passes.
- */
-function validateSingleEdge(edgeToCheck: Edge): ValidationResult {
-  assert(!!edgeToCheck.check, 'Edge does not have any check conditions.');
-
-  const check = edgeToCheck.check;
-
-  const finalResult = new ValidationResult();
-
-  // Check every input path into the given edge.
-  // NOTE: This is very inefficient. We check every single check condition against every single edge in every single input path.
-  for (const path of allInputPaths(edgeToCheck)) {
-    const flow = computeFlowForPath(path);
-    if (!flow.evaluateCheck(check)) {
-      finalResult.failures.push(`'${check.originalCheck.toManifestString()}' failed for path: ${path.toString()}`);
-    }
-  }
-
-  return finalResult;
+  const solver = new Solver(graph);
+  solver.resolve();
+  return solver.validateAllChecks();
 }
 
 /**
- * Collects all the tags, nodes and edges along the given path, canceling tag claims that are
- * negated by "not" claims for the same tag downstream in the path, and ignoring
- * "not" claims without corresponding positive claims upstream, as these dangling
- * "not" claims are irrelevant for the given path. Note that "derives from" claims
- * only prune paths, and are dealt with during path generation, so they are ignored.
+ * A flow expression for an edge. When fully resolved, it contains a set of
+ * resolved flows into the edge. When unresolved, it contains references to each
+ * parent edge, and a set of modifiers which should be applied to the flow from
+ * that edge.
  */
-function computeFlowForPath(path: BackwardsPath): Flow {
-  const flow = new Flow();
-  for (const edge of path.edgesInForwardDirection()) {
-    if (edge.modifier) {
-      flow.modify(edge.modifier);
+class EdgeExpression {
+  /** The edge we're talking about. */
+  readonly edge: Edge;
+
+  /** Fully resolved flows coming into this edge. */
+  readonly resolvedFlows: FlowSet = new FlowSet();
+
+  /**
+   * Edges upon which this edge depends. Not yet resolved. Maps from a parent
+   * edge to the set of modifiers which should be applied to it.
+   */
+  readonly unresolvedFlows: Map<Edge, FlowModifierSet> = new Map();
+
+  constructor(edge: Edge) {
+    this.edge = edge;
+
+    const modifier = edge.modifier || new FlowModifier();
+    const parentEdges = edge.start.inEdgesFromOutEdge(edge);
+    if (parentEdges.length > 0) {
+      // Indicate that this edge inherits from its parents (and apply 
+      // modifiers).
+      parentEdges.forEach(e => this.inheritFromEdge(e, modifier));
+    } else {
+      this.resolvedFlows.add(modifier.toFlow());
     }
   }
-  return flow;
+
+  get isResolved(): boolean {
+    return this.unresolvedFlows.size === 0;
+  }
+
+  get parents(): Edge[] {
+    return [...this.unresolvedFlows.keys()];
+  }
+
+  /**
+   * Replaces an unresolved parent with the parent's own expression. Any
+   * resolved flows into the parent get copied and modified to become resolved
+   * into the child. Unresolved flows into the parent become unresolved flows
+   * into the child (with the child's modifiers added too).
+   */
+  expandParent(parentExpr: EdgeExpression) {
+    assert(
+      this.unresolvedFlows.has(parentExpr.edge),
+      `Can't substitute parent edge, it's not an unresolved parent.`);
+    assert(!parentExpr.unresolvedFlows.has(this.edge), `Cycles aren't supported (yet).`);  
+
+    // Remove unresolved parent, and replace with inherited unresolved parents.
+    const modifierSet = this.unresolvedFlows.get(parentExpr.edge);
+    this.unresolvedFlows.delete(parentExpr.edge);
+
+    for (const modifier of modifierSet) {
+      // Copy flows from parent (and apply modifiers).
+      const newFlows = parentExpr.resolvedFlows.copyAndModify(modifier);
+      this.resolvedFlows.addAll(newFlows);
+
+      // Copy any unresolved grandparents (and apply modifiers).
+      for (const [edge, parentModifierSet] of parentExpr.unresolvedFlows) {
+        for (const parentModifier of parentModifierSet) {
+          // Combine modifiers, applying parent's modifiers first.
+          const combinedModifier = parentModifier.copyAndModify(modifier);
+          this.inheritFromEdge(edge, combinedModifier);
+        }
+      }
+    }
+  }
+
+  /** Add a new unresolved flow, consisting of the given edge and a modifier for it. */
+  private inheritFromEdge(edge: Edge, modifier: FlowModifier) {
+    if (this.unresolvedFlows.has(edge)) {
+      this.unresolvedFlows.get(edge).add(modifier);
+    } else {
+      this.unresolvedFlows.set(edge, new FlowModifierSet(modifier));
+    }
+  }
+
+  toString() {
+    const result: string[] = [`EdgeExpression(${this.edge.label}) {`];
+
+    for (const flow of this.resolvedFlows) {
+      result.push('  ' + flow.toUniqueString());
+    }
+    for (const [edge, modifierSets] of this.unresolvedFlows) {
+      for (const modifiers of modifierSets) {
+        result.push(`  ${edge.label} + ${modifiers.toString()}`);
+      }
+    }
+    result.push('}');
+    return result.join('\n');
+  }
+}
+
+class Solver {
+  readonly graph: FlowGraph;
+
+  /** Maps from an edge to a "expression" for it. */
+  readonly edgeExpressions: Map<Edge, EdgeExpression> = new Map();
+
+  /** Maps from an edge to the set of edges which depends upon it. */
+  readonly dependentEdges: Map<Edge, Set<Edge>>;
+
+  private _isResolved = false;
+
+  constructor(graph: FlowGraph) {
+    this.graph = graph;
+
+    // Fill dependentEdges map with empty sets.
+    this.dependentEdges = new Map();
+    for (const edge of graph.edges) {
+      this.dependentEdges.set(edge, new Set());
+    }
+  }
+
+  /** Returns true if every edge in the graph has been fully resolved to a FlowSet. */
+  get isResolved() {
+    return this._isResolved;
+  }
+
+  /**
+   * Runs through every check on an edge in the graph, and validates it against
+   * the resolved flows into that edge.
+   */
+  validateAllChecks(): ValidationResult {
+    assert(this._isResolved, 'Graph must be resolved before checks can be validated.');
+
+    const finalResult = new ValidationResult();
+    for (const edge of this.graph.edges) {
+      if (edge.check) {
+        const result = this.validateCheckOnEdge(edge);
+        result.failures.forEach(f => finalResult.failures.add(f));
+      }
+    }
+    return finalResult;
+  }
+
+  validateCheckOnEdge(edge: Edge): ValidationResult {
+    assert(this._isResolved, 'Graph must be resolved before checks can be validated.');
+    assert(edge.check, 'Edge does not have any check conditions.');
+
+    const check = edge.check;
+    const finalResult = new ValidationResult();
+
+    const edgeExpression = this.edgeExpressions.get(edge);
+    const flows = edgeExpression.resolvedFlows;
+
+    for (const flow of flows) {
+      if (!flow.evaluateCheck(check)) {
+        // TODO: Figure out how to report the path that caused the failure.
+        finalResult.failures.add(`'${check.originalCheck.toManifestString()}' failed`);
+      }
+    }
+
+    return finalResult;
+  }
+
+  /**
+   * Fully resolves the graph. All edges will have a fully resolved edge
+   * expression at the end of this function.
+   */
+  resolve() {
+    if (this._isResolved) {
+      return;
+    }
+
+    for (const edge of this.graph.edges) {
+      this.processEdge(edge);
+    }
+
+    // Verify that all edges are fully resolved.
+    const numEdges = this.graph.edges.length;
+    assert(this.edgeExpressions.size === numEdges);
+    assert(this.dependentEdges.size === numEdges);
+    for (const edgeExpression of this.edgeExpressions.values()) {
+      assert(edgeExpression.isResolved, `Unresolved edge expression: ${edgeExpression.toString()}`);
+    }
+
+    this._isResolved = true;
+  }
+
+  /**
+   * Constructs a new EdgeExpression for the given edge, and tries to expand as
+   * many of its unresolved parents as is possible. The edge expression might
+   * still not be fully resolved at the end of this function.
+   */
+  processEdge(edge: Edge) {
+    if (this.edgeExpressions.has(edge)) {
+      // Edge has already been processed.
+      return;
+    }
+
+    const edgeExpression = new EdgeExpression(edge);
+    this.edgeExpressions.set(edge, edgeExpression);
+
+    // Try to expand all of the parents we already know about.
+    for (const parent of edgeExpression.parents) {
+      // Indicate that edge depends on parent.
+      this.dependentEdges.get(parent).add(edge);
+
+      this.tryExpandParent(edgeExpression, parent);
+    }
+  }
+
+  /**
+   * Takes an edge expression with an unresolved parent edge, and tries to
+   * expand out that parent edge using the parent edge's own expression.
+   */
+  tryExpandParent(expression: EdgeExpression, parentEdge: Edge) {
+    if (!expression.unresolvedFlows.has(parentEdge)) {
+      return;
+    }
+    const edge = expression.edge;
+    const parentExpression = this.edgeExpressions.get(parentEdge);
+    if (parentExpression) {
+      this.dependentEdges.get(parentEdge).delete(edge);
+      expression.expandParent(parentExpression);
+
+      // Try expanding further.
+      for (const newParent of parentExpression.parents) {
+        this.dependentEdges.get(newParent).add(edge);
+        this.tryExpandParent(expression, newParent);
+      }
+    }
+
+    // Now go through and expand this edge in the edges which depend on it.
+    for (const dependentEdge of this.dependentEdges.get(edge)) {
+      const dependentEdgeExpression = this.edgeExpressions.get(dependentEdge);
+      this.tryExpandParent(dependentEdgeExpression, edge);
+    }
+  }
 }
