5037496a9f8f6396db513b40ad25a88cd29bf95e
Alex Rosengarten <alxrsngrtn@google.com>
2019-07-26 16:27:55 -0700

    hard coding ...

--- tools/setup.ps1
+++ tools/setup.ps1
@@ -16,7 +16,7 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";"
 Status("2. Install Node")
 Set-Location -Path (Get-Item -Path $PSScriptRoot).Parent.FullName
 Install-NodeVersion
-Set-NodeVersion -Persist User
+Set-NodeVersion 10.15.3 -Persist User
 
 Status("3. Update npm to latest version")
 npm install -g npm@latest
