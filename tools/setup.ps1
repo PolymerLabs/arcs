d9f7e5e383bac927ba8730d874efa3963522c41e
Alex Rosengarten <alxrsngrtn@google.com>
2019-07-26 12:50:09 -0700

    ok draft of powershell setup

---
+++ tools/setup.ps1
@@ -0,0 +1,38 @@
+
+$ROOT = ( get-item $scriptPath ).Directory.parent.parent.FullName
+
+# Stop when error occurs
+$ErrorActionPreference = "Stop"
+
+$ChocoInstalled = $false
+
+function local:Status([String] $Message){
+  Write-Host $Message -ForegroundColor DarkMagenta
+}
+
+
+Status("1. Install nvm")
+if (Get-Command choco.exe -ErrorAction SilentlyContinue) {
+  $ChocoInstalled = $true
+  choco install nvm
+} else {
+  Install-Module -Name nvm -RequiredVersion 2.1.0
+}
+
+Status("1.1 Ensure nvm is in your current process")
+if($ChocoInstalled) {
+  refreshenv
+} else {
+  $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+}
+
+Status("2. Install Node")
+CD $ROOT
+nvm install
+nvm use
+
+Status("3. Install dependencies")
+CD $ROOT
+npm ci
+
+
