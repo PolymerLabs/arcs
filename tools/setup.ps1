80b032772a933462a861a6303700a342e48a7656
Alex Rosengarten <alxrsngrtn@google.com>
2019-07-26 17:17:16 -0700

    better equality check

--- tools/setup.ps1
+++ tools/setup.ps1
@@ -26,7 +26,9 @@ Status("3. Update npm to latest version")
 npm install -g npm@latest
 
 Status("4. Failsafe: manually download node if necessary")
-If(node --version -eq $version) {
+$actual_version = node --version
+
+If($actual_version -eq $version) {
   Status("...skipping, version $version satisfied")
 } else {
   $filename = "node.msi"
