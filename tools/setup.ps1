27946aa2b692d54c12ee86fb064519dc9af936a9
Alex Rosengarten <alxrsngrtn@google.com>
2019-07-26 17:05:06 -0700

    added failsafe, going to test

--- tools/setup.ps1
+++ tools/setup.ps1
@@ -1,6 +1,10 @@
 # Stop when error occurs
 # $ErrorActionPreference = "Stop"
 
+$root = (Get-Item -Path $PSScriptRoot).Parent.FullName
+$version = Get-Content -Path $root\.nvmrc
+$url = "https://nodejs.org/dist/$version/node-$version-x64.msi"
+
 $ChocoInstalled = $false
 
 function local:Status([String] $Message){
@@ -14,13 +18,27 @@ Status("1.1 Ensure nvm is in your current process")
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 
 Status("2. Install Node")
-Set-Location -Path (Get-Item -Path $PSScriptRoot).Parent.FullName
+Set-Location -Path $root
 Install-NodeVersion
-Set-NodeVersion -Version
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+Set-NodeVersion -Version $version
 
 Status("3. Update npm to latest version")
 npm install -g npm@latest
+
+Status("4. Failsafe: manually download node if necessary")
+If(node --version -eq $version) {
+  Status("...skipping, version satisfied")
+} else {
+  $node_msi = $PSScriptRoot\"node.msi"
+  $start_time = Get-Date
+  $wc = New-Object System.Net.WebClient
+  $wc.DownloadFile($url, $node_msi)
+  Write-Output "Node downloaded"
+  Write-Output "Time taken $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+  Start-Process $node_msi -Wait
+  $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+}
 
 Status("4. Install dependencies")
 node --version
