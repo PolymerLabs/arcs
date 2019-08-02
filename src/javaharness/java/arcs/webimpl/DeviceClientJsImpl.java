2ee255bb6cb3e8acda6d202d637bfef25264a657
Maria Mandlis <mmandlis@chromium.org>
2019-08-02 16:29:03 -0700

    fix deviceclientjsimpl and externs.js

--- src/javaharness/java/arcs/webimpl/DeviceClientJsImpl.java
+++ src/javaharness/java/arcs/webimpl/DeviceClientJsImpl.java
@@ -12,12 +12,16 @@ import java.util.Map;
 import static elemental2.dom.DomGlobal.window;
 
 @JsType(namespace = "arcs")
-
 public class DeviceClientJsImpl extends DeviceClientImpl {
     @Inject
     public DeviceClientJsImpl(PortableJsonParser jsonParser,
                               Map<String, ArcsEnvironment.DataListener> inProgress,
                               PECInnerPortFactory portFactory) {
         super(jsonParser, inProgress, portFactory);
+    }
+
+    @Override
+    public void receive(String json) {
+        super.receive(json);
     }
 }
