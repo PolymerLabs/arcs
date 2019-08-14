ad28d4a5bff2232a92075a914c8aa5951dd47f1f
Cameron Silvestrini <csilvestrini@google.com>
2019-08-14 10:29:17 +1000

    Reverted hotreload.sh, install ibazel globally

--- src/javaharness/hotreload.sh
+++ src/javaharness/hotreload.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-# ibazel installed to local node_modules in install_dependencies.sh
-"$(npm bin)/ibazel" run java/arcs:javaharness_dev_server
+ibazel run java/arcs:javaharness_dev_server
+
+
