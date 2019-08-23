2b400204af4e9cb64136409062a70f81fffaa842
Sherry Prawiro <sherrypra@google.com>
2019-08-22 19:59:33 -0700

    Get the magenta visualizer particle working with a simple demo song

--- shells/configuration/whitelisted.js
+++ shells/configuration/whitelisted.js
@@ -12,13 +12,14 @@
 import '../../modalities/dom/components/elements/corellia-xen/cx-input.js';
 import '../../modalities/dom/components/elements/corellia-xen/cx-tabs.js';
 import '../../modalities/dom/components/elements/corellia-xen/cx-button.js';
-import '../../modalities/dom/components/elements/video-controller.js';
-import '../../modalities/dom/components/elements/mic-input.js';
+import '../../modalities/dom/components/elements/dom-repeater.js';
 import '../../modalities/dom/components/elements/good-map.js';
 import '../../modalities/dom/components/elements/geo-location.js';
+import '../../modalities/dom/components/elements/magenta-visualizer.js';
+import '../../modalities/dom/components/elements/mic-input.js';
 import '../../modalities/dom/components/elements/model-input.js';
 import '../../modalities/dom/components/elements/model-img.js';
-import '../../modalities/dom/components/elements/dom-repeater.js';
+import '../../modalities/dom/components/elements/video-controller.js';
 import '../../modalities/dom/components/elements/youtube-viewer.js';
 
 // requires app-level firebase configuration
