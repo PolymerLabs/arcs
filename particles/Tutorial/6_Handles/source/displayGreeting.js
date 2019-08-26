5728f0efe7ddeed770de6a3cd1bd90ff5cdddb59
Sarah Heimlich <heimlich@google.com>
2019-08-26 10:38:46 -0700

    Add tutorial for handles.
    e	new file:   particles/Tutorial/6_Handles/Handles.arcs
    e	new file:   particles/Tutorial/6_Handles/source/displayGreeting.js
    e	new file:   particles/Tutorial/6_Handles/source/getPerson.js
    e	modified:   particles/Tutorial/Tutorial.arcs
    e	modified:   particles/canonical.arcs

---
+++ particles/Tutorial/6_Handles/source/displayGreeting.js
@@ -0,0 +1,40 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+'use strict';
+
+/* global defineParticle */
+
+defineParticle(({DomParticle, html}) => {   
+
+const template = html`Hello, <span>{{name}}</span>!`;
+
+  return class extends DomParticle {
+    get template() {
+      return template;
+    }
+
+    shouldRender(props) {
+      // Here we check that props is not null/undefined, and that it contains the expected inputData. This is what the render function below
+      // needs.
+      return props && props.person;
+    }
+
+    render({person}) {
+      // Check that the person exists, and then return the data.
+      if (person) {
+        return {
+          name: person.name,
+        };
+      }
+    }
+
+  };
+});
