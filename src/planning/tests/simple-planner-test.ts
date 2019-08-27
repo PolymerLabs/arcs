1e4b204b20dbf5f27b6cf2bfa7b051a7f4b910a9
raulverag <42563960+raulverag@users.noreply.github.com>
2019-08-27 11:19:03 +1000

    Consolidate RecipeSelector into SimplePlanner and move to planning (#3493)
    * consolidate RecipeSelector into SimplePlanner and move to planning
    
    * tweaks per review

--- src/planning/tests/simple-planner-test.ts
+++ src/planning/tests/simple-planner-test.ts
@@ -13,9 +13,10 @@ import {checkDefined} from '../../runtime/testing/preconditions.js';
 import {Arc} from '../../runtime/arc.js';
 import {Loader} from '../../runtime/loader.js';
 import {Manifest} from '../../runtime/manifest.js';
-import {Match, RecipeSelector, SimplePlanner} from '../recipe-selector.js';
+import {Match, SimplePlanner} from '../simple-planner.js';
 import {FakeSlotComposer} from '../../runtime/testing/fake-slot-composer.js';
 import {Id, ArcId} from '../../runtime/id.js';
+import {RecipeResolver} from '../../runtime/recipe/recipe-resolver.js';
 
 describe('trigger parsing', () => {
   it('trigger annotations parse with one key-value pair', async () => {
@@ -106,7 +107,9 @@ describe('trigger parsing', () => {
   });
 });
 
-describe('recipe-selector', () => {
+describe('simple planner', () => {
+  const createArc = (manifest) => new Arc({id: ArcId.newForTest('test'), slotComposer: new FakeSlotComposer(), loader: new Loader(), context: manifest});
+
   it('If no triggers are present, the table is empty', async () => {
     const manifest = await Manifest.parse(`
       particle P1
@@ -120,12 +123,33 @@ describe('recipe-selector', () => {
           bar <- h
     `);
     const recipe = checkDefined(manifest.recipes[0]);
-    const rs = new RecipeSelector(manifest.recipes);
-    assert.lengthOf(rs.table, 0);
+    const sp = new SimplePlanner(manifest.recipes);
+    assert.lengthOf(sp.recipesByTrigger, 0);
+  });
+  it('accepts duplicate triggers', async () => {
+    const manifest = await Manifest.parse(`
+      particle P1
+        out Foo {} foo
+      particle P2
+        in Foo {} bar
+      @trigger
+        key1 value1
+      @trigger
+        key1 value1
+      recipe R
+        P1
+          foo -> h
+        P2
+          bar <- h
+    `);
+    const recipe = checkDefined(manifest.recipes[0]);
+    const sp = new SimplePlanner(manifest.recipes);
+    assert.lengthOf(sp.recipesByTrigger, 2);
   });
   it('works with one trigger with one key-value pair and one recipe', async () => {
     const manifest = await Manifest.parse(`
       particle P1
+        consume root
         out Foo {} foo
       particle P2
         in Foo {} bar
@@ -138,41 +162,20 @@ describe('recipe-selector', () => {
           bar <- h
     `);
     const recipe = checkDefined(manifest.recipes[0]);
-    const rs = new RecipeSelector(manifest.recipes);
-    assert.lengthOf(rs.table, 1);
-    assert.deepEqual(rs.table[0].trigger, [['key1', 'value1']]);
-    assert.equal(rs.table[0].recipe, recipe);
-    assert.equal(rs.select([['key1', 'value1']]), recipe);
-    assert.isNull(rs.select([['key2', 'value2']]));
-  });
-  it('Throws on duplicate triggers', async () => {
-    const manifest = await Manifest.parse(`
-      particle P1
-        out Foo {} foo
-      particle P2
-        in Foo {} bar
-      @trigger
-        key1 value1
-      @trigger
-        key1 value1
-      recipe R
-        P1
-          foo -> h
-        P2
-          bar <- h
-    `);
-    const recipe = checkDefined(manifest.recipes[0]);
-    let threw = false;
-    try {
-      const rs = new RecipeSelector(manifest.recipes);
-    } catch (e) {
-      threw = true;
-    }
-    assert.isTrue(threw);
+    const planner = new SimplePlanner(manifest.recipes);
+    assert.lengthOf(planner.recipesByTrigger, 1);
+    assert.deepEqual(planner.recipesByTrigger[0].trigger, [['key1', 'value1']]);
+    assert.equal(planner.recipesByTrigger[0].recipe, recipe);
+    const arc = createArc(manifest);
+    const resolver = new RecipeResolver(arc);
+    const resolved = await resolver.resolve(recipe);
+    assert.deepEqual(await planner.plan(arc, [['key1', 'value1']]), resolved);
+    assert.isNull(await planner.plan(arc, [['key2', 'value2']]));
   });
   it('works with one trigger with multipe key-value pairs and one recipe', async () => {
     const manifest = await Manifest.parse(`
       particle P1
+        consume root
         out Foo {} foo
       particle P2
         in Foo {} bar
@@ -186,18 +189,22 @@ describe('recipe-selector', () => {
           bar <- h
     `);
     const recipe = checkDefined(manifest.recipes[0]);
-    const rs = new RecipeSelector(manifest.recipes);
-    assert.lengthOf(rs.table, 1);
-    assert.deepEqual(rs.table[0].trigger, [['key1', 'value1'], ['key2', 'value2']]);
-    assert.equal(rs.table[0].recipe, recipe);
-    assert.equal(rs.select([['key1', 'value1'], ['key2', 'value2']]), recipe);
-    assert.isNull(rs.select([['key1', 'value1']]));
-    assert.isNull(rs.select([['key2', 'value2']]));
-    assert.isNull(rs.select([['key3', 'value3']]));
+    const planner = new SimplePlanner(manifest.recipes);
+    assert.lengthOf(planner.recipesByTrigger, 1);
+    assert.deepEqual(planner.recipesByTrigger[0].trigger, [['key1', 'value1'], ['key2', 'value2']]);
+    assert.equal(planner.recipesByTrigger[0].recipe, recipe);
+    const arc = createArc(manifest);
+    const resolver = new RecipeResolver(arc);
+    const resolved = await resolver.resolve(recipe);
+    assert.deepEqual(await planner.plan(arc, [['key1', 'value1'], ['key2', 'value2']]), resolved);
+    assert.isNull(await planner.plan(arc, [['key1', 'value1']]));
+    assert.isNull(await planner.plan(arc, [['key2', 'value2']]));
+    assert.isNull(await planner.plan(arc, [['key3', 'value3']]));
   });
   it('works with two triggers for one recipe', async () => {
      const manifest = await Manifest.parse(`
       particle P1
+        consume root
         out Foo {} foo
       particle P2
         in Foo {} bar
@@ -213,19 +220,23 @@ describe('recipe-selector', () => {
           bar <- h
     `);
     const recipe = checkDefined(manifest.recipes[0]);
-    const rs = new RecipeSelector(manifest.recipes);
-    assert.lengthOf(rs.table, 2);
-    assert.deepEqual(rs.table[0].trigger, [['key1', 'value1'], ['key2', 'value2']]);
-    assert.deepEqual(rs.table[1].trigger, [['key3', 'value3']]);
-    assert.equal(rs.select([['key1', 'value1'], ['key2', 'value2']]), recipe);
-    assert.equal(rs.select([['key3', 'value3']]), recipe);
-    assert.equal(rs.select([['key1', 'value1'], ['key3', 'value3']]), recipe);
-    assert.isNull(rs.select([['key1', 'value1']]));
-    assert.isNull(rs.select([['key1', 'value1'], ['key4', 'value4']]));
+    const planner = new SimplePlanner(manifest.recipes);
+    assert.lengthOf(planner.recipesByTrigger, 2);
+    assert.deepEqual(planner.recipesByTrigger[0].trigger, [['key1', 'value1'], ['key2', 'value2']]);
+    assert.deepEqual(planner.recipesByTrigger[1].trigger, [['key3', 'value3']]);
+    const arc = createArc(manifest);
+    const resolver = new RecipeResolver(arc);
+    const resolved = await resolver.resolve(recipe);
+    assert.deepEqual(await planner.plan(arc, [['key1', 'value1'], ['key2', 'value2']]), resolved);
+    assert.deepEqual(await planner.plan(arc, [['key3', 'value3']]), resolved);
+    assert.deepEqual(await planner.plan(arc, [['key1', 'value1'], ['key3', 'value3']]), resolved);
+    assert.isNull(await planner.plan(arc, [['key1', 'value1']]));
+    assert.isNull(await planner.plan(arc, [['key1', 'value1'], ['key4', 'value4']]));
   });
   it('works with two recipes, one with a trigger', async () => {
      const manifest = await Manifest.parse(`
       particle P1
+        consume root
         out Foo {} foo
       particle P2
         in Foo {} bar
@@ -246,14 +257,18 @@ describe('recipe-selector', () => {
     `);
     const recipe = checkDefined(manifest.recipes[1]);
     assert.lengthOf(manifest.recipes, 2);
-    const rs = new RecipeSelector(manifest.recipes);
-    assert.lengthOf(rs.table, 1);
-    assert.equal(rs.table[0].recipe, recipe);    
-    assert.equal(rs.select([['key1', 'value1']]), recipe);
+    const planner = new SimplePlanner(manifest.recipes);
+    assert.lengthOf(planner.recipesByTrigger, 1);
+    assert.equal(planner.recipesByTrigger[0].recipe, recipe);    
+    const arc = createArc(manifest);
+    const resolver = new RecipeResolver(arc);
+    const resolved = await resolver.resolve(recipe);
+    assert.deepEqual(await planner.plan(arc, [['key1', 'value1']]), resolved);
   });
   it('works with two recipes, both with triggers, preserving recipe order', async () => {
      const manifest = await Manifest.parse(`
       particle P1
+        consume root
         out Foo {} foo
       particle P2
         in Foo {} bar
@@ -278,37 +293,16 @@ describe('recipe-selector', () => {
     const recipe1 = checkDefined(manifest.recipes[0]);
     const recipe2 = checkDefined(manifest.recipes[1]);
     assert.lengthOf(manifest.recipes, 2);
-    const rs = new RecipeSelector(manifest.recipes);
-    assert.lengthOf(rs.table, 2);
-    assert.equal(rs.table[0].recipe, recipe1);
-    assert.equal(rs.table[1].recipe, recipe2);
-    assert.equal(rs.select([['key1', 'value1'], ['key2', 'value2']]), recipe1);
-    assert.equal(rs.select([['key3', 'value3']]), recipe2);
-    assert.isNull(rs.select([['key1', 'value1']]));
-  });
-}); 
-
-describe('simple planner', () => {
-  const createArc = (manifest) => new Arc({id: ArcId.newForTest('test'), slotComposer: new FakeSlotComposer(), loader: new Loader(), context: manifest});
-
-  it('works with one trigger with one key-value pair and one recipe', async () => {
-    const manifest = await Manifest.parse(`
-      particle P1
-        out Foo {} foo
-        consume root
-      particle P2
-        in Foo {} bar
-      @trigger
-        key1 value1
-      recipe R
-        P1
-          foo -> h
-        P2
-          bar <- h
-    `);
     const planner = new SimplePlanner(manifest.recipes);
+    assert.lengthOf(planner.recipesByTrigger, 2);
+    assert.equal(planner.recipesByTrigger[0].recipe, recipe1);
+    assert.equal(planner.recipesByTrigger[1].recipe, recipe2);
     const arc = createArc(manifest);
-    const result = await planner.plan(arc, [['key1', 'value1']]);
-    assert.isNotNull(result);
+    const resolver = new RecipeResolver(arc);
+    const resolved1 = await resolver.resolve(recipe1);
+    const resolved2 = await resolver.resolve(recipe2);
+    assert.deepEqual(await planner.plan(arc, [['key1', 'value1'], ['key2', 'value2']]), resolved1);
+    assert.deepEqual(await planner.plan(arc, [['key3', 'value3']]), resolved2);
+    assert.isNull(await planner.plan(arc, [['key1', 'value1']]));
   });
 });
