54567c627fd492c3c0625f825aabcc7491951e86
Ray Cromwell <cromwellian@gmail.com>
2019-09-03 16:38:18 -0700

    Fixes needed for blaze compilation

---
+++ src/platform/log-web.d.ts
@@ -0,0 +1,6 @@
+/**
+ * @fileoverview A description of this module.  What would someone
+ * new to your team want to know about the code in this file?
+ * (DO NOT SUBMIT as is; replace this comment.)
+ */
+export declare function logFactory(...args:any):(...args:any) => void;
