cca9ea8bd87622cbbb357dfe02ca4f0f5b2ca6a5
Sarah Heimlich <heimlich@google.com>
2019-09-10 17:41:57 -0700

    Added board.

---
+++ particles/Tutorial/Javascript/Demo/source/Board.js
@@ -0,0 +1,27 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+'use strict';
+
+/* global defineParticle */
+
+defineParticle(({DomParticle, html}) => {   
+
+const template = html`
+  <div slotid="cellSlot"></div>
+`;
+
+  return class extends DomParticle {
+    get template() {
+      return template;
+    }
+    
+  };
+});
