b322fd73026902316283c4221f4c702c67a0c176
Sarah Heimlich <heimlich@google.com>
2019-09-23 11:01:20 -0700

    Got winner working.

--- particles/Tutorial/Javascript/Demo/source/MoveApplier.js
+++ particles/Tutorial/Javascript/Demo/source/MoveApplier.js
@@ -19,60 +19,69 @@
 
     update({move, gameState}, {lastMove}) {
       if (!gameState) { // Gamestate has not been set yet
-        this.updateSingleton('gameState', {gameOver: false, moves: 0, board: ',,,,,,,,,', attemptedMoves: 0});
+        const gs = {
+          board: ',,,,,,,,,',
+          moves: 0,
+          gameOver: false,
+          winnerAvatar: null,
+          attemptedMoves: 0
+        };
+        this.updateSingleton('gameState', gs);
         this.setState({lastMove: ``});
       } else if (move) {
-        console.log(`move: `, move);
-        const arr = gameState.board.split(`,`);
-        const mv = parseInt(move.move, 10) - 1;
+        // Get the old gameState values to update.
         let newMoves = gameState.moves;
         let newBoard = gameState.board;
         let newAttemptedMoves = gameState.attemptedMoves;
         let newGameOver = gameState.gameOver;
-        let winnerAv = ``;
-        //this.setState({lastMove: move.move});
+        let newWinnerAvatar = gameState.winnerAvatar;
+
+        // Get the board and move in a usable state
+        const arr = gameState.board.split(`,`);
+        const mv = parseInt(move.move, 10) - 1;
+        
+        // If the move is valid
         if (arr[mv] == ``) {
-          console.log(`applying Move`);
+          // Apply move
           arr[mv] = move.playerAvatar;
           newBoard = arr.join();
           newMoves = newMoves + 1;
-          if (this.checkIfWon(arr[0], arr[1], arr[2])) {
+          // Check if the game is over
+          if (this.checkIfWon(arr[0], arr[1], arr[2])) { // top row
             newGameOver = true;
-            winnerAv = arr[0];
-          } else if (this.checkIfWon(arr[3], arr[4], arr[5])) {
+            newWinnerAvatar = arr[0];
+          } else if (this.checkIfWon(arr[3], arr[4], arr[5])) { // middle row
             newGameOver = true;
-            winnerAv = arr[0];
-          } else if (this.checkIfWon(arr[6], arr[7], arr[8])) {
+            newWinnerAvatar = arr[3];
+          } else if (this.checkIfWon(arr[6], arr[7], arr[8])) { // bottom row
             newGameOver = true;
-            winnerAv = arr[0];
-          } else if (this.checkIfWon(arr[0], arr[3], arr[6])) {
+            newWinnerAvatar = arr[6];
+          } else if (this.checkIfWon(arr[0], arr[3], arr[6])) { // left col
             newGameOver = true;
-            winnerAv = arr[0];
-          } else if (this.checkIfWon(arr[1], arr[4], arr[6])) {
+            newWinnerAvatar = arr[0];
+          } else if (this.checkIfWon(arr[1], arr[4], arr[6])) { // middle col
             newGameOver = true;
-            winnerAv = arr[0];
-          } else if (this.checkIfWon(arr[2], arr[5], arr[8])) {
+            newWinnerAvatar = arr[1];
+          } else if (this.checkIfWon(arr[2], arr[5], arr[8])) { // right col
             newGameOver = true;
-            winnerAv = arr[0];
-          } else if (this.checkIfWon(arr[0], arr[4], arr[8])) {
+            newWinnerAvatar = arr[2];
+          } else if (this.checkIfWon(arr[0], arr[4], arr[8])) { // L-R diag
             newGameOver = true;
-            winnerAv = arr[0];
-          } else if (this.checkIfWon(arr[2], arr[4], arr[6])) {
+            newWinnerAvatar = arr[0];
+          } else if (this.checkIfWon(arr[2], arr[4], arr[6])) { // R-: diag
             newGameOver = true;
-            winnerAv = arr[0];
-          } else if (gameState.moves == 8) {
+            newWinnerAvatar = arr[2];
+          } else if (newMoves == 9) { // Tied game
             newGameOver = true;
-            winnerAv = arr[0];
           }
         } else {
           newAttemptedMoves++;
-          console.log(`invalid move, try again`);
         }
         const gs = {
           board: newBoard,
           moves: newMoves,
           gameOver: newGameOver,
-          winnerAvatar: winnerAv,
+          winnerAvatar: newWinnerAvatar,
           attemptedMoves: newAttemptedMoves
         };
         this.updateSingleton('gameState', gs);
