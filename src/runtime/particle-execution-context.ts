2dd89764c4f1d6b138743b4240e9b6c5b2b52af1
Joshua Pratt <jp10010101010000@gmail.com>
2019-09-24 11:46:26 +1000

    Ensure that errors in URL requests produce proper errors in particles (#3607)

--- src/runtime/particle-execution-context.ts
+++ src/runtime/particle-execution-context.ts
@@ -321,7 +321,7 @@ export class ParticleExecutionContext {
     } else {
       const clazz = await this.loader.loadParticleClass(spec);
       if (!clazz) {
-        return Promise.reject(new Error(`Could not load particle ${id} ${spec.name}`));
+        return Promise.reject(new Error(`Unknown error loading particle ${id} ${spec.name}`));
       }
       particle = new clazz();
       particle.setCapabilities(this.capabilities(true));
