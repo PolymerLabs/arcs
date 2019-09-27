b9c0330b3fd17c0254353c608253eceb0f6bd380
Scott J Miles <sjmiles@google.com>
2019-09-27 13:24:00 -0700

    plumb in renderOutput

--- src/runtime/wasm.ts
+++ src/runtime/wasm.ts
@@ -443,6 +443,7 @@ export class WasmContainer {
       _collectionStore: (p, handle, entity) => this.getParticle(p).collectionStore(handle, entity),
       _collectionRemove: (p, handle, entity) => this.getParticle(p).collectionRemove(handle, entity),
       _collectionClear: (p, handle) => this.getParticle(p).collectionClear(handle),
+      _onRenderOutput: (p, template, model) => this.getParticle(p).onRenderOutput(template, model),
       _render: (p, slotName, template, model) => this.getParticle(p).renderImpl(slotName, template, model),
       _serviceRequest: (p, call, args, tag) => this.getParticle(p).serviceRequest(call, args, tag),
       _resolveUrl: (url) => this.resolve(url),
@@ -526,6 +527,13 @@ export class WasmParticle extends Particle {
     }
     this.innerParticle = this.exports[fn]();
     this.container.register(this, this.innerParticle);
+    // TODO(sjmiles): probably too soon; we need to render at least once, but we may have handle
+    // work pending ... consult with Shane
+    setTimeout(() => this.renderOutput(), 100);
+  }
+
+  renderOutput() {
+    this.exports._renderOutput(this.innerParticle);
   }
 
   // TODO: for now we set up Handle objects with onDefineHandle and map them into the
@@ -546,6 +554,8 @@ export class WasmParticle extends Particle {
       this.converters.set(handle, new EntityPackager(handle.entityClass.schema));
     }
     this.exports._init(this.innerParticle);
+    // Setting this.handles since reload function needs to be able to grab all particle's handles
+    this.handles = handles;
   }
 
   async onHandleSync(handle: Handle, model) {
@@ -667,13 +677,30 @@ export class WasmParticle extends Particle {
     return p;
   }
 
+  // TODO(sjmiles): UiBroker changes ... we don't have `capabilities` for some reason, so just
+  // go straight to output
+  output(content) {
+    this.container.apiPort.Output(this, content);
+  }
+
+  // render request call-back from wasm
+  onRenderOutput(templatePtr: WasmAddress, modelPtr: WasmAddress) {
+    const content: Content = {templateName: 'default'};
+    content.template = this.container.read(templatePtr);
+    content.model = new StringDecoder().decodeDictionary(this.container.read(modelPtr));
+    this.output(content);
+  }
+
   // Called by the shell to initiate rendering; the particle will call env._render in response.
   renderSlot(slotName: string, contentTypes: string[]) {
+    /*
     const p = this.container.store(slotName);
-    const sendTemplate = contentTypes.includes('template');
-    const sendModel = contentTypes.includes('model');
-    this.exports._renderSlot(this.innerParticle, p, sendTemplate, sendModel);
+    //const sendTemplate = contentTypes.includes('template');
+    //const sendModel = contentTypes.includes('model');
+    //this.exports._renderSlot(this.innerParticle, p, sendTemplate, sendModel);
+    this.exports._renderSlot(this.innerParticle, p, true, true);
     this.container.free(p);
+    */
   }
 
   // TODO
@@ -685,6 +712,7 @@ export class WasmParticle extends Particle {
   // or directly from the wasm particle itself (e.g. in response to a data update).
   // template is a string provided by the particle. model is an encoded Dictionary.
   renderImpl(slotNamePtr: WasmAddress, templatePtr: WasmAddress, modelPtr: WasmAddress) {
+    /*
     const slot = this.slotProxiesByName.get(this.container.read(slotNamePtr));
     if (slot) {
       const content: Content = {templateName: 'default'};
@@ -698,6 +726,7 @@ export class WasmParticle extends Particle {
       }
       slot.render(content);
     }
+    */
   }
 
   // Wasm particles can request service calls with a Dictionary of arguments and an optional string
