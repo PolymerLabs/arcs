521fdb9dde7ca81c163b438d0d932b3588e7a7c2
Scott J Miles <sjmiles@google.com>
2019-10-01 17:42:48 -0700

    remove dead file

--- shells/dev-shell/ui-composer.js
+++
@@ -1,9 +0,0 @@
-import {SlotObserver} from '../lib/xen-renderer.js';
-import {UiSlotComposer} from '../../build/runtime/ui-slot-composer.js';
-
-export const createUiComposer = domRoot => {
-  const composer = new UiSlotComposer();
-  const observer = new SlotObserver(domRoot);
-  composer.observeSlots(observer);
-  return composer;
-};
