720721137ca2cd6b2b4fd9e5d04dd2fc16054088
Maria Mandlis <mmandlis@chromium.org>
2019-10-07 09:50:52 -0400

    refactor ArcData (in preparation for multi particle support)

--- javaharness/java/arcs/api/ArcData.java
+++ javaharness/java/arcs/api/ArcData.java
@@ -4,11 +4,7 @@ public class ArcData {
   private String arcId;
   private String pecId;
   private String recipe;
-  // Only one of either {particleId, particleName}, or particle can be set.
-  private String particleId;
-  private String particleName;
-  private Particle particle;
-  private String providedSlotId;
+  private ParticleData particleData = new ParticleData();
   private String sessionId;
 
   public String getArcId() {
@@ -24,19 +20,19 @@ public class ArcData {
   }
 
   public String getParticleId() {
-    return particle == null ? particleId : particle.getId();
+    return particleData.getId();
   }
 
   public String getParticleName() {
-    return particle == null ? particleName : particle.getName();
+    return particleData.getName();
   }
 
   public Particle getParticle() {
-    return particle;
+    return particleData.particle;
   }
 
   public String getProvidedSlotId() {
-    return providedSlotId;
+    return particleData.providedSlotId;
   }
 
   public String getSessionId() {
@@ -63,32 +59,22 @@ public class ArcData {
     }
 
     public Builder setParticleId(String particleId) {
-      if (arcData.particle != null) {
-        throw new IllegalArgumentException("Cannot set particle id - particle already set");
-      }
-      arcData.particleId = particleId;
+      arcData.particleData.setId(particleId);
       return this;
     }
 
     public Builder setParticleName(String particleName) {
-      if (arcData.particle != null) {
-        throw new IllegalArgumentException("Cannot set particle name - particle already set");
-      }
-      arcData.particleName = particleName;
+      arcData.particleData.setName(particleName);
       return this;
     }
 
     public Builder setParticle(Particle particle) {
-      if (arcData.particleName != null || arcData.particleId != null) {
-        throw new IllegalArgumentException(
-            "Cannot set particle - particle name and/or id already set");
-      }
-      arcData.particle = particle;
+      arcData.particleData.setParticle(particle);
       return this;
     }
 
     public Builder setProvidedSlotId(String providedSlotId) {
-      arcData.providedSlotId = providedSlotId;
+      arcData.particleData.setProvidedSlotId(providedSlotId);
       return this;
     }
 
@@ -103,21 +89,67 @@ public class ArcData {
       if (arcData.pecId == null) {
         arcData.pecId = idGenerator.newChildId(arcId, "pec").toString();
       }
-      if (arcData.particleName != null) {
-        if (arcData.particleId == null) {
-          arcData.particleId = idGenerator.newChildId(arcId, "particle").toString();
-        }
-      }
-      if (arcData.particle != null) {
-        if (arcData.particle.getId() == null) {
-          arcData.particle.setId(idGenerator.newChildId(arcId, "particle").toString());
-        }
-        arcData.providedSlotId =
-            arcData.particle.providesSlot() ? idGenerator.newChildId(arcId, "slotId").toString() : null;
-      }
+      arcData.particleData.normalize(idGenerator, arcId);
       arcData.sessionId = idGenerator.getSessionId();
-
       return arcData;
+    }
+  }
+
+  static class ParticleData {
+    String name;
+    String id;
+    // Only one of either {particleId, particleName}, or particle can be set.
+    Particle particle;
+    // TODO: should be a name->id map. atm only support providing a single slot.
+    String providedSlotId;
+
+    String getId() {
+      return particle == null ? id : particle.getId();
+    }
+
+    String getName() {
+      return particle == null ? name : particle.getName();
+    }
+    
+    void setId(String id) {
+      if (this.particle != null) {
+        throw new IllegalArgumentException("Cannot set particle id - particle already set");
+      }
+      this.id = id;
+    }
+
+    void setName(String name) {
+      if (this.particle != null) {
+        throw new IllegalArgumentException("Cannot set particle name - particle already set");
+      }
+      this.name = name;
+    }
+
+    void setParticle(Particle particle) {
+      if (name != null || id != null) {
+        throw new IllegalArgumentException(
+            "Cannot set particle - particle name and/or id already set");
+      }
+      this.particle = particle;
+    }
+
+    void setProvidedSlotId(String providedSlotId) {
+      this.providedSlotId = providedSlotId;
+    }
+
+    void normalize(IdGenerator idGenerator, Id arcId) {
+      if (name != null) {
+        if (id == null) {
+          id = idGenerator.newChildId(arcId, "particle").toString();
+        }
+      }
+      if (particle != null) {
+        if (particle.getId() == null) {
+          particle.setId(idGenerator.newChildId(arcId, "particle").toString());
+        }
+        providedSlotId =
+            particle.providesSlot() ? idGenerator.newChildId(arcId, "slotId").toString() : null;
+      }
     }
   }
 }
