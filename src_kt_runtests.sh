52129e7ccf9b07ceec7f84cead93bb445bfb91d2
Jason Feinstein <jwf@google.com>
2019-10-13 19:40:35 -0700

    Maybe travis will work now?

--- src_kt_runtests.sh
+++ src_kt_runtests.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+bazel clean --expunge
 cd src_kt
-bazel test --noshow_progress --noshow_loading_progress --test_output=errors --incompatible_depset_is_not_iterable=false //javatests/...
+bazel clean --expunge
+bazel test \
+  --noshow_progress \
+  --noshow_loading_progress \
+  --test_output=errors \
+  --incompatible_depset_is_not_iterable=false \
+  //javatests/...
+TEST_RESULT=$?
 cd -
+exit $TEST_RESULT
