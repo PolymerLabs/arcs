d3b4ecd1c0ced5ed0bc7409cffc05ba2801be149
Alex Rosengarten <alxrsngrtn@google.com>
2019-10-14 15:01:34 -0700

    first iteration of working test

--- src/tools/tests/schema2base-test.ts
+++ src/tools/tests/schema2base-test.ts
@@ -8,10 +8,13 @@
  * http://polymer.github.io/PATENTS.txt
  */
 
-import assert from '../../platform/chai-web.js';
+import {assert} from '../../platform/chai-web.js';
 import {Schema2Base} from '../schema2base.js';
 import {Schema} from '../../runtime/schema.js';
+import fs from 'fs';
+import {promisify} from 'util';
 
+const writeFile = promisify(fs.writeFile);
 
 class Schema2Mock extends Schema2Base {
   public readonly entityArgs: [string, Schema][] = [];
@@ -34,13 +37,36 @@ class Schema2Mock extends Schema2Base {
 
   outputName(baseName: string): string {
     this.basenameArgs.push(baseName);
-    return 'schema-mock.tmp';
+    return baseName;
   }
 
 }
 
 describe('schema2base', () => {
-  it('', async () => {
+  const inputName = './testInput.arcs';
+  const outputName = './schema-mock.tmp';
+
+  const overwriteFile = (filePath: string) => async (contents: string) => writeFile(filePath, contents);
+
+  const overwriteInput = overwriteFile(inputName);
+  const overwriteOutput = overwriteFile(outputName);
+
+  beforeEach(async () => await overwriteInput(''));
+  afterEach( async () => await overwriteOutput(''));
+
+  it('creates a name for anonymous schemas', async () => {
+    await overwriteInput(`\
+  alias schema * as MySchema
+    Text value`);
+
+    const opts = {'_': [inputName], 'outdir': '.', 'd': '.'};
+    const mock = new Schema2Mock(opts);
+    await mock.call();
+
+    assert.isNotEmpty(mock.entityArgs);
+    const entityArg = mock.entityArgs[0];
+    assert.containsAllKeys(entityArg[1].fields, ['value']);
+    assert.equal(entityArg[0], 'AnonText');
 
   });
 });
