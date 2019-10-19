96595586d4a9d4a7e7f55b7947018e7c8f2d03d4
Piotr Swigon <piotr.swigon@gmail.com>
2019-10-18 17:38:13 -0700

    Make loader configurable to load wasm modules from build directory

--- src/platform/loader-platform.ts
+++ src/platform/loader-platform.ts
@@ -18,24 +18,41 @@ import {UiMultiplexerParticle} from '../runtime/ui-multiplexer-particle.js';
 
 const html = (strings, ...values) => (strings[0] + values.map((v, i) => v + strings[i + 1]).join('')).trim();
 
-export class PlatformLoaderBase extends Loader {
-  _urlMap;
+interface UrlMap {
+  [macro: string]: string | {
+    root: string
+    path?: string
+    buildDir: string
+    buildOutputRegex: RegExp
+  };
+}
 
-  constructor(urlMap) {
+export class PlatformLoaderBase extends Loader {
+  readonly _urlMap: UrlMap;
+
+  constructor(urlMap: UrlMap) {
     super();
-    this._urlMap = urlMap || [];
+    this._urlMap = urlMap || {};
   }
   async loadResource(name: string): Promise<string> {
     const path = this.resolve(name);
     return super.loadResource(path);
   }
-  resolve(path: string) {
-    let url = this._urlMap[path];
-    if (!url && path) {
-      // TODO(sjmiles): inefficient!
-      const macro = Object.keys(this._urlMap).sort((a, b) => b.length - a.length).find(k => path.slice(0, k.length) === k);
-      if (macro) {
-        url = this._urlMap[macro] + path.slice(macro.length);
+  resolve(path: string): string {
+    let url: string = null;
+
+    // TODO(sjmiles): inefficient!
+    const macro = Object.keys(this._urlMap).sort((a, b) => b.length - a.length).find(k => path.slice(0, k.length) === k);
+    if (macro) {
+      const config = this._urlMap[macro];
+
+      if (typeof config === 'string') {
+        url = config + path.slice(macro.length);
+      } else {
+        url = config.root
+            + (path.match(config.buildOutputRegex) ? config.buildDir : '')
+            + (config.path || '')
+            + path.slice(macro.length);
       }
     }
     url = this.normalizeDots(url || path);
@@ -45,16 +62,16 @@ export class PlatformLoaderBase extends Loader {
     if (!path) {
       return undefined;
     }
-    const parts = path.split('/');
+    const resolved = this.resolve(path);
+    const parts = resolved.split('/');
     const suffix = parts.pop();
     const folder = parts.join('/');
-    const resolved = this.resolve(folder);
     if (!suffix.endsWith('.wasm')) {
       const name = suffix.split('.').shift();
-      this._urlMap[name] = resolved;
+      this._urlMap[name] = folder;
     }
-    this._urlMap['$here'] = resolved;
-    this._urlMap['$module'] = resolved;
+    this._urlMap['$here'] = folder;
+    this._urlMap['$module'] = folder;
   }
   unwrapParticle(particleWrapper, log?) {
     return particleWrapper({
