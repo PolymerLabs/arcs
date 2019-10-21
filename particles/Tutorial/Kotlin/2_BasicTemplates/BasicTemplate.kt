dded3a8341421796a0df5bdd43326e894d50c5c4
Sarah Heimlich <heimlich@google.com>
2019-10-21 14:53:54 -0700

    Update Particle.kt so particles do not have to implement onHandleUpdate and onHandleSync. Removed renderSlot now that UIBroker is implemented. Updated naming of TestParticle so the correct file is being executed.

--- particles/Tutorial/Kotlin/2_BasicTemplates/BasicTemplate.kt
+++ particles/Tutorial/Kotlin/2_BasicTemplates/BasicTemplate.kt
@@ -7,35 +7,19 @@ import kotlin.native.internal.ExportForCppRuntime
  */
 class BasicTemplateParticle : Particle() {
 
-  override fun populateModel(slotName: String, model: Map<String, String>): Map<String, String> {
-    return model + mapOf(
-      "name" to "Human"
-    )
-  } 
-
-  override fun onHandleUpdate(handle: Handle) {
-    renderSlot("root")
-  }
-
-  override fun onHandleSync(handle: Handle, allSynced: Boolean) {
-    if(allSynced) {
-      log("All handles synched\n")
-      renderSlot("root")
-    }
-  }
-
-    private fun console(s: String) {
-      log(s)
+    override fun populateModel(slotName: String, model: Map<String, String>): Map<String, String> {
+        return model + mapOf(
+            "name" to "Human"
+        )
     }
 
     override fun getTemplate(slotName: String): String {
         return """<b>Hello, <span>{{name}}</span>!</b>"""
-      }
+    }
 }
 
 @Retain
 @ExportForCppRuntime("_newBasicTemplateParticle")
 fun constructBasicTemplateParticle(): WasmAddress {
-    log("_newBasicTemplateParticle called")
     return BasicTemplateParticle().toWasmAddress()
 }
--- particles/Tutorial/Kotlin/4_RenderSlots/ChildParticle.kt
+++ particles/Tutorial/Kotlin/4_RenderSlots/ChildParticle.kt
@@ -6,31 +6,13 @@ import kotlin.native.internal.ExportForCppRuntime
  * Sample WASM Particle.
  */
 class ChildParticle : Particle() {
-
-  override fun onHandleUpdate(handle: Handle) {
-    renderSlot("mySlot")
-  }
-
-  override fun onHandleSync(handle: Handle, willSync: Boolean) {
-    if(willSync) {
-      log("All handles synched\n")
-      renderSlot("mySlot")
-    }
-  }
-
-    private fun console(s: String) {
-      log(s)
-    }
-
     override fun getTemplate(slotName: String): String {
-      log("In Child Getting Template")
-      return """Child"""
+        return """Child"""
     }
 }
 
 @Retain
 @ExportForCppRuntime("_newChildParticle")
 fun constructChildParticle(): WasmAddress {
-    log("_newChildParticle called")
     return ChildParticle().toWasmAddress()
 }
