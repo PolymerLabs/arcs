b68a4d0c96563e209891838b31f0e9df4e799289
Jason Feinstein <jwf@google.com>
2019-10-21 21:43:33 -0700

    Create the storage package, with the start of things that will be needed.

---
+++ src_kt/java/arcs/storage/StorageKey.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2019 Google LLC.
+ *
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ *
+ * Code distributed by Google as part of this project is also subject to an additional IP rights
+ * grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+package arcs.storage
+
+/** Locator for a specific piece of data within the storage layer. */
+abstract class StorageKey(val protocol: String) {
+  val childKeyForArcInfo: StorageKey
+    get() = childKeyWithComponent("arc-info")
+
+  abstract fun toKeyString(): String
+
+  abstract fun childKeyWithComponent(component: String): StorageKey
+
+  fun childKeyForHandle(handleId: String): StorageKey =
+    childKeyWithComponent("handle/$handleId")
+
+  override fun toString(): String = "$protocol://${toKeyString()}"
+}
