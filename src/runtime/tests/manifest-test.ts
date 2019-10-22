badfdf9ae920cacfc774846317e87566c535b22a
Joshua Pratt <jp10010101010000@gmail.com>
2019-10-23 06:55:51 +1100

    Convert RecipeSlot definitions to unified syntax (#3854)
    * Introduce unified ParticleSlotConnection syntax and improve errors message
    
    * Update tests for new syntax
    
    * Unified RecipeSlot syntax

--- src/runtime/tests/manifest-test.ts
+++ src/runtime/tests/manifest-test.ts
@@ -613,6 +613,56 @@ ${particleStr1}
     verify(manifest);
     verify(await Manifest.parse(manifest.toString()));
   });
+  it('SLANDLES SYNTAX can parse a manifest containing a recipe with slots', Flags.withPostSlandlesSyntax(async () => {
+    const manifest = await Manifest.parse(`
+      schema Thing
+      particle SomeParticle in 'some-particle.js'
+        someParam: in Thing
+        mySlot: consume Slot
+          formFactor big
+          otherSlot: provide Slot
+            handle someParam
+          oneMoreSlot: provide Slot
+            formFactor small
+
+      particle OtherParticle
+        aParam: out Thing
+        mySlot: consume Slot
+        oneMoreSlot: consume Slot
+
+      recipe SomeRecipe
+        ? #someHandle1 as myHandle
+        slot0: slot 'slotIDs:A' #someSlot
+        SomeParticle
+          someParam: in myHandle
+          mySlot: consume slot0
+            otherSlot: provide slot2
+            oneMoreSlot: provide slot1
+        OtherParticle
+          aParam: out myHandle
+          mySlot: consume slot0
+          oneMoreSlot: consume slot1
+    `);
+    const verify = (manifest: Manifest) => {
+      const recipe = manifest.recipes[0];
+      assert(recipe);
+      recipe.normalize();
+
+      assert.lengthOf(recipe.particles, 2);
+      assert.lengthOf(recipe.handles, 1);
+      assert.lengthOf(recipe.handleConnections, 2);
+      assert.lengthOf(recipe.slots, 3);
+      assert.lengthOf(recipe.slotConnections, 3);
+      assert.lengthOf(recipe.particles[0].getSlotConnectionNames(), 2);
+      assert.lengthOf(recipe.particles[1].getSlotConnectionNames(), 1);
+      const mySlot = recipe.particles[1].getSlotConnectionByName('mySlot');
+      assert.isDefined(mySlot.targetSlot);
+      assert.lengthOf(Object.keys(mySlot.providedSlots), 2);
+      assert.strictEqual(mySlot.providedSlots['oneMoreSlot'], recipe.particles[0].getSlotConnectionByName('oneMoreSlot').targetSlot);
+    };
+    verify(manifest);
+    verify(await Manifest.parse(manifest.toString()));
+  }));
   it('SLANDLES can parse a manifest containing a recipe with slots', Flags.withPostSlandlesSyntax(async () => {
     const manifest = await Manifest.parse(`
       schema Thing
@@ -2452,7 +2502,7 @@ resource SomeName
       recipe
         require
           handle as h0
-          slot as s0
+          s0: slot
           P1
             *: out h0
             root: consume
@@ -2789,6 +2839,14 @@ resource SomeName
 
       assert.include(manifest.toString(), '  claim is property1 and is property2');
     });
+
+    it(`SLANDLES SYNTAX doesn't allow mixing 'and' and 'or' operations without nesting`, Flags.withPostSlandlesSyntax(async () => {
+      assertThrowsAsync(async () => await Manifest.parse(`
+        particle A
+          input: in T {}
+          check input is property1 or is property2 and is property3
+      `), `You cannot combine 'and' and 'or' operations in a single check expression.`);
+    }));
 
     it(`doesn't allow mixing 'and' and 'or' operations without nesting`, async () => {
       assertThrowsAsync(async () => await Manifest.parse(`
