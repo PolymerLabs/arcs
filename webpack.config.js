7ccc73700f175f48f2bfa8922b60de9045c03ec8
Raul Vera <rvera@google.com>
2019-10-30 13:42:09 +1100

    update max cycles

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,12 @@
  */
 const path = require('path');
 const webpack = require('webpack');
-const CircularDependencyPlugin = require('circular-dependency-plugin')
+const CircularDependencyPlugin = require('circular-dependency-plugin');
 
 const lib = './shells/lib';
 
 // Decrease MAX_CYCLES every time you eliminate circular dependencies from the codebase.
-const MAX_CYCLES = 11;
+const MAX_CYCLES = 15;
 let numCyclesDetected = 0;
 
 module.exports = {
@@ -50,7 +50,7 @@ module.exports = {
       },
       onDetected({module, paths, compilation}) {
         numCyclesDetected++;
-        compilation.warnings.push(new Error(paths.join(' -> ')))
+        compilation.warnings.push(new Error(paths.join(' -> ')));
       },
       onEnd({compilation}) {
         if (numCyclesDetected > MAX_CYCLES) {
