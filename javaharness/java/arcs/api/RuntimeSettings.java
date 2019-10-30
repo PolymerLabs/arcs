719d3a9a69fe888a23ad443e373e0c9090dbc99b
Piotr Swigon <piotr.swigon@gmail.com>
2019-10-30 13:31:40 -0700

    Make Arcs Service load stuff from the workstation

--- javaharness/java/arcs/api/RuntimeSettings.java
+++ javaharness/java/arcs/api/RuntimeSettings.java
@@ -4,20 +4,20 @@ package arcs.api;
 public interface RuntimeSettings {
   // Used by Javascript-based and/or other types of Arcs runtime(s).
   // Equivalent to the &log=<level> parameter at JS Arcs runtime.
-  default int logLevel() {
-    return 0;
-  }
+  int logLevel();
 
-  // Used mainly by Javascript-based Arcs runtime to establish WebSocket
-  // connection to the host from the device during runtime initialization.
+  // Controls whether JS Arcs Runtime should wait on the connection to the Arcs Explorer tool.
   // Equivalent to the &explore-proxy parameter at JS Arcs runtime.
-  default boolean useDevServerProxy() {
-    return false;
-  }
+  boolean enableArcsExplorer();
+
+  // Controls whether to override loading of the particles and recipes to the Development Server,
+  // which loads them from the developer's workstation.
+  boolean loadAssetsFromWorkstation();
+
+  // Value of the port to be used for connecting to the developer server.
+  int devServerPort();
 
   // Used only by Javascript-based Arcs runtime to specify which shell
   // either on-device or on-host to connect with.
-  default String shellUrl() {
-    return "";
-  }
+  String shellUrl();
 }
