PolymerLabs/arcs

commit e8035626e3cc6777043ac454bf9a33af67d42e0e

Make Arcs Service load stuff from the workstation (#3954)

javaharness/java/arcs/android/AndroidArcsEnvironment.java
@@ -3,29 +3,23 @@ package arcs.android;
 import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.webkit.JavascriptInterface;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
-import arcs.api.Constants;
-import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
-
 import java.util.stream.Collectors;
+
 import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.inject.Singleton;
 
 import arcs.api.ArcsMessageSender;
+import arcs.api.Constants;
 import arcs.api.PecPortManager;
 import arcs.api.PortableJson;
 import arcs.api.PortableJsonParser;
@@ -46,9 +40,6 @@ final class AndroidArcsEnvironment {
 
   private static final Logger logger =
     Logger.getLogger(AndroidArcsEnvironment.class.getName());
-
-  private static final String DYNAMIC_MANIFEST_URL =
-      "file:///android_asset/arcs/dynamic.manifest";
 
   private static final String FIELD_MESSAGE = "message";
   private static final String MESSAGE_READY = "ready";
@@ -110,28 +101,8 @@ final class AndroidArcsEnvironment {
 
     dynamicManifest = manifests
         .stream()
-        .map(s -> String.format("import \'%s\'", s))
+        .map(s -> String.format("import \'https://$assets/%s\'", s))
         .collect(Collectors.joining("\n"));
-
-    webView.setWebViewClient(new WebViewClient() {
-      @Override
-      public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
-        Log.d(TAG, "Intercepting request " + request.getUrl());
-        if (TextUtils.equals(DYNAMIC_MANIFEST_URL, request.getUrl().toString())) {
-          try {
-            Log.d(TAG, "Intercepted dynamic manifest request, offering " + dynamicManifest);
-            return new WebResourceResponse(
-                "text/plain",
-                "utf-8",
-                new ByteArrayInputStream(dynamicManifest.getBytes(StandardCharsets.UTF_8)));
-          } catch (Exception e) {
-            Log.e(TAG, "Failed to return dynamic manifest.", e);
-          }
-        }
-
-        return super.shouldInterceptRequest(view, request);
-      }
-    });
 
     webView.addJavascriptInterface(this, "DeviceClient");
 
@@ -143,8 +114,8 @@ final class AndroidArcsEnvironment {
     //    android:usesCleartextTraffic="true"
     String url = settings.shellUrl();
     url += "log=" + settings.logLevel();
-    if (settings.useDevServerProxy()) {
-      url += "&explore-proxy";
+    if (settings.enableArcsExplorer()) {
+      url += "&explore-proxy=" + settings.devServerPort();
     }
 
     Log.i("Arcs", "runtime url: " + url);
@@ -216,16 +187,24 @@ final class AndroidArcsEnvironment {
   }
 
   private void configureShell() {
-    PortableJson msg =
-        jsonParser
-            .emptyObject()
-            .put(Constants.MESSAGE_FIELD, Constants.CONFIGURE_MESSAGE)
-            .put("config", jsonParser.emptyObject()
-                .put("rootPath", ".")
-                .put("storage", "volatile://")
-                .put("manifest", dynamicManifest)
-                .put("urlMap", jsonParser.emptyObject()
-                    .put("https://$build/", "")));
-    sendMessageToArcs(jsonParser.stringify(msg));
+    RuntimeSettings settings = runtimeSettings.get();
+    sendMessageToArcs(jsonParser.stringify(jsonParser
+        .emptyObject()
+        .put(Constants.MESSAGE_FIELD, Constants.CONFIGURE_MESSAGE)
+        .put("config", jsonParser.emptyObject()
+            .put("rootPath", ".")
+            .put("storage", "volatile://")
+            .put("manifest", dynamicManifest)
+            .put("urlMap", jsonParser.emptyObject()
+                .put("https://$build/", "") // For fetching bundled javascript.
+                .put("https://$assets/", settings.loadAssetsFromWorkstation()
+                    // Fetch .wasm modules and generated manifest from the build directory.
+                    ? jsonParser.emptyObject()
+                        .put("root", "http://localhost:" + settings.devServerPort() + "/")
+                        .put("buildDir", "bazel-bin/")
+                        .put("buildOutputRegex", "(\\\\.wasm)|(\\\\.root\\\\.arcs)$")
+                    // Fetch all assets from the APK assets directory.
+                    : "file:////android_asset/arcs/"
+                ))));
   }
 }

javaharness/java/arcs/android/AndroidRuntimeSettings.java
@@ -14,17 +14,26 @@ public final class AndroidRuntimeSettings implements RuntimeSettings {
   // Equivalent to &log parameter
   private static final String LOG_LEVEL_PROPERTY = "debug.arcs.runtime.log";
   // Equivalent to &explore-proxy parameter
-  private static final String USE_DEV_SERVER_PROXY_PROPERTY = "debug.arcs.runtime.use_alds";
+  private static final String ENABLE_ARCS_EXPLORER_PROPERTY = "debug.arcs.runtime.enable_arcs_explorer";
   // The target shell to be loaded (on-device) or be connected (on-host)
   private static final String SHELL_URL_PROPERTY = "debug.arcs.runtime.shell_url";
+  // Whether to load particles and recipes from the workstation
+  private static final String LOAD_ASSETS_FROM_WORKSTATION_PROPERTY =
+      "debug.arcs.runtime.load_workstation_assets";
+  // Port to be used for the communication with the dev server.
+  private static final String DEV_SERVER_PORT_PROPERTY = "debug.arcs.runtime.dev_server_port";
 
   // Default settings:
-  // Logs the most information, loads the on-device pipes-shell
-  // and not uses ALDS proxy.
+  // Logs the most information
   private static final int DEFAULT_LOG_LEVEL = 2;
-  private static final boolean DEFAULT_USE_DEV_SERVER = false;
-  private static final String DEFAULT_SHELL_URL = "file:///android_asset/arcs/index.html?solo=dynamic.manifest&";
-  private static final String LOCALHOST_SHELL_URL = "http://localhost:8786/shells/pipes-shell/web/deploy/dist/?";
+  // Does *not* connect Arcs Explorer
+  private static final boolean DEFAULT_ENABLE_ARCS_EXPLORER = false;
+  // Loads the on-device pipes-shell
+  private static final String DEFAULT_SHELL_URL = "file:///android_asset/arcs/index.html?";
+  // Load the on-device assets
+  private static final boolean DEFAULT_ASSETS_FROM_WORKSTATION = false;
+  // Uses the standard 8786 port
+  private static final int DEFAULT_DEV_SERVER_PORT = 8786;
 
   private static final Logger logger = Logger.getLogger(
       AndroidRuntimeSettings.class.getName());
@@ -32,8 +41,10 @@ public final class AndroidRuntimeSettings implements RuntimeSettings {
   @AutoValue
   abstract static class Settings {
     abstract int logLevel();
-    abstract boolean useDevServerProxy();
+    abstract boolean enableArcsExplorer();
     abstract String shellUrl();
+    abstract boolean loadAssetsFromWorkstation();
+    abstract int devServerPort();
 
     static Builder builder() {
       return new AutoValue_AndroidRuntimeSettings_Settings.Builder();
@@ -42,8 +53,10 @@ public final class AndroidRuntimeSettings implements RuntimeSettings {
     @AutoValue.Builder
     abstract static class Builder {
       abstract Builder setLogLevel(int level);
-      abstract Builder setUseDevServerProxy(boolean useDevServerProxy);
+      abstract Builder setEnableArcsExplorer(boolean useDevServerProxy);
       abstract Builder setShellUrl(String shellUrl);
+      abstract Builder setLoadAssetsFromWorkstation(boolean loadAssetsFromWorkstation);
+      abstract Builder setDevServerPort(int devServerPort);
       abstract Settings build();
     }
   }
@@ -56,10 +69,15 @@ public final class AndroidRuntimeSettings implements RuntimeSettings {
     settings = Settings.builder()
         .setLogLevel(
             getProperty(LOG_LEVEL_PROPERTY, Integer::valueOf, DEFAULT_LOG_LEVEL))
-        .setUseDevServerProxy(
-            getProperty(USE_DEV_SERVER_PROXY_PROPERTY, Boolean::valueOf, DEFAULT_USE_DEV_SERVER))
+        .setEnableArcsExplorer(
+            getProperty(ENABLE_ARCS_EXPLORER_PROPERTY, Boolean::valueOf, DEFAULT_ENABLE_ARCS_EXPLORER))
         .setShellUrl(
             getProperty(SHELL_URL_PROPERTY, String::valueOf, DEFAULT_SHELL_URL))
+        .setLoadAssetsFromWorkstation(
+            getProperty(LOAD_ASSETS_FROM_WORKSTATION_PROPERTY, Boolean::valueOf,
+                DEFAULT_ASSETS_FROM_WORKSTATION))
+        .setDevServerPort(
+            getProperty(DEV_SERVER_PORT_PROPERTY, Integer::valueOf, DEFAULT_DEV_SERVER_PORT))
         .build();
   }
 
@@ -69,13 +87,23 @@ public final class AndroidRuntimeSettings implements RuntimeSettings {
   }
 
   @Override
-  public boolean useDevServerProxy() {
-    return settings.useDevServerProxy();
+  public boolean enableArcsExplorer() {
+    return settings.enableArcsExplorer();
   }
 
   @Override
   public String shellUrl() {
     return settings.shellUrl();
+  }
+
+  @Override
+  public boolean loadAssetsFromWorkstation() {
+    return settings.loadAssetsFromWorkstation();
+  }
+
+  @Override
+  public int devServerPort() {
+    return settings.devServerPort();
   }
 
   /**

javaharness/java/arcs/api/RuntimeSettings.java
@@ -4,20 +4,20 @@ package arcs.api;
 public interface RuntimeSettings {
   // Used by Javascript-based and/or other types of Arcs runtime(s).
   // Equivalent to the &log=<level> parameter at JS Arcs runtime.
-  default int logLevel() {
-    return 0;
-  }
+  int logLevel();
 
-  // Used mainly by Javascript-based Arcs runtime to establish WebSocket
-  // connection to the host from the device during runtime initialization.
+  // Controls whether JS Arcs Runtime should wait on the connection to the Arcs Explorer tool.
   // Equivalent to the &explore-proxy parameter at JS Arcs runtime.
-  default boolean useDevServerProxy() {
-    return false;
-  }
+  boolean enableArcsExplorer();
+
+  // Controls whether to override loading of the particles and recipes to the Development Server,
+  // which loads them from the developer's workstation.
+  boolean loadAssetsFromWorkstation();
+
+  // Value of the port to be used for connecting to the developer server.
+  int devServerPort();
 
   // Used only by Javascript-based Arcs runtime to specify which shell
   // either on-device or on-host to connect with.
-  default String shellUrl() {
-    return "";
-  }
+  String shellUrl();
 }

commit 73c29a88a1ffbb666e067a2b141bbbd9e6f88efa

Fixes the javaharness broken by #3954

javaharness/java/arcs/android/AndroidArcsEnvironment.java
@@ -41,6 +41,8 @@ final class AndroidArcsEnvironment {
   private static final Logger logger =
     Logger.getLogger(AndroidArcsEnvironment.class.getName());
 
+  private static final String ASSETS_PREFIX = "https://$assets/";
+
   private static final String FIELD_MESSAGE = "message";
   private static final String MESSAGE_READY = "ready";
   private static final String MESSAGE_CONTEXT = "context";
@@ -101,7 +103,7 @@ final class AndroidArcsEnvironment {
 
     dynamicManifest = manifests
         .stream()
-        .map(s -> String.format("import \'https://$assets/%s\'", s))
+        .map(s -> String.format("import \'%s%s\'", ASSETS_PREFIX, s))
         .collect(Collectors.joining("\n"));
 
     webView.addJavascriptInterface(this, "DeviceClient");
@@ -188,6 +190,22 @@ final class AndroidArcsEnvironment {
 
   private void configureShell() {
     RuntimeSettings settings = runtimeSettings.get();
+
+    PortableJson urlMap = jsonParser.emptyObject()
+        // For fetching bundled javascript.
+        .put("https://$build/", "");
+
+    if (settings.loadAssetsFromWorkstation()) {
+      // Fetch .wasm modules and generated manifest from the build directory.
+      urlMap.put(ASSETS_PREFIX, jsonParser.emptyObject()
+          .put("root", "http://localhost:" + settings.devServerPort() + "/")
+          .put("buildDir", "bazel-bin/")
+          .put("buildOutputRegex", "(\\\\.wasm)|(\\\\.root\\\\.arcs)$"));
+    } else {
+      // Fetch all assets from the APK assets directory.
+      urlMap.put(ASSETS_PREFIX, "file:////android_asset/arcs/");
+    }
+                
     sendMessageToArcs(jsonParser.stringify(jsonParser
         .emptyObject()
         .put(Constants.MESSAGE_FIELD, Constants.CONFIGURE_MESSAGE)
@@ -195,16 +213,6 @@ final class AndroidArcsEnvironment {
             .put("rootPath", ".")
             .put("storage", "volatile://")
             .put("manifest", dynamicManifest)
-            .put("urlMap", jsonParser.emptyObject()
-                .put("https://$build/", "") // For fetching bundled javascript.
-                .put("https://$assets/", settings.loadAssetsFromWorkstation()
-                    // Fetch .wasm modules and generated manifest from the build directory.
-                    ? jsonParser.emptyObject()
-                        .put("root", "http://localhost:" + settings.devServerPort() + "/")
-                        .put("buildDir", "bazel-bin/")
-                        .put("buildOutputRegex", "(\\\\.wasm)|(\\\\.root\\\\.arcs)$")
-                    // Fetch all assets from the APK assets directory.
-                    : "file:////android_asset/arcs/"
-                ))));
+            .put("urlMap", urlMap))));
   }
 }