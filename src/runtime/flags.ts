04f570e945ddb91d9dead1fdacf679ff16d37555
Josh Pratt <jopra@chromium.org>
2019-11-06 10:22:03 +1100

    Fix up language server - TMP

--- src/runtime/flags.ts
+++ src/runtime/flags.ts
@@ -14,10 +14,10 @@ class FlagDefaults {
   static useNewStorageStack = false;
   // Enables the parsing of both pre and post slandles (unified) syntaxes.
   // Preslandles syntax is to be deprecated.
-  static parseBothSyntaxes = true;
+  static parseBothSyntaxes = false;
   // Use pre slandles syntax for parsing and toString by default.
   // If parseBothSyntaxes is off, this will set which syntax is enabled.
-  static defaultToPreSlandlesSyntax = true;
+  static defaultToPreSlandlesSyntax = false;
 }
 
 export class Flags extends FlagDefaults {
