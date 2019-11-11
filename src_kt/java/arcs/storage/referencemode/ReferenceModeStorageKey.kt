1fa75bcb29d12ffa082f5567861ff59a2ba80d9a
Jason Feinstein <jwf@google.com>
2019-11-10 20:35:48 -0800

    Add unit tests for StorageKey classes. (#4052)
    Also adds parsing capability for ReferenceModeStorageKey - something that was done by Shane in #4000.
    
    Ref #4037

--- src_kt/java/arcs/storage/referencemode/ReferenceModeStorageKey.kt
+++ src_kt/java/arcs/storage/referencemode/ReferenceModeStorageKey.kt
@@ -12,6 +12,9 @@
 package arcs.storage.referencemode
 
 import arcs.storage.StorageKey
+import arcs.storage.StorageKeyParser
+
+const val REFERENCE_MODE_PROTOCOL = "reference-mode"
 
 /**
  * Special subclass of [StorageKey] used to identify data managed by a
@@ -26,13 +29,54 @@ data class ReferenceModeStorageKey(
 
     override fun toKeyString(): String = "{${backingKey.embed()}}{${storageKey.embed()}}"
 
-    override fun toString(): String = "$protocol://${toKeyString()}"
+    override fun toString(): String = super.toString()
 
     companion object {
-        const val REFERENCE_MODE_PROTOCOL = "reference-mode"
+        init {
+            StorageKeyParser.addParser(REFERENCE_MODE_PROTOCOL, ::fromString)
+        }
 
-        private fun StorageKey.embed() =
-            toString().replace("\\{".toRegex(), "{{")
-                .replace("}".toRegex(), "}}")
+        private fun fromString(rawValue: String): ReferenceModeStorageKey {
+            val invalidFormatMessage: () -> String =
+                { "Invalid format for ReferenceModeStorageKey: $rawValue" }
+            var backing: StorageKey? = null
+            var direct: StorageKey? = null
+
+            var openCount = 0
+            var openIndex = -1
+            rawValue.forEachIndexed { i, char ->
+                require(direct == null, invalidFormatMessage)
+                when (char) {
+                    '{' -> {
+                        openCount++
+                        if (openIndex < 0) openIndex = i
+                    }
+                    '}' -> {
+                        openCount--
+                        if (openCount == 0) {
+                            require(openIndex >= 0, invalidFormatMessage)
+                            val childComponent = rawValue.substring(openIndex + 1, i).unEmbed()
+                            if (backing == null) {
+                                backing = childComponent
+                            } else {
+                                direct = childComponent
+                            }
+                            // Reset to negative, so we mark openIndex when we see the next '{'
+                            openIndex = -1
+                        }
+                    }
+                }
+            }
+            return ReferenceModeStorageKey(
+                requireNotNull(backing, invalidFormatMessage),
+                requireNotNull(direct, invalidFormatMessage)
+            )
+        }
     }
 }
+
+internal fun String.unEmbed(): StorageKey =
+    StorageKeyParser.parse(replace("\\{\\{".toRegex(), "{").replace("}}".toRegex(), "}"))
+
+internal fun StorageKey.embed() =
+    toString().replace("\\{".toRegex(), "{{").replace("}".toRegex(), "}}")
---
+++ src_kt/javatests/arcs/storage/driver/VolatileStorageKeyTest.kt
@@ -0,0 +1,34 @@
+package arcs.storage.driver
+
+import arcs.common.ArcId
+import arcs.storage.StorageKeyParser
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+/** Tests for [VolatileStorageKey]. */
+@RunWith(JUnit4::class)
+class VolatileStorageKeyTest {
+    @Test
+    fun toString_rendersCorrectly() {
+        val arcId = ArcId.newForTest("arc")
+        val key = VolatileStorageKey(arcId, "foo")
+        assertThat(key.toString()).isEqualTo("$VOLATILE_DRIVER_PROTOCOL://$arcId/foo")
+    }
+
+    @Test
+    fun childKeyWithComponent_isCorrect() {
+        val arcId = ArcId.newForTest("arc")
+        val parent = VolatileStorageKey(arcId, "parent")
+        val child = parent.childKeyWithComponent("child")
+        assertThat(child.toString()).isEqualTo("$VOLATILE_DRIVER_PROTOCOL://$arcId/parent/child")
+    }
+
+    @Test
+    fun registersSelf_withStorageKeyParser() {
+        val arcId = ArcId.newForTest("arc")
+        val key = VolatileStorageKey(arcId, "foo")
+        assertThat(StorageKeyParser.parse(key.toString())).isEqualTo(key)
+    }
+}
