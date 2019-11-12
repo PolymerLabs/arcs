a082958673de45bcfc1cba573054f7f7f38eafe8
Sarah Heimlich <heimlich@google.com>
2019-11-12 15:52:28 -0800

    Add base HumanPlayer, no funcionality.

--- particles/Tutorial/Kotlin/Demo/TTTGame.kt
+++ particles/Tutorial/Kotlin/Demo/TTTGame.kt
@@ -5,7 +5,6 @@ import arcs.Particle
 import arcs.Singleton
 import arcs.TTTGame_GameState
 import arcs.TTTGame_PlayerOne
-import arcs.log
 import kotlin.native.internal.ExportForCppRuntime
 
 class TTTGame : Particle() {
@@ -18,23 +17,16 @@ class TTTGame : Particle() {
     }
 
     override fun onHandleSync(handle: Handle, allSynced: Boolean) {
-        log("In TTTGame onHandleSync, ${this.gameState.get()}")
-
-        val gs = this.gameState.get() ?: TTTGame_GameState(
-            board = "Yo there",
-            currentPlayer = 57.0
-        )
-
-        val board = gs.board ?: ""
-
-        log("In TTTGame onHandleSync, gs = $gs")
-        log("In TTTGame onHandleSync, board = $board")
-
         if (this.gameState.get()?.board == null) {
             this.gameState.set(TTTGame_GameState(
                 board = ",,,,,,,,",
                 currentPlayer = 971.0
             ))
+        }
+        if (this.playerOne.get()?.id != 1.0) {
+            val p1 = playerOne.get() ?: TTTGame_PlayerOne()
+            p1.id = 1.0
+            this.playerOne.set(p1)
         }
     }
 
