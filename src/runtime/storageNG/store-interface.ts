0d950f197cee964b82c1365d28c4b97250ca24b1
galganif <4754402+galganif@users.noreply.github.com>
2019-11-13 15:46:36 +1100

    Remove getLocalData method as it is a duplicate of serializeContents. (#4005)

--- src/runtime/storageNG/store-interface.ts
+++ src/runtime/storageNG/store-interface.ts
@@ -98,7 +98,7 @@ export abstract class ActiveStore<T extends CRDTTypeRecord>
     assert(this.mode === activeStore.mode);
     await this.onProxyMessage({
       type: ProxyMessageType.ModelUpdate,
-      model: await activeStore.getLocalData()
+      model: await activeStore.serializeContents()
     });
   }
 
@@ -106,7 +106,6 @@ export abstract class ActiveStore<T extends CRDTTypeRecord>
     return this.serializeContents();
   }
 
-  abstract getLocalData(): Promise<CRDTData>;
   abstract on(callback: ProxyCallback<T>): number;
   abstract off(callback: number): void;
   abstract async onProxyMessage(message: ProxyMessage<T>): Promise<boolean>;
