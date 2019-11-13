2f6546d85050e0c5fff055c79c7e4e1b6edea22a
Jason Feinstein <jwf@google.com>
2019-11-13 13:57:42 -0800

    arcs.arcs.util - no thanks

--- src_kt/java/arcs/util/testutil/TestLog.kt
+++ src_kt/java/arcs/util/testutil/TestLog.kt
@@ -1,4 +1,15 @@
-package arcs.arcs.util.testutil
+/*
+ * Copyright 2019 Google LLC.
+ *
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ *
+ * Code distributed by Google as part of this project is also subject to an additional IP rights
+ * grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+package arcs.util.testutil
 
 import arcs.arcs.util.Log
 import java.io.PrintWriter
