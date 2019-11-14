bc7a6a7778a8b3f60919ab289c61ac8c579bfc88
Alex Rosengarten <alxrsngrtn@google.com>
2019-11-14 15:08:38 -0800

    WIP adding collections assert

--- src/wasm/kotlin/tests/arcs/TestBase.kt
+++ src/wasm/kotlin/tests/arcs/TestBase.kt
@@ -9,29 +9,41 @@ import kotlin.test.AsserterContributor
 import kotlin.test.AssertionError
 
 
-class TestBase : Particle() {
-
-}
-
-class ParticleTestContributor : AsserterContributor {
-    override fun contributeO(): Asserter? {
-        return if (hasArcsParticleInClassPath) ParticleAsserter else null
-    }
-
-    private val hasArcsParticleInClassPath = try {
-        Class.forName("arcs.Particle")
-        true
-    } catch (_: java.lang.ClassNotFoundException) {
-        false
-    }
-}
-
-object ParticleAsserter : Particle(), Asserter {
+class ParticleAsserter : Particle(), Asserter {
     private val errors = Collection { Test_Data() }
 
     init {
         registerHandle("errors", errors)
     }
+
+    private fun <T> assertContainerEqualOrdered(container: Collection<T>, converter: (T) -> String, expected: List<String>, isOrdered: Boolean = true) {
+        if (container.size != expected.size) {
+            fail("expected container to have ${expected.size} items; " +
+                "actual size ${container.size}")
+        }
+
+        // Convert result values to strings and sort them when checking an unordered container.
+        val res = mutableListOf<String>()
+        for (val it in container) {
+            res.add(converter(it))
+        }
+        if(!isOrdered) {
+            res.sort()
+        }
+
+        // Compare against expected.
+        val marks = mutableListOf<String>()
+        var ok = true
+        for (pair in expected zip res) {
+            val match = pair.first.equals(pair.second)
+            marks.add(if(match) " " else "*")
+            ok = ok && match
+        }
+
+    }
+
+    fun assertContainerEqualUnordered() = Unit
+
 
     override fun fail(message: String?): Nothing {
         val err = if (message == null) Test_Data(txt="Failure") else Test_Data(txt=message)
