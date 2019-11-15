6de552501ae8e98544d19cfdb6705ae5a41e5202
Alex Rosengarten <alxrsngrtn@google.com>
2019-11-15 12:56:33 -0800

    setting up entity tests

--- src/wasm/kotlin/tests/arcs/EntityClassApiTest.kt
+++ src/wasm/kotlin/tests/arcs/EntityClassApiTest.kt
@@ -1,12 +1,32 @@
 package wasm.kotlin.tests.arcs
 
+import arcs.Singleton
 import arcs.WasmAddress
 import kotlin.native.internal.ExportForCppRuntime
 
 
 class EntityClassApiTest: TestBase() {
+    private val unused1 = Singleton { EntityClassApiTest_Data() }
+    private val unused2 = Singleton { EntityClassApiTest_Empty() }
+
+    init {
+        registerHandle("data", unused1)
+        registerHandle("empty", unused2)
+    }
+
+    /** Run tests on particle initialization */
+    override fun init() {
+        fieldMethodsTest()
+
+    }
+
+    @Test
+    fun fieldMethodsTest() {
+
+    }
 
 }
+
 
 @Retain
 @ExportForCppRuntime("_newEntityClassApiTest")
