fae44b5e038eef2fd1303981362de300bd04b5fd
Alex Rosengarten <alxrsngrtn@google.com>
2019-11-19 11:01:04 -0800

    Passing CollectionApiTest

--- src/wasm/kotlin/tests/arcs/CollectionApiTest.kt
+++ src/wasm/kotlin/tests/arcs/CollectionApiTest.kt
@@ -1,60 +1,81 @@
 package wasm.kotlin.tests.arcs
 
-import arcs.Particle
 import arcs.Collection
+import arcs.Particle
 import arcs.WasmAddress
 import kotlin.native.internal.ExportForCppRuntime
 
 class CollectionApiTest : Particle() {
-    private val in_ = Collection { CollectionApiTest_InHandle() }
-    private val out_ = Collection { CollectionApiTest_OutHandle() }
-    private val io_ = Collection { CollectionApiTest_IoHandle() }
+    private val _in = Collection { CollectionApiTest_InHandle() }
+    private val out = Collection { CollectionApiTest_OutHandle() }
+    private val io = Collection { CollectionApiTest_IoHandle() }
     private val stored = CollectionApiTest_OutHandle()
 
     init {
-        registerHandle("inHandle", in_)
-        registerHandle("outHandle", out_)
-        registerHandle("ioHandle", io_)
+        registerHandle("inHandle", _in)
+        registerHandle("outHandle", out)
+        registerHandle("ioHandle", io)
     }
 
     override fun fireEvent(slotName: String, eventName: String, eventData: Map<String, String>) {
         when (eventName) {
             "case1" -> {
-                out_.clear()
-                io_.clear()
+                out.clear()
+                io.clear()
             }
             "case2" -> {
-                stored.flg = in_.isEmpty()
-                stored.num = in_.size
-                out_.store(stored)
+                stored.flg = _in.isEmpty()
+                stored.num = _in.size.toDouble()
+                out.store(stored)
             }
             "case3" -> {
-                out_.remove(stored)
+                out.remove(stored)
             }
             "case4" -> {
                 val d1 = CollectionApiTest_OutHandle()
-                val iter = in_.iterator()
+                val iter = _in.iterator()
                 val i1 = iter.next()
                 d1.txt = i1.toString()
                 d1.num = d1.num?.times(2)
                 d1.flg = iter.hasNext()
-                out_.store(d1)
+                out.store(d1)
 
                 val d2 = CollectionApiTest_OutHandle()
                 val i2 = iter.next()
                 d2.txt = if (i1.equals(i2)) "eq" else "ne"
                 d2.flg = iter.hasNext()
-                out_.store(d2)
+                out.store(d2)
 
                 val d3 = CollectionApiTest_OutHandle()
-                val i3 = iter.next()
+                iter.next()
                 d3.txt = if (i2.equals(i1)) "ne" else "eq"
                 d3.flg = iter.hasNext()
-                out_.store(d3)
+                out.store(d3)
             }
             "case5" -> {
-                // TODO(alxr)
+                val extra = CollectionApiTest_IoHandle()
 
+                extra.txt = "abc"
+                io.store(extra)
+                val d1 = CollectionApiTest_OutHandle(num = io.size.toDouble(), flg = io.isEmpty())
+                out.store(d1)
+
+                io.remove(extra)
+                val d2 = CollectionApiTest_OutHandle(num = io.size.toDouble())
+                out.store(d2)
+
+                // Ranged iteration; order is not guaranteed so use 'num' to assign sorted array slots.
+                val res = mutableListOf<String>()
+                for (data in io) {
+                    res.add(data.encodeEntity())
+                }
+                res
+                    .map { s: String -> CollectionApiTest_OutHandle(txt = s) }
+                    .forEach { h: CollectionApiTest_OutHandle -> out.store(h) }
+
+                io.clear()
+                val d3 = CollectionApiTest_OutHandle(num = io.size.toDouble(), flg = io.isEmpty())
+                out.store(d3)
             }
         }
     }
