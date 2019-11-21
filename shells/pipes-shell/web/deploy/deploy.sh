6020f22df806b91493290d18d65f9cac42646613
ianull <ianchang@google.com>
2019-11-20 18:28:18 -0800

    Fix checksum on linux and mac hosts

--- shells/pipes-shell/web/deploy/deploy.sh
+++ shells/pipes-shell/web/deploy/deploy.sh
@@ -14,5 +14,5 @@ npx webpack --display=errors-only
 # Arcs cache manager and versioning
 # Must be done at the last step to ensure the correct dist/** checksum
 cp -fR ../cache-mgr*.js dist/
-sed -i "s/__ARCS_MD5__/$(tar -cf - dist | md5sum | cut -d' ' -f1)/g" \
+sed -i "s/__ARCS_MD5__/$(tar -cf - dist | shasum | cut -d' ' -f1)/g" \
   dist/cache-mgr-sw.js
