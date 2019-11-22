a5bf92fe9f0ac0df3a85634a948ca33890138585
Alex Rosengarten <alxrsngrtn@google.com>
2019-11-21 18:13:00 -0800

    Throwing NotImplError for future implementation

--- src/wasm/kotlin/java/arcs/jvm/JvmRuntimeClient.kt
+++ src/wasm/kotlin/java/arcs/jvm/JvmRuntimeClient.kt
@@ -1,38 +1,33 @@
 package arcs
 
 actual object RuntimeClient {
-    actual fun <T : Entity<T>> singletonClear(particle: Particle, singleton: Singleton<T>) {
-    }
+    actual fun <T : Entity<T>> singletonClear(particle: Particle, singleton: Singleton<T>): Unit =
+        throw NotImplementedError()
 
-    actual fun <T : Entity<T>> singletonSet(particle: Particle, singleton: Singleton<T>, encoded: String) {
-    }
+    actual fun <T : Entity<T>> singletonSet(particle: Particle, singleton: Singleton<T>, encoded: String): Unit =
+        throw NotImplementedError()
 
-    actual fun <T : Entity<T>> collectionRemove(particle: Particle, collection: Collection<T>, encoded: String) {
-    }
+    actual fun <T : Entity<T>> collectionRemove(particle: Particle, collection: Collection<T>, encoded: String): Unit =
+        throw NotImplementedError()
 
-    actual fun <T : Entity<T>> collectionClear(particle: Particle, collection: Collection<T>) {
-    }
+    actual fun <T : Entity<T>> collectionClear(particle: Particle, collection: Collection<T>): Unit =
+        throw NotImplementedError()
 
-    actual fun <T : Entity<T>> collectionStore(particle: Particle, collection: Collection<T>, encoded: String) {
-    }
+    actual fun <T : Entity<T>> collectionStore(particle: Particle, collection: Collection<T>, encoded: String): Unit =
+        throw NotImplementedError()
 
-    actual fun log(msg: String) {
-    }
+    actual fun log(msg: String): Unit = throw NotImplementedError()
 
-    actual fun onRenderOutput(particle: Particle, template: String?, model: String?) {
+    actual fun onRenderOutput(particle: Particle, template: String?, model: String?): Unit =
+        throw NotImplementedError()
 
-    }
+    actual fun serviceRequest(particle: Particle, call: String, encoded: String, tag: String): Unit =
+        throw NotImplementedError()
 
-    actual fun serviceRequest(particle: Particle, call: String, encoded: String, tag: String) {
+    actual fun resolveUrl(url: String): String = throw NotImplementedError()
 
-    }
+    actual fun abort(): Unit = throw NotImplementedError()
 
-    actual fun resolveUrl(url: String): String = ""
-
-    actual fun abort() {
-    }
-
-    actual fun assert(cond: Boolean) {
-    }
+    actual fun assert(cond: Boolean): Unit = throw NotImplementedError()
 }
 
