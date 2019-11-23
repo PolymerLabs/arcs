7d99f268816ac1a0dc3eb0569038ba6cbc6ba9e1
Alex Rosengarten <alxrsngrtn@google.com>
2019-11-22 17:19:31 -0800

    KDocs for a few funs of RuntimeClient

--- src/wasm/kotlin/java/arcs/RuntimeClient.kt
+++ src/wasm/kotlin/java/arcs/RuntimeClient.kt
@@ -1,8 +1,24 @@
 package arcs
 
+/**
+ * Delegate core runtime operations to the appropriate platform.
+ */
 expect object RuntimeClient {
+    /**
+     * Remove the entity within the Singleton handle of [particle].
+     *
+     * @param particle Particle in scope
+     * @param singleton target to be cleared
+     */
     fun <T : Entity<T>> singletonClear(particle: Particle, singleton: Singleton<T>)
 
+    /**
+     * Set the entity of the Singleton handle of [particle].
+     *
+     * @param particle Particle in scope
+     * @param singleton target to be cleared
+     * @param encoded Serialized representation of an entity
+     */
     fun <T : Entity<T>> singletonSet(particle: Particle, singleton: Singleton<T>, encoded: String)
 
     fun <T : Entity<T>> collectionRemove(particle: Particle, collection: Collection<T>, encoded: String)
