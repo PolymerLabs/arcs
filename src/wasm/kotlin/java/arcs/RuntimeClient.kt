ae72853246ba66ebcca185bb199d6dd78ea30b54
Alex Rosengarten <alxrsngrtn@google.com>
2019-11-25 10:40:54 -0800

    Added more KDocs

--- src/wasm/kotlin/java/arcs/RuntimeClient.kt
+++ src/wasm/kotlin/java/arcs/RuntimeClient.kt
@@ -21,14 +21,38 @@ expect object RuntimeClient {
      */
     fun <T : Entity<T>> singletonSet(particle: Particle, singleton: Singleton<T>, encoded: String)
 
-    fun <T : Entity<T>> collectionRemove(particle: Particle, collection: Collection<T>, encoded: String)
-
+    /**
+     * Removes all entities to produce an empty collection.
+     *
+     * @param particle Particle in scope
+     * @param collection target to be made empty
+     */
     fun <T : Entity<T>> collectionClear(particle: Particle, collection: Collection<T>)
 
+    /**
+     * Remove a single entity from a collection
+     *
+     * @param particle Particle in scope
+     * @param collection target to be mutated
+     * @param encoded Serialized representation of an entity
+     */
+    fun <T : Entity<T>> collectionRemove(particle: Particle, collection: Collection<T>, encoded: String)
+
+    /**
+     * Add a single entity to a collection
+     *
+     * @param particle Particle in scope
+     * @param collection target to be mutated
+     * @param encoded Serialized representation of an entity
+     */
     fun <T : Entity<T>> collectionStore(particle: Particle, collection: Collection<T>, encoded: String)
 
+    /** @param msg Message to write to a logging system. */
     fun log(msg: String)
 
+    /**
+     *
+     */
     fun onRenderOutput(particle: Particle, template: String?, model: String?)
 
     fun serviceRequest(particle: Particle, call: String, encoded: String, tag: String)
