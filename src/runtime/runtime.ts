26a53989e957f994f8eb28577d18f36ab4d9d6b5
Shane Stephens <shanestephens@google.com>
2019-11-26 15:49:35 +1100

    Review changes

--- src/runtime/runtime.ts
+++ src/runtime/runtime.ts
@@ -31,7 +31,6 @@ import {logsFactory} from '../platform/logs-factory.js';
 import {devtoolsArcInspectorFactory} from '../devtools-connector/devtools-arc-inspector.js';
 
 const {warn} = logsFactory('Runtime', 'orange');
-import {Flags} from './flags.js';
 
 export type RuntimeArcOptions = Readonly<{
   pecFactories?: PecFactory[];
