3d3a593b22fa1bff30a44de222be10bd85edfa58
Alex Rosengarten <alxrsngrtn@google.com>
2019-11-27 14:53:22 -0800

    KDoc: start w/ lowercase

--- src/wasm/kotlin/java/arcs/RuntimeClient.kt
+++ src/wasm/kotlin/java/arcs/RuntimeClient.kt
@@ -7,7 +7,7 @@ expect object RuntimeClient {
     /**
      * Remove the entity within the Singleton handle of [particle].
      *
-     * @param particle Particle in scope
+     * @param particle particle in scope
      * @param singleton target to be cleared
      */
     fun <T : Entity<T>> singletonClear(particle: Particle, singleton: Singleton<T>)
@@ -15,16 +15,16 @@ expect object RuntimeClient {
     /**
      * Set the entity of the Singleton handle of [particle].
      *
-     * @param particle Particle in scope
+     * @param particle particle in scope
      * @param singleton target to be cleared
-     * @param encoded Serialized representation of an entity
+     * @param encoded serialized representation of an entity
      */
     fun <T : Entity<T>> singletonSet(particle: Particle, singleton: Singleton<T>, encoded: String)
 
     /**
      * Removes all entities to produce an empty collection.
      *
-     * @param particle Particle in scope
+     * @param particle particle in scope
      * @param collection target to be made empty
      */
     fun <T : Entity<T>> collectionClear(particle: Particle, collection: Collection<T>)
@@ -32,41 +32,41 @@ expect object RuntimeClient {
     /**
      * Remove a single entity from a collection
      *
-     * @param particle Particle in scope
+     * @param particle particle in scope
      * @param collection target to be mutated
-     * @param encoded Serialized representation of an entity
+     * @param encoded serialized representation of an entity
      */
     fun <T : Entity<T>> collectionRemove(particle: Particle, collection: Collection<T>, encoded: String)
 
     /**
      * Add a single entity to a collection
      *
-     * @param particle Particle in scope
+     * @param particle particle in scope
      * @param collection target to be mutated
-     * @param encoded Serialized representation of an entity
-     * @return The ID [String] of the stored entity, or null.
+     * @param encoded serialized representation of an entity
+     * @return the ID [String] of the stored entity, or null.
      */
     fun <T : Entity<T>> collectionStore(particle: Particle, collection: Collection<T>, encoded: String): String?
 
-    /** @param msg Message to write to a logging system. */
+    /** @param msg message to write to a logging system. */
     fun log(msg: String)
 
     /**
      * React to UI Rendering
      *
-     * @param particle Particle in scope
-     * @param template String encoding of UI template
-     * @param model Data model to be interpolated in the template
+     * @param particle particle in scope
+     * @param template string encoding of UI template
+     * @param model data model to be interpolated in the template
      */
     fun onRenderOutput(particle: Particle, template: String?, model: String?)
 
     /**
      * Request an action to be performed by a Service
      *
-     * @param particle Particle in scope
-     * @param call A call string. Format: `<serviceName>.<functionName>`
-     * @param encoded Serialized key-value data to be passed to the service
-     * @param tag Name the request to the service
+     * @param particle particle in scope
+     * @param call a call string. Format: `<serviceName>.<functionName>`
+     * @param encoded serialized key-value data to be passed to the service
+     * @param tag name the request to the service
      */
     fun serviceRequest(particle: Particle, call: String, encoded: String, tag: String)
 
@@ -76,8 +76,8 @@ expect object RuntimeClient {
     /**
      * Ensure that a Boolean condition is true, otherwise [abort].
      *
-     * @param message Description of assertion condition
-     * @param cond Evaluation of assertion condition
+     * @param message description of assertion condition
+     * @param cond evaluation of assertion condition
      */
     fun assert(message: String, cond: Boolean)
 
