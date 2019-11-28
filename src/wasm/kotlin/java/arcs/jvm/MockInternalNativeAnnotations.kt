312a668fe2aa6034561474d98c2eeb7c5f6fca8a
Alex Rosengarten <alxrsngrtn@google.com>
2019-11-27 18:05:22 -0800

    fix: grammar

--- src/wasm/kotlin/java/arcs/jvm/MockInternalNativeAnnotations.kt
+++ src/wasm/kotlin/java/arcs/jvm/MockInternalNativeAnnotations.kt
@@ -12,7 +12,7 @@ package kotlin.native.internal
 import kotlin.annotation.AnnotationTarget.*
 
 /**
- * Makes this function to be possible to call by given name from C++ part of runtime using C ABI.
+ * Allows functions to be called by their given name from C++ part of runtime using C ABI.
  * The parameters are mapped in an implementation-dependent manner.
  *
  * The function to call from C++ can be a wrapper around the original function.
