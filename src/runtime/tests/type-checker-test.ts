cead7a425c6355aad95a2bb2d75a4efc9bfd0a58
Josh Pratt <jopra@chromium.org>
2019-12-02 17:02:15 +1100

    Remove preslandles syntax for type checker tests

--- src/runtime/tests/type-checker-test.ts
+++ src/runtime/tests/type-checker-test.ts
@@ -290,37 +290,6 @@ describe('TypeChecker', () => {
     assert.strictEqual(true, recipe.isResolved());
   });
 
-  it('correctly applies then resolves a one-sided Entity constraint', Flags.withPreSlandlesSyntax(async () => {
-    const manifest = await Manifest.parse(`
-      interface Interface
-        in ~a item
-
-      particle Concrete
-        in Product {} item
-
-      particle Transformation
-        host Interface particle0
-        in [~a] collection
-
-      recipe
-        create as h0
-        Transformation
-          particle0 <- Concrete
-          collection <- h0
-    `);
-
-    const recipe = manifest.recipes[0];
-    const type = Handle.effectiveType(null, recipe.handles[0].connections);
-    assert.strictEqual(false, type.isResolved());
-    assert.strictEqual(true, type.canEnsureResolved());
-    assert.strictEqual(true, type.maybeEnsureResolved());
-    assert.strictEqual(true, type.isResolved());
-    assert.strictEqual('Product', (type.resolvedType() as CollectionType<EntityType>).collectionType.entitySchema.names[0]);
-
-    recipe.normalize();
-    assert.strictEqual(true, recipe.isResolved());
-  }));
-
   it(`doesn't resolve Entity and Collection`, async () => {
     const entity: TypeListInfo = {
       type: EntityType.make(['Product', 'Thing'], {}),
