bceb4c8b76c9d1dc33d03491a8f49a54fcb32124
Cameron Silvestrini <csilvestrini@google.com>
2019-12-04 14:05:28 +1100

    Another really important change

--- javaharness/java/arcs/android/ArcsShellApi.java
+++ javaharness/java/arcs/android/ArcsShellApi.java
@@ -23,6 +23,7 @@ import arcs.api.UiBroker;
 
 @Singleton
 class ArcsShellApi {
+
   private final PortableJsonParser jsonParser;
   private final AndroidArcsEnvironment environment;
   private final PecPortManager pecPortManager;
