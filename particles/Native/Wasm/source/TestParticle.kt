d4d6b8294cdc1497f6b731d6d900c3c1105371bf
Alex Rosengarten <alxr@google.com>
2019-12-04 15:01:10 +1100

    Addressed readability issues for testparticle (#3974)

--- particles/Native/Wasm/source/TestParticle.kt
+++ particles/Native/Wasm/source/TestParticle.kt
@@ -119,10 +119,10 @@ class TestParticle : Particle() {
         registerHandle("info", info)
 
         eventHandler("add") {
-            val newData = data.get() ?: TestParticle_Data()
-            newData.num = newData.num?.let { it + 2 } ?: 0.0
-            newData.txt = (newData.txt ?: "") + "!!!!!!"
-            this.data.set(newData)
+          val newData = data.get() ?: TestParticle_Data()
+          newData.num = newData.num?.let { it + 2 } ?: 0.0
+          newData.txt = "${newData.txt}!!!!!!"
+          this.data.set(newData)
         }
 
         eventHandler("dataclear") {
