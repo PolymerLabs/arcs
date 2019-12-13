82f4babd83de6b196580d896d84bb21286ef3092
Sarah Heimlich <heimlich@google.com>
2019-12-13 15:29:50 +1100

    Fix CollectionApiTest

--- src/wasm/kotlin/javatests/arcs/CollectionApiTest.kt
+++ src/wasm/kotlin/javatests/arcs/CollectionApiTest.kt
@@ -11,17 +11,33 @@
 
 package sdk.kotlin.javatests.arcs
 
-import arcs.addressable.toAddress
 import arcs.Collection
 import arcs.Particle
-import kotlin.native.internal.ExportForCppRuntime
+import arcs.addressable.toAddress
 import kotlin.native.Retain
+import kotlin.native.internal.ExportForCppRuntime
 
 class CollectionApiTest : Particle() {
-    private val _in = Collection(this, "inHandle") { CollectionApiTest_InHandle() }
-    private val out = Collection(this, "outHandle") { CollectionApiTest_OutHandle() }
-    private val io = Collection(this, "ioHandle") { CollectionApiTest_IoHandle() }
-    private val stored = CollectionApiTest_OutHandle()
+    private val _in = Collection(this, "inHandle") { CollectionApiTest_InHandle(0.0) }
+    private val out = Collection(this, "outHandle") {
+        CollectionApiTest_OutHandle(
+            num = 0.0,
+            txt = "",
+            flg = false
+        )
+    }
+    private val io = Collection(this, "ioHandle") {
+        CollectionApiTest_IoHandle(
+            num = 0.0,
+            txt = "",
+            flg = false
+        )
+    }
+    private val stored = CollectionApiTest_OutHandle(
+        num = 0.0,
+        txt = "",
+        flg = false
+    )
 
     override fun fireEvent(slotName: String, eventName: String, eventData: Map<String, String>) {
         when (eventName) {
@@ -38,7 +54,11 @@ class CollectionApiTest : Particle() {
                 out.remove(stored)
             }
             "case4" -> {
-                val d1 = CollectionApiTest_OutHandle()
+                val d1 = CollectionApiTest_OutHandle(
+                    num = 0.0,
+                    txt = "",
+                    flg = false
+                )
                 val iter = _in.iterator()
                 d1.flg = iter.hasNext()
                 val i1 = iter.next()
@@ -46,26 +66,45 @@ class CollectionApiTest : Particle() {
                 d1.num = i1.num?.let { it * 2 }
                 out.store(d1)
 
-                val d2 = CollectionApiTest_OutHandle()
+                val d2 = CollectionApiTest_OutHandle(
+                    num = 0.0,
+                    txt = "",
+                    flg = false
+                )
                 d2.txt = "eq"
                 d2.flg = iter.hasNext()
                 out.store(d2)
 
-                val d3 = CollectionApiTest_OutHandle()
-                d3.txt =  "ne"
+                val d3 = CollectionApiTest_OutHandle(
+                    num = 0.0,
+                    txt = "",
+                    flg = false
+                )
+                d3.txt = "ne"
                 d3.flg = !iter.hasNext()
                 out.store(d3)
             }
             "case5" -> {
-                val extra = CollectionApiTest_IoHandle()
+                val extra = CollectionApiTest_IoHandle(
+                    num = 0.0,
+                    txt = "",
+                    flg = false
+                )
 
                 extra.txt = "abc"
                 io.store(extra)
-                val d1 = CollectionApiTest_OutHandle(num = io.size.toDouble(), flg = io.isEmpty())
+                val d1 = CollectionApiTest_OutHandle(
+                    num = io.size.toDouble(),
+                    txt = "",
+                    flg = io.isEmpty())
                 out.store(d1)
 
                 io.remove(extra)
-                val d2 = CollectionApiTest_OutHandle(num = io.size.toDouble())
+                val d2 = CollectionApiTest_OutHandle(
+                    num = io.size.toDouble(),
+                    txt = "",
+                    flg = false
+                )
                 out.store(d2)
 
                 // Ranged iteration; order is not guaranteed so use 'num' to assign sorted array slots.
@@ -75,17 +114,26 @@ class CollectionApiTest : Particle() {
                 }
 
                 res
-                    .map { s: String -> CollectionApiTest_OutHandle(txt = s) }
+                    .map { s: String ->
+                        CollectionApiTest_OutHandle(
+                            num = 0.0,
+                            txt = s,
+                            flg = false
+                        )
+                    }
                     .forEach { h: CollectionApiTest_OutHandle -> out.store(h) }
 
                 io.clear()
-                val d3 = CollectionApiTest_OutHandle(num = io.size.toDouble(), flg = io.isEmpty())
+                val d3 = CollectionApiTest_OutHandle(
+                    num = io.size.toDouble(),
+                    txt = "",
+                    flg = io.isEmpty()
+                )
                 out.store(d3)
             }
         }
     }
 }
-
 
 @Retain
 @ExportForCppRuntime("_newCollectionApiTest")
