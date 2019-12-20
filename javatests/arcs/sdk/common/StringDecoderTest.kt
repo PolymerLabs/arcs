81190e585a5d2ed604775a143e1e3a08b475d191
Alex Rosengarten <alxrsngrtn@google.com>
2019-12-20 12:01:14 -0800

    javatests/arcs/sdk/common building

--- javatests/arcs/sdk/common/StringDecoderTest.kt
+++ javatests/arcs/sdk/common/StringDecoderTest.kt
@@ -11,8 +11,6 @@
 
 package arcs.sdk.common
 
-import arcs.sdk.kotlin.StringDecoder
-import arcs.sdk.kotlin.stringToUtf8
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
--- javatests/arcs/sdk/common/StringEncoderTest.kt
+++ javatests/arcs/sdk/common/StringEncoderTest.kt
@@ -11,8 +11,6 @@
 
 package arcs.sdk.common
 
-import arcs.sdk.kotlin.StringEncoder
-import arcs.sdk.kotlin.utf8ToString
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
