beefed3acec6fde234fe682230464b849a67cff0
Walter Korman <wkorman@chromium.org>
2020-01-10 14:44:32 -0800

    Fix minor auto-detected issues found from internal import.

--- javatests/arcs/sdk/android/dev/api/ArcDataTest.java
+++ javatests/arcs/sdk/android/dev/api/ArcDataTest.java
@@ -1,6 +1,11 @@
 package arcs.sdk.android.dev.api;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
