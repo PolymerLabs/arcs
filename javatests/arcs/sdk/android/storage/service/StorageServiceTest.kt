1e24cb624bf3fcfa6a746af1a0b57ed8060aec1e
Jason Feinstein <jwf@google.com>
2020-01-10 16:52:56 -0800

    Create StorageServiceTest

---
+++ javatests/arcs/sdk/android/storage/service/StorageServiceTest.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ *
+ * Code distributed by Google as part of this project is also subject to an additional IP rights
+ * grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+package arcs.sdk.android.storage.service
+
+import android.app.Application
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class StorageServiceTest {
+    private lateinit var app: Application
+
+    @Before
+    fun setUp() {
+    }
+}
