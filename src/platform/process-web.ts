ad099d335f6620ca6498394715f08d31e8ad29ff
Piotr Swigon <piotr.swigon@gmail.com>
2020-01-13 13:04:08 -0800

    Switch timing and pid in tracelib using platform/ mechanism

---
+++ src/platform/process-web.ts
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright (c) 2020 Google Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+// There's no web equivalent for the process, 42 will do.
+export const pid = 42;
