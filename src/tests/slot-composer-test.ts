df2c839679a24a268503503405378ae04b79a348
Scott J Miles <sjmiles@google.com>
2020-01-13 19:51:29 -0800

    update slot-composer-test

--- src/tests/slot-composer-test.ts
+++ src/tests/slot-composer-test.ts
@@ -11,13 +11,11 @@
 import {Planner} from '../planning/arcs-planning.js';
 import {assert} from '../platform/chai-web.js';
 import {Arc} from '../runtime/arc.js';
-import {HeadlessSlotDomConsumer} from '../runtime/headless-slot-dom-consumer.js';
 import {Loader} from '../platform/loader.js';
-import {HostedSlotContext, ProvidedSlotContext} from '../runtime/slot-context.js';
 import {SlotComposer} from '../runtime/slot-composer.js';
 import {SlotTestObserver} from '../runtime/testing/slot-test-observer.js';
 import {StrategyTestHelper} from '../planning/testing/strategy-test-helper.js';
-import {Id, ArcId} from '../runtime/id.js';
+import {ArcId} from '../runtime/id.js';
 import {Manifest} from '../runtime/manifest.js';
 import {Runtime} from '../runtime/runtime.js';
 import {storageKeyPrefixForTest} from '../runtime/testing/handle-for-test.js';
@@ -26,10 +24,16 @@ async function initSlotComposer(recipeStr) {
   const manifest = await Manifest.parse(recipeStr);
 
   const loader = new Loader(null, {
-    '*': `defineParticle(({Particle}) => { return class P extends Particle {} });`
+    '*': `
+      defineParticle(({UiParticle}) => {
+        return class P extends UiParticle {
+          get template() {
+            return '&nbsp;';
+          }
+        }
+      });
+    `
   });
-  console.warn(loader.staticMap);
-  console.warn(await loader.loadResource('foo.js'));
 
   const slotComposer = new SlotComposer();
   const observer = new SlotTestObserver();
@@ -45,16 +49,16 @@ async function initSlotComposer(recipeStr) {
   const planner = new Planner();
   const options = {strategyArgs: StrategyTestHelper.createTestStrategyArgs(arc)};
   planner.init(arc, options);
+
   await planner.strategizer.generate();
   assert.lengthOf(planner.strategizer.population, 1);
+
   const plan = planner.strategizer.population[0].result;
+
   return {arc, slotComposer, observer, plan};
 }
 
-describe('slot composer FOOB', function() {
-  // TODO(sjmiles): what is/was this for?
-  //this.timeout(4000);
-
+describe('slot composer', function() {
   it('initialize recipe and render slots', async () => {
     const manifestStr = `
 particle A in 'a.js'
@@ -91,29 +95,13 @@ recipe
 
     observer.newExpectations()
       .expectRenderSlot('A', 'root')
+      .expectRenderSlot('B', 'mySlot')
+      .expectRenderSlot('BB', 'mySlot')
+      .expectRenderSlot('C', 'otherSlot')
       ;
 
     await arc.instantiate(plan);
     await arc.idle;
-
-    // render root slot
-    // const particle = arc.activeRecipe.particles[0];
-    // const rootSlot = slotComposer.getSlotConsumer(particle, 'root');
-    //const mySlotId = slotComposer.findContextsByName('mySlot')[0].id;
-    // rootSlot.getInnerContainer = (slotId) => slotId === mySlotId ? 'dummy-inner-container' : null;
-    // startRenderParticles.length = 0;
-    // await slotComposer.renderSlot(particle, 'root', {model: {'foo': 'bar'}});
-    // assert.deepEqual(['B', 'BB'], startRenderParticles);
-
-    // TODO(sjmiles): render data no longer captured by slot objects
-    //assert.deepEqual({foo: 'bar'}, rootSlot.getRendering().model);
-
-    // TODO(sjmiles): contexts deprecated
-    // assert.lengthOf(slotComposer.getAvailableContexts(), 3);
-    // verifyContext('root', {hasContainer: true, consumeConnNames: ['A::root']});
-    // verifyContext('mySlot', {hasContainer: true, sourceSlotName: 'root', consumeConnNames: ['B::mySlot', 'BB::mySlot']});
-    // verifyContext('otherSlot', {hasContainer: false, sourceSlotName: 'root', consumeConnNames: ['C::otherSlot']});
-
     await observer.expectationsCompleted();
   });
 
@@ -124,10 +112,16 @@ recipe
     const runtime = new Runtime({loader, context});
     const arc = runtime.newArc('demo', storageKeyPrefixForTest());
 
+    const suggestions = await StrategyTestHelper.planForArc(arc);
+    const suggestion = suggestions.find(s => s.plan.name === 'FilterAndDisplayBooks');
+    assert.deepEqual(
+      suggestion.plan.particles.map(p => p.name).sort(),
+      ['ItemMultiplexer', 'List', 'ProductFilter']
+    );
+
     const slotComposer = arc.pec.slotComposer;
     const observer = new SlotTestObserver();
     slotComposer.observeSlots(observer);
-
     observer
       .newExpectations()
       .expectRenderSlot('List', 'root')
@@ -135,35 +129,29 @@ recipe
       .expectRenderSlot('ShowProduct', 'item')
       ;
 
-    const suggestions = await StrategyTestHelper.planForArc(arc);
-    const suggestion = suggestions.find(s => s.plan.name === 'FilterAndDisplayBooks');
-    assert.deepEqual(
-        suggestion.plan.particles.map(p => p.name).sort(),
-        ['ItemMultiplexer', 'List', 'ProductFilter']);
     await suggestion.instantiate(arc);
     await arc.idle;
-
     await observer.expectationsCompleted();
   });
 
   it('allows set slots to be consumed as a singleton slot', async () => {
     const manifestStr = `
-    particle A in 'a.js'
-      root: consumes Slot
-        item: provides? [Slot]
-    particle B in 'b.js'
-      item: consumes Slot
-    particle C in 'c.js'
-      item: consumes Slot
-    recipe
-      slot0: slot 'rootslotid-root'
-      A
-        root: consumes slot0
-          item: provides slot1
-      B
-        item: consumes slot1
-      C
-        item: consumes slot1
+      particle A in 'a.js'
+        root: consumes Slot
+          item: provides? [Slot]
+      particle B in 'b.js'
+        item: consumes Slot
+      particle C in 'c.js'
+        item: consumes Slot
+      recipe
+        slot0: slot 'rootslotid-root'
+        A
+          root: consumes slot0
+            item: provides slot1
+        B
+          item: consumes slot1
+        C
+          item: consumes slot1
     `;
 
     let {arc, observer, plan} = await initSlotComposer(manifestStr);
@@ -176,9 +164,8 @@ recipe
       .expectRenderSlot('A', 'root')
       .expectRenderSlot('B', 'item')
       .expectRenderSlot('C', 'item')
-      .expectRenderSlot('B', 'item')
-      .expectRenderSlot('C', 'item')
       ;
+
     await arc.instantiate(plan);
     await observer.expectationsCompleted();
   });
