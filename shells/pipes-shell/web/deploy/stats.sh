97425c09984f4f82be555b96bd2be7140c210bc9
Scott J Miles <sjmiles@google.com>
2020-01-17 13:54:30 -0800

    tweak android_deploy.sh so it can be the source-of-truth which is invoked from deploy.sh and stats.sh (instead of forking)

--- shells/pipes-shell/web/deploy/stats.sh
+++ shells/pipes-shell/web/deploy/stats.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+
 echo packing...
-npx webpack --profile --json > pack-stats.json
+./android_deploy.sh dist --display=verbose --profile --json > pack-stats.json
+
 echo studying...
 whybundled pack-stats.json
-
