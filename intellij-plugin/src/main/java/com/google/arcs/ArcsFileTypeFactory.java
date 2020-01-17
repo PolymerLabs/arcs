cfd4047b0d4b9e9ff98299476df639e9ffe81501
yuangu-google <yuangu@google.com>
2020-01-17 11:08:19 -0800

    Improve IntellijPlugin. (#4470)
    * Remove intellij-plugin/src/main/gen as they are generated codes.
    
    * Fix .arcs not recognized.

--- intellij-plugin/src/main/java/com/google/arcs/ArcsFileTypeFactory.java
+++ intellij-plugin/src/main/java/com/google/arcs/ArcsFileTypeFactory.java
@@ -4,8 +4,11 @@ import com.intellij.openapi.fileTypes.*;
 import org.jetbrains.annotations.NotNull;
 
 public class ArcsFileTypeFactory extends FileTypeFactory {
+
+  private static final FileNameMatcher EXTENSION_MATCHER = new ExtensionFileNameMatcher("arcs");
+
   @Override
   public void createFileTypes(@NotNull FileTypeConsumer fileTypeConsumer) {
-    fileTypeConsumer.consume(ArcsFileType.INSTANCE);
+    fileTypeConsumer.consume(ArcsFileType.INSTANCE, EXTENSION_MATCHER);
   }
 }
