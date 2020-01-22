bf3c33a33d1e25cd6f282dc4869d17ce8f7d9c8b
ianull <ianchang@google.com>
2020-01-22 12:32:51 +0800

    Fix presubmit wdio error (#4495)
    * Generate test changes
    
    * Avoid wdio piping in changed git object
    
    Reference: https://github.com/webdriverio/webdriverio/issues/4957
    
    * Add comment for better future tracking

--- tools/sigh.ts
+++ tools/sigh.ts
@@ -1026,7 +1026,12 @@ function testWdioShells(args: string[]) : boolean {
       // windows machine (`e:/path/` becomes `e:/e:/path/`)
       //fixPathForWindows(path.resolve('shells/tests/wdio.conf.js'))*/,
       path.resolve('shells/tests/wdio.conf.js'),
-      ...args
+      ...args,
+      // WebdriverIO reads the spec data from stdin which pipes in the changed
+      // git object within a git hook. Redirects stdin to the null device to
+      // avoid reading from stdin but from the wdio.conf.js directly.
+      // Also see {@link https://github.com/webdriverio/webdriverio/issues/4957}
+      '< /dev/null',
   ]);
 }
 
