34509cbf4dbad1c1153a7a0635adfacfbaaa7632
Josh Pratt <jopra@chromium.org>
2020-01-24 15:26:13 +1100

    One more fix

--- src/planning/plan/tests/planificator-test.ts
+++ src/planning/plan/tests/planificator-test.ts
@@ -66,6 +66,7 @@ describe('remote planificator', () => {
 
   let memoryProvider;
   beforeEach(() => {
+    Flags.reset();
     arcStorageKey = storageKeyPrefixForTest();
     memoryProvider = new TestVolatileMemoryProvider();
     RamDiskStorageDriverProvider.register(memoryProvider);
