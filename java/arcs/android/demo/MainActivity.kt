3c2d74fafc00c6ada5d2fe74a30440c5d56659f1
yuangu-google <yuangu@google.com>
2020-02-06 10:52:16 -0800

    Add a demo app for new design. (#4594)
    * Add a demo app for new design.
    
    * Resolve comments.
    
    * Move to arcs.android.demo package.
    
    * Fix lint.

---
+++ java/arcs/android/demo/MainActivity.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ *
+ * Code distributed by Google as part of this project is also subject to an additional IP rights
+ * grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+package arcs.android.demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/** Entry UI to launch different demo scenarios. */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
