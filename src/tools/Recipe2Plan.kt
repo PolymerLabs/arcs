25a863d61d5ac261911b478807c58bc5af034c19
Alex Rosengarten <alxrsngrtn@google.com>
2020-02-06 17:34:16 -0800

    Created / document CLI params and args

--- src/tools/Recipe2Plan.kt
+++ src/tools/Recipe2Plan.kt
@@ -1,7 +1,29 @@
-package test
+package arcs.tools
 
-import com.squareup.kotlinpoet.ClassName
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.multiple
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.file
 
-fun main(args: Array<String>) {
-    println("hello")
+/** Generates plans from recipes. */
+class Recipe2Plan : CliktCommand(
+    help = """Generate plans from recipes.
+    
+    This script reads recipes from a serialized manifest and generates Kotlin `Plan` classes.""",
+    printHelpOnEmptyArgs = true
+) {
+
+    val outdir by option(help = "output directory; defaults to '.'").file(fileOkay = false)
+    val outfile by option(help = "output filename; if omitted").file(folderOkay = false)
+    val packageName by option(help = "scope to specified package; default: 'arcs'").default("arcs")
+    val manifests by argument(help = "paths to JSON serialized manifests").file(exists = true).multiple()
+
+    override fun run() {
+        echo("hello")
+    }
+
 }
+
+fun main(args: Array<String>) = Recipe2Plan().main(args)
