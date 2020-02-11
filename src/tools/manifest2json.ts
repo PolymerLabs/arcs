3d27e0849f9b1e8771f623c9ccb525fce0b5377a
Alex Rosengarten <alxr@google.com>
2020-02-10 17:01:04 -0800

    Manifest2json revision: Simplifying for later use (#4626)
    * Input: multi manifests, output: single json file
    
    * bazel rules updates: multi input, single output
    
    * updated docs
    
    * lint --fix
    
    * Including parse errors in the msg
    
    * logging warnings, throwing errors
    
    * properly truthy if statement

--- src/tools/manifest2json.ts
+++ src/tools/manifest2json.ts
@@ -11,7 +11,17 @@ import minimist from 'minimist';
 import fs from 'fs';
 import path from 'path';
 import {Runtime} from '../runtime/runtime.js';
-import {Manifest} from '../runtime/manifest.js';
+import {ErrorSeverity, Manifest, ManifestError} from '../runtime/manifest.js';
+
+class Serialization {
+  particles: object[] = [];
+  schemas: object[] = [];
+
+  merge(other: Serialization) {
+    this.particles = this.particles.concat(other.particles);
+    this.schemas = this.schemas.concat(other.schemas);
+  }
+}
 
 const opts = minimist(process.argv.slice(2), {
   string: ['outdir', 'outfile'],
@@ -25,14 +35,19 @@ Usage
   $ tools/sigh manifest2json [options] [file ...]
 
 Description
-  Serializes manifests to JSON. 
+  Serializes manifests to a JSON file. 
 
 Options
+  --outfile, -f output filename; required
   --outdir, -d  output directory; defaults to '.'
-  --outfile, -f output filename; if omitted, generated from the manifest name
   --help        usage info
 `);
   process.exit(0);
+}
+
+if (!opts.outfile) {
+  console.error(`Parameter --outfile is required.`);
+  process.exit(1);
 }
 
 if (opts._.some((file) => !file.endsWith('.arcs'))) {
@@ -40,44 +55,63 @@ if (opts._.some((file) => !file.endsWith('.arcs'))) {
   process.exit(1);
 }
 
-/** Format the name of the output file. */
-function outputName(baseName: string): string {
-  return baseName.replace(/\.arcs$/, '.json');
-}
-
 /** Extract JSON serializations from manifest. */
-function toLiteral(manifest: Manifest): object {
-  const lit = {};
-  lit['particles'] = manifest.particles.map(p => p.toLiteral());
-  lit['schemas'] = Object.values(manifest.schemas).map(s => s.toLiteral());
+function toLiteral(manifest: Manifest): Serialization {
+  const lit = new Serialization();
+  lit.particles = manifest.particles.map(p => p.toLiteral());
+  lit.schemas = Object.values(manifest.schemas).map(s => s.toLiteral());
   return lit;
 }
 
-/** Write file serializations for each input file. */
-async function processFile(src: string) {
-  if (!fs.existsSync(src)) {
-    throw new Error(`File not found: ${src}`);
-  }
-
-  const outName = opts.outfile || outputName(path.basename(src));
-  const outPath = path.join(opts.outdir, outName);
+/** Write literals to a file. */
+function processFile(literals: Serialization) {
+  const outPath = path.join(opts.outdir, opts.outfile);
   console.log(outPath);
-
-  const manifest: Manifest = await Runtime.parseFile(src);
-  if (manifest.errors.length) {
-    return;
-  }
-
   const outFile = fs.openSync(outPath, 'w');
-  fs.writeSync(outFile, JSON.stringify(toLiteral(manifest)));
+  fs.writeSync(outFile, JSON.stringify(literals));
   fs.closeSync(outFile);
+}
+
+/** Parse manifests and aggregate literals into a single object. */
+async function aggregateLiterals(srcs: string[]): Promise<Serialization> {
+  const aggregate = new Serialization();
+  for (const src of srcs) {
+    if (!fs.existsSync(src)) {
+      throw new Error(`File not found: ${src}`);
+    }
+    const manifest: Manifest = await Runtime.parseFile(src);
+
+    manifest.errors
+      .filter(e => e.severity === ErrorSeverity.Warning)
+      .map(formatManifestErrors)
+      .forEach(console.warn);
+
+    const errMsgs = manifest.errors
+      .filter(e => e.severity === ErrorSeverity.Error)
+      .map(formatManifestErrors);
+
+    if (errMsgs.length) {
+      throw new Error(`Problems found in manifest '${src}':\n` +
+                      `${errMsgs.join('\n')}`);
+    }
+
+    aggregate.merge(toLiteral(manifest));
+  }
+  return aggregate;
+}
+
+/** Converts `ManifestError` into debug string */
+function formatManifestErrors(error: ManifestError): string {
+  const location = `${error.location.filename}:${error.location.start}:${error.location.end}`;
+  return `${error.severity} ${location} ${error.message}`;
 }
 
 async function main() {
   try {
     Runtime.init('../..');
     fs.mkdirSync(opts.outdir, {recursive: true});
-    opts._.forEach(await processFile);
+    const literals = await aggregateLiterals(opts._);
+    processFile(literals);
   } catch (e) {
     console.error(e);
     process.exit(1);
