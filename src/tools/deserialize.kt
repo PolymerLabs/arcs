caf30e22f1d422433b4552436de98805afa6bb14
Alex Rosengarten <alxrsngrtn@google.com>
2020-02-11 20:57:03 -0800

    WIP, messy deserialization

--- src/tools/deserialize.kt
+++ src/tools/deserialize.kt
@@ -1,6 +1,25 @@
 package arcs.tools
 
-data class Type (val tag: String)
+
+data class Type(
+    val tag: String,
+    val data: TypeData
+)
+
+sealed class TypeData
+
+data class NestedType(
+    val tag: String,
+    val data: Type
+) : TypeData()
+//data class Count(val tag: String) : TypeData()
+data class Entity(
+    val names: List<String>,
+    val fields: Map<String, SerializedSchemaField>,
+    val description: Description
+) : TypeData()
+//data class Singleton(val tag: String) : TypeData()
+
 
 data class Description(val pattern: String?)
 
@@ -8,7 +27,7 @@ data class SerializedHandleConnectionSpec(
     val direction: String,
     val relaxed: Boolean,
     val name: String,
-    // val type: Type
+    val type: TypeData,
     val isOptional: Boolean,
     val tags: List<String>?,
     val dependantConnections: List<SerializedHandleConnectionSpec>,
@@ -37,4 +56,48 @@ data class SerializedParticleSpec(
     val implBlobUrl: String?,
     val modality: List<String>,
     val slotConnections: List<SerializedSlotConnectionSpec>
+)
+
+data class SerializedSchemaLocation(
+    val offset: Integer,
+    val line: Integer,
+    val column: Integer
+)
+
+sealed class Field
+
+data class PrimitiveField(
+    val kind: String,
+    val type: String,
+    // val refinement: Refinement,
+    val location: Map<String, SerializedSchemaLocation>
+): Field()
+
+data class ReferenceField(
+   val kind: String,
+   val schema: Field
+): Field()
+
+data class InlineField(
+    val kind: String,
+    val model: Type
+): Field()
+
+data class SerializedSchemaField(
+    val kind: String,
+    val type: String,
+    // val refinement: Refinement
+    val location: Map<String, SerializedSchemaLocation>
+)
+
+data class SerializedSchema(
+    val names: List<String>,
+    val fields: Map<String, SerializedSchemaField>,
+    // val description: Description,
+    val refinement: Boolean
+)
+
+data class EntryPoint(
+    val particles: List<SerializedParticleSpec>,
+    val schemas: List<SerializedSchema>
 )
