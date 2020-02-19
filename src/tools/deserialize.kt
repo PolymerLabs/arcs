f6a34baef061c93330aa18e4f66fb661c25dc41e
Alex Rosengarten <alxrsngrtn@google.com>
2020-02-18 19:00:35 -0600

    Updated data classes used for deserialization

--- src/tools/deserialize.kt
+++ src/tools/deserialize.kt
@@ -12,92 +12,51 @@ data class NestedType(
     val tag: String,
     val data: Type
 ) : TypeData()
-//data class Count(val tag: String) : TypeData()
+
 data class Entity(
     val names: List<String>,
-    val fields: Map<String, SerializedSchemaField>,
+    val fields: Map<String, SchemaField>,
     val description: Description
+//  val refinement: Refinement
 ) : TypeData()
-//data class Singleton(val tag: String) : TypeData()
 
 
 data class Description(val pattern: String?)
 
-data class SerializedHandleConnectionSpec(
-    val direction: String,
-    val relaxed: Boolean,
+data class HandleConnectionSpec(
     val name: String,
     val type: TypeData,
+    val direction: String,
     val isOptional: Boolean,
-    val tags: List<String>?,
-    val dependantConnections: List<SerializedHandleConnectionSpec>,
-    val check: String?
+    val dependantConnections: List<HandleConnectionSpec>
 )
 
-data class SerializedSlotConnectionSpec(
-    val name: String,
-    val isRequired: Boolean?,
-    val isSet: Boolean?,
-    val tags: List<String>?,
-    val formFactor: String,
-    val handles: List<String>?,
-    val provideSlotConnections: List<SerializedSlotConnectionSpec>
-    // val check: Check?
+data class ParticleSpec(
+    val particleName: String,
+    val location: String,
+    val handles: List<HandleConnectionSpec>
 )
 
-data class SerializedParticleSpec(
-    val name: String,
-    val id: String?,
-    val verbs: List<String>,
-    val args: List<SerializedHandleConnectionSpec>,
-    // val description: Description,
-    val external: Boolean,
-    val implFile: String,
-    val implBlobUrl: String?,
-    val modality: List<String>,
-    val slotConnections: List<SerializedSlotConnectionSpec>
-)
-
-data class SerializedSchemaLocation(
+data class SchemaLocation(
     val offset: Integer,
     val line: Integer,
     val column: Integer
 )
 
-sealed class Field
-
-data class PrimitiveField(
+data class SchemaField(
     val kind: String,
-    val type: String,
-    // val refinement: Refinement,
-    val location: Map<String, SerializedSchemaLocation>
-): Field()
+    val type: String?,
+    val schema: SchemaModel?,
+    // val refinement: Refinement
+    val location: Map<String, SchemaLocation>?
+)
 
-data class ReferenceField(
-   val kind: String,
-   val schema: Field
-): Field()
-
-data class InlineField(
+data class SchemaModel(
     val kind: String,
     val model: Type
-): Field()
-
-data class SerializedSchemaField(
-    val kind: String,
-    val type: String,
-    // val refinement: Refinement
-    val location: Map<String, SerializedSchemaLocation>
 )
 
-data class SerializedSchema(
-    val names: List<String>,
-    val fields: Map<String, SerializedSchemaField>,
-    // val description: Description,
-    val refinement: Boolean
-)
-
-data class EntryPoint(
-    val particles: List<SerializedParticleSpec>,
-    val schemas: List<SerializedSchema>
+data class SerializedManifest(
+    val particles: List<ParticleSpec>,
+    val schemas: List<Entity>
 )
