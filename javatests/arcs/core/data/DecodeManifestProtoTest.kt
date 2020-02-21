f0475ba7b4c6e75aa1350b4879df4d2b14a0c73f
Cameron Silvestrini <csilvestrini@google.com>
2020-02-21 14:38:00 +1100

    Disable failing test

--- javatests/arcs/core/data/DecodeManifestProtoTest.kt
+++ javatests/arcs/core/data/DecodeManifestProtoTest.kt
@@ -2,6 +2,7 @@ package arcs.core.data
 
 import arcs.core.data.Manifest
 import com.google.common.truth.Truth.assertThat
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -14,7 +15,9 @@ class DecodeManifestProtoTest {
      * Validate that decoding of the proto encoded in JS works. Proto not final!
      */
     @Test
+    @Ignore // Broken internally.
     fun decodesEncodedManifest() {
+        // TODO: Hardcoding this path causes it to fail internally.
         val bytes = File("java/arcs/core/data/testdata/example.pb.bin").readBytes()
 
         val manifest = Manifest.parseFrom(bytes);
