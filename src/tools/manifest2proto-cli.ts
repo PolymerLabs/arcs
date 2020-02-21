0433ce4f25a09c64ac29bc6c85de8afb4e34f68a
Alex Rosengarten <alxrsngrtn@google.com>
2020-02-21 11:20:31 -0800

    lint

--- src/tools/manifest2proto-cli.ts
+++ src/tools/manifest2proto-cli.ts
@@ -1,8 +1,17 @@
+/**
+ * @license
+ * Copyright (c) 2020 Google Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
 import minimist from 'minimist';
 import fs from 'fs';
 import path from 'path';
-import {Runtime} from "../runtime/runtime.js";
-import {serialize2proto} from "./manifest2proto.js";
+import {Runtime} from '../runtime/runtime.js';
+import {serialize2proto} from './manifest2proto.js';
 
 const opts = minimist(process.argv.slice(2), {
   string: ['outdir', 'outfile'],
