7c498aed068a21957a8daf3f51aa316661422c6b
Alex Rosengarten <alxrsngrtn@google.com>
2020-02-21 15:35:58 -0800

    Got single test passing

--- javatests/arcs/core/tools/Proto2SchemaTest.kt
+++ javatests/arcs/core/tools/Proto2SchemaTest.kt
@@ -1,7 +1,11 @@
 package arcs.core.tools
 
-import arcs.core.data.*
-import arcs.sdk.wasm.Test
+import arcs.core.data.FieldType
+import arcs.core.data.Schema
+import arcs.core.data.SchemaFields
+import arcs.core.data.SchemaName
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
@@ -30,5 +34,23 @@ class Proto2SchemaTest {
 
         println(schemaProperty.toString())
         // TODO(alxr): Add asserts
+
+        assertThat(schemaProperty.toString()).isEqualTo("""val sliceSchema: arcs.core.data.Schema = arcs.core.data.Schema(
+    listOf(
+        arcs.core.data.SchemaName("Slice")
+    ),
+    arcs.core.data.SchemaFields(
+        singletons = mapOf(
+            "num" to arcs.core.data.FieldType.Number,
+            "flg" to arcs.core.data.FieldType.Boolean,
+            "txt" to arcs.core.data.FieldType.Text
+        ),
+        collections = mapOf(
+        )
+    ),
+    "f4907f97574693c81b5d62eb009d1f0f209000b8"
+    )
+
+""")
     }
 }
