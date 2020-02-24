1b049ffeb96d5f115213f26af51aaf773730f034
Ray Cromwell <cromwellian@gmail.com>
2020-02-24 01:20:28 -0800

    data class

--- java/arcs/core/data/CreateableStorageKey.kt
+++ java/arcs/core/data/CreateableStorageKey.kt
@@ -5,7 +5,7 @@ import arcs.core.storage.StorageKey
 /**
  * This class represents a storage key in a compiled [Plan] with 'create' fate.
  */
-class CreateableStorageKey(val nameFromManifest: String) : StorageKey("create") {
+data class CreateableStorageKey(val nameFromManifest: String) : StorageKey("create") {
     override fun toKeyString() = nameFromManifest
 
     override fun childKeyWithComponent(component: String): StorageKey {
