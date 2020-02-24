a4c6693e84e0614a1d57753e01a6e5a17f0dc2b2
Alex Rosengarten <alxrsngrtn@google.com>
2020-02-24 14:26:53 -0800

    Break down generation

--- java/arcs/core/tools/PlanGenerator.kt
+++ java/arcs/core/tools/PlanGenerator.kt
@@ -1,15 +1,15 @@
 package arcs.core.tools
 
+import arcs.core.data.ParticleProto
 import arcs.core.data.Plan
 import arcs.core.data.RecipeEnvelopeProto
 import arcs.core.data.RecipeProto
-import com.squareup.kotlinpoet.ClassName
-import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.*
 import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
-import com.squareup.kotlinpoet.PropertySpec
-import com.squareup.kotlinpoet.TypeSpec
 
 class PlanGenerator(private val fileBuilder: FileSpec.Builder) {
+
+    /** Generate code from a Recipe proto. */
     fun generate(recipeEnvelopeProto: RecipeEnvelopeProto) {
         fileBuilder.addType(
             generatePlan(recipeEnvelopeProto.recipe)
@@ -17,23 +17,26 @@ class PlanGenerator(private val fileBuilder: FileSpec.Builder) {
         )
     }
 
+    /** Generate a Plan class from a recipe. */
     private fun generatePlan(recipe: RecipeProto): TypeSpec.Builder {
         val particleSpecClass = ClassName("arcs.core.data.Plan", "Particle")
         val list = ClassName("kotlin.collections", "List")
         val listOfParticleSpecs = list.parameterizedBy(particleSpecClass)
 
         val particleSpecProperty = PropertySpec.builder("particles", listOfParticleSpecs)
-            .initializer("listOf()") // TODO(alxr): initialize with codeblock later
+            .initializer(generateParticles(recipe.particlesList).build())
             .build()
 
-        val planCompanionBuilder = TypeSpec.companionObjectBuilder()
+        val planBuilder = TypeSpec.objectBuilder("${recipe.name}Plan")
             .addProperty(particleSpecProperty)
-
-        val planBuilder = TypeSpec.classBuilder("${recipe.name}Plan")
-            .addType(planCompanionBuilder.build())
             .superclass(Plan::class)
             .addSuperclassConstructorParameter("%N", particleSpecProperty)
 
         return planBuilder
+    }
+
+    /** Generate a List of Particle Literals. */
+    private fun generateParticles(particles: List<ParticleProto>): CodeBlock.Builder {
+        return CodeBlock.builder().add("listOf()")
     }
 }
