53e38435f35851f7b154c702647f0fcce5c7ae1b
Piotr Swigon <piotr.swigon@gmail.com>
2020-02-25 12:28:28 -0800

    Add repoutils for repo-dependent code (#4726)

--- javatests/arcs/core/data/DecodeManifestProtoTest.kt
+++ javatests/arcs/core/data/DecodeManifestProtoTest.kt
@@ -1,12 +1,11 @@
 package arcs.core.data
 
-import arcs.core.data.RecipeEnvelopeProto
+import arcs.repoutils.runfilesDir
 import com.google.common.truth.Truth.assertThat
-import org.junit.Ignore
+import java.io.File
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import java.io.File
 
 @RunWith(JUnit4::class)
 class DecodeManifestProtoTest {
@@ -15,11 +14,9 @@ class DecodeManifestProtoTest {
      * Validate that decoding of the proto encoded in JS works.
      */
     @Test
-    @Ignore // Broken internally.
     fun decodesEncodedManifest() {
-        // TODO: Hardcoding this path causes it to fail internally.
-        val bytes = File("java/arcs/core/data/testdata/example.pb.bin").readBytes()
-        val recipe = RecipeEnvelopeProto.parseFrom(bytes).recipe
+        val path = runfilesDir() + "java/arcs/core/data/testdata/example.pb.bin"
+        val recipe = RecipeEnvelopeProto.parseFrom(File(path).readBytes()).recipe
         assertThat(recipe.name).isEqualTo("PassThrough")
         assertThat(recipe.particlesList.map {it.specName}).containsExactly("Reader", "Writer")
         assertThat(recipe.handlesList.map {it.name}).containsExactly("thing")
