ebf4c9bf61b3c967c405d09b61d7e27c1083ec31
Piotr Swigon <piotr.swigon@gmail.com>
2020-02-25 22:50:51 -0800

    Makes it possible to decode a proto from the text format

--- javatests/arcs/core/data/DecodeManifestProtoTest.kt
+++ javatests/arcs/core/data/DecodeManifestProtoTest.kt
@@ -2,6 +2,7 @@ package arcs.core.data
 
 import arcs.repoutils.runfilesDir
 import com.google.common.truth.Truth.assertThat
+import com.google.protobuf.TextFormat
 import java.io.File
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -10,15 +11,23 @@ import org.junit.runners.JUnit4
 @RunWith(JUnit4::class)
 class DecodeManifestProtoTest {
 
-    /**
-     * Validate that decoding of the proto encoded in JS works.
-     */
     @Test
-    fun decodesEncodedManifest() {
+    fun decodesManifestEncodedInTypeScript() {
         val path = runfilesDir() + "java/arcs/core/data/testdata/example.pb.bin"
         val recipe = RecipeEnvelopeProto.parseFrom(File(path).readBytes()).recipe
         assertThat(recipe.name).isEqualTo("PassThrough")
-        assertThat(recipe.particlesList.map {it.specName}).containsExactly("Reader", "Writer")
-        assertThat(recipe.handlesList.map {it.name}).containsExactly("thing")
+        assertThat(recipe.particlesList.map { it.specName }).containsExactly("Reader", "Writer")
+        assertThat(recipe.handlesList.map { it.name }).containsExactly("thing")
+    }
+
+    @Test
+    fun decodesManifestEncodedInTextFormat() {
+        val path = runfilesDir() + "java/arcs/core/data/testdata/example.textproto"
+        val builder = RecipeEnvelopeProto.newBuilder()
+        TextFormat.getParser().merge(File(path).readText(), builder)
+        val recipe = builder.build().recipe
+        assertThat(recipe.name).isEqualTo("PassThrough")
+        assertThat(recipe.particlesList.map { it.specName }).containsExactly("Reader", "Writer")
+        assertThat(recipe.handlesList.map { it.name }).containsExactly("thing")
     }
 }
