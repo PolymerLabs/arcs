9205067eeabbd98ab41faf5409a4338b328aa152
Gogul Balakrishnan <bgogul@google.com>
2020-02-27 17:12:59 -0800

    Add decoder for PrimitiveTypeProto.

---
+++ java/arcs/core/data/proto/TypeProtoDecoders.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ *
+ * Code distributed by Google as part of this project is also subject to an additional IP rights
+ * grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+package arcs.core.data.proto
+
+import arcs.core.data.PrimitiveType
+
+fun PrimitiveTypeProto.decode(): PrimitiveType =
+    when (this) {
+        PrimitiveTypeProto.TEXT -> PrimitiveType.Text
+        PrimitiveTypeProto.NUMBER -> PrimitiveType.Number
+        PrimitiveTypeProto.BOOLEAN -> PrimitiveType.Boolean
+        PrimitiveTypeProto.UNRECOGNIZED ->
+            throw IllegalArgumentException("Unknown PrimitiveTypeProto value.")
+    }
