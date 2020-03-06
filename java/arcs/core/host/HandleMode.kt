beb0c20ca181b1388fa22fa3e2f93ff4a1040a5d
Ray Cromwell <cromwellian@gmail.com>
2020-03-05 19:56:40 -0800

    Add temporary alias to prevent G3 build breakage

---
+++ java/arcs/core/host/HandleMode.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ *
+ * Code distributed by Google as part of this project is also subject to an additional IP rights
+ * grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+package arcs.core.host
+
+// TODO: Prevent G3 breakage, remove after internal dependencies refactored to new location.
+typealias HandleMode = arcs.core.data.HandleMode
+
