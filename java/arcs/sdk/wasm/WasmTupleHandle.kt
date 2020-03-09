419f815c67adb34613820032097c381a7b6c4c95
Sarah Heimlich <sarahheimlich@google.com>
2020-03-09 11:26:59 -0700

    Move to combineUpdate()

--- java/arcs/sdk/wasm/WasmTupleHandle.kt
+++ java/arcs/sdk/wasm/WasmTupleHandle.kt
@@ -11,6 +11,7 @@
 
 package arcs.sdk.wasm
 
+<<<<<<< HEAD
 fun <T, U> combineUpdates(
     handle1: WasmHandleEvents<T>,
     handle2: WasmHandleEvents<U>,
@@ -20,6 +21,16 @@ fun <T, U> combineUpdates(
     handles.forEach { handle ->
         handle.onUpdate {
             action(handle1.getContent(), handle2.getContent())
+=======
+fun<T, U> combineUpdates(
+    handle1: WasmHandleEvents<T>, 
+    handle2: WasmHandleEvents<U>,
+    action: (T, U) -> Unit) {
+        val handles = listOf(handle1, handle2)
+        handles.forEach { handle ->
+            handle.onUpdate {
+                action(handle1.getContent(), handle2.getContent())
+            }
+>>>>>>> Move to combineUpdate()
         }
     }
-}
