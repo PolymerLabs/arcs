8a6f2c96543aea603aa49e0feab24b2985bdfcf8
Alex Rosengarten <alxrsngrtn@google.com>
2020-03-10 17:01:27 -0700

    added Ttls to goldens

--- src/tools/tests/goldens/WriterReaderExample.kt
+++ src/tools/tests/goldens/WriterReaderExample.kt
@@ -20,7 +20,7 @@ object IngestionPlan : Plan(
                     StorageKeyParser.parse(""),
                     HandleMode.Write,
                     EntityType(Writer_Data_Spec.SCHEMA),
-                    null
+                    Ttl.Infinite
                 )
             )
         )
@@ -36,7 +36,7 @@ object ConsumptionPlan : Plan(
                     StorageKeyParser.parse("reference-mode://{db://25e71af4e9fc8b6958fc46a8f4b7cdf6b5f31516@arcs/Thing}{db://25e71af4e9fc8b6958fc46a8f4b7cdf6b5f31516@arcs/!:writingArcId/handle/my-handle-id}"),
                     HandleMode.Read,
                     EntityType(Reader_Data_Spec.SCHEMA),
-                    null
+                    Ttl.Infinite
                 )
             )
         )
