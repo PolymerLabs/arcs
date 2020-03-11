fb489bfb0274fc788570eb36c844558b9691a7ce
Maria Mandlis <mmandlis@chromium.org>
2020-03-11 16:00:31 -0700

    address comments

--- java/arcs/core/storage/ttl/RemovalManager.kt
+++ java/arcs/core/storage/ttl/RemovalManager.kt
@@ -12,6 +12,7 @@
 package arcs.core.storage.ttl
 
 import arcs.core.common.Referencable
+import arcs.core.data.RawEntity
 import arcs.core.storage.handle.SetHandle
 import arcs.core.storage.handle.SingletonHandle
 import arcs.core.util.Time
@@ -20,44 +21,45 @@ import arcs.core.util.Time
  * [RemovalManager] is a convenience for managing removal of entities or references from storage,
  * for example, removal of expired data.
  */
-open class RemovalManager(val time: Time) {
+abstract class RemovalManager(val time: Time) {
+
+    data class TimeRange(val startMillis: Long?, val endMillis: Long?) {
+        fun inRange(timeMillis: Long): Boolean {
+            return timeMillis != RawEntity.UNINITIALIZED_TIMESTAMP &&
+                (startMillis?.let { timeMillis > startMillis } ?: true) &&
+                (endMillis?.let { timeMillis < endMillis } ?: true)
+        }
+    }
 
     /** Removes all expired items from the given singleton handle. */
     suspend fun <T : Referencable> removeExpired(handle: SingletonHandle<T>) {
-        val data = handle.fetch()
-        if (data != null && data.expirationTimestamp > time.currentTimeMillis) {
-            handle.clear()
-        }
+        handle.fetch()?.takeIf { it.expirationTimestamp > time.currentTimeMillis }
+            ?.let { handle.clear() }
     }
 
     /** Removes all items newer than the given time from the given singleton handle. */
-    suspend fun <T : Referencable> removeNewerThan(handle: SingletonHandle<T>, timeMillis: Long) {
-        val data = handle.fetch()
-        if (data != null && data.creationTimestamp > timeMillis) {
-            handle.clear()
-        }
-    }
+    suspend fun <T : Referencable> removeCreatedBetween(
+        handle: SingletonHandle<T>,
+        timeRange: TimeRange
+    ) {
+        handle.fetch()?.takeIf { timeRange.inRange(it.creationTimestamp) }
+            ?.let { handle.clear() } }
 
     /** Removes all expired items from the given collection handle. */
     suspend fun <T : Referencable> removeExpired(handle: SetHandle<T>) {
-        val all = handle.fetchAll()
         val nowMillis = time.currentTimeMillis
-        for (data in all) {
-            if (data.expirationTimestamp > nowMillis) {
-                // TODO: implement batch removal in CollectionImpl.kt
-                handle.remove(data)
-            }
+        handle.fetchAll().forEach { data ->
+            takeIf { data.expirationTimestamp > nowMillis }?.apply { handle.remove(data) }
         }
     }
 
     /** Removes all items newer than the given time from the given collection handle. */
-    suspend fun <T : Referencable> removeNewerThan(handle: SetHandle<T>, timeMillis: Long) {
-        val all = handle.fetchAll()
-        for (data in all) {
-            if (data.creationTimestamp > timeMillis) {
-                // TODO: implement batch removal in CollectionImpl.kt
-                handle.remove(data)
-            }
+    suspend fun <T : Referencable> removeCreatedBetween(
+        handle: SetHandle<T>,
+        timeRange: TimeRange
+    ) {
+        handle.fetchAll().forEach { data ->
+            takeIf { timeRange.inRange(data.creationTimestamp) }?.apply { handle.remove(data) }
         }
     }
 }
