033bdaf4d63313c53e71e41c72e3ec1f29d4eca2
Gogul Balakrishnan <bgogul@google.com>
2020-03-13 00:00:27 -0700

    Convert [ParticleProto] to [Plan.Particle].

---
+++ java/arcs/core/data/proto/ParticleProtoDecoder.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ *
+ * Code distributed by Google as part of this project is also subject to an additional IP rights
+ * grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+package arcs.core.data.proto
+
+import arcs.core.data.HandleConnectionSpec.Direction
+import arcs.core.data.HandleMode
+import arcs.core.data.ParticleSpec
+import arcs.core.data.Plan.HandleConnection
+import arcs.core.data.Plan.Particle
+
+/** Class contains information that is required when decoding particle protos. */
+class DecodingContext(
+    var particleSpecs: Map<String, ParticleSpec>,
+    var handleProtos: Map<String, HandleProto>
+)
+
+/** Converts a [HandleConnectionProto] into a [HandleConnection]. */
+fun HandleConnectionProto.decode(
+    particleSpec: ParticleSpec,
+    context: DecodingContext
+): HandleConnection {
+    val handleSpec = particleSpec.connections[name]
+    require(handleSpec != null) {
+        "HandleConnectionSpec '$name' not found in ParticleSpec '${particleSpec.name}'."
+    }
+    val handleProto = context.handleProtos[handle]
+    require(handleProto != null) {
+        "HandleProto for '$handle' not found when decoding ParticleProto '${particleSpec.name}'."
+    }
+    val handleMode = when (handleSpec.direction) {
+        Direction.READS -> HandleMode.Read
+        Direction.WRITES -> HandleMode.Write
+        Direction.READS_WRITES -> HandleMode.ReadWrite
+    }
+    return handleProto.decodeAsHandleConnection(handleMode)
+}
+
+/** Converts a [ParticleProto] into a [Particle]. */
+fun ParticleProto.decode(context: DecodingContext): Particle {
+    val particleSpec = context.particleSpecs[specName]
+    require(particleSpec != null) {
+        "ParticleSpec '$specName' not found in decoding context."
+    }
+    val handleConnections = connectionsList.map {
+        it.handle to it.decode(particleSpec, context)
+    }.toMap()
+    return Particle(specName, particleSpec.location, handleConnections)
+}
---
+++ javatests/arcs/core/data/proto/ParticleProtoDecoderTest.kt
@@ -0,0 +1,126 @@
+package arcs.core.data.proto
+
+import arcs.core.data.*
+import arcs.core.storage.StorageKeyParser
+import arcs.core.storage.keys.RamDiskStorageKey
+import arcs.core.testutil.assertThrows
+import arcs.core.testutil.fail
+import arcs.core.util.Result
+import arcs.repoutils.runfilesDir
+import com.google.common.truth.Truth.assertThat
+import com.google.protobuf.Message.Builder
+import com.google.protobuf.Message
+import com.google.protobuf.TextFormat
+import java.io.File
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+typealias Direction = HandleConnectionSpec.Direction
+
+@RunWith(JUnit4::class)
+class ParticleProtoDecoderTest {
+    @Before
+    fun setupTest() {
+        RamDiskStorageKey.registerParser()
+    }
+
+    @Test
+    fun handleConnnectionPicksModeFromSpec() {
+        var handleConnectionProto = HandleConnectionProto
+            .newBuilder()
+            .setName("data")
+            .setHandle("thing")
+            .build()
+        // When decoded as part of readerSpec, the mode is [HandleMode.Read].
+        val readConnection = handleConnectionProto.decode(readerSpec, context)
+        assertThat(readConnection.mode).isEqualTo(HandleMode.Read)
+        assertThat(readConnection.type).isEqualTo(TypeVariable("thing"))
+        assertThat(readConnection.storageKey).isEqualTo(StorageKeyParser.parse(ramdiskStorageKey))
+        // When decoded as part of writerSpec, the mode is [HandleMode.Write].
+        val writeConnection = handleConnectionProto.decode(writerSpec, context)
+        assertThat(writeConnection.mode).isEqualTo(HandleMode.Write)
+        assertThat(writeConnection.type).isEqualTo(TypeVariable("thing"))
+        assertThat(writeConnection.storageKey).isEqualTo(StorageKeyParser.parse(ramdiskStorageKey))
+    }
+
+    @Test
+    fun decodeHandleConnectionDetectsMissingConnectionSpec() {
+        var proto = HandleConnectionProto
+            .newBuilder()
+            .setName("unknown")
+            .setHandle("thing")
+            .build()
+        val exception = assertThrows(IllegalArgumentException::class) {
+            proto.decode(readerSpec, context)
+        }
+        assertThat(exception)
+            .hasMessageThat()
+            .contains("HandleConnectionSpec 'unknown' not found in ParticleSpec 'Reader'")
+    }
+
+    @Test
+    fun decodeHandleConnectionDetectsMissingHandleProto() {
+        val proto = HandleConnectionProto
+            .newBuilder()
+            .setName("data")
+            .setHandle("unknown")
+            .build()
+        val exception = assertThrows(IllegalArgumentException::class) {
+            proto.decode(writerSpec, context)
+        }
+        assertThat(exception)
+            .hasMessageThat()
+            .contains("HandleProto for 'unknown' not found when decoding ParticleProto 'Writer'")
+    }
+
+    @Test
+    fun decodesParticleProto() {
+        val readConnectionProto = HandleConnectionProto
+            .newBuilder()
+            .setName("data")
+            .setHandle("thing")
+            .build()
+        val particleProto = ParticleProto
+            .newBuilder()
+            .setSpecName("Reader")
+            .addConnections(readConnectionProto)
+            .build()
+        val particle = particleProto.decode(context)
+        val readConnection = readConnectionProto.decode(readerSpec, context)
+        assertThat(particle.particleName).isEqualTo("Reader")
+        assertThat(particle.location).isEqualTo(readerSpec.location)
+        assertThat(particle.handles).isEqualTo(mapOf("thing" to readConnection))
+    }
+
+    @Test
+    fun decodeParticleDetectsMissingSpecs() {
+        val particleProto = ParticleProto
+            .newBuilder()
+            .setSpecName("NonExistent")
+            .build()
+        val exception = assertThrows(IllegalArgumentException::class) {
+            particleProto.decode(context)
+        }
+        assertThat(exception).hasMessageThat().contains("ParticleSpec 'NonExistent' not found")
+    }
+
+    // The test environment.
+    var ramdiskStorageKey = "ramdisk://something"
+    val thingHandleProto = HandleProto
+        .newBuilder()
+        .setName("thing")
+        .setFate(HandleProto.Fate.CREATE)
+        .setStorageKey(ramdiskStorageKey)
+        .build()
+    val readConnectionSpec = HandleConnectionSpec("data", Direction.READS, TypeVariable("data"))
+    val readerSpec = ParticleSpec("Reader", mapOf("data" to readConnectionSpec), "ReaderLocation")
+    val writeConnectionSpec = HandleConnectionSpec("data", Direction.WRITES, TypeVariable("data"))
+    val writerSpec = ParticleSpec("Writer", mapOf("data" to writeConnectionSpec), "WriterLocation")
+    val context = DecodingContext(
+        particleSpecs = mapOf("Reader" to readerSpec, "Writer" to writerSpec),
+        handleProtos = mapOf("thing" to thingHandleProto)
+    )
+}
+
