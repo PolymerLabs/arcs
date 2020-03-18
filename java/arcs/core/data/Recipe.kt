2bfacb68f36e206cdab97dba358f9cbaa592a8a0
Gogul Balakrishnan <bgogul@google.com>
2020-03-18 15:30:43 -0700

    Convert [ParticleProto] to [Recipe.Particle]. (#4979)
    * Convert [ParticleProto] to [Recipe.Particle].
    
    * Introduce a [Recipe.Particle.HandleConnection] type.
    
    * Remove 'import arcs.core.data.*' and extraneous whitelines.
    
    * Addresssing readability comments.

--- java/arcs/core/data/Recipe.kt
+++ java/arcs/core/data/Recipe.kt
@@ -14,7 +14,23 @@ package arcs.core.data
 import arcs.core.type.Type
 
 /** Representation of recipe in an Arcs manifest. */
-data class Recipe(val name: String) {
+data class Recipe(
+    val name: String,
+    val handles: Map<String, Handle>,
+    val particles: List<Particle>
+) {
+    /** Representation of a particle in a recipe. */
+    data class Particle(
+        val spec: ParticleSpec,
+        val handleConnections: List<HandleConnection>
+    ) {
+        /** Representation of a handle connection in a particle. */
+        data class HandleConnection(
+            val spec: HandleConnectionSpec,
+            val handle: Handle
+        )
+    }
+
     /** Definition of a handle in a recipe. */
     data class Handle(
         val name: String,
