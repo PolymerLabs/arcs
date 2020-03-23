b72cad80aa3c6875b63952e61ba3773e605eb6bc
Gogul Balakrishnan <bgogul@google.com>
2020-03-23 00:20:25 -0700

    Attaching [TypeResolutionInfo] to the type inference.

--- java/arcs/core/analysis/TypeInference.kt
+++ java/arcs/core/analysis/TypeInference.kt
@@ -43,24 +43,65 @@ fun Particle.HandleConnection.getTypeConstraint(particleSpec: ParticleSpec) = Ty
     TypeConstraintNode.Handle(handle)
 )
 
+/** Returns the initial [TypeVariableInfo] for the equivalence class of a [HandleConnectionSpec]. */
+fun HandleConnectionSpec.getTypeResolutionInfo() = when(direction) {
+    HandleConnectionSpec.Direction.READS -> TypeResolutionInfo.ResolvedType(
+        canWriteSuperset = null, canReadSubset = type
+    )
+    HandleConnectionSpec.Direction.WRITES -> TypeResolutionInfo.ResolvedType(
+        canWriteSuperset = type, canReadSubset = null
+    )
+    HandleConnectionSpec.Direction.READS_WRITES -> TypeResolutionInfo.ResolvedType(type)
+}
+
 /* Pretty print the equivalence classes of the given recipe. */
 fun Recipe.prettyPrintTypeInferenceResults(
-    unionFind: UnionFind<TypeConstraintNode, Unit>,
+    unionFind: UnionFind<TypeConstraintNode, TypeResolutionInfo>,
     message: String
 ) {
     print("$message\n")
     print("Handles:\n")
     handles.values.forEach {
         val node = TypeConstraintNode.Handle(it)
-        print("  $node ~ ${unionFind.find(node)}\n")
+        print("  $node ~ ${unionFind.find(node)} [${unionFind.getInfo(node)}]\n")
     }
     print("ConnectionSpecs:\n")
     particles.forEach { particle ->
         particle.handleConnections.forEach {
             val node = TypeConstraintNode.HandleConnection(particle.spec, it.spec)
-            print("  $node ~ ${unionFind.find(node)}\n")
+            print("  $node ~ ${unionFind.find(node)} [${unionFind.getInfo(node)}]\n")
         }
     }
+}
+
+fun unification(destInfo: TypeResolutionInfo?, srcInfo: TypeResolutionInfo?): TypeResolutionInfo? {
+    if (srcInfo == null) {
+        return destInfo
+    }
+    if (destInfo == null) {
+        return srcInfo
+    }
+
+    // if (destInfo is TypeResolution.TypeError || srcInfo is TypeResolutionInfo.TypeError) {
+    //     return TypeResolution.TypeError(
+    // }
+    // return when (destInfo) {
+    //     is TypeResolutionInfo.ResolvedType -> when (srcInfo) {
+    //         is TypeResolutionInfo.ResolvedType -> destInfo
+    //         is TypeResolutionInfo.TypeError -> srcInfo
+    //     }
+    //     is TypeResolutionInfo.TypeError -> when (srcInfo) {
+    //         is TypeResolutionInfo.ResolvedType -> destInfo
+    //         is TypeResolutionInfo.TypeError ->
+    //             TypeResolutionInfo.TypeError("${destInfo.reason}\n${srcInfo.reason}")
+    //     }
+    // }
+
+    // return TypeResolutionInfo.ResolvedType(
+    //     canWriteSuperset = destInfo.canWriteSuperset?.union(srcInfo.canWriteSuperset),
+    //     canReadSubset = destInfo.canReadSubset
+    // )
+    return TypeResolutionInfo.TypeError("Blah")
 }
 
 /** Runs type inference on the given recipe and updates the types of relevant entities in recipe.
@@ -69,7 +110,19 @@ fun Recipe.prettyPrintTypeInferenceResults(
  * are updated.
  */
 fun runTypeInference(recipe: Recipe) {
-    var unionFind = UnionFind<TypeConstraintNode, Unit>()
+    var unionFind = UnionFind<TypeConstraintNode, TypeResolutionInfo>()
+    // Make sets with default TypeResolutionInfo..
+    recipe.handles.values.forEach {
+        val node = TypeConstraintNode.Handle(it)
+        unionFind.makeSet(node, TypeResolutionInfo.ResolvedType(it.type))
+    }
+    recipe.particles.forEach { particle ->
+        particle.handleConnections.forEach {
+            val node = TypeConstraintNode.HandleConnection(particle.spec, it.spec)
+            unionFind.makeSet(node, it.spec.getTypeResolutionInfo())
+        }
+    }
+    recipe.prettyPrintTypeInferenceResults(unionFind, "Before Unification:")
     // Extract the unification constraints.
     //
     // TODO(bgogul): Take care of unification due to type variable names. Note that the automatic
@@ -81,7 +134,7 @@ fun runTypeInference(recipe: Recipe) {
         particle.handleConnections.map { it.getTypeConstraint(particle.spec) }
     }
     // Process the unification constraints.
-    constraints.forEach { (a, b): TypeConstraint -> unionFind.union(a, b) }
+    constraints.forEach { (a, b): TypeConstraint -> unionFind.union(a, b, {a, b -> unification(a, b)}) }
 
     // Display the results for now.
     recipe.prettyPrintTypeInferenceResults(unionFind, "Results of type inference:")
