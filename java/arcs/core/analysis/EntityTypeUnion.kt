5ff4a368e420444ed3b203381531cea6d18ef201
Gogul Balakrishnan <bgogul@google.com>
2020-03-24 00:50:13 -0700

    Add union for [EntityType].

---
+++ java/arcs/core/analysis/EntityTypeUnion.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ *
+ * Code distributed by Google as part of this project is also subject to an additional IP rights
+ * grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+package arcs.core.analysis
+
+import arcs.core.data.EntityType
+import arcs.core.data.FieldName
+import arcs.core.data.FieldType
+import arcs.core.data.Schema
+import arcs.core.data.SchemaFields
+
+/**
+ * Computes the union of the two [Schema] instances. Returns [Outcome.Failure] if the union
+ * is not possible as the inputs are incompatible.
+ */
+fun Schema.union(other: Schema): Outcome<Schema> {
+    val newNames = names.union(other.names)
+    val newFields = fields.union(other.fields).getOrElse { return Outcome.Failure(it) }
+    // TODO(bgogul): hash, refinement, query
+    return Schema(names = newNames, fields = newFields, hash = "").toSuccess()
+}
+
+fun EntityType.union(other: EntityType): Outcome<EntityType> {
+    val newSchema = entitySchema.union(other.entitySchema)
+        .getOrElse { return Outcome.Failure(it) }
+    return EntityType(newSchema).toSuccess()
+}
+
+/**
+ * Returns the result of combining two different field maps.
+ *
+ * If the maps have common [FieldName] entries, the union succeeds if and only if the corresponding
+ * [FieldType] values are the same.
+ */
+private fun Map<FieldName, FieldType>.unionFields(
+    other: Map<FieldName, FieldType>
+): Outcome<Map<FieldName, FieldType>> {
+    val result = mutableMapOf<FieldName, FieldType>()
+    result.putAll(this)
+    other.forEach { entry ->
+        val existing = this.get(entry.key)
+        if (existing != null && entry.value != existing) {
+            return Outcome.Failure(
+                "Incompatible types for field '${entry.key}': ${entry.value} vs. $existing."
+            )
+        }
+        result.put(entry.key, entry.value)
+    }
+    return result.toSuccess()
+}
+
+/**
+ * Computes the union of [SchemaField] instances. Returns [Outcome.Failure] if the union
+ * results in any incompatibility. e.g., incompatible [FieldType] with the same name.
+ */
+private fun SchemaFields.union(other: SchemaFields): Outcome<SchemaFields> {
+    val newSingletons = singletons.unionFields(other.singletons)
+        .getOrElse { return Outcome.Failure(it) }
+    val newCollections = singletons.unionFields(other.singletons)
+        .getOrElse { return Outcome.Failure(it) }
+    return SchemaFields(singletons = newSingletons, collections = newCollections).toSuccess()
+}
---
+++ javatests/arcs/core/analysis/EntityTypeUnionTest.kt
@@ -0,0 +1,100 @@
+package arcs.core.analysis
+
+import arcs.core.data.EntityType
+import arcs.core.data.FieldType
+import arcs.core.data.Schema
+import arcs.core.data.SchemaFields
+import arcs.core.data.SchemaName
+import com.google.common.truth.Truth.assertThat
+import org.junit.Assert.assertTrue
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class EntityTypeUnionTest {
+    // TODO(bgogul): hash for all Schema instances.
+
+    @Test
+    fun schemaUnionMergesNames() {
+        val emptySchemaFields = SchemaFields(emptyMap(), emptyMap())
+        val thingSchema = Schema(
+            listOf(SchemaName("Thing"), SchemaName("Another")),
+            emptySchemaFields,
+            ""
+        )
+        val objectSchema = Schema(
+            listOf(SchemaName("Object"), SchemaName("Thing")),
+            emptySchemaFields,
+            ""
+        )
+        val thingObjectSchema = thingSchema.union(objectSchema).getOrNull()
+        assertThat(thingObjectSchema).isNotNull()
+        thingObjectSchema?.let {
+            assertThat(it.names).containsExactly(
+                SchemaName("Thing"),
+                SchemaName("Object"),
+                SchemaName("Another")
+            )
+        }
+    }
+
+    @Test
+    fun schemaUnionMergesSingletons() {
+        val textField = SchemaFields(
+            mapOf("text" to FieldType.Text),
+            emptyMap()
+        )
+        val numberField = SchemaFields(
+            mapOf("number" to FieldType.Number),
+            emptyMap()
+        )
+        val textSchema = Schema(listOf(SchemaName("Example")), textField, "")
+        val numberSchema = Schema(listOf(SchemaName("Example")), numberField, "")
+        val result = textSchema.union(numberSchema).getOrNull()
+        result?.let {
+            assertThat(it.names).containsExactly(SchemaName("Example"))
+            assertThat(it.fields.singletons).isEqualTo(
+                mapOf("text" to FieldType.Text, "number" to FieldType.Number)
+            )
+        }
+    }
+
+    @Test
+    fun schemaUnionDetectsIncompatibleFieldTypes() {
+        val textField = SchemaFields(
+            mapOf("num_text" to FieldType.Text),
+            emptyMap()
+        )
+        val numberField = SchemaFields(
+            mapOf("num_text" to FieldType.Number),
+            emptyMap()
+        )
+        val textSchema = Schema(listOf(SchemaName("Example")), textField, "")
+        val numberSchema = Schema(listOf(SchemaName("Example")), numberField, "")
+        with(textSchema.union(numberSchema)) {
+            assertThat(getFailureReason()).contains("Incompatible types for field 'num_text'")
+        }
+    }
+
+    @Test
+    fun entityTypeUnionComputesUnionOfSchemas() {
+        val textField = SchemaFields(
+            mapOf("text" to FieldType.Text),
+            emptyMap()
+        )
+        val numberField = SchemaFields(
+            mapOf("number" to FieldType.Number),
+            emptyMap()
+        )
+        val textSchema = Schema(listOf(SchemaName("Example")), textField, "")
+        val numberSchema = Schema(listOf(SchemaName("Example")), numberField, "")
+        val textEntity = EntityType(textSchema)
+        val numberEntity = EntityType(numberSchema)
+        val result = textEntity.union(numberEntity).getOrNull()
+        result?.let {
+            assertThat(it.entitySchema).isEqualTo(numberSchema.union(textSchema).getOrNull())
+        }
+    }
+}
