7576729a8832df81bf1b6fb780aefe174f8b8634
Ray Cromwell <cromwellian@gmail.com>
2020-04-06 16:31:42 -0700

    rebase

--- javatests/arcs/core/host/TestingJvmProdHost.kt
+++ javatests/arcs/core/host/TestingJvmProdHost.kt
@@ -1,29 +1,7 @@
 package arcs.core.host
 
-import arcs.core.data.Plan
-import arcs.core.util.Time
-import arcs.jvm.host.AnnotationBasedJvmProdHost
-import arcs.jvm.util.testutil.FakeTime
-
+import arcs.jvm.host.scanForParticles
 
 open class TestingJvmProdHost(vararg particles: ParticleRegistration) :
-    AnnotationBasedJvmProdHost(TestingJvmProdHost::class, *particles) {
-
-    fun arcHostContext(arcId: String) = getArcHostContext(arcId)
-
-    var started = mutableListOf<Plan.Partition>()
-
-    override suspend fun startArc(partition: Plan.Partition) {
-        super.startArc(partition)
-        started.add(partition)
-    }
-
-    val isIdle = isArcHostIdle
-    
-    override val platformTime: Time = FakeTime()
-
-    fun setup() {
-        started.clear()
-        clearCache()
-    }
-}
+    TestingHost(*scanForParticles(TestingJvmProdHost::class), *particles),
+    ProdHost
