9faae32955f03a855613f88531918a91f4e87a8d
Ray Cromwell <cromwellian@gmail.com>
2020-04-09 03:19:41 -0700

    tidy imports

--- java/arcs/android/sdk/host/ArcHostHelper.kt
+++ java/arcs/android/sdk/host/ArcHostHelper.kt
@@ -33,7 +33,6 @@ import kotlin.reflect.KClass
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
