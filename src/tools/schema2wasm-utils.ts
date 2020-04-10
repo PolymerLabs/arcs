edda0b9311dae16b2c7fa153c15964b2cd4cc4e8
Sarah Heimlich <sarahheimlich@google.com>
2020-04-10 16:20:15 -0700

    Add Handle class names as keywords.

--- src/tools/schema2wasm-utils.ts
+++ src/tools/schema2wasm-utils.ts
@@ -11,12 +11,13 @@
 // https://kotlinlang.org/docs/reference/keyword-reference.html
 // [...document.getElementsByTagName('code')].map(x => x.innerHTML);
 // Includes reserve words for Entity Interface
-const kotlinKeywords = [
+export const kotlinKeywords = [
     'as', 'as?', 'break', 'class', 'continue', 'do', 'else', 'false', 'for', 'fun', 'if', 'in', '!in', 'interface', 'is',
     '!is', 'null', 'object', 'package', 'return', 'super', 'this', 'throw', 'true', 'try', 'typealias', 'val', 'var',
     'when', 'while', 'by', 'catch', 'constructor', 'delegate', 'dynamic', 'field', 'file', 'finally', 'get', 'import',
     'init', 'param', 'property', 'receiver', 'set', 'setparam', 'where', 'actual', 'abstract', 'annotation', 'companion',
     'const', 'crossinline', 'data', 'enum', 'expect', 'external', 'final', 'infix', 'inline', 'inner', 'internal',
     'lateinit', 'noinline', 'open', 'operator', 'out', 'override', 'private', 'protected', 'public', 'reified', 'sealed',
-    'suspend', 'tailrec', 'vararg', 'it', 'entityId'
+    'suspend', 'tailrec', 'vararg', 'it', 'entityId', 'ReadCollectionHandle', 'WriteCollectionHandle',
+    'ReadWriteCollectionHandle', 'ReadSingletonHandle', 'WriteSingletonHandle', 'ReadWriteSingletonHandle'
   ];
