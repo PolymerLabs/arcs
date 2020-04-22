7fc647c66fba232c2c9ef4b4b61594fec82d34f6
Cameron Silvestrini <csilvestrini@users.noreply.github.com>
2020-04-22 13:57:29 +1000

    Convert ParcelableCrdtEntity to protos (#5175)
    * Convert ParcelableCrdtEntity to protos
    
    * Review

--- java/arcs/android/crdt/CrdtEntityReferenceProto.kt
+++ java/arcs/android/crdt/CrdtEntityReferenceProto.kt
@@ -7,8 +7,8 @@ import arcs.core.crdt.CrdtEntity
 /** Constructs a [CrdtEntity.ReferenceImpl] from the given [CrdtEntityReferenceProto]. */
 fun CrdtEntityReferenceProto.toCrdtEntityReference() = CrdtEntity.ReferenceImpl(id)
 
-/** Serializes a [CrdtEntity.ReferenceImpl] to its proto form. */
-fun CrdtEntity.ReferenceImpl.toProto() = CrdtEntityReferenceProto.newBuilder().setId(id).build()
+/** Serializes a [CrdtEntity.Reference] to its proto form. */
+fun CrdtEntity.Reference.toProto() = CrdtEntityReferenceProto.newBuilder().setId(id).build()
 
 /** Reads a [CrdtEntity.ReferenceImpl] out of a [Parcel]. */
 fun Parcel.readCrdtEntityReference(): CrdtEntity.ReferenceImpl? =
