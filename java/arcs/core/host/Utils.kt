ce932a1bef0f9d0bc0a0437adf2e111111158c11
Alex Rosengarten <alxr@google.com>
2020-04-29 10:55:21 -0700

    missing `return` stmt

--- java/arcs/core/host/Utils.kt
+++ java/arcs/core/host/Utils.kt
@@ -38,7 +38,7 @@ fun KClass<*>.className(): String {
 /** Returns a pair mapping [ParticleIdentifier] to [ParticleConstructor] */
 inline fun <reified T : Particle> (() -> T).toRegistration(): ParticleRegistration {
     val construct: suspend (Plan.Particle?) -> T = { _ -> this.invoke() }
-    T::class.toParticleIdentifier() to construct
+    return T::class.toParticleIdentifier() to construct
 }
 
 /** Returns a pair mapping [ParticleIdentifier] to [ParticleConstructor] */
