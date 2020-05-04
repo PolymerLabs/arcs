09d67b995e4ed3bc4cee342d9f0218a3f95f6edd
Yuan Gu <yuangu@google.com>
2020-05-04 14:05:56 -0700

    Copyrights.

--- javatests/arcs/android/systemhealth/testapp/TestActivity.kt
+++ javatests/arcs/android/systemhealth/testapp/TestActivity.kt
@@ -1,5 +1,15 @@
-package arcs.android.systemhealth.testapp
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ *
+ * Code distributed by Google as part of this project is also subject to an additional IP rights
+ * grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
 
+package arcs.android.systemhealth.testapp
 
 import android.content.BroadcastReceiver
 import android.content.Context
