0e93819f9280af2c28771fe435e5b01f150e22d0
jblebrun <jibbl@google.com>
2020-05-06 10:57:45 -0700

    Add some tests that exercise patterns used by us (#5236)
    There are a few goals for these tests:
    
    * Give an example of how currently access particles for ingress/egress
    * Give an example of how we are managing datatype conversion
    * Highlight an issue with schema hashes
    * Show an issue with resolving references nested more than 1 level

---
+++ javatests/arcs/schematests/Items.kt
@@ -0,0 +1,16 @@
+package arcs.schematests
+
+data class Level0(
+    val name: String
+)
+
+data class Level1(
+    val name: String,
+    val children: Set<Level0>
+)
+
+data class Level2(
+    val name: String,
+    val children: Set<Level1>
+)
+
