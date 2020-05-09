9d7f0ad04ed325b971e2586507f031e287cb308d
Gogul Balakrishnan <bgogul@google.com>
2020-05-09 01:04:35 -0700

    Split the classes in `ClaimsAndChecks.kt` off to respective files.

---
+++ java/arcs/core/data/Check.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ *
+ * Code distributed by Google as part of this project is also subject to an additional IP rights
+ * grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+package arcs.core.data
+
+import arcs.core.data.InformationFlowLabel.Predicate
+
+/** Describes a check in a trusted particle. */
+sealed class Check {
+    /** A check to specify that the labels on [accessPath] satisfy [predicate]. */
+    data class Assert(val accessPath: AccessPath, val predicate: Predicate) : Check()
+}
--- javatests/arcs/core/data/InformationFlowLabelTest.kt
+++ javatests/arcs/core/data/InformationFlowLabelTest.kt
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
-class ClaimsAndChecksTest {
+class InformationFlowLabelTest {
     @Test
     fun prettyPrintSemanticTag() {
         assertThat("${SemanticTag("packageName")}").isEqualTo("packageName")
