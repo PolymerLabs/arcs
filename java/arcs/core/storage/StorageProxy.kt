be10c1c7077db13f5169631b9d0a3626931a9661
Ray Cromwell <cromwellian@gmail.com>
2020-05-14 17:11:50 -0700

    fix test

--- java/arcs/core/storage/StorageProxy.kt
+++ java/arcs/core/storage/StorageProxy.kt
@@ -159,7 +159,7 @@ class StorageProxy<Data : CrdtData, Op : CrdtOperationAtTime, T>(
      * being thrown.
      */
     fun close() {
-        scheduler.scope.launch(dispatcher) {
+        scheduler.scope.launch {
             _crdt = null
         }
         store.close()
