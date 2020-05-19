02d152588d194ffc54aaeb2926db5dc755ce02f0
Cameron Silvestrini <csilvestrini@users.noreply.github.com>
2020-05-20 09:17:14 +1000

    Fixes bug when unioning collection fields (code was using singletons). (#5308)
    * Fixes bug when unioning collection fields (code was using singletons).
    
    Also has a few extra tidy ups: reorders code, adds bug numbers to TODOs, renames file in anticipation of adding intersect functions.
    
    * Review

--- java/arcs/core/analysis/EntityTypeOperations.kt
+++ java/arcs/core/analysis/EntityTypeOperations.kt
@@ -17,6 +17,12 @@ import arcs.core.data.FieldType
 import arcs.core.data.Schema
 import arcs.core.data.SchemaFields
 
+/** Returns the union of two [EntityType] instances. */
+infix fun EntityType.union(other: EntityType): Outcome<EntityType> {
+    val newSchema = (entitySchema union other.entitySchema).getOrElse { return Outcome.Failure(it) }
+    return EntityType(newSchema).toSuccess()
+}
+
 /**
  * Computes the union of the two [Schema] instances. Returns [Outcome.Failure] if the union
  * is not possible as the inputs are incompatible.
@@ -24,14 +30,20 @@ import arcs.core.data.SchemaFields
 infix fun Schema.union(other: Schema): Outcome<Schema> {
     val newNames = names union other.names
     val newFields = (fields union other.fields).getOrElse { return Outcome.Failure(it) }
-    // TODO(bgogul): hash, refinement, query
+    // TODO(b/154235149): hash, refinement, query
     return Schema(names = newNames, fields = newFields, hash = "").toSuccess()
 }
 
-infix fun EntityType.union(other: EntityType): Outcome<EntityType> {
-    val newSchema = (entitySchema union other.entitySchema)
-        .getOrElse { return Outcome.Failure(it) }
-    return EntityType(newSchema).toSuccess()
+/**
+ * Computes the union of [SchemaFields] instances. Returns [Outcome.Failure] if the union
+ * results in any incompatibility. e.g., incompatible [FieldType] with the same name.
+ */
+private infix fun SchemaFields.union(other: SchemaFields): Outcome<SchemaFields> {
+    val newSingletons = (singletons union other.singletons).getOrElse { return Outcome.Failure(it) }
+    val newCollections = (collections union other.collections).getOrElse {
+        return Outcome.Failure(it)
+    }
+    return SchemaFields(singletons = newSingletons, collections = newCollections).toSuccess()
 }
 
 /**
@@ -40,32 +52,19 @@ infix fun EntityType.union(other: EntityType): Outcome<EntityType> {
  * If the maps have common [FieldName] entries, the union succeeds if and only if the corresponding
  * [FieldType] values are the same.
  */
-private fun Map<FieldName, FieldType>.unionFields(
+private infix fun Map<FieldName, FieldType>.union(
     other: Map<FieldName, FieldType>
 ): Outcome<Map<FieldName, FieldType>> {
     val result = mutableMapOf<FieldName, FieldType>()
     result.putAll(this)
     other.forEach { (name, type) ->
-        val existing = this.get(name)
+        val existing = this[name]
         if (existing != null && type != existing) {
-            // TODO(bgogul): `type != exisiting` check is not sufficient for non-primitive types.
             return Outcome.Failure(
                 "Incompatible types for field '$name': $type vs. $existing."
             )
         }
-        result.put(name, type)
+        result[name] = type
     }
     return result.toSuccess()
-}
-
-/**
- * Computes the union of [SchemaField] instances. Returns [Outcome.Failure] if the union
- * results in any incompatibility. e.g., incompatible [FieldType] with the same name.
- */
-private infix fun SchemaFields.union(other: SchemaFields): Outcome<SchemaFields> {
-    val newSingletons = singletons.unionFields(other.singletons)
-        .getOrElse { return Outcome.Failure(it) }
-    val newCollections = singletons.unionFields(other.singletons)
-        .getOrElse { return Outcome.Failure(it) }
-    return SchemaFields(singletons = newSingletons, collections = newCollections).toSuccess()
 }
