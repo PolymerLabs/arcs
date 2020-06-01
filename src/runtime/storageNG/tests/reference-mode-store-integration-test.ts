6f013e57860e676655c1d69558bd5f5e9b0e0d9b
shans <shanestephens@google.com>
2020-06-01 14:04:27 +1000

    support ordered list fields inside typeScript (#5398)

--- src/runtime/storageNG/tests/reference-mode-store-integration-test.ts
+++ src/runtime/storageNG/tests/reference-mode-store-integration-test.ts
@@ -14,9 +14,12 @@ import {DriverFactory} from '../drivers/driver-factory.js';
 import {Runtime} from '../../runtime.js';
 import {EntityType} from '../../type.js';
 import {ReferenceModeStorageKey} from '../reference-mode-storage-key.js';
-import {newHandle} from '../storage-ng.js';
+import {newHandle, handleForStore, newStore} from '../storage-ng.js';
 import {Schema} from '../../schema.js';
 import {Particle} from '../../particle.js';
+import {Exists} from '../drivers/driver.js';
+import {StorageProxy} from '../storage-proxy.js';
+import {CollectionHandle} from '../handle.js';
 
 describe('ReferenceModeStore Integration', async () => {
 
@@ -24,7 +27,7 @@ describe('ReferenceModeStore Integration', async () => {
     DriverFactory.clearRegistrationsForTesting();
   });
 
-  it('will store and retrieve entities through referenceModeStores', async () => {
+  it('will store and retrieve entities through referenceModeStores (separate stores)', async () => {
     const runtime = new Runtime();
     RamDiskStorageDriverProvider.register(runtime.getMemoryProvider());
     const storageKey = new ReferenceModeStorageKey(new RamDiskStorageKey('backing'), new RamDiskStorageKey('container'));
@@ -36,7 +39,6 @@ describe('ReferenceModeStore Integration', async () => {
     // are on top of different storage stacks.
     const writeHandle = await newHandle(type, storageKey, arc, {id: 'write-handle'});
     const readHandle = await newHandle(type, storageKey, arc, {id: 'read-handle'});
-
 
     readHandle.particle = new Particle();
     const returnPromise = new Promise((resolve, reject) => {
@@ -57,6 +59,154 @@ describe('ReferenceModeStore Integration', async () => {
     });
 
     await writeHandle.addFromData({foo: 'This is text in foo'});
+    return returnPromise;
+  });
+
+  it('will store and retrieve entities through referenceModeStores (shared stores)', async () => {
+    const runtime = new Runtime();
+    RamDiskStorageDriverProvider.register(runtime.getMemoryProvider());
+    const storageKey = new ReferenceModeStorageKey(new RamDiskStorageKey('backing'), new RamDiskStorageKey('container'));
+    const arc = Runtime.newForNodeTesting().newArc('testArc');
+
+    const type = new EntityType(new Schema(['AnEntity'], {foo: 'Text'})).collectionOf();
+
+    // Set up a common store and host both handles on top. This will result in one store but two different proxies.
+    const store = newStore(type, {storageKey, id: 'store', exists: Exists.MayExist});
+    const writeHandle = await handleForStore(store, arc);
+    const readHandle = await handleForStore(store, arc);
+
+    readHandle.particle = new Particle();
+    const returnPromise = new Promise((resolve, reject) => {
+
+      let state = 0;
+
+      readHandle.particle['onHandleUpdate'] = async (handle, update) => {
+        assert.equal(state, 1);
+        assert.equal(update.added.length, 1);
+        assert.equal(update.added[0].foo, 'This is text in foo');
+        resolve();
+      };
+
+      readHandle.particle['onHandleSync'] = async (handle, model) => {
+        assert.equal(state, 0);
+        assert.deepEqual(model, []);
+        state = 1;
+      };
+
+    });
+
+    await writeHandle.addFromData({foo: 'This is text in foo'});
+    return returnPromise;
+  });
+
+  it('will store and retrieve entities through referenceModeStores (shared proxies)', async () => {
+    const runtime = new Runtime();
+    RamDiskStorageDriverProvider.register(runtime.getMemoryProvider());
+    const storageKey = new ReferenceModeStorageKey(new RamDiskStorageKey('backing'), new RamDiskStorageKey('container'));
+    const arc = Runtime.newForNodeTesting().newArc('testArc');
+
+    const type = new EntityType(new Schema(['AnEntity'], {foo: 'Text'})).collectionOf();
+
+    // Set up a common store and host both handles on top. This will result in one store but two different proxies.
+    const store = newStore(type, {storageKey, id: 'store', exists: Exists.MayExist});
+    const activestore = await store.activate();
+    const proxy = new StorageProxy('proxy', activestore, type, storageKey.toString());
+    const writeHandle = new CollectionHandle('write-handle', proxy, arc.idGenerator, null, false, true, 'write-handle');
+    const particle = new Particle();
+    const readHandle = new CollectionHandle('read-handle', proxy, arc.idGenerator, particle, true, false, 'read-handle');
+
+    const returnPromise = new Promise((resolve, reject) => {
+
+      let state = 0;
+
+      readHandle.particle['onHandleUpdate'] = async (handle, update) => {
+        assert.equal(state, 1);
+        assert.equal(update.added.length, 1);
+        assert.equal(update.added[0].foo, 'This is text in foo');
+        resolve();
+      };
+
+      readHandle.particle['onHandleSync'] = async (handle, model) => {
+        assert.equal(state, 0);
+        assert.deepEqual(model, []);
+        state = 1;
+      };
+
+    });
+
+    await writeHandle.addFromData({foo: 'This is text in foo'});
+    return returnPromise;
+  });
+
+  it('will send an ordered list from one handle to another (separate store)', async () => {
+    const runtime = new Runtime();
+    RamDiskStorageDriverProvider.register(runtime.getMemoryProvider());
+    const storageKey = new ReferenceModeStorageKey(new RamDiskStorageKey('backing'), new RamDiskStorageKey('container'));
+    const arc = Runtime.newForNodeTesting().newArc('testArc');
+
+    const type = new EntityType(new Schema(['AnEntity'], {foo: {kind: 'schema-ordered-list', schema: {kind: 'schema-primitive', type: 'Text'}}})).collectionOf();
+
+    // Use newHandle here rather than setting up a store inside the arc, as this ensures writeHandle and readHandle
+    // are on top of different storage stacks.
+    const writeHandle = await newHandle(type, storageKey, arc, {id: 'write-handle'});
+    const readHandle = await newHandle(type, storageKey, arc, {id: 'read-handle'});
+
+    readHandle.particle = new Particle();
+    const returnPromise = new Promise((resolve, reject) => {
+
+      let state = 0;
+
+      readHandle.particle['onHandleSync'] = async (handle, model) => {
+        if (state === 0) {
+          assert.deepEqual(model, []);
+          state = 1;
+        } else {
+          assert.equal(model.length, 1);
+          assert.deepEqual(model[0].foo, ['This', 'is', 'text', 'in', 'foo']);
+          resolve();
+        }
+      };
+
+    });
+
+    await writeHandle.addFromData({foo: ['This', 'is', 'text', 'in', 'foo']});
+    return returnPromise;
+  });
+
+  it('will send an ordered list from one handle to another (shared store)', async () => {
+    const runtime = new Runtime();
+    RamDiskStorageDriverProvider.register(runtime.getMemoryProvider());
+    const storageKey = new ReferenceModeStorageKey(new RamDiskStorageKey('backing'), new RamDiskStorageKey('container'));
+    const arc = Runtime.newForNodeTesting().newArc('testArc');
+
+    const type = new EntityType(new Schema(['AnEntity'], {foo: {kind: 'schema-ordered-list', schema: {kind: 'schema-primitive', type: 'Text'}}})).collectionOf();
+
+    // Set up a common store and host both handles on top. This will result in one store but two different proxies.
+    const store = newStore(type, {storageKey, id: 'store', exists: Exists.MayExist});
+    const writeHandle = await handleForStore(store, arc);
+    const readHandle = await handleForStore(store, arc);
+
+    readHandle.particle = new Particle();
+    const returnPromise = new Promise((resolve, reject) => {
+
+      let state = 0;
+
+      readHandle.particle['onHandleUpdate'] = async (handle, update) => {
+        assert.equal(state, 1);
+        assert.equal(update.added.length, 1);
+        assert.deepEqual(update.added[0].foo, ['This', 'is', 'text', 'in', 'foo']);
+        resolve();
+      };
+
+      readHandle.particle['onHandleSync'] = async (handle, model) => {
+        assert.equal(state, 0);
+        assert.deepEqual(model, []);
+        state = 1;
+      };
+
+    });
+
+    await writeHandle.addFromData({foo: ['This', 'is', 'text', 'in', 'foo']});
     return returnPromise;
   });
 });
