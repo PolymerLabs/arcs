fc8e212203d5860b5f07732c6b2771348c4caedb
galganif <4754402+galganif@users.noreply.github.com>
2020-06-05 09:05:34 +1000

    register a periodic task to do garbage collection (#5435)
    * register a periodic task to do garbage collection
    
    * strict deps
    
    * review comments

---
+++ java/arcs/android/storage/database/DatabaseGarbageCollectionPeriodicTask.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ *
+ * Code distributed by Google as part of this project is also subject to an additional IP rights
+ * grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+package arcs.android.storage.database
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import arcs.core.util.TaggedLog
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Implementation of a [Worker] which performs periodic scan of storage and deletes unused data.
+ */
+class DatabaseGarbageCollectionPeriodicTask(
+    val appContext: Context,
+    workerParams: WorkerParameters
+) : Worker(appContext, workerParams) {
+
+    private val log = TaggedLog { WORKER_TAG }
+    init { log.debug { "Created." } }
+
+    override fun doWork(): Result = runBlocking(Dispatchers.IO) {
+        log.debug { "Running." }
+        val databaseManager = AndroidSqliteDatabaseManager(appContext)
+        databaseManager.runGarbageCollection().join()
+        databaseManager.close()
+        log.debug { "Success." }
+        // Indicate whether the task finished successfully with the Result
+        Result.success()
+    }
+
+    companion object {
+        /** Unique name of the worker, used to enqueue the periodic task in [WorkManager]. */
+        const val WORKER_TAG = "DatabaseGarbageCollectionPeriodicTask"
+    }
+}
