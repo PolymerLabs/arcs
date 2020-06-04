e19ab71396892da7479a45ffe294ae80daf33aa4
Cameron Silvestrini <csilvestrini@google.com>
2020-06-04 11:12:37 +1000

    Updated annotation docs

--- src/runtime/policy/canonical-policy-annotations.ts
+++ src/runtime/policy/canonical-policy-annotations.ts
@@ -24,12 +24,12 @@ annotation allowedRetention(medium: Text, encryption: Boolean)
   targets: [PolicyTarget]
   retention: Source
   allowMultiple: true
-  doc: 'Indicates the conditions under which the target data can be retained. Valid values for the medium are: Ram, Disk.'
+  doc: 'Indicates the conditions under which the target data can be retained. Valid values for the medium are: Ram, Disk. Encryption is not supported yet.'
 
 annotation maxAge(age: Text)
   targets: [PolicyTarget]
   retention: Source
-  doc: 'Indicates the maximum age the target data can be in order to be used. Age is a string like "2days".'
+  doc: 'Indicates the maximum age the target data can be in order to be used. Age is a string like "2 days". Supported units are minutes, hours, days.'
 
 // TODO(b/157961278): Support varargs or dictionaries, and then convert this to
 // be: @allowedUsages(raw: [join], someLabel: [egress]).
@@ -37,5 +37,5 @@ annotation allowedUsage(label: Text, usageType: Text)
   targets: [PolicyField]
   retention: Source
   allowMultiple: true
-  doc: 'Indicates the conditions under which a field can be used. Valid values for usageType are: egress, join, *.'
+  doc: 'Indicates the conditions under which a field can be used. Valid values for usageType are: egress, join, *. "raw" is a special value for label meaning that no redaction is required. The default if no @allowedUsage annotation is supplied is for all usages to be valid.'
 `;
