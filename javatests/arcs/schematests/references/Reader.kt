a2a02d1dc46ac070929b7c425ee2d3877b44661e
Piotr Swigon <piotr.swigon@gmail.com>
2020-06-08 16:49:19 -0700

    Uses schema name based entity names in schematests/references (#5456)

--- javatests/arcs/schematests/references/Reader.kt
+++ javatests/arcs/schematests/references/Reader.kt
@@ -11,7 +11,7 @@ class Reader0 : AbstractReader0() {
     private suspend fun initialize() = this.apply {
         handles.level0.awaitReady()
     }
-    private fun Reader0_Level0.fromArcs() = MyLevel0(name)
+    private fun Level0.fromArcs() = MyLevel0(name)
 
     suspend fun read(): List<MyLevel0> = withContext(handles.level0.dispatcher) {
         initialize()
@@ -24,14 +24,12 @@ class Reader1 : AbstractReader1() {
     private suspend fun initialize() = this.apply {
         handles.level1.awaitReady()
     }
-    private fun Reader1_Level1_Children.fromArcs() = MyLevel0(name)
+    private fun Level0.fromArcs() = MyLevel0(name)
 
-    private suspend fun Reader1_Level1.fromArcs(): MyLevel1 {
-        return MyLevel1(
-            name = name,
-            children = children.map { it.dereference()!!.fromArcs() }.toSet()
-        )
-    }
+    private suspend fun Level1.fromArcs() = MyLevel1(
+        name = name,
+        children = children.map { it.dereference()!!.fromArcs() }.toSet()
+    )
 
     suspend fun read(): List<MyLevel1> = withContext(handles.level1.dispatcher) {
         initialize()
@@ -44,21 +42,17 @@ class Reader2 : AbstractReader2() {
     private suspend fun initialize() = this.apply {
         handles.level2.awaitReady()
     }
-    private fun Reader2_Level2_Children_Children.fromArcs() = MyLevel0(name)
+    private fun Level0.fromArcs() = MyLevel0(name)
 
-    private suspend fun Reader2_Level2_Children.fromArcs(): MyLevel1 {
-        return MyLevel1(
-            name = name,
-            children = children.map { it.dereference()!!.fromArcs() }.toSet()
-        )
-    }
+    private suspend fun Level1.fromArcs() = MyLevel1(
+        name = name,
+        children = children.map { it.dereference()!!.fromArcs() }.toSet()
+    )
 
-    private suspend fun Reader2_Level2.fromArcs(): MyLevel2 {
-        return MyLevel2(
-            name = name,
-            children = children.map { it.dereference()!!.fromArcs() }.toSet()
-        )
-    }
+    private suspend fun Level2.fromArcs() = MyLevel2(
+        name = name,
+        children = children.map { it.dereference()!!.fromArcs() }.toSet()
+    )
 
     suspend fun read(): List<MyLevel2> = withContext(handles.level2.dispatcher) {
         initialize()
