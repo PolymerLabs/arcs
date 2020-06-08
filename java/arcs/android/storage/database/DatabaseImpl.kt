53eb4bf93017e8fe4aace2925121d7d8ac0c2b98
Jason Feinstein <jwf@google.com>
2020-06-08 10:37:33 -0700

    Fix droped transaction due to return target.

--- java/arcs/android/storage/database/DatabaseImpl.kt
+++ java/arcs/android/storage/database/DatabaseImpl.kt
@@ -244,7 +244,7 @@ class DatabaseImpl(
             dbSingletons.forEach { (fieldName, value) -> rawSingletons[fieldName] = value }
             dbCollections.forEach { (fieldName, value) -> rawCollections[fieldName] = value }
 
-            return DatabaseData.Entity(
+            return@forSingleResult DatabaseData.Entity(
                 RawEntity(
                     id = entityId,
                     singletons = rawSingletons,
