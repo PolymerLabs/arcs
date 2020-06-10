2c45a5961def54eb397117c38fd9e862bbf30a0b
Shane Stephens <shanestephens@google.com>
2020-06-10 16:59:18 +1000

    Complete proto story

--- java/arcs/android/crdt/ReferencablePrimitiveListProto.kt
+++ java/arcs/android/crdt/ReferencablePrimitiveListProto.kt
@@ -1,17 +1,51 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ *
+ * Code distributed by Google as part of this project is also subject to an additional IP rights
+ * grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
 package arcs.android.crdt
 
 import android.os.Parcel
 import arcs.android.util.readProto
+import arcs.core.common.Referencable
+import arcs.core.data.FieldType
+import arcs.core.data.PrimitiveType
 import arcs.core.data.util.ReferencableList
+import arcs.core.data.util.ReferencablePrimitive
 import arcs.core.data.util.toReferencable
 
 /** Constructs a [ReferencableList] from the given [ReferencablePrimitiveListProto]. */
-fun ReferencablePrimitiveListProto.toReferencableList(): ReferencableList<*> = 
-  valueList.mapTo(mutableListOf<Any>()) { it.toReferencablePrimitive().value!! }.toReferencable()
+fun ReferencablePrimitiveListProto.toReferencableList(): ReferencableList<Referencable> {
+    val fieldType = FieldType.Primitive(PrimitiveType.values()[type])
+    return valueList.mapTo(mutableListOf<ReferencablePrimitive<*>>()) {
+        it.toReferencablePrimitive()
+    }.toReferencable(fieldType)
+}
 
 /** Serializes a [ReferencablePrimitive] to its proto form. */
-fun ReferencableList<*>.toProto() = ReferencablePrimitiveListProto.newBuilder().addAllValue(value.map { it.toReferencable().toProto() }).build()
+fun ReferencableList<*>.toPrimitiveListProto(): ReferencablePrimitiveListProto {
+    val type = itemType
+    return when (type) {
+        is FieldType.Primitive -> ReferencablePrimitiveListProto
+            .newBuilder()
+            .setType(type.primitiveType.ordinal)
+            .addAllValue(value.map {
+                require(it is ReferencablePrimitive<*>) {
+                    "Non-primitive found in primitive list"
+                }
+                it.toProto()
+            })
+            .build()
+        else -> throw IllegalStateException("Invalid FieldType for primitive ReferencableList")
+    }
+}
 
 /** Reads a [ReferencablePrimitive] out of a [Parcel]. */
-fun Parcel.readOrderedListPrimitive(): ReferencableList<*>? =
+fun Parcel.readOrderedPrimitiveList(): ReferencableList<*>? =
     readProto(ReferencablePrimitiveListProto.getDefaultInstance())?.toReferencableList()
--- java/arcs/android/crdt/ReferencableProto.kt
+++ java/arcs/android/crdt/ReferencableProto.kt
@@ -4,6 +4,7 @@ import android.os.Parcel
 import arcs.android.util.readProto
 import arcs.core.common.Referencable
 import arcs.core.crdt.CrdtEntity
+import arcs.core.data.FieldType
 import arcs.core.data.RawEntity
 import arcs.core.data.util.ReferencableList
 import arcs.core.data.util.ReferencablePrimitive
@@ -18,6 +19,7 @@ fun ReferencableProto.toReferencable(): Referencable? = when (referencableCase) 
     ReferencableProto.ReferencableCase.REFERENCE -> reference.toReference()
     ReferencableProto.ReferencableCase.PRIMITIVE -> primitive.toReferencablePrimitive()
     ReferencableProto.ReferencableCase.PRIMITIVE_LIST -> primitiveList.toReferencableList()
+    ReferencableProto.ReferencableCase.REFERENCE_LIST -> referenceList.toReferencableList()
     else -> throw UnsupportedOperationException(
         "Unknown ReferencableProto type: $referencableCase."
     )
@@ -31,7 +33,11 @@ fun Referencable.toProto(): ReferencableProto {
         is CrdtEntity.ReferenceImpl -> proto.crdtEntityReference = toProto()
         is Reference -> proto.reference = toProto()
         is ReferencablePrimitive<*> -> proto.primitive = toProto()
-        is ReferencableList<*> -> proto.primitiveList = toProto()
+        is ReferencableList<*> -> when (this.itemType) {
+            is FieldType.Primitive -> proto.primitiveList = toPrimitiveListProto()
+            is FieldType.EntityRef -> proto.referenceList = toReferenceListProto()
+            else -> throw UnsupportedOperationException("Unsupported Referencable: $this.")
+        }
         else -> throw UnsupportedOperationException("Unsupported Referencable: $this.")
     }
     return proto.build()
