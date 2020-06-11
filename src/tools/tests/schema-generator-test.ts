fd65471cd15f7b6e3e57c1bd29e9c24a867d2070
Alex Rosengarten <alxr@google.com>
2020-06-11 14:05:35 -0700

    clearer verbage

--- src/tools/tests/schema-generator-test.ts
+++ src/tools/tests/schema-generator-test.ts
@@ -73,7 +73,7 @@ testData.forEach(data => {
 
     ${diffLog.join('\n    ')}
 
-    Run the following command to update the golden file:
+    Run the following command to update all the golden files:
 
     tools/update-goldens"
 `);
