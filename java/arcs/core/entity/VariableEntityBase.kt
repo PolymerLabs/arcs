82b3f0284e88920812b4de875137c35a499ac8c7
Alex Rosengarten <alxr@google.com>
2020-06-11 17:39:03 -0700

    Created VariableEntityBase to support Entities for Type Variables. (#5483)
    * VariableEntityBase from #5431
    
    * Impl feedback for Piotr
    
    * rm inline
    
    * Updated doc
    
    * Revert, now we have default values
    
    * updated doc
    
    * ctor overloading instead over mutliple layers of defaults
    
    * lint fix

---
+++ java/arcs/core/entity/VariableEntityBase.kt
@@ -0,0 +1,60 @@
+package arcs.core.entity
+
+import arcs.core.common.Referencable
+import arcs.core.data.FieldName
+import arcs.core.data.RawEntity
+import arcs.core.data.Schema
+
+/**
+ * A base [Entity] to access data from type variables.
+ *
+ * This class behaves just like [EntityBase], except for (de)serialization. During deserialization,
+ * all the fields from [RawEntity] are stored for later use in serialization.
+ *
+ * In this way, an entity representing a type variable will pass data through the system without
+ * a specific description of the data (an exact match with a [Schema]).
+ */
+open class VariableEntityBase : EntityBase {
+
+    constructor(entityClassName: String, schema: Schema) : super(entityClassName, schema)
+
+    constructor(
+        entityClassName: String,
+        schema: Schema,
+        entityId: String?
+    ) : super(entityClassName, schema, entityId)
+
+    constructor(
+        entityClassName: String,
+        schema: Schema,
+        entityId: String?,
+        creationTimestamp: Long,
+        expirationTimestamp: Long
+    ) : super(entityClassName, schema, entityId, creationTimestamp, expirationTimestamp)
+
+    private val rawSingletons = mutableMapOf<FieldName, Referencable?>()
+    private val rawCollections = mutableMapOf<FieldName, Set<Referencable>>()
+
+    override fun serialize(): RawEntity {
+        val rawEntity = super.serialize()
+        return rawEntity.copy(
+            singletons = rawSingletons + rawEntity.singletons,
+            collections = rawCollections + rawEntity.collections
+        )
+    }
+
+    override fun deserialize(
+        rawEntity: RawEntity,
+        nestedEntitySpecs: Map<SchemaHash, EntitySpec<out Entity>>
+    ) {
+        rawEntity.singletons
+            .filter { !hasSingletonField(it.key) }
+            .forEach { rawSingletons[it.key] = it.value }
+
+        rawEntity.collections
+            .filter { !hasCollectionField(it.key) }
+            .forEach { rawCollections[it.key] = it.value }
+
+        super.deserialize(rawEntity, nestedEntitySpecs)
+    }
+}
