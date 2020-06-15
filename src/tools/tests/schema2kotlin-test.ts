924f4d06b3ac6608f17157b83f4af39f46b813ee
Alex Rosengarten <alxr@google.com>
2020-06-15 15:34:42 -0700

    Breaking up schema2kt concepts into separate classes (#5511)

--- src/tools/tests/schema2kotlin-test.ts
+++ src/tools/tests/schema2kotlin-test.ts
@@ -9,9 +9,8 @@
  */
 import {assert} from '../../platform/chai-web.js';
 import {Manifest} from '../../runtime/manifest.js';
-import {KotlinGenerator, Schema2Kotlin} from '../schema2kotlin.js';
+import {Schema2Kotlin} from '../schema2kotlin.js';
 import {SchemaGraph} from '../schema2graph.js';
-import minimist from 'minimist';
 
 describe('schema2kotlin', () => {
   describe('Handle Interface Type', () => {
@@ -299,145 +298,6 @@ describe('schema2kotlin', () => {
       await assertComponent(manifest, ({typeAliases}) => typeAliases.sort(), expectedAliases);
     }
   });
-  describe('Entity Class Generation', () => {
-    it('generates entity with public constructor', async () => await assertClassDefinition(
-      `particle T
-         h1: reads Thing {num: Number}`,
-      `@Suppress("UNCHECKED_CAST")
-    class Thing(
-        num: Double = 0.0,
-        entityId: String? = null,
-        creationTimestamp: Long = RawEntity.UNINITIALIZED_TIMESTAMP,
-        expirationTimestamp: Long = RawEntity.UNINITIALIZED_TIMESTAMP
-    ) : EntityBase("Thing", SCHEMA, entityId, creationTimestamp, expirationTimestamp)`
-    ));
-    it('generates variable entity with private constructor', async () => await assertClassDefinition(
-      `particle T
-         h1: reads ~a with {num: Number}
-         `,
-      `@Suppress("UNCHECKED_CAST")
-    class T_H1 private constructor(
-        num: Double = 0.0,
-        entityId: String? = null,
-        creationTimestamp: Long = RawEntity.UNINITIALIZED_TIMESTAMP,
-        expirationTimestamp: Long = RawEntity.UNINITIALIZED_TIMESTAMP
-    ) : VariableEntityBase("T_H1", SCHEMA, entityId, creationTimestamp, expirationTimestamp)`
-    ));
-    it('generates copy and mutate by entity fields', async () => await assertCopyMethods(
-      `particle T
-         h1: reads Thing {num: Number}`,
-      `/**
-         * Use this method to create a new, distinctly identified copy of the entity.
-         * Storing the copy will result in a new copy of the data being stored.
-         */
-        fun copy(num: Double = this.num) = Thing(num = num)
-        /**
-         * Use this method to create a new version of an existing entity.
-         * Storing the mutation will overwrite the existing entity in the set, if it exists.
-         */
-        fun mutate(num: Double = this.num) = Thing(
-            num = num,
-            entityId = entityId,
-            creationTimestamp = creationTimestamp,
-            expirationTimestamp = expirationTimestamp
-        )`
-    ));
-    // TODO(alxr): Why do we omit the docstring for Wasm?
-    it('generates only copy method by entity fields for wasm', async () => await assertCopyMethodsForWasm(
-      `particle T
-         h1: reads Thing {num: Number}`,
-      `
-        fun copy(num: Double = this.num) = Thing(num = num)
-        `
-    ));
-    it('copies underlying data for type variables.', async () => await assertCopyMethods(
-      `particle T
-         h1: reads ~a with Thing {num: Number}`,
-      `/**
-         * Use this method to create a new, distinctly identified copy of the entity.
-         * Storing the copy will result in a new copy of the data being stored.
-         */
-        fun copy(num: Double = this.num) = Thing(num = num)
-            .also { this.copyLatentDataInto(it) }
-        /**
-         * Use this method to create a new version of an existing entity.
-         * Storing the mutation will overwrite the existing entity in the set, if it exists.
-         */
-        fun mutate(num: Double = this.num) = Thing(
-            num = num,
-            entityId = entityId,
-            creationTimestamp = creationTimestamp,
-            expirationTimestamp = expirationTimestamp
-        ).also { this.copyLatentDataInto(it) }`
-    ));
-    it('generates only copy method for variables for wasm', async () => await assertCopyMethodsForWasm(
-      `particle T
-         h1: reads  ~a with Thing {num: Number}`,
-      `
-        fun copy(num: Double = this.num) = Thing(num = num)
-        `
-    ));
-    it('generates fields for entity when available', async () => await assertFieldsDefinition(
-      `particle T
-         h1: reads Thing {num: Number}`,
-      `\
-
-        var num: Double
-            get() = super.getSingletonValue("num") as Double? ?: 0.0
-            private set(_value) = super.setSingletonValue("num", _value)
-        
-        init {
-            this.num = num
-        }
-        `
-    ));
-    it('generates an entityId property with Wasm', async () => await assertFieldsDefinitionForWasm(
-      `particle T
-         h1: reads Thing {num: Number}`,
-      `\
-
-        var num = num
-            get() = field
-            private set(_value) {
-                field = _value
-            }
-        
-        override var entityId = ""`
-    ));
-    async function assertClassDefinition(manifestString: string, expectedValue: string) {
-      await assertGeneratorComponent<string>(
-        manifestString,
-        generator => generator.generateClassDefinition(),
-        expectedValue);
-    }
-    async function assertCopyMethodsForWasm(manifestString: string, expectedValue: string) {
-      await assertGeneratorComponent<string>(
-        manifestString,
-        generator => generator.generateCopyMethods(),
-        expectedValue,
-        {_: [], wasm: true});
-    }
-    async function assertCopyMethods(manifestString: string, expectedValue: string) {
-      await assertGeneratorComponent<string>(
-        manifestString,
-        generator => generator.generateCopyMethods(),
-        expectedValue);
-    }
-    async function assertFieldsDefinition(manifestString: string, expectedValue: string) {
-      await assertGeneratorComponent<string>(
-        manifestString,
-        generator => generator.generateFieldsDefinitions(),
-        expectedValue);
-    }
-    async function assertFieldsDefinitionForWasm(manifestString: string, expectedValue: string) {
-      await assertGeneratorComponent<string>(
-        manifestString,
-        generator => generator.generateFieldsDefinitions(),
-        expectedValue,
-        {_: [], wasm: true});
-    }
-
-  });
   describe('Test Harness', () => {
     it('exposes a handle as a read write handle regardless of particle spec direction', async () => await assertTestHarness(
       `particle P
@@ -538,22 +398,4 @@ class PTestHarness<P : AbstractP>(
     const components = schema2kotlin.generateParticleClassComponents(particle, generators);
     assert.deepEqual(extractor(components), expectedValue);
   }
-
-  // Asserts that a certain component from the Kotlin Generator, equals the
-  // expected value.
-  async function assertGeneratorComponent<T>(
-    manifestString: string,
-    extractor: (generator: KotlinGenerator) => T,
-    expectedValue: T,
-    opts: minimist.ParsedArgs = {_: []}) {
-    const manifest = await Manifest.parse(manifestString);
-    assert.lengthOf(manifest.particles, 1);
-    const [particle] = manifest.particles;
-
-    const schema2kotlin = new Schema2Kotlin(opts);
-    const generators = await schema2kotlin.calculateNodeAndGenerators(particle);
-    const generator = (generators[0].generator as KotlinGenerator);
-    assert.deepEqual(extractor(generator), expectedValue);
-  }
-
 });
