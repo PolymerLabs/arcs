a581b5748e8fed67170b09ea3cd54cd7f190093b
Alex Rosengarten <alxr@google.com>
2020-06-17 14:10:08 -0700

    move behavior tests to typevariable showcase

--- javatests/arcs/showcase/typevariables/VariableEntityTest.kt
+++ javatests/arcs/showcase/typevariables/VariableEntityTest.kt
@@ -1,4 +1,4 @@
-package src.tools.tests
+package arcs.showcase.typevariables
 
 import arcs.core.data.RawEntity
 import arcs.core.data.Schema
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
-class GenerationBehaviorTest {
+class VariableEntityTest {
 
     @Test
     fun variable_equality() {
