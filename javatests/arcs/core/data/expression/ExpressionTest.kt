a8338b70679c42e171b2ba7de00b96a8f8dd21b7
Ray Cromwell <cromwellian@gmail.com>
2020-06-17 16:25:00 -0700

    add tests

--- javatests/arcs/core/data/expression/ExpressionTest.kt
+++ javatests/arcs/core/data/expression/ExpressionTest.kt
@@ -20,11 +20,44 @@ import kotlin.test.assertFailsWith
 /** Tests for [TypeVariable]. */
 @RunWith(JUnit4::class)
 class ExpressionTest {
-    fun <T> eval(expression: Expression<T>) = evalExpression<T, Boolean>(expression)
+    fun <T> evalBool(expression: Expression<T>) = evalExpression<T, Boolean>(expression)
+    fun <T> evalNum(expression: Expression<T>) = evalExpression<T, Number>(expression)
 
     @Test
     fun testEvaluator() {
-        // Test Math binary ops, field lookups, and parameter lookups
+        // numeric binary ops
+        assertThat(evalNum(2.asExpr() + 1.asExpr())).isEqualTo(3)
+        assertThat(evalNum(2.asExpr() - 1.asExpr())).isEqualTo(1)
+        assertThat(evalNum(2.asExpr() * 2.asExpr())).isEqualTo(4)
+        assertThat(evalNum(6.asExpr() / 3.asExpr())).isEqualTo(2)
+
+        // field ops
+        assertThat(evalNum<Number>(mapOf("foo" to 42).asScope()["foo"])).isEqualTo(42)
+
+        // query ops
+        assertThat(evalExpression<Number, Number>(query("arg"), "arg" to 42)).isEqualTo(42)
+
+        // Boolean ops
+        assertThat(evalBool(1.asExpr() lt 2.asExpr())).isTrue()
+        assertThat(evalBool(2.asExpr() lte 2.asExpr())).isTrue()
+        assertThat(evalBool(2.asExpr() gt 1.asExpr())).isTrue()
+        assertThat(evalBool(2.asExpr() gte 2.asExpr())).isTrue()
+        assertThat(evalBool((1.asExpr() lt 2.asExpr()) and (2.asExpr() gt 1.asExpr()))).isTrue()
+
+        // Unary ops
+        assertThat(evalNum(-2.asExpr())).isEqualTo(-2)
+        assertThat(evalBool(!(2.asExpr() lt 1.asExpr()))).isTrue()
+
+        // Equality ops
+        assertThat(evalBool(2.asExpr() eq 2.asExpr())).isTrue()
+        assertThat(evalBool("Hello".asExpr() eq "Hello".asExpr())).isTrue()
+        assertThat(evalBool(true.asExpr() eq true.asExpr())).isTrue()
+
+        assertThat(evalBool(2.asExpr() neq 1.asExpr())).isTrue()
+        assertThat(evalBool("Hello".asExpr() neq "World".asExpr())).isTrue()
+        assertThat(evalBool(true.asExpr() neq false.asExpr())).isTrue()
+
+        // Test complex expression
         // (2 + (3 * 4) + scope.foo + ?arg - 1) / 2
         val obj = mapOf("foo" to 42).asScope("handle")
         val expr = (2.0.asExpr() + (3.asExpr() * 4.asExpr()) + obj["foo"] + query(
@@ -32,26 +65,6 @@ class ExpressionTest {
         ) - 1.asExpr()) / 2.asExpr()
 
         assertThat(evalExpression<Number, Number>(expr, "arg" to 1)).isEqualTo(28)
-
-        // Boolean ops
-        assertThat(eval(1.asExpr() lt 2.asExpr())).isTrue()
-        assertThat(eval(2.asExpr() lte 2.asExpr())).isTrue()
-        assertThat(eval(2.asExpr() gt 1.asExpr())).isTrue()
-        assertThat(eval(2.asExpr() gte 2.asExpr())).isTrue()
-        assertThat(eval((1.asExpr() lt 2.asExpr()) and (2.asExpr() gt 1.asExpr()))).isTrue()
-
-        // Unary ops
-        assertThat(evalExpression<Number, Number>(-2.asExpr())).isEqualTo(-2)
-        assertThat(eval(!(2.asExpr() lt 1.asExpr()))).isTrue()
-
-        // Equality ops
-        assertThat(eval(2.asExpr() eq 2.asExpr())).isTrue()
-        assertThat(eval("Hello".asExpr() eq "Hello".asExpr())).isTrue()
-        assertThat(eval(true.asExpr() eq true.asExpr())).isTrue()
-
-        assertThat(eval(2.asExpr() neq 1.asExpr())).isTrue()
-        assertThat(eval("Hello".asExpr() neq "World".asExpr())).isTrue()
-        assertThat(eval(true.asExpr() neq false.asExpr())).isTrue()
     }
 
     @Test
@@ -59,7 +72,7 @@ class ExpressionTest {
         val obj = mapOf("foo" to 42).asScope("handle")
         val expr = 1.asExpr() + obj["bar"]
         assertFailsWith<IllegalArgumentException> {
-            evalExpression<Number, Number>(expr)
+            evalNum(expr)
         }
     }
 
@@ -67,7 +80,7 @@ class ExpressionTest {
     fun testExpressionWithQueryParamLookupError() {
         val expr = 1.asExpr() + query("arg")
         assertFailsWith<IllegalArgumentException> {
-            evalExpression<Number, Number>(expr)
+            evalNum(expr)
         }
     }
 
