cbf2391fca105b8fc832c9214a4f61d820e84e0b
Piotr Swigon <piotr.swigon@gmail.com>
2020-06-17 18:02:50 -0700

    Proper test for recipe2plan with proto output (#5540)

--- src/tools/tests/recipe2plan-test.ts
+++ src/tools/tests/recipe2plan-test.ts
@@ -12,20 +12,23 @@ import {fs} from '../../platform/fs-web.js';
 import {recipe2plan, OutputFormat} from '../recipe2plan.js';
 import {Flags} from '../../runtime/flags.js';
 import {ManifestProto} from '../manifest-proto.js';
+import {Runtime} from '../../runtime/runtime.js';
+import {Manifest} from '../../runtime/manifest.js';
 
 const inputManifestPath = 'java/arcs/core/data/testdata/WriterReaderExample.arcs';
+const readManifest = async () => await Runtime.parseFile(inputManifestPath);
 
 describe('recipe2plan', () => {
   it('generates Kotlin plans from recipes in a manifest', Flags.withDefaultReferenceMode(async () => {
     assert.deepStrictEqual(
-      await recipe2plan(inputManifestPath, OutputFormat.Kotlin),
+      await recipe2plan(await readManifest(), OutputFormat.Kotlin),
       fs.readFileSync('src/tools/tests/goldens/WriterReaderExample.kt', 'utf8'),
       `Golden is out of date! Make sure the new script is correct. If it is, update the goldens with: 
 $ tools/update-goldens \n\n`
     );
   }));
-  it('generates Proto plans from recipes in a manifest', Flags.withDefaultReferenceMode(async () => {
-    const encoded = await recipe2plan(inputManifestPath, OutputFormat.Proto) as Uint8Array;
+  it('generates Proto plans for multiple recipes in a manifest', Flags.withDefaultReferenceMode(async () => {
+    const encoded = await recipe2plan(await readManifest(), OutputFormat.Proto) as Uint8Array;
     const decoded = ManifestProto.decode(encoded);
 
     // Only validating that the output can be can be decoded as a ManifestProto and right counts.
@@ -34,9 +37,128 @@ $ tools/update-goldens \n\n`
     assert.lengthOf(decoded['particleSpecs'], 3);
   }));
   it('filters generated plans by provided name', Flags.withDefaultReferenceMode(async () => {
-    const encoded = await recipe2plan(inputManifestPath, OutputFormat.Proto, 'Consumption') as Uint8Array;
+    const encoded = await recipe2plan(await readManifest(), OutputFormat.Proto, 'Consumption') as Uint8Array;
     const decoded = ManifestProto.decode(encoded);
     assert.lengthOf(decoded['recipes'], 1);
     assert.lengthOf(decoded['particleSpecs'], 1);
   }));
+  it('outputs a valid protocol buffer for resolved recipes', Flags.withDefaultReferenceMode(async () => {
+    const payload = await protoPayloadFor(`
+      particle Reader
+        data: reads Thing {name: Text}
+      particle Writer
+        data: writes Thing {name: Text}
+
+      @arcId('writeArcId')
+      recipe WritingRecipe
+        thing: create 'my-handle-id' @persistent
+        Writer
+          data: writes thing
+
+      @arcId('readArcId')
+      recipe ReadingRecipe
+        data: map 'my-handle-id'
+        Reader
+          data: reads data
+      
+      recipe ReadWriteRecipe
+        thing: create
+        Writer
+          data: writes thing
+        Reader
+          data: reads thing
+    `);
+
+    // Only validating that the output can be can be decoded as a ManifestProto and right counts.
+    // Tests for for encoding works are in manifest2proto-test.ts.
+    assert.deepEqual(payload, {
+      particleSpecs: [{
+        name: 'Writer',
+        connections: [{
+          name: 'data',
+          direction: 'WRITES',
+          type: {entity: {schema: {
+            names: ['Thing'],
+            fields: {name: {primitive: 'TEXT'}},
+            hash: '25e71af4e9fc8b6958fc46a8f4b7cdf6b5f31516',
+          }}}
+        }]
+      }, {
+        name: 'Reader',
+        connections: [{
+          name: 'data',
+          direction: 'READS',
+          type: {entity: {schema: {
+            names: ['Thing'],
+            fields: {name: {primitive: 'TEXT'}},
+            hash: '25e71af4e9fc8b6958fc46a8f4b7cdf6b5f31516',
+          }}}
+        }]
+      }],
+      recipes: [{
+        name: 'WritingRecipe',
+        arcId: 'writeArcId',
+        handles: [{
+          fate: 'CREATE',
+          name: 'handle0',
+          id: 'my-handle-id',
+          capabilities: ['PERSISTENT'],
+          storageKey: 'reference-mode://{db://25e71af4e9fc8b6958fc46a8f4b7cdf6b5f31516@arcs/Thing}{db://25e71af4e9fc8b6958fc46a8f4b7cdf6b5f31516@arcs/!:writeArcId/handle/my-handle-id}',
+          type: {entity: {schema: {
+            names: ['Thing'],
+            fields: {name: {primitive: 'TEXT'}},
+            hash: '25e71af4e9fc8b6958fc46a8f4b7cdf6b5f31516',
+          }}}
+        }],
+        particles: [{
+          specName: 'Writer',
+          connections: [{name: 'data', handle: 'handle0'}]
+        }]
+      },
+      {
+        name: 'ReadingRecipe',
+        arcId: 'readArcId',
+        handles: [{
+          fate: 'MAP',
+          name: 'handle0',
+          id: 'my-handle-id',
+          storageKey: 'reference-mode://{db://25e71af4e9fc8b6958fc46a8f4b7cdf6b5f31516@arcs/Thing}{db://25e71af4e9fc8b6958fc46a8f4b7cdf6b5f31516@arcs/!:writeArcId/handle/my-handle-id}',
+          type: {entity: {schema: {
+            names: ['Thing'],
+            fields: {name: {primitive: 'TEXT'}},
+            hash: '25e71af4e9fc8b6958fc46a8f4b7cdf6b5f31516',
+          }}}
+        }],
+        particles: [{
+          specName: 'Reader',
+          connections: [{name: 'data', handle: 'handle0'}],
+        }]
+      }, {
+        name: 'ReadWriteRecipe',
+        handles: [{
+          fate: 'CREATE',
+          name: 'handle0',
+          storageKey: 'create://67835270998a62139f8b366f1cb545fb9b72a90b',
+          type: {entity: {schema: {
+            names: ['Thing'],
+            fields: {name: {primitive: 'TEXT'}},
+            hash: '25e71af4e9fc8b6958fc46a8f4b7cdf6b5f31516',
+          }}}
+        }],
+        particles: [{
+          specName: 'Reader',
+          connections: [{name: 'data', handle: 'handle0'}]
+        }, {
+          specName: 'Writer',
+          connections: [{name: 'data', handle: 'handle0'}],
+        }]
+      }]
+    });
+  }));
+  async function protoPayloadFor(manifestString: string) {
+    const manifest = await Manifest.parse(manifestString);
+    // We encode and decode back to ensure that data can be serialized to proto and deserialized back.
+    const encoded = await recipe2plan(manifest, OutputFormat.Proto, /* recipeFilter= */ null, 'random_salt') as Uint8Array;
+    return ManifestProto.decode(encoded).toJSON();
+  }
 });
