07d1577e3272e066db7d92e716a39ba48d6218b5
Jason Feinstein <jwf@google.com>
2020-06-22 10:35:37 -0700

    Pull the dereference call out of the withContext.

--- javatests/arcs/core/entity/DifferentHandleManagerDifferentStoresTest.kt
+++ javatests/arcs/core/entity/DifferentHandleManagerDifferentStoresTest.kt
@@ -16,16 +16,19 @@ import org.junit.runners.JUnit4
 @Suppress("EXPERIMENTAL_API_USAGE")
 @RunWith(JUnit4::class)
 class DifferentHandleManagerDifferentStoresTest : HandleManagerTestBase() {
+    private var i = 0
+
     @Before
     override fun setUp() {
         super.setUp()
+        i++
         StoreWriteBack.writeBackFactoryOverride = WriteBackForTesting
         schedulerProvider = JvmSchedulerProvider(EmptyCoroutineContext)
         readHandleManager = EntityHandleManager(
             arcId = "testArcId",
             hostId = "testHostId",
             time = fakeTime,
-            scheduler = schedulerProvider("reader"),
+            scheduler = schedulerProvider("reader-#$i"),
             stores = StoreManager()
         )
         writeHandleManager = EntityHandleManager(
