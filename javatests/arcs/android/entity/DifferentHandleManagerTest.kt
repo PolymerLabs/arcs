c858d6ce12ddc6a7a3fb6a031ed9da9a245eddf2
Jason Feinstein <jwf@google.com>
2020-06-25 21:07:29 -0700

    Cleanup. Also mark the TtlHandleTest as non-flaky.. since it isnt.

--- javatests/arcs/android/entity/DifferentHandleManagerTest.kt
+++ javatests/arcs/android/entity/DifferentHandleManagerTest.kt
@@ -14,12 +14,8 @@ import arcs.jvm.host.JvmSchedulerProvider
 import arcs.sdk.android.storage.ServiceStoreFactory
 import arcs.sdk.android.storage.service.testutil.TestConnectionFactory
 import kotlin.coroutines.EmptyCoroutineContext
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
-import org.junit.Ignore
-import org.junit.Test
 import org.junit.runner.RunWith
 
 @Suppress("EXPERIMENTAL_API_USAGE")
@@ -32,7 +28,7 @@ class DifferentHandleManagerTest : HandleManagerTestBase() {
     @Before
     override fun setUp() {
         super.setUp()
-        testTimeout = 30000
+        testTimeout = 60000
         fakeLifecycleOwner = FakeLifecycleOwner()
         fakeLifecycleOwner.lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
         fakeLifecycleOwner.lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START)
@@ -66,77 +62,6 @@ class DifferentHandleManagerTest : HandleManagerTestBase() {
     override fun tearDown() {
         super.tearDown()
         fakeLifecycleOwner.lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-    }
-
-    @Test
-    override fun collection_dereferenceEntity_nestedReference() {
-        super.collection_dereferenceEntity_nestedReference()
-    }
-
-    @Test
-    override fun collection_clearingElementsFromA_clearsThemFromB() {
-        super.collection_clearingElementsFromA_clearsThemFromB()
-    }
-
-    @Test
-    override fun singleton_clearOnAClearDataWrittenByA(){
-        super.singleton_clearOnAClearDataWrittenByA()
-    }
-
-    @Test
-    override fun collection_referenceLiveness() {
-        super.collection_referenceLiveness()
-    }
-
-    @Test
-    override fun singleton_writeAndOnUpdate() {
-        super.singleton_writeAndOnUpdate()
-    }
-
-    @Test
-    override fun collection_writeAndReadBack() {
-        super.collection_writeAndReadBack()
-    }
-
-    @Test
-    override fun singleton_referenceLiveness() {
-        super.singleton_referenceLiveness()
-    }
-
-    @Test
-    override fun collection_entityDereference() {
-        super.collection_entityDereference()
-    }
-
-    @Test
-    override fun singleton_clearOnAClearDataWrittenByB() {
-        super.singleton_clearOnAClearDataWrittenByB()
-    }
-
-    @Test
-    override fun singleton_dereferenceEntity() {
-        super.singleton_dereferenceEntity()
-    }
-
-
-    @Test
-    override fun singleton_dereferenceEntity_nestedReference() {
-        super.singleton_dereferenceEntity_nestedReference()
-    }
-
-    @Test
-    override fun collection_removingFromA_isRemovedFromB() {
-        super.collection_removingFromA_isRemovedFromB()
-    }
-
-    @Test
-    override fun collection_withTTL() {
-        super.collection_withTTL()
-    }
-
-    @Test
-    override fun collection_noTTL() {
-        super.collection_noTTL()
     }
 
     class FakeLifecycleOwner : LifecycleOwner {
