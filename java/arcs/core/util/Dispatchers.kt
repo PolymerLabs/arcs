641de7f21ee9a30f0a528c9d9dfc8a9602f568bd
Yuan Gu <yuangu@google.com>
2020-07-01 14:28:40 -0700

    resolve commenst

--- java/arcs/core/util/Dispatchers.kt
+++ java/arcs/core/util/Dispatchers.kt
@@ -3,6 +3,10 @@ package arcs.core.util
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
+/**
+ * Centralized dispatcher references for use across Arcs to allow
+ * Arcs users to customize with their own dispatchers as needed.
+ */
 object Dispatchers {
 
     @JvmStatic
