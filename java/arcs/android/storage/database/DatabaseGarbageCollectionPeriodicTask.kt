3d79f4e1f8b7a56b9cbc28e6db2290f18690abfa
Yuan Gu <yuangu@google.com>
2020-07-06 13:41:12 -0700

    Run periodic task on the default background thread.

--- java/arcs/android/storage/database/DatabaseGarbageCollectionPeriodicTask.kt
+++ java/arcs/android/storage/database/DatabaseGarbageCollectionPeriodicTask.kt
@@ -22,14 +22,14 @@ import kotlinx.coroutines.runBlocking
  * Implementation of a [Worker] which performs periodic scan of storage and deletes unused data.
  */
 class DatabaseGarbageCollectionPeriodicTask(
-    val appContext: Context,
+    appContext: Context,
     workerParams: WorkerParameters
 ) : Worker(appContext, workerParams) {
 
     private val log = TaggedLog { WORKER_TAG }
     init { log.debug { "Created." } }
 
-    override fun doWork(): Result = runBlocking(Dispatchers.IO) {
+    override fun doWork(): Result = runBlocking {
         log.debug { "Running." }
         // Use the DatabaseDriverProvider instance of the databaseManager to make sure changes by
         // GC are propagated to listening Stores.
