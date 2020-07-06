19f68b2a56d6727a8b41712020cd330d1a2060cf
Alex Rosengarten <alxr@google.com>
2020-07-06 15:00:13 -0700

    Wrote failing type variable e2e test

--- javatests/arcs/showcase/typevariables/Consumer.kt
+++ javatests/arcs/showcase/typevariables/Consumer.kt
@@ -1,10 +1,17 @@
 package arcs.showcase.typevariables
 
 import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.Job
 
 class Consumer : AbstractConsumer() {
     // Assert that all items in the collection have been redacted.
     override fun onUpdate() =
-        handles.data.fetchAll().forEach { assertThat(it.sku).endsWith("*****") }
+        handles.data.fetchAll()
+            .forEach { assertThat(it.sku).endsWith("*****") }
+            .also { updated.complete() }
 
+
+    companion object {
+       val updated = Job()
+    }
 }
---
+++ javatests/arcs/showcase/typevariables/VariableEntityTest.kt
@@ -0,0 +1,60 @@
+package arcs.showcase.typevariables
+
+import arcs.core.allocator.Allocator
+import arcs.core.host.*
+import arcs.core.storage.api.DriverAndKeyConfigurator
+import arcs.core.storage.driver.RamDisk
+import arcs.core.storage.driver.RamDiskDriverProvider
+import arcs.jvm.host.ExplicitHostRegistry
+import arcs.jvm.host.JvmHost
+import arcs.jvm.host.JvmSchedulerProvider
+import arcs.jvm.util.testutil.FakeTime
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import kotlin.coroutines.EmptyCoroutineContext
+
+@RunWith(JUnit4::class)
+@ExperimentalCoroutinesApi
+class VariableEntityTest {
+
+    class TestJvmProdHost(
+        schedulerProvider: SchedulerProvider,
+        vararg particles: ParticleRegistration
+    ): JvmHost(schedulerProvider, *particles), ProdHost
+
+    @Test
+    fun shop_redactsSkew() = runBlocking {
+        RamDisk.clear()
+        DriverAndKeyConfigurator.configureKeyParsers()
+        RamDiskDriverProvider()
+
+        val hostRegistry = ExplicitHostRegistry()
+        val schedulerProvider = JvmSchedulerProvider(EmptyCoroutineContext)
+
+        hostRegistry.registerHost(
+            TestJvmProdHost(schedulerProvider,
+                ::OrderIngestion.toRegistration(),
+                ::SkuRedactor.toRegistration(),
+                ::Consumer.toRegistration()
+            )
+        )
+
+        val allocator = Allocator.create(
+            hostRegistry,
+            EntityHandleManager(
+                time = FakeTime(),
+                scheduler = schedulerProvider("allocator")
+            )
+        )
+
+        val arcId = allocator.startArcForPlan(ShopPlan).waitForStart().id
+
+        // Ensure that the shop recipe is fully processed.
+        withTimeout(1500) { Consumer.updated.join() }
+        allocator.stopArc(arcId)
+    }
+}
