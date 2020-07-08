4de5a83e90cbfb274af448cdf5b70ce75da6e46c
Cameron Silvestrini <csilvestrini@users.noreply.github.com>
2020-07-08 12:35:21 +1000

    Creates an applyPolicy function, and adds egress particle checking to it (#5629)
    * Creates an applyPolicy function, and adds egress particle checking to it.
    
    I'm still not particularly content with the way we match egress particles to policies, but this is the plan of record so far (and it will be easy to update later if we have to): each policy has a corresponding egress particle with the name `Egress_<PolicyName>`.
    
    This PR adds the `applyPolicy` function, but all it does so far is check that no invalid egress particles are present in it.
    
    * lint
    
    * kdoc
    
    * kdoc

---
+++ java/arcs/core/policy/PolicyTranslation.kt
@@ -0,0 +1,39 @@
+package arcs.core.policy
+
+import arcs.core.analysis.RecipeGraph
+
+/**
+ * Translates the given [policy] into dataflow analysis checks and claims on the given [graph],
+ * returning a modified copy of the graph.
+ *
+ * @throws PolicyViolation if the [graph] violates the [policy]
+ */
+fun applyPolicy(policy: Policy, graph: RecipeGraph): RecipeGraph {
+    checkEgressParticles(policy, graph)
+    // TODO(b/157605232): Modify the graph and attach checks and claims.
+    return graph
+}
+
+private fun checkEgressParticles(policy: Policy, graph: RecipeGraph) {
+    val invalidEgressParticles = graph.particleNodes
+        .map { it.particle.spec }
+        .filter { !it.isolated && it.name != policy.egressParticleName }
+    if (invalidEgressParticles.isNotEmpty()) {
+        throw PolicyViolation.InvalidEgressParticle(policy, invalidEgressParticles.map { it.name })
+    }
+}
+
+/** Indicates that a policy was violated by a recipe. */
+sealed class PolicyViolation(val policy: Policy, message: String) : Exception(
+    "Policy ${policy.name} violated: $message"
+) {
+    /** Thrown when egress particles were found in the recipe that are not allowed by policy. */
+    class InvalidEgressParticle(
+        policy: Policy,
+        val particleNames: List<String>
+    ) : PolicyViolation(
+        policy,
+        "Egress particle allowed by policy is ${policy.egressParticleName} but found: " +
+            particleNames.joinToString()
+    )
+}
