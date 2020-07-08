cab4ca2c5be014b1a2025adefe67c758cf5bbf90
Alex Rosengarten <alxr@google.com>
2020-07-08 15:13:45 -0700

    Plan integration tests for proto / codegen pathways (#5598)
    Wrote test comparing Plan particles (protos --> Recipe --> Plan vs code-gen'd Plan).
    
    ## Changelog
    * Created broken test
    
    * proto plan / codegen plan integration test
    
    * sugar fix
    
    * update
    
    * lint
    
    * better impl of particle path
    
    * Added bug to comment
    
    * Squashed commit of the following:
    
    commit 4bb5a26c02dbf599aca4c5b02562c0b2b3484a76
    Merge: 27b026fab 4f779c021
    Author: Alex Rosengarten <alxr@google.com>
    Date:   Tue Jun 30 15:47:12 2020 -0700
    
        Merge branch 'master' of https://github.com/PolymerLabs/arcs into kt-handle-inst
    
    commit 27b026fabcce09f44e84a6bd0bdff6322df4744a
    Author: Alex Rosengarten <alxr@google.com>
    Date:   Tue Jun 30 15:42:05 2020 -0700
    
        updated exception handling + test
    
    commit 4f779c021d2c830c94d690b9f2bb00a4c90553f2
    Author: Walter Korman <wkorman@chromium.org>
    Date:   Tue Jun 30 15:38:04 2020 -0700
    
        Revert #5610 as it breaks Firebase emulator tests. (#5623)
    
        Revert "Handle helper methods for dispatch and wait functionality (#5610)"
    
        This reverts commit b7a4ba8ef26f45fc617b63515d1f8a2df8917d1a.
    
    commit 97260a20dc2f3727fb24a4e12671eb7312d19a63
    Author: Alex Rosengarten <alxr@google.com>
    Date:   Tue Jun 30 14:08:03 2020 -0700
    
        Updated test to reflect new translation decision
    
    commit 8428f448bd3377a7a9c4f1af52664bf2ded6069b
    Author: Alex Rosengarten <alxr@google.com>
    Date:   Tue Jun 30 13:58:49 2020 -0700
    
        Recipe.kt --> Plan.kt: Getting type from connection instance
    
    commit 7f45bccf8fe85531c042225d3ce5897017c0685e
    Author: Alex Rosengarten <alxr@google.com>
    Date:   Tue Jun 30 13:53:39 2020 -0700
    
        Updaing other tests
    
    commit 9f691a4ca8c4e1a8013817ab53de016a6adad21a
    Merge: dc97737d8 698ec0a11
    Author: Alex Rosengarten <alxr@google.com>
    Date:   Tue Jun 30 12:58:12 2020 -0700
    
        Merge branch 'master' of https://github.com/PolymerLabs/arcs into kt-handle-inst
    
    commit dc97737d8b3af6d64644e2539de81486bfa8e9ce
    Author: Alex Rosengarten <alxr@google.com>
    Date:   Tue Jun 30 12:58:00 2020 -0700
    
        Decoding handle connection instance from proto to Kotlin Recipe
    
    commit fed8d955eb3a1a550a7207bb448a736eb532495e
    Author: Alex Rosengarten <alxr@google.com>
    Date:   Tue Jun 30 11:51:09 2020 -0700
    
        Squashed commit of the following:
    
        commit c309a26ac8dc9d426ae31519725d77a4452e4370
        Author: Alex Rosengarten <alxr@google.com>
        Date:   Mon Jun 29 22:42:32 2020 -0700
    
            Impl feedback
    
        commit 790e9fee2d20be98eb0fedd0e96f5a0ef4deeb2d
        Author: Alex Rosengarten <alxr@google.com>
        Date:   Mon Jun 29 15:02:52 2020 -0700
    
            lint issues
    
        commit ac010bfb429bf6bc2c1bcb91e6a9cb03b6ebb998
        Author: Alex Rosengarten <alxr@google.com>
        Date:   Mon Jun 29 14:55:18 2020 -0700
    
            Updating textproto -- kotlin proto deserialization works
    
        commit ac44a4b4854fb6253c3d01f75b86a9bde52ace55
        Author: Alex Rosengarten <alxr@google.com>
        Date:   Mon Jun 29 14:45:06 2020 -0700
    
            writing handle connection instance to proto
    
        commit cc5d11e0064c7adcbdb8edac737afbe32cb8c118
        Author: Alex Rosengarten <alxr@google.com>
        Date:   Mon Jun 29 14:44:41 2020 -0700
    
            Handle connection instance has type

--- src/tools/tests/VariablePlanTest.kt
+++ src/tools/tests/VariablePlanTest.kt
@@ -1,16 +1,47 @@
 package src.tools.tests
 
 import arcs.core.data.FieldType
+import arcs.core.data.Plan
 import arcs.core.data.SchemaFields
+import arcs.core.data.proto.ManifestProto
+import arcs.core.data.proto.decodeRecipes
+import arcs.core.data.toPlan
 import arcs.core.host.toSchema
 import arcs.core.storage.api.DriverAndKeyConfigurator
+import arcs.repoutils.runfilesDir
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import java.io.File
 
 @RunWith(JUnit4::class)
 class VariablePlanTest {
+
+    // Assert two particles are equal since [Schema] data classes have lambdas.
+    // This should no longer be necessary once b/155813124 is implemented.
+    private fun particleEquality(left: Plan.Particle, right: Plan.Particle) {
+        assertThat(left.particleName).isEqualTo(right.particleName)
+        assertThat(left.location).isEqualTo(right.location)
+
+        val handlePairs = (left.handles.toList() + right.handles.toList())
+            .groupBy({ it.first }, { it.second })
+
+        for (pairs in handlePairs.values) {
+            assertThat(pairs).hasSize(2)
+
+            val firstHandle = pairs[0]
+            val secondHandle = pairs[1]
+
+            assertThat(firstHandle.storageKey).isEqualTo(secondHandle.storageKey)
+            assertThat(firstHandle.annotations).isEqualTo(secondHandle.annotations)
+            assertThat(firstHandle.mode).isEqualTo(secondHandle.mode)
+            assertThat(firstHandle.ttl).isEqualTo(secondHandle.ttl)
+            assertThat(firstHandle.type.toLiteral()).isEqualTo(secondHandle.type.toLiteral())
+        }
+
+    }
+
     @Test
     fun variable_hasSameSchema() {
         DriverAndKeyConfigurator.configure(null)
@@ -32,5 +63,22 @@ class VariablePlanTest {
         ))
 
         assertThat(processSchema.toLiteral()).isEqualTo(ingestSchema.toLiteral())
+    }
+
+    @Test
+    fun variable_recipePlanEquivalence() {
+        DriverAndKeyConfigurator.configure(null)
+
+        val manifestProto = ManifestProto.parseFrom(
+            File(runfilesDir() + "src/tools/tests/variable_proto.pb.bin").readBytes()
+        )
+        val recipes = manifestProto.decodeRecipes()
+
+        val variableIngestionRecipe = recipes.first { it.name == "VariableIngestion" }
+
+        val fromRecipe = variableIngestionRecipe.toPlan()
+
+        particleEquality(fromRecipe.particles[0], VariableIngestionPlan.particles[0])
+        particleEquality(fromRecipe.particles[1], VariableIngestionPlan.particles[1])
     }
 }
