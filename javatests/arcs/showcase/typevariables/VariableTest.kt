778b23cbd4e769b7e84daa5984a56ef78b4c4f31
Alex Rosengarten <alxr@google.com>
2020-07-08 11:15:05 -0700

    Simpler test, simpler build file

--- javatests/arcs/showcase/typevariables/VariableTest.kt
+++ javatests/arcs/showcase/typevariables/VariableTest.kt
@@ -1,57 +1,29 @@
 package arcs.showcase.typevariables
 
-import arcs.core.allocator.Allocator
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import arcs.core.host.*
-import arcs.core.storage.api.DriverAndKeyConfigurator
-import arcs.core.storage.driver.RamDisk
-import arcs.core.storage.driver.RamDiskDriverProvider
-import arcs.jvm.host.ExplicitHostRegistry
-import arcs.jvm.host.JvmHost
-import arcs.jvm.host.JvmSchedulerProvider
-import arcs.jvm.util.testutil.FakeTime
+import arcs.core.testutil.runTest
+import arcs.showcase.ShowcaseEnvironment
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeout
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
-import kotlin.coroutines.EmptyCoroutineContext
 
-@RunWith(JUnit4::class)
 @ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
 class VariableTest {
 
-    class TestJvmProdHost(
-        schedulerProvider: SchedulerProvider,
-        vararg particles: ParticleRegistration
-    ): JvmHost(schedulerProvider, *particles), ProdHost
+    @get:Rule val env = ShowcaseEnvironment(
+        ::OrderIngestion.toRegistration(),
+        ::SkuRedactor.toRegistration(),
+        ::Consumer.toRegistration()
+    )
 
     @Test
-    fun shop_redactsSku() = runBlocking {
-        RamDisk.clear()
-        DriverAndKeyConfigurator.configureKeyParsers()
-        RamDiskDriverProvider()
-
-        val hostRegistry = ExplicitHostRegistry()
-        val schedulerProvider = JvmSchedulerProvider(EmptyCoroutineContext)
-
-        hostRegistry.registerHost(
-            TestJvmProdHost(schedulerProvider,
-                ::OrderIngestion.toRegistration(),
-                ::SkuRedactor.toRegistration(),
-                ::Consumer.toRegistration()
-            )
-        )
-
-        val allocator = Allocator.create(
-            hostRegistry,
-            EntityHandleManager(
-                time = FakeTime(),
-                scheduler = schedulerProvider("allocator")
-            )
-        )
-
-        val arcId = allocator.startArcForPlan(ShopPlan).waitForStart().id
+    fun shop_redactsSku() = runTest {
+        val arc = env.startArc(ShopPlan)
 
         // Ensure that the shop recipe is fully processed.
         withTimeout(1500) {
@@ -59,6 +31,7 @@ class VariableTest {
             SkuRedactor.redacted.join()
             Consumer.updated.join()
         }
-        allocator.stopArc(arcId)
+
+        env.stopArc(arc)
     }
 }
