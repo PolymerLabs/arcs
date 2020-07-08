04093dcd1ce0d018882bdbebd1171997b5f07b2d
Alex Rosengarten <alxr@google.com>
2020-07-08 11:18:13 -0700

    prefering require over assert

--- javatests/arcs/showcase/typevariables/SkuRedactor.kt
+++ javatests/arcs/showcase/typevariables/SkuRedactor.kt
@@ -1,13 +1,12 @@
 package arcs.showcase.typevariables
 
-import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.Job
 
 class SkuRedactor : AbstractSkuRedactor() {
 
     /** Redact each input sku, and store the results to the output handle. */
     override fun onReady() {
-        assertThat(handles.input.size()).isEqualTo(3)
+        require(handles.input.size() == 3)
         for (item in handles.input.fetchAll()) {
             require(item.sku.isNotEmpty())
             handles.output.store(item.copy(sku = redactSku(item.sku)))
