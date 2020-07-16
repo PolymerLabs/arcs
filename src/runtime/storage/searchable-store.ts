4da6eb7c30a18f8dd7a819e2fd5e62b92fc8af15
Alex Rosengarten <alxr@google.com>
2020-07-16 16:15:55 -0700

    lint + rm todo

--- src/runtime/storage/searchable-store.ts
+++ src/runtime/storage/searchable-store.ts
@@ -12,7 +12,6 @@ import {Manifest} from '../manifest.js';
 import {ToStore} from './storage.js';
 import {AbstractStore} from './abstract-store.js';
 
-// TODO(alxr) name bike-shedding: `StoreClosure`? `StoreContext`? `StoreHolder` Other ideas?
 export interface SearchableStore {
   context: Manifest;
   findStoresByType<T extends Type>(type: T, options?: { tags: string[] }): ToStore<T>[];
