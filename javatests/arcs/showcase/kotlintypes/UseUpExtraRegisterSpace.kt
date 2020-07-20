6a0ce7107a703f33d566aae4c69a8a115f0cc451
Shane Stephens <shanestephens@google.com>
2020-07-20 15:35:13 +1000

    Showcase for kotlin native types

---
+++ javatests/arcs/showcase/kotlintypes/UseUpExtraRegisterSpace.kt
@@ -0,0 +1,32 @@
+package arcs.showcase.kotlintypes
+
+class UseUpExtraRegisterSpace : AbstractUseUpExtraRegisterSpace() {
+
+    override fun onReady() {
+        val entity = requireNotNull(handles.inputs.fetch()) {
+            "Failed to read entity from input handle!"            
+        }
+        
+        val bytes = setOf(entity.aByte)
+        val shorts = setOf(entity.aShort, entity.aByte.toShort())
+        val ints = setOf(entity.anInt, entity.aShort.toInt(), entity.aByte.toInt())
+        val longs = setOf(entity.aLong, entity.anInt.toLong(), entity.aShort.toLong(), entity.aByte.toLong())
+
+        val chars = setOf(entity.aChar)
+
+        val floats = setOf(entity.aFloat)
+        val doubles = setOf(entity.aDouble, entity.aFloat.toDouble())
+
+        handles.outputs.store(
+            KotlinTypeSets(
+                someBytes = bytes,
+                someShorts = shorts,
+                someInts = ints,
+                someLongs = longs,
+                someChars = chars,
+                someFloats = floats,
+                someDoubles = doubles
+            )        
+        )
+    }
+}
