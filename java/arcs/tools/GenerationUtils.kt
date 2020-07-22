078dd7205c6bf7e168d9eebc3aa501423ef111ad
Alex Rosengarten <alxr@google.com>
2020-07-22 14:51:47 -0700

    Created a Map.toGeneration util; rough impl of handle generation.

--- java/arcs/tools/GenerationUtils.kt
+++ java/arcs/tools/GenerationUtils.kt
@@ -3,7 +3,7 @@ package arcs.tools
 import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.buildCodeBlock
 
-fun <T> List<T>.toGeneration(template: String = "%N") =
+fun List<*>.toGeneration(template: String = "%N") =
     toGeneration { builder, item -> builder.add(template, item) }
 
 fun <T> List<T>.toGeneration(template: (builder: CodeBlock.Builder, item: T) -> Unit) = buildCodeBlock {
@@ -21,7 +21,7 @@ fun <T> List<T>.toGeneration(template: (builder: CodeBlock.Builder, item: T) -> 
 }
 
 // TODO(alxr) Dedupe repeated logic via HOF
-fun <T> Set<T>.toGeneration(template: String = "%N") =
+fun Set<*>.toGeneration(template: String = "%N") =
     toGeneration { builder, item -> builder.add(template, item) }
 
 fun <T> Set<T>.toGeneration(template: (builder: CodeBlock.Builder, item: T) -> Unit) = buildCodeBlock {
@@ -33,6 +33,23 @@ fun <T> Set<T>.toGeneration(template: (builder: CodeBlock.Builder, item: T) -> U
     add("setOf(")
     this@toGeneration.forEachIndexed { idx, it ->
         template(this, it)
+        if (idx != size - 1) { add(", ") }
+    }
+    add(")")
+}
+
+fun Map<*, *>.toGeneration(template: String = "%S to %L") =
+    toGeneration { builder, entry -> builder.add(template, entry.key, entry.value) }
+
+fun <K, V> Map<K, V>.toGeneration(template: (builder: CodeBlock.Builder, entry: Map.Entry<K, V>) -> Unit) = buildCodeBlock {
+    if (this@toGeneration.isEmpty()) {
+        add("emptyMap()")
+        return build()
+    }
+
+    add("mapOf(")
+    this@toGeneration.entries.forEachIndexed { idx, entry ->
+        template(this, entry)
         if (idx != size - 1) { add(", ") }
     }
     add(")")
--- java/arcs/tools/PlanGenerator.kt
+++ java/arcs/tools/PlanGenerator.kt
@@ -2,7 +2,10 @@ package arcs.tools
 
 import arcs.core.data.CountType
 import arcs.core.data.EntityType
+import arcs.core.data.FieldName
+import arcs.core.data.FieldType
 import arcs.core.data.Plan
+import arcs.core.data.PrimitiveType
 import arcs.core.data.Recipe
 import arcs.core.data.Schema
 import arcs.core.data.SchemaFields
@@ -11,6 +14,7 @@ import arcs.core.data.TupleType
 import arcs.core.data.TypeVariable
 import arcs.core.storage.StorageKeyParser
 import arcs.core.type.Type
+import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.buildCodeBlock
@@ -26,12 +30,12 @@ fun Recipe.toGeneration(builder: FileSpec.Builder) {
     val plan = PropertySpec.builder("${name}Plan", Plan::class)
         .initializer(buildCodeBlock {
             addNamed("""
-            %plan:T(
-                particles = %particles:L,
-                handles = %handles:L,
-                annotations = %annotations:L
-            )
-            """.trimIndent(), ctx)
+                %plan:T(
+                    particles = %particles:L,
+                    handles = %handles:L,
+                    annotations = %annotations:L
+                )
+                """.trimIndent(), ctx)
         })
         .build()
 
@@ -52,18 +56,17 @@ fun Recipe.Handle.toGeneration(planName: String) = PropertySpec.builder("${planN
             "annotations" to "emptyList()"
         )
         addNamed("""
-        %handle:T(
-            storageKey = %storageParser:T.parse(%key:S),
-            type = %type:L,    
-            annotations = %annotations:L
-        )
+            %handle:T(
+                storageKey = %storageParser:T.parse(%key:S),
+                type = %type:L,    
+                annotations = %annotations:L
+            )
         """.trimIndent(), ctx)
     })
     .build()
 
-fun Type.toGeneration() = buildCodeBlock {
-    val type = this@toGeneration
-    when (type) {
+fun Type.toGeneration(): CodeBlock = buildCodeBlock {
+    when (val type = this@toGeneration) {
         is EntityType -> add("%T(%L)", EntityType::class, type.entitySchema.toGeneration())
         is Type.TypeContainer<*> -> add("%T(%L)", type::class, type.containedType.toGeneration())
         is CountType -> add("%T()", CountType::class)
@@ -78,23 +81,65 @@ fun Schema.toGeneration() = buildCodeBlock {
     val ctx = mapOf<String, Any>(
         "schema" to Schema::class,
         "names" to schema.names.toGeneration { builder, item ->
-            builder.add("%T(%S)", SchemaName::class, item)
+            builder.add("%T(%S)", SchemaName::class, item.name)
         },
         "fields" to schema.fields.toGeneration(),
         "hash" to schema.hash
     )
     addNamed("""
-    %schema:T(
-        names = %names:L,
-        fields = %fields:L,
-        hash = %hash:S
-    )
+        %schema:T(
+            names = %names:L,
+            fields = %fields:L,
+            hash = %hash:S
+        )
     """.trimIndent(), ctx)
 }
 
 fun SchemaFields.toGeneration() = buildCodeBlock {
-    // TODO(alxr) Implement
-    add("null")
+    val fields = this@toGeneration
+    val toSchemaField  = { builder: CodeBlock.Builder, entry: Map.Entry<FieldName, FieldType> ->
+        builder.add("%S to %L", entry.key, entry.value.toGeneration())
+        Unit
+    }
+    val ctx = mapOf<String, Any>(
+        "fields" to SchemaFields::class,
+        "singletons" to fields.singletons.toGeneration(toSchemaField),
+        "collections" to fields.collections.toGeneration(toSchemaField)
+    )
+    addNamed("""
+        %fields:T(
+            singletons = %singletons:L,
+            collections = %collections:L
+        )
+    """.trimIndent(), ctx)
+}
+
+fun FieldType.toGeneration(): CodeBlock = buildCodeBlock {
+    when (val field = this@toGeneration) {
+        is FieldType.Primitive -> add(
+            "%T.%L",
+            FieldType::class,
+            field.primitiveType
+        )
+        is FieldType.InlineEntity,
+        is FieldType.EntityRef -> add(
+            "%T(%S)",
+            field::class,
+            (field as FieldType.EntityRef).schemaHash
+        )
+        is FieldType.Tuple -> add(
+            "%T(%L)",
+            FieldType.Tuple::class,
+            field.types.toGeneration { builder, item ->
+                builder.add(item.toGeneration())
+            }
+        )
+        is FieldType.ListOf -> add(
+            "%T(%L)",
+            FieldType.ListOf::class,
+            field.toGeneration()
+        )
+    }
 }
 
 fun Annotation.toGeneration() {}
