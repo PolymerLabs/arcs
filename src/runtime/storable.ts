7ae85c497aaca541487bc040f01d1a0e68f7269a
Josh Pratt <jopra@chromium.org>
2020-07-31 17:22:03 +1000

    Part of the cycle breaking hacks

--- src/runtime/storable.ts
+++ src/runtime/storable.ts
@@ -8,9 +8,36 @@
  * http://polymer.github.io/PATENTS.txt
  */
 
+import {assert} from '../platform/assert-web.js';
 import {SYMBOL_INTERNALS} from './symbols.js';
 import {SerializedEntity} from './entity.js';
 
-export interface Storable {
-  [SYMBOL_INTERNALS]: {serialize: () => SerializedEntity};
+export abstract class Storable {
+  // This lets us say some type T, that is at least StorableInternals, but we don't care what else.
+  // tslint:disable-next-line: no-any
+  [SYMBOL_INTERNALS]: StorableInternals & any;
+
+  static creationTimestamp(entity: Storable): Date | null {
+    return getStorableInternals(entity).hasCreationTimestamp()
+        ? getStorableInternals(entity).getCreationTimestamp() : null;
+  }
+
+  static expirationTimestamp(entity: Storable): Date | null {
+    return getStorableInternals(entity).hasExpirationTimestamp()
+        ? getStorableInternals(entity).getExpirationTimestamp() : null;
+  }
+}
+
+export abstract class StorableInternals {
+  abstract hasCreationTimestamp(): boolean;
+  abstract getCreationTimestamp(): Date;
+  abstract hasExpirationTimestamp(): boolean;
+  abstract getExpirationTimestamp(): Date;
+  abstract serialize(): SerializedEntity;
+}
+
+export function getStorableInternals<T extends StorableInternals>(entity: Storable): T {
+  const internals = entity[SYMBOL_INTERNALS];
+  assert(internals !== undefined, 'SYMBOL_INTERNALS lookup on non-entity');
+  return internals;
 }
