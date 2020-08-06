06fe27f73c0f9d55dbb297f3a2ff8abbc427d824
ianull <ianchang@google.com>
2020-08-05 18:51:32 -0700

    undo changes on ShowcaseEnvironment

--- javatests/arcs/showcase/ShowcaseEnvironment.kt
+++ javatests/arcs/showcase/ShowcaseEnvironment.kt
@@ -218,7 +218,6 @@ class ShowcaseHost(
     vararg particleRegistrations: ParticleRegistration
 ) : AbstractArcHost(
     coroutineContext,
-    coroutineContext,
     schedulerProvider,
     activationFactory,
     *particleRegistrations
