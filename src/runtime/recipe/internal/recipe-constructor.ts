23263d066971941bb3f8ec89accc4833c16b43b0
Shane Stephens <shanestephens@google.com>
2020-08-31 11:20:39 +1000

    lint fixes

--- src/runtime/recipe/internal/recipe-constructor.ts
+++ src/runtime/recipe/internal/recipe-constructor.ts
@@ -1,7 +1,16 @@
-import {Recipe} from "./recipe.js";
+/**
+ * @license
+ * Copyright 2019 Google LLC.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+import {Recipe} from './recipe.js';
 import {Handle} from './recipe-interface.js';
-import {HandleEndPoint, ParticleEndPoint, TagEndPoint} from "./connection-constraint";
-import {ParticleSpec} from "../../arcs-types/particle-spec.js";
+import {HandleEndPoint, ParticleEndPoint, TagEndPoint} from './connection-constraint.js';
+import {ParticleSpec} from '../../arcs-types/particle-spec.js';
 
 // TODO(shanestephens): This should be a RecipeBuilder
 export const newRecipe = (name?: string) => new Recipe(name);
