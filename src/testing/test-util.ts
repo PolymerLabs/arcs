a10c8dba72b47f47ff3a74bd2e202d50935baf35
mykmartin <mykmartin@google.com>
2020-09-07 13:33:49 +1000

    Lint error promptec a restructure.

--- src/testing/test-util.ts
+++ src/testing/test-util.ts
@@ -29,7 +29,7 @@ export async function assertThrowsAsync(fn: Function, ...args): Promise<void> {
  *
  *   const cc = ConCap.capture(() => testAnErrorCase('parse failure'));
  *   assert.equals(cc.result, 'this holds whatever testAnErrorCase returned');
- *   assert.match(cc.log[0], /Error parsing/);
+ *   assert.match(cc.log[0][0], /Error parsing/);
  *
  * To simply discard all console logging:
  *
@@ -38,21 +38,14 @@ export async function assertThrowsAsync(fn: Function, ...args): Promise<void> {
  * Both sync and async functions can be wrapped; in the latter case you just need to
  * await the capture/silence call.
  */
+// tslint:disable: no-any
 export class ConCap {
-  // tslint:disable: no-any
   result: any;
   log: any[][] = [];
   warn: any[][] = [];
   error: any[][] = [];
   dir: any[][] = [];
   private save: (() => void)[];
-  private restore: () => void;
-  // tslint:enable: no-any
-
-  private constructor() {
-    this.save = [console.log, console.warn, console.error, console.dir];
-    this.restore = () => [console.log, console.warn, console.error, console.dir] = this.save;
-  }
 
   /**
    * Captures the arguments for any calls to console.log and its friends while fn is being executed.
@@ -60,67 +53,60 @@ export class ConCap {
    * If fn is asynchronous, returns a Promise with a ConCap whose `result` holds the awaited fn return.
    * In both cases, the log/warn/etc fields hold whatever `fn` wrote to the corresponding console function.
    */
-  // tslint:disable-next-line: no-any
   static capture(fn: () => any): any {
     const cc = new ConCap();
     console.log   = (...args) => cc.log.push([...args]);
     console.warn  = (...args) => cc.warn.push([...args]);
     console.error = (...args) => cc.error.push([...args]);
     console.dir   = (...args) => cc.dir.push([...args]);
-
-    const result = fn();
-    if (result && typeof result.then === 'function') {
-      let resolve, reject;
-      const wrap = new Promise((res, rej) => [resolve, reject] = [res, rej]);
-      result.then(value => {
-        cc.result = value;
-        cc.restore();
-        resolve(cc);
-      }, err => {
-        cc.restore();
-        reject(err)
-      });
-      return wrap;
-    } else {
-      cc.result = result;
-      cc.restore();
-      return cc;
-    }
+    return cc.go(true, fn);
   }
 
   /** Discards all calls to console.log and its friends. Returns the result of fn. */
-  // tslint:disable-next-line: no-any
   static silence(fn: () => any): any {
     const cc = new ConCap();
     console.log = console.warn = console.error = console.dir = () => {};
-
-    const result = fn();
-    if (result && typeof (result as any).then === 'function') {
-      let resolve, reject;
-      const wrap = new Promise((res, rej) => [resolve, reject] = [res, rej]);
-      result.then(value => {
-        cc.restore();
-        resolve(value);
-      }, err => {
-        cc.restore();
-        reject(err)
-      });
-      return wrap;
-    } else {
-      cc.restore();
-      return result;
-    }
+    return cc.go(false, fn);
   }
 
   /** Returns a function that will invoke `fn` and capture anything logged to the console. */
-  // tslint:disable-next-line: no-any
   static wrapCaptured<T, Args extends any[]>(fn: (...args: Args) => any): (...args: Args) => any {
     return (...args: Args) => ConCap.capture(() => fn(...args));
   }
 
   /** Returns a function that will invoke `fn` without logging anything to the console. */
-  // tslint:disable-next-line: no-any
   static wrapSilent<T, Args extends any[]>(fn: (...args: Args) => any): (...args: Args) => any {
     return (...args: Args) => ConCap.silence(() => fn(...args));
   }
+
+  private constructor() {
+    this.save = [console.log, console.warn, console.error, console.dir];
+  }
+
+  private restore() {
+    [console.log, console.warn, console.error, console.dir] = this.save;
+  }
+
+  private go(capture: boolean, fn: () => any): any {
+    const result = fn();
+    if (result && typeof result.then === 'function') {
+      let resolve;
+      let reject;
+      const wrap = new Promise((rs, rj) => [resolve, reject] = [rs, rj]);
+      result.then(value => {
+        this.result = value;
+        this.restore();
+        resolve(capture ? this : value);
+      }, err => {
+        this.restore();
+        reject(err);
+      });
+      return wrap;
+    } else {
+      this.result = result;
+      this.restore();
+      return capture ? this : result;
+    }
+  }
 }
+// tslint:enable: no-any
