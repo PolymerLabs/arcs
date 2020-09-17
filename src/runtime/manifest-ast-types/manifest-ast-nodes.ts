e843ce28b1e437c5b2a98d5440b9fa03161cac23
Piotr Swigon <piotr.swigon@gmail.com>
2020-09-17 14:11:50 -0700

    sum() aggregation function for Paxel (#6153)

--- src/runtime/manifest-ast-types/manifest-ast-nodes.ts
+++ src/runtime/manifest-ast-types/manifest-ast-nodes.ts
@@ -848,6 +848,7 @@ export enum PaxelFunctionName {
   Min = 'min',
   Max = 'max',
   Average = 'average',
+  Sum = 'sum',
   Count = 'count',
   Union = 'union',
   First = 'first'
@@ -901,6 +902,7 @@ export const PAXEL_FUNCTIONS: PaxelFunction[] = [
   makePaxelNumericFunction(PaxelFunctionName.Max, 1, 'Number'),
   makePaxelNumericFunction(PaxelFunctionName.Average, 1, 'Number'),
   makePaxelNumericFunction(PaxelFunctionName.Count, 1, 'Number'),
+  makePaxelNumericFunction(PaxelFunctionName.Sum, 1, 'Number'),
   makePaxelCollectionTypeFunction(PaxelFunctionName.Union, -1),
   makePaxelCollectionTypeFunction(PaxelFunctionName.First, 1)
 ];
