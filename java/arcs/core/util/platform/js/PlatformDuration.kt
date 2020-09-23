376d8cb507aa4d3c416000684ce1478feca95ed0
Josh Pratt <jp10010101010000@gmail.com>
2020-09-23 17:27:18 +1000

    Add link to task for adding new type support for native and js platforms

--- java/arcs/core/util/platform/js/PlatformDuration.kt
+++ java/arcs/core/util/platform/js/PlatformDuration.kt
@@ -13,11 +13,11 @@ package arcs.core.util
 
 /** Provides a platform-dependent version of [ArcsDuration]. */
 class PlatformDuration {
-    override fun toString(): String = TODO("Add support for ArcsDuration in Kotlin JS")
+    override fun toString(): String = TODO("Add support for ArcsDuration in Kotlin JS") // See b/169213588
 
-    fun toMillis(): String = TODO("Add support for ArcsDuration in Kotlin JS")
+    fun toMillis(): String = TODO("Add support for ArcsDuration in Kotlin JS") // See b/169213588
 
-    fun compareTo(other: PlatformDuration): Int = TODO("Add support for ArcsDuration in Kotlin JS")
+    fun compareTo(other: PlatformDuration): Int = TODO("Add support for ArcsDuration in Kotlin JS") // See b/169213588
 
     override fun equals(other: Any?): Boolean {
         if (other == null || other !is PlatformDuration) return false
@@ -27,16 +27,16 @@ class PlatformDuration {
     companion object {
         @Suppress("UNUSED_PARAMETER")
         fun ofMillis(value: Long): PlatformDuration =
-            TODO("Add support for ArcsDuration in Kotlin JS")
+            TODO("Add support for ArcsDuration in Kotlin JS") // See b/169213588
 
         @Suppress("UNUSED_PARAMETER")
         fun valueOf(value: Long): PlatformDuration =
-            TODO("Add support for ArcsDuration in Kotlin JS")
+            TODO("Add support for ArcsDuration in Kotlin JS") // See b/169213588
 
         @Suppress("UNUSED_PARAMETER")
-        fun ofDays(days: Long): PlatformDuration = TODO("Add support for ArcsDuration in Kotlin JS")
+        fun ofDays(days: Long): PlatformDuration = TODO("Add support for ArcsDuration in Kotlin JS") // See b/169213588
         @Suppress("UNUSED_PARAMETER")
         fun ofHours(days: Long): PlatformDuration =
-            TODO("Add support for ArcsDuration in Kotlin JS")
+            TODO("Add support for ArcsDuration in Kotlin JS") // See b/169213588
     }
 }
