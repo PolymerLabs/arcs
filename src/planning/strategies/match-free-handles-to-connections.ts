bf50231cfb76be259000125cc63a92aa7eadc36a
Shane Stephens <shanestephens@google.com>
2020-09-24 16:34:33 +1000

    lint

--- src/planning/strategies/match-free-handles-to-connections.ts
+++ src/planning/strategies/match-free-handles-to-connections.ts
@@ -1,4 +1,13 @@
-import { HandleConnectionSpec } from '../../runtime/arcs-types/particle-spec.js';
+/**
+ * @license
+ * Copyright 2019 Google LLC.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+import {HandleConnectionSpec} from '../../runtime/arcs-types/particle-spec.js';
 /**
  * @license
  * Copyright (c) 2017 Google Inc. All rights reserved.
@@ -20,7 +29,7 @@ export class MatchFreeHandlesToConnections extends Strategy {
   async generate(inputParams) {
     return StrategizerWalker.over(this.getResults(inputParams), new class extends StrategizerWalker {
       onPotentialHandleConnection(recipe: Recipe, particle: Particle, connectionSpec: HandleConnectionSpec) {
-        const freeHandles = recipe.handles.filter(h => h.connections.length == 0);
+        const freeHandles = recipe.handles.filter(h => h.connections.length === 0);
 
         return freeHandles.map(handle => {
           return (recipe: Recipe, particle: Particle, connectionSpec: HandleConnectionSpec) => {
@@ -28,7 +37,7 @@ export class MatchFreeHandlesToConnections extends Strategy {
             particle.addConnectionName(connectionSpec.name).connectToHandle(cloneHandle);
             return 1;
           };
-        })
+        });
       }
     }(StrategizerWalker.Permuted), this);
   }
