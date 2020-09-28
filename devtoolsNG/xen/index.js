e15b66fd3ca5453de02c43e38473c03ed6713a40
Scott J. Miles <sjmiles@google.com>
2020-09-28 15:30:57 -0700

    fix lint errors

--- devtoolsNG/xen/index.js
+++ devtoolsNG/xen/index.js
@@ -1,3 +1,12 @@
+/**
+ * @license
+ * Copyright (c) 2020 Google Inc. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * Code distributed by Google as part of this project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
 import './elements/inspector/data-explorer.js';
 
 const {logElt, opsByStoreElt, de} = window; // extract DOM elements
@@ -17,21 +26,21 @@ const addRawEntry = function(raw) {
   entries.push(entry);
   createUi({msg: entry}, logElt);
   populateCollatedStoreEntries();
-}
+};
 
 const processRawEntry = (rawEntry) => {
   let data = rawEntry;
   try {
     data = JSON.parse(rawEntry);
-  } catch(x) {
+  } catch (x) {
     //
   }
   return data;
-}
+};
 
 const populateCollatedStoreEntries = () => {
   opsByStoreElt.object = collateStoreMessages(entries);
-}
+};
 
 const collateStoreMessages = entries => {
   let data = entries.map(entry => entry.kind === 'StoreMessage' ? entry : null);
@@ -42,7 +51,7 @@ const collateStoreMessages = entries => {
     id: entry.message.id,
     operations: entry.message.operations
   }));
-  let results = {};
+  const results = {};
   data.forEach(entry => {
     const id = `id[${entry.id}]`;
     const group = results[id] || (results[id] = []);
@@ -56,12 +65,12 @@ const collateStoreMessages = entries => {
   const websocket = new WebSocket('ws://localhost:12345');
   websocket.onmessage = msg => {
     addRawEntry(msg.data);
-  }
+  };
   websocket.onerror = e => {
     console.log('No connection found');
   };
   websocket.onclose = e => {
     console.log(`Websocket closing. ${e}`);
     addRawEntry(`Web Socket is Closed, try restarting the Arc.`);
-  }
+  };
 })();
