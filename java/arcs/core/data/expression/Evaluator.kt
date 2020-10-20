bda28b69899cadcf3345775e336e55cec381769c
Ray Cromwell <cromwellian@gmail.com>
2020-10-19 21:14:57 -0700

    Paxel Type Inferencer (#6369)
    Will be used for the following:
    * Checking Paxel Expressions against HandleConnection types before evaluating
    * Linting/Suggesting tighter bounds on inputs
    * Allowing some `GlobalFunction` expressions to know their expected return type
    
    Theory of operation: A form of abstract interpretation
    * `InferredType` hierarchy represents types
    * Literals become `InferredType.Primitive`
    * Mathematical operations on compatible types widen to the biggest type that encloses operands
    * Unary/Binary ops produce Numeric or Boolean types
    * Functions may produce `UnionType` as can the `IfNull` operator
    * QualifiedExpressions are `SeqType<ScopeType>` -> `SeqType<ScopeType>`
    
    * Populate a scope with the types of variables, instead of their values
    * Start at bottom of tree and propagate types upward
    * Each node is "evaluated" similar to how Evaluator would, but without iteration over sequences
    * `InferredType.isAssignableFrom()` allows one to check if the resulting type is assignment compatible with an expected type
    
    ** Followup:
    ** Wire up into `EvaluatorParticle`
    
    Closes #6369
    
    PiperOrigin-RevId: 337991835

--- java/arcs/core/data/expression/Evaluator.kt
+++ java/arcs/core/data/expression/Evaluator.kt
@@ -153,9 +153,30 @@ sealed class GlobalFunction(val name: String) {
   /** Functions accept a varargs list of [Sequence] and return any type. */
   abstract fun invoke(evaluator: ExpressionEvaluator, args: List<Any?>): Any?
 
+  /**
+   * Returns the [InferredType] given the input argument types.
+   * [expr] denotes the expression encapsulating the [GlobalFunction]
+   */
+  abstract fun inferredType(
+    expr: Expression<*>,
+    evaluator: TypeEvaluator,
+    args: List<InferredType>
+  ): InferredType
+
   object Now : GlobalFunction("now") {
     override fun invoke(evaluator: ExpressionEvaluator, args: List<Any?>) =
       evaluator.time.currentTimeMillis
+
+    override fun inferredType(
+      expr: Expression<*>,
+      evaluator: TypeEvaluator,
+      args: List<InferredType>
+    ): InferredType {
+      evaluator.require(expr, args.isEmpty()) {
+        "now() does not allow arguments."
+      }
+      return InferredType.Primitive.LongType
+    }
   }
 
   /** Performs [Sequence.union] of two [Sequence]s. */
@@ -164,42 +185,134 @@ sealed class GlobalFunction(val name: String) {
       toSequence<Any>(args[0]).asIterable().union(
         toSequence<Any>(args[1]).asIterable()
       ).asSequence()
+
+    override fun inferredType(
+      expr: Expression<*>,
+      evaluator: TypeEvaluator,
+      args: List<InferredType>
+    ): InferredType {
+      evaluator.require(expr, args.isNotEmpty()) {
+        "union() must be called with at least one argument"
+      }
+      args.forEach {
+        evaluator.require(expr, it is InferredType.SeqType) {
+          "union() may only be called with sequences, but was called with $it"
+        }
+      }
+
+      val seqSets: Set<InferredType.SeqType> = (args.toSet() as Set<InferredType.SeqType>)
+      return InferredType.SeqType(InferredType.UnionType(seqSets.map { it.type }.toSet()))
+    }
   }
 
   /** Find the maximum of a [Sequence]. */
   object Max : GlobalFunction("max") {
     override fun invoke(evaluator: ExpressionEvaluator, args: List<Any?>) =
       toSequence<Comparable<Comparable<*>>>(args[0]).max()
+
+    override fun inferredType(
+      expr: Expression<*>,
+      evaluator: TypeEvaluator,
+      args: List<InferredType>
+    ): InferredType {
+      return checkInnerSequenceType("max", args, expr, evaluator)
+    }
   }
 
   /** Find the minimum of a [Sequence]. */
   object Min : GlobalFunction("min") {
     override fun invoke(evaluator: ExpressionEvaluator, args: List<Any?>) =
       toSequence<Comparable<Comparable<*>>>(args[0]).min()
+
+    override fun inferredType(
+      expr: Expression<*>,
+      evaluator: TypeEvaluator,
+      args: List<InferredType>
+    ): InferredType {
+      return checkInnerSequenceType("min", args, expr, evaluator)
+    }
   }
 
   /** Find the average of a [Sequence]. */
   object Average : GlobalFunction("average") {
     override fun invoke(evaluator: ExpressionEvaluator, args: List<Any?>) =
       toSequence<Int>(args[0]).average()
+
+    override fun inferredType(
+      expr: Expression<*>,
+      evaluator: TypeEvaluator,
+      args: List<InferredType>
+    ): InferredType {
+      checkInnerSequenceType("average", args, expr, evaluator)
+      return InferredType.Primitive.DoubleType
+    }
   }
 
   /** Find the average of a [Sequence]. */
   object Sum : GlobalFunction("sum") {
     override fun invoke(evaluator: ExpressionEvaluator, args: List<Any?>) =
       toSequence<Int>(args[0]).sum()
+
+    override fun inferredType(
+      expr: Expression<*>,
+      evaluator: TypeEvaluator,
+      args: List<InferredType>
+    ): InferredType {
+      return checkInnerSequenceType("sum", args, expr, evaluator)
+    }
   }
 
   /** Count the number of elements in a [Sequence]. */
   object Count : GlobalFunction("count") {
     override fun invoke(evaluator: ExpressionEvaluator, args: List<Any?>) =
       toSequence<Any>(args[0]).count()
+
+    override fun inferredType(
+      expr: Expression<*>,
+      evaluator: TypeEvaluator,
+      args: List<InferredType>
+    ): InferredType {
+      checkInnerSequenceType("count", args, expr, evaluator) { true }
+      return InferredType.Primitive.IntType
+    }
   }
 
   /** Return the first item of a [Sequence]. */
   object First : GlobalFunction("first") {
     override fun invoke(evaluator: ExpressionEvaluator, args: List<Any?>) =
       toSequence<Any>(args[0]).firstOrNull()
+
+    override fun inferredType(
+      expr: Expression<*>,
+      evaluator: TypeEvaluator,
+      args: List<InferredType>
+    ): InferredType {
+      checkInnerSequenceType("first", args, expr, evaluator) { true }
+      return InferredType.UnionType(
+        InferredType.Primitive.NullType,
+        (args[0] as InferredType.SeqType).type
+      )
+    }
+  }
+
+  protected fun checkInnerSequenceType(
+    name: String,
+    args: List<InferredType>,
+    expr: Expression<*>,
+    evaluator: TypeEvaluator,
+    innerCheck: (InferredType) -> Boolean = { it is InferredType.Numeric }
+  ): InferredType {
+    evaluator.require(expr, args.size == 1) {
+      "$name() may only be called with one argument"
+    }
+    evaluator.require(expr, args.isNotEmpty() && args[0] is InferredType.SeqType) {
+      "argument to $name() must be a sequence"
+    }
+    val innerType = (args[0] as InferredType.SeqType).type
+    evaluator.require(expr, innerCheck(innerType)) {
+      "argument to $name must be a sequence of numeric types"
+    }
+    return innerType
   }
 
   companion object {
---
+++ java/arcs/core/data/expression/InferredType.kt
@@ -0,0 +1,143 @@
+package arcs.core.data.expression
+
+import arcs.core.util.BigInt
+import kotlin.reflect.KClass
+
+/**
+ * Matches [widenAndApply] in Expression.kt
+ *
+ * The types are listed in terms of decreasing width, so that a type at position n can always
+ * be widened to a type at position n - 1.
+ */
+private val widenOrder = listOf<InferredType.Primitive>(
+  InferredType.Primitive.NumberType,
+  InferredType.Primitive.DoubleType,
+  InferredType.Primitive.FloatType,
+  InferredType.Primitive.BigIntType,
+  InferredType.Primitive.LongType,
+  InferredType.Primitive.IntType,
+  InferredType.Primitive.ShortType,
+  InferredType.Primitive.ByteType
+)
+
+/** Convert a [KClass] into an appropriate [InferredType] */
+fun KClass<*>.toType() = InferredType.Primitive.of(this)
+
+/** Widen a list of types to the largest type that encompasses all of them. */
+fun widen(vararg types: InferredType): InferredType.Primitive {
+  types.forEach {
+    require(it is InferredType.Numeric) {
+      "$it is not numeric"
+    }
+  }
+
+  return widenOrder.firstOrNull {
+    types.contains(it)
+  } ?: throw IllegalArgumentException("$types contains incompatible types.")
+}
+
+/**
+ * An [InferredType] is based on following a tree of DSL expressions from the leaves to the root,
+ * flowing types upward so that each node can be assigned a conservative bound on the types it
+ * may output.
+ */
+sealed class InferredType {
+  /** True of this type can be assigned the [other] type. */
+  open fun isAssignableFrom(other: InferredType): Boolean = this == other
+
+  /** Union two types together to create an [UnionType]. */
+  fun union(other: InferredType) = UnionType(this, other)
+
+  /** Subtract the type from this type if possible. */
+  open fun nonNull(): InferredType = this
+
+  /** Represents all primitive types such as numbers, text, booleans, etc. */
+  sealed class Primitive(val kClass: KClass<*>) : InferredType() {
+    override fun isAssignableFrom(other: InferredType): Boolean = when {
+      this is Numeric && other is Numeric -> widen(this, other) == this
+      else -> this == other
+    }
+
+    override fun toString() = "${kClass.simpleName ?: super.toString()}"
+
+    object DoubleType : Primitive(Double::class), Numeric
+    object FloatType : Primitive(Float::class), Numeric
+    object BigIntType : Primitive(BigInt::class), Numeric
+    object LongType : Primitive(Long::class), Numeric
+    object IntType : Primitive(Int::class), Numeric
+    object ShortType : Primitive(Short::class), Numeric
+    object ByteType : Primitive(Byte::class), Numeric
+    object NumberType : Primitive(Number::class), Numeric
+    object BooleanType : Primitive(Boolean::class)
+    object TextType : Primitive(String::class)
+    object NullType : Primitive(NullType::class)
+
+    companion object {
+      private val kclassToType by lazy {
+        listOf(
+          DoubleType,
+          FloatType,
+          BigIntType,
+          LongType,
+          IntType,
+          ShortType,
+          ByteType,
+          NumberType,
+          BooleanType,
+          TextType,
+          NullType
+        ).associateBy({ it.kClass }, { it })
+      }
+
+      /** All primitive types. */
+      val allTypes by lazy {
+        kclassToType.values.toSet()
+      }
+
+      /** Given a [KClass] return the corresponding [InferredType]. */
+      fun of(kClass: KClass<*>) = requireNotNull(kclassToType[kClass]) {
+        "Unable to map $kClass to an InferredType."
+      }
+    }
+  }
+
+  /** Represents union of several possible types. */
+  data class UnionType(val types: Set<InferredType>) : InferredType() {
+    constructor(vararg types: InferredType) : this(setOf(*types))
+
+    override fun nonNull(): InferredType = UnionType(types.map { it.nonNull() }.filterNot {
+      it is Primitive.NullType
+    }.toSet())
+
+    override fun toString() =
+      "${types.joinToString(separator = "|")}"
+
+    override fun isAssignableFrom(other: InferredType): Boolean = when (other) {
+      is UnionType -> other.types.all { this.isAssignableFrom(it) }
+      else -> types.any { it.isAssignableFrom(other) }
+    }
+  }
+
+  /** Represents a seequence of values of the given type. */
+  data class SeqType(val type: InferredType) : InferredType() {
+    override fun toString() = "Sequence<$type>"
+    override fun isAssignableFrom(other: InferredType): Boolean = when (other) {
+      is SeqType -> type.isAssignableFrom(other.type)
+      else -> false
+    }
+  }
+
+  /** Represents a type corresponding to a scope with variables of various types. */
+  data class ScopeType(val scope: Expression.Scope) : InferredType() {
+    override fun toString() = "$scope"
+    override fun isAssignableFrom(other: InferredType): Boolean {
+      if (other !is ScopeType) {
+        return false
+      }
+      return scope == other.scope
+    }
+  }
+
+  /** Tagging interface for all [Primitive] types that are numeric. */
+  interface Numeric
+}
