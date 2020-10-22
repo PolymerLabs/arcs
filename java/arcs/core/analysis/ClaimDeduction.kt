7af17102fb395d748321fb4a45751a2e6826dd24
Alex Rosengarten <alxr@google.com>
2020-10-22 12:12:37 -0700

    Refresh ClaimDeduction.kt.

--- java/arcs/core/analysis/ClaimDeduction.kt
+++ java/arcs/core/analysis/ClaimDeduction.kt
@@ -10,14 +10,14 @@ import arcs.core.data.expression.Expression
 /**
  * Deduce [Claim]s on Paxel [Expression]s from all connections on a [Recipe.Particle].
  *
- * See [DependencyGraph] for more information.
+ * See [DependencyNode] for more information.
  */
 fun RecipeGraph.Node.Particle.deduceClaims(): List<Claim> {
   return particle.spec.connections.values
     .filter { connection -> connection.expression != null }
     .flatMap { connection ->
       val root = connection.expression?.analyze()
-      require(root is DependencyGraph.Associate) {
+      require(root is DependencyNode.AssociationNode) {
         "Expression on '${particle.spec.name}.${connection.name}' is invalid."
       }
       root.toClaims(particle, connection)
@@ -28,38 +28,44 @@ fun RecipeGraph.Node.Particle.deduceClaims(): List<Claim> {
 private fun List<String>.asFields() = map { AccessPath.Selector.Field(it) }
 
 /**
- * This flattens nested [DependencyGraph]s into a map of [Path]s and terminal [DependencyGraph]s.
+ * This flattens nested [DependencyNode]s into a map of [Path]s and terminal [DependencyNode]s.
  *
  * Example:
  *   ```
- *   DependencyGraph.Associate(
- *     "a" to DependencyGraph.Associate(
- *        "x" to DependencyGraph.Associate(
- *          "q": DependencyGraph.Input("input", "foo")
+ *   DependencyNode.AssociationNode(
+ *     "a" to DependencyNode.AssociationNode(
+ *        "x" to DependencyNode.AssociationNode(
+ *          "q": DependencyNode.Input("input", "foo")
  *        ),
- *        "y" to DependencyGraph.Input("input", "bar")
+ *        "y" to DependencyNode.Input("input", "bar")
  *     ),
- *     "b" to DependencyGraph.Derive(listOf("input", "foo"), listOf("input", "bar"))
+ *     "b" to DependencyNode.DerivedFrom(
+ *       DependencyNode.Input("input", "foo"),
+ *       DependencyNode.Input("input", "bar")
+ *     )
  *   )
  *   ```
  * This would be flattened to:
  *
  *   ```
  *   mapOf(
- *     listOf("a", "x", "q") to DependencyGraph.Input("input", "foo"),
- *     listOf("a", "y") to DependencyGraph.Input("input", "bar"),
- *     listOf("b") to DependencyGraph.Derive(listOf("input", "foo"), listOf("input", "bar"))
+ *     listOf("a", "x", "q") to DependencyNode.Input("input", "foo"),
+ *     listOf("a", "y") to DependencyNode.Input("input", "bar"),
+ *     listOf("b") to DependencyNode.DerivedFrom(
+ *       DependencyNode.Input("input", "foo"),
+ *       DependencyNode.Input("input", "bar")
+ *     )
  *   )
  *   ```
  */
-private fun DependencyGraph.Associate.flatten(
+private fun DependencyNode.AssociationNode.flatten(
   prefix: List<String> = emptyList()
-): Map<List<String>, DependencyGraph> {
-  val output = mutableMapOf<List<String>, DependencyGraph>()
+): Map<List<String>, DependencyNode> {
+  val output = mutableMapOf<List<String>, DependencyNode>()
   for ((key, value) in this.associations) {
     val path = prefix + listOf(key)
     when (value) {
-      is DependencyGraph.Associate -> output += value.flatten(path)
+      is DependencyNode.AssociationNode -> output += value.flatten(path)
       else -> output += path to value
     }
   }
@@ -67,27 +73,28 @@ private fun DependencyGraph.Associate.flatten(
 }
 
 /**
- * Converts a [DependencyGraph.Associate] into a list of [Claim]s, given contextual information
+ * Converts a [DependencyNode.AssociationNode] into a list of [Claim]s, given contextual information
  * about the particle.
  *
- * [DependencyGraph.Associate]s is a DAG-like structure that can represent data flow relationships
- * from the target `connection`s to other [HandleConnectionSpec]s in the [ParticleSpec].
+ * [DependencyNode.AssociationNode]s is a DAG-like structure that can represent data flow
+ * relationships from the target `connection`s to other [HandleConnectionSpec]s in the
+ * [ParticleSpec].
  *
- * This [flatten]s the nested [DependencyGraph.Associate] and then transforms its data into
+ * This [flatten]s the nested [DependencyNode.AssociationNode] and then transforms its data into
  * [Claim.DerivesFrom] statements.
  */
-private fun DependencyGraph.Associate.toClaims(
+private fun DependencyNode.AssociationNode.toClaims(
   particle: Recipe.Particle,
   connection: HandleConnectionSpec
 ): List<Claim> {
   return this.flatten().flatMap { (lhsPath, graph) ->
     val lhs = AccessPath(particle, connection, lhsPath.asFields())
     when (graph) {
-      is DependencyGraph.Input -> listOf(Claim.DerivesFrom(lhs, graph.toAccessPath(particle)))
-      is DependencyGraph.Derive -> graph.toAccessPaths(particle)
+      is DependencyNode.Input -> listOf(Claim.DerivesFrom(lhs, graph.toAccessPath(particle)))
+      is DependencyNode.DerivedFrom -> graph.toAccessPaths(particle)
         .map { rhs -> Claim.DerivesFrom(lhs, rhs) }
-      is DependencyGraph.Associate -> throw UnsupportedOperationException(
-        "Associate is not a terminal DependencyGraph."
+      is DependencyNode.AssociationNode -> throw UnsupportedOperationException(
+        "AssociationNode is not a terminal DependencyNode."
       )
     }
   }
@@ -107,10 +114,10 @@ private fun Recipe.Particle.connectionFrom(path: List<String>): HandleConnection
 private fun List<String>.asAccessPath(particle: Recipe.Particle): AccessPath =
   AccessPath(particle, particle.connectionFrom(this), this.drop(1).asFields())
 
-/** Converts a [DependencyGraph.Input] into an [AccessPath], given Particle context. */
-private fun DependencyGraph.Input.toAccessPath(particle: Recipe.Particle) =
+/** Converts a [DependencyNode.Input] into an [AccessPath], given Particle context. */
+private fun DependencyNode.Input.toAccessPath(particle: Recipe.Particle): AccessPath =
   path.asAccessPath(particle)
 
-/** Converts a [DependencyGraph.Derive] into [AccessPath]s, given Particle context. */
-private fun DependencyGraph.Derive.toAccessPaths(particle: Recipe.Particle): List<AccessPath> =
-  this.paths.map { path -> path.asAccessPath(particle) }
+/** Converts a [DependencyNode.DerivedFrom] into [AccessPath]s, given Particle context. */
+private fun DependencyNode.DerivedFrom.toAccessPaths(particle: Recipe.Particle): List<AccessPath> =
+  this.inputs.map { input -> input.path.asAccessPath(particle) }
