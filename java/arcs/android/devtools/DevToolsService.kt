90a47653285d7606f795d9b65284bca178dfbed8
Sarah Heimlich <heimlich@google.com>
2020-10-26 19:41:25 -0700

    Devtools proto (#6453)
    - Add blanket DevToolsMessage proto
    - Only send across DevToolsMessageProtos
    - Refactor creation of protos to simplify
    
    cc: @piotrswigon @jasonwyatt
    
    Closes #6453
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/polymerlabs/arcs/pull/6453 from SHeimlich:devtools-proto d7161b1736ab9c084f06ba751c5e1cb15fbd9376
    PiperOrigin-RevId: 339168407

--- java/arcs/android/devtools/DevToolsService.kt
+++ java/arcs/android/devtools/DevToolsService.kt
@@ -63,9 +63,13 @@ open class DevToolsService : Service() {
   private var refModeStoreCallbackToken: Int = -1
   private var directStoreCallbackToken: Int = -1
 
-  private val initProto = DevtoolsMessage.DevToolsInitProto.newBuilder()
-    .setDevToolsVersion(0)
-    .setPlatform(DevtoolsMessage.DevToolsInitProto.Platform.ANDROID)
+  private val initProto = DevtoolsMessage.DevToolsMessageProto.newBuilder()
+    .setInit(
+      DevtoolsMessage.DevToolsInitProto.newBuilder()
+        .setDevToolsVersion(0)
+        .setPlatform(DevtoolsMessage.DevToolsInitProto.Platform.ANDROID)
+        .build()
+    )
     .build()
 
   override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -85,12 +89,13 @@ open class DevToolsService : Service() {
         object : IDevToolsProxyCallback.Stub() {
           override fun onProxyMessage(proxyMessage: ByteArray, storageKey: String) {
             scope.launch {
-              val proto = DevtoolsMessage.DevToolsProxyMessageProto.newBuilder()
-                .setProxyMessage(proxyMessage.decodeProxyMessage().toProto())
-                .setStoreType(DevtoolsMessage.DevToolsProxyMessageProto.StoreType.REFERENCE_MODE)
-                .setStorageKey(storageKey)
-                .build()
-              devToolsServer.send(proto.toString())
+              createAndSendProxyMessageProto(
+                proxyMessage,
+                DevtoolsMessage.DevToolsProxyMessageProto.StoreType.REFERENCE_MODE,
+                storageKey
+              )
+
+              // Remove once the client uses protos
               createAndSendProxyMessages(
                 proxyMessage.decodeProxyMessage(),
                 REFERENCEMODE,
@@ -105,6 +110,13 @@ open class DevToolsService : Service() {
         object : IDevToolsProxyCallback.Stub() {
           override fun onProxyMessage(proxyMessage: ByteArray, storageKey: String) {
             scope.launch {
+              createAndSendProxyMessageProto(
+                proxyMessage,
+                DevtoolsMessage.DevToolsProxyMessageProto.StoreType.DIRECT,
+                storageKey
+              )
+
+              // Remove once the client uses protos
               createAndSendProxyMessages(
                 proxyMessage.decodeProxyMessage(),
                 DIRECT,
@@ -190,6 +202,7 @@ open class DevToolsService : Service() {
     )
   }
 
+  // TODO(171754270): Remove once the client uses protos
   private fun createAndSendProxyMessages(
     actualMessage: ProxyMessage<CrdtData, CrdtOperation, Any?>,
     storeType: String,
@@ -211,6 +224,24 @@ open class DevToolsService : Service() {
     )
     devToolsServer.send(message.toJson())
     devToolsServer.send(rawMessage.toJson())
+  }
+
+  fun createAndSendProxyMessageProto(
+    proxyMessage: ByteArray,
+    storeType: DevtoolsMessage.DevToolsProxyMessageProto.StoreType,
+    storageKey: String
+  ) {
+    val messageProto = DevtoolsMessage.DevToolsProxyMessageProto.newBuilder()
+      .setProxyMessage(proxyMessage.decodeProxyMessage().toProto())
+      .setStoreType(storeType)
+      .setStorageKey(storageKey)
+      .build()
+
+    val proto = DevtoolsMessage.DevToolsMessageProto.newBuilder()
+      .setProxyMessage(messageProto)
+      .build()
+
+    devToolsServer.send(proto.toString())
   }
 
   @VisibleForTesting(otherwise = VisibleForTesting.PACKAGE_PRIVATE)
