f414f9e4637ec6233cd2d90a2e3859607e260767
Scott J. Miles <sjmiles@google.com>
2020-11-18 15:03:18 -0800

    clean ups

--- src/planning/planner.ts
+++ src/planning/planner.ts
@@ -73,8 +73,6 @@ export interface PlannerInitOptions {
   runtime?: Runtime;
 }
 
-const gRuntime = new Runtime();
-
 export class Planner implements InspectablePlanner {
   public arc: Arc;
   runtime: Runtime;
