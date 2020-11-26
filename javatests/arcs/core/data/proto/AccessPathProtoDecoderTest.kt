c2a9d42709708f0d03c44048a76f304a59ed89c8
Arcs C3PO <arcs-c3po@google.com>
2020-11-26 01:22:26 -0800

    Add additional tests for AccessPathProtoDecoder
    The additional tests cover additional code paths in `encode` and error paths in `decode`.
    
    This change also updates decoding exception messages to be clearer.
    
    PiperOrigin-RevId: 344382665

--- javatests/arcs/core/data/proto/AccessPathProtoDecoderTest.kt
+++ javatests/arcs/core/data/proto/AccessPathProtoDecoderTest.kt
@@ -3,6 +3,8 @@ package arcs.core.data.proto
 import arcs.core.data.AccessPath
 import arcs.core.data.HandleConnectionSpec
 import arcs.core.data.HandleMode
+import arcs.core.data.ParticleSpec
+import arcs.core.data.Recipe
 import arcs.core.data.TypeVariable
 import com.google.common.truth.Truth.assertThat
 import kotlin.test.assertFailsWith
@@ -60,6 +62,72 @@ class AccessPathProtoDecoderTest {
   }
 
   @Test
+  fun encode_handleConnectionSpec() {
+    val expected = AccessPathProto.newBuilder()
+    expected.handle = AccessPathProto.HandleRoot.newBuilder()
+      .setHandleConnection("input")
+      .setParticleSpec("TestSpec")
+      .build()
+
+    val handleConnectionSpec = HandleConnectionSpec(
+      "input",
+      HandleMode.Write,
+      TypeVariable("input")
+    )
+    val accessPath = AccessPath("TestSpec", handleConnectionSpec)
+    val encoded = accessPath.encode()
+
+    assertThat(encoded).isEqualTo(expected.build())
+  }
+
+  @Test
+  fun encode_handleConnection() {
+    val expected = AccessPathProto.newBuilder()
+    expected.handle = AccessPathProto.HandleRoot.newBuilder()
+      .setHandleConnection("input")
+      .setParticleSpec("TestSpec")
+      .build()
+
+    val particleSpec = ParticleSpec("TestSpec", emptyMap(), "Location")
+    val particle = Recipe.Particle(particleSpec, emptyList())
+    val handleConnectionSpec = HandleConnectionSpec(
+      "input",
+      HandleMode.Write,
+      TypeVariable("input")
+    )
+    val accessPath = AccessPath(particle, handleConnectionSpec)
+
+    assertThat(accessPath.encode()).isEqualTo(expected.build())
+  }
+
+  @Test
+  fun encode_handle() {
+    val expected = AccessPathProto.newBuilder()
+    expected.handle = AccessPathProto.HandleRoot.newBuilder()
+      .setHandleConnection("handle1")
+      .build()
+
+    val handle = Recipe.Handle(
+      "handle1",
+      Recipe.Handle.Fate.MAP,
+      TypeVariable("handle1")
+    )
+    val accessPath = AccessPath(handle)
+
+    assertThat(accessPath.encode()).isEqualTo(expected.build())
+  }
+
+  @Test
+  fun encode_store() {
+    val expected = AccessPathProto.newBuilder()
+    expected.storeId = "store_id"
+
+    val accessPath = AccessPath(AccessPath.Root.Store("store_id"))
+
+    assertThat(accessPath.encode()).isEqualTo(expected.build())
+  }
+
+  @Test
   fun decode_detectsMissingConnections() {
     val proto = AccessPathProto.newBuilder()
       .setHandle(AccessPathProto.HandleRoot.newBuilder().setHandleConnection("input"))
@@ -70,5 +138,27 @@ class AccessPathProtoDecoderTest {
     assertThat(exception)
       .hasMessageThat()
       .contains("Connection 'input' not found in connection specs!")
+  }
+
+  @Test
+  fun decode_emptyRootFails() {
+    val proto = AccessPathProto.newBuilder().build()
+    val exception = assertFailsWith<UnsupportedOperationException> {
+      proto.decode(emptyMap())
+    }
+    assertThat(exception)
+      .hasMessageThat()
+      .isEqualTo("Unsupported AccessPathProto.Root: ROOT_NOT_SET")
+  }
+
+  @Test
+  fun decode_selector_emptySelectorFails() {
+    val proto = AccessPathProto.Selector.newBuilder().build()
+    val exception = assertFailsWith<UnsupportedOperationException> {
+      proto.decode()
+    }
+    assertThat(exception)
+      .hasMessageThat()
+      .isEqualTo("Unsupported AccessPathProto.Selector: SELECTOR_NOT_SET")
   }
 }
