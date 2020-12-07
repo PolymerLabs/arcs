cd1cd8783c665b0196bac43e370ab2f742e40c88
David Drysdale <drysdale@google.com>
2020-12-07 00:08:03 -0800

    More unit test variants for Parcelables
    PiperOrigin-RevId: 346031350

--- javatests/arcs/android/host/parcelables/ParcelableAnnotationParamTest.kt
+++ javatests/arcs/android/host/parcelables/ParcelableAnnotationParamTest.kt
@@ -38,8 +38,84 @@ class ParcelableAnnotationParamTest {
       readTypedObject(requireNotNull(ParcelableAnnotationParam.CREATOR))
     }
 
+    val remarshalled = with(Parcel.obtain()) {
+      writeTypedObject(unmarshalled, 0)
+      marshall()
+    }
+
     assertThat(unmarshalled?.describeContents()).isEqualTo(0)
     assertThat(unmarshalled?.actual).isEqualTo(annotationParam)
+    assertThat(remarshalled).isEqualTo(marshalled)
+  }
+
+  @Test
+  fun annotationParamBool_parcelableRoundTrip_works() {
+    val annotationParam = AnnotationParam.Bool(true)
+
+    val marshalled = with(Parcel.obtain()) {
+      writeTypedObject(annotationParam.toParcelable(), 0)
+      marshall()
+    }
+
+    val unmarshalled = with(Parcel.obtain()) {
+      unmarshall(marshalled, 0, marshalled.size)
+      setDataPosition(0)
+      readTypedObject(requireNotNull(ParcelableAnnotationParam.CREATOR))
+    }
+
+    val remarshalled = with(Parcel.obtain()) {
+      writeTypedObject(unmarshalled, 0)
+      marshall()
+    }
+
+    assertThat(unmarshalled?.describeContents()).isEqualTo(0)
+    assertThat(unmarshalled?.actual).isEqualTo(annotationParam)
+    assertThat(remarshalled).isEqualTo(marshalled)
+  }
+
+  @Test
+  fun annotationParamNum_parcelableRoundTrip_works() {
+    val annotationParam = AnnotationParam.Num(42)
+
+    val marshalled = with(Parcel.obtain()) {
+      writeTypedObject(annotationParam.toParcelable(), 0)
+      marshall()
+    }
+
+    val unmarshalled = with(Parcel.obtain()) {
+      unmarshall(marshalled, 0, marshalled.size)
+      setDataPosition(0)
+      readTypedObject(requireNotNull(ParcelableAnnotationParam.CREATOR))
+    }
+
+    val remarshalled = with(Parcel.obtain()) {
+      writeTypedObject(unmarshalled, 0)
+      marshall()
+    }
+
+    assertThat(unmarshalled?.describeContents()).isEqualTo(0)
+    assertThat(unmarshalled?.actual).isEqualTo(annotationParam)
+    assertThat(remarshalled).isEqualTo(marshalled)
+  }
+
+  @Test
+  fun annotationParamNum_parcelableDirectRoundTrip_works() {
+    val annotationParam = AnnotationParam.Num(42)
+
+    // Repeat test above calling directly into serialization methods.
+    val marshalled = with(Parcel.obtain()) {
+      annotationParam.toParcelable().writeToParcel(this, 0)
+      marshall()
+    }
+
+    val unmarshalled = with(Parcel.obtain()) {
+      unmarshall(marshalled, 0, marshalled.size)
+      setDataPosition(0)
+      ParcelableAnnotationParam.CREATOR.createFromParcel(this)
+    }
+
+    assertThat(unmarshalled.describeContents()).isEqualTo(0)
+    assertThat(unmarshalled.actual).isEqualTo(annotationParam)
   }
 
   @Test
@@ -58,7 +134,7 @@ class ParcelableAnnotationParamTest {
   }
 
   @Test
-  fun readTypedObject_malformedParcel_fails() {
+  fun readTypedObject_malformedMissingTypeParcel_fails() {
     val malformedParcel =
       Parcel.obtain().apply {
         writeInt(1) // Says value to come is non-empty.
@@ -69,6 +145,28 @@ class ParcelableAnnotationParamTest {
     assertFailsWith<IllegalArgumentException>("no name found") {
       malformedParcel.readTypedObject(requireNotNull(ParcelableAnnotationParam))
     }
+  }
+
+  @Test
+  fun readTypedObject_malformedNoTypeParcel_fails() {
+    val malformedParcel =
+      Parcel.obtain().apply {
+        writeInt(1) // Says value to come is non-empty.
+      }
+
+    malformedParcel.setDataPosition(0)
+    assertFailsWith<IllegalArgumentException>("no name found") {
+      malformedParcel.readTypedObject(requireNotNull(ParcelableAnnotationParam))
+    }
+  }
+
+  @Test
+  fun readTypedObject_emptyParcel_isNull() {
+    val emptyParcel = Parcel.obtain()
+
+    emptyParcel.setDataPosition(0)
+    val unmarshalled = emptyParcel.readTypedObject(requireNotNull(ParcelableAnnotationParam))
+    assertThat(unmarshalled).isEqualTo(null)
   }
 
   @Test
@@ -88,5 +186,35 @@ class ParcelableAnnotationParamTest {
 
     assertThat(unmarshalled?.size).isEqualTo(2)
     assertThat(unmarshalled?.get(0)?.actual).isEqualTo(annotationParam.actual)
+  }
+
+  @Test
+  fun emptyArray_parcelableRoundTrip_works() {
+    val arr = arrayOf<ParcelableAnnotationParam>()
+    val marshalled = with(Parcel.obtain()) {
+      writeTypedArray(arr, 0)
+      marshall()
+    }
+
+    val unmarshalled = with(Parcel.obtain()) {
+      unmarshall(marshalled, 0, marshalled.size)
+      setDataPosition(0)
+      createTypedArray(requireNotNull(ParcelableAnnotationParam.CREATOR))
+    }
+
+    assertThat(unmarshalled?.size).isEqualTo(0)
+  }
+
+  @Test
+  fun newArray_directCall_works() {
+    val arr0 = ParcelableAnnotationParam.CREATOR.newArray(0)
+    val arr1 = ParcelableAnnotationParam.CREATOR.newArray(1)
+    val arr3 = ParcelableAnnotationParam.CREATOR.newArray(3)
+
+    assertThat(arr0.size).isEqualTo(0)
+    assertThat(arr1.size).isEqualTo(1)
+    assertThat(arr3.size).isEqualTo(3)
+    assertThat(arr1.get(0)).isEqualTo(null)
+    assertThat(arr3.get(2)).isEqualTo(null)
   }
 }
