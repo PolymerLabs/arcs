8e5dc98890e58be76c3b51e4dc73cc6de77b39f1
Joshua Pratt <jp10010101010000@gmail.com>
2020-12-16 13:10:44 +1100

    Fix for intersection type checking

--- src/types/internal/schema.ts
+++ src/types/internal/schema.ts
@@ -221,15 +221,71 @@ export class Schema {
     return new Schema(names, fields, {refinement: Refinement.intersectionOf(schema1.refinement, schema2.refinement)});
   }
 
+  private static fieldTypeIntersection(field1: FieldType, field2: FieldType): FieldType|null {
+    const missingField1 = (field1 === null || field1 === undefined);
+    const missingField2 = (field2 === null || field2 === undefined);
+    if (missingField1 || missingField2) {
+      // TODO: Handle nullables
+      return null;
+    }
+    // TODO: non-eq Kinds?
+    if (field1.kind !== field2.kind) return null;
+    switch (field1.kind) {
+      case 'schema-collection': {
+        const intersectSchema = Schema.fieldTypeIntersection(field1.getFieldType(), field2.getFieldType());
+        if (!intersectSchema) {
+          return null;
+        }
+        return new CollectionField(intersectSchema);
+      }
+      case 'schema-reference': {
+        const intersectSchema = Schema.intersect(
+          field1.getEntityType().entitySchema, field2.getEntityType().entitySchema);
+        if (!intersectSchema) {
+          return null;
+        }
+        // Note: this is done because new EntityType(intersectSchema) causes circular dependency.
+        // tslint:disable-next-line: no-any
+        const inlineIntersectionLiteral: any = field1.getFieldType().toLiteral();
+        inlineIntersectionLiteral.model.entitySchema = intersectSchema;
+        return new ReferenceField(FieldType.create(inlineIntersectionLiteral));
+      }
+      case 'schema-nested': {
+        const intersectSchema = Schema.intersect(
+            field1.getEntityType().entitySchema, field2.getEntityType().entitySchema);
+        if (!intersectSchema) {
+          return null;
+        }
+        // Note: this is done because new EntityType(intersectSchema) causes circular dependency.
+        // tslint:disable-next-line: no-any
+        const inlineIntersectionLiteral: any = field1.getFieldType().toLiteral();
+        inlineIntersectionLiteral.model.entitySchema = intersectSchema;
+        return new NestedField(FieldType.create(inlineIntersectionLiteral));
+      }
+      case 'schema-ordered-list': {
+        const intersectSchema = Schema.fieldTypeIntersection(field1.getFieldType(), field2.getFieldType());
+        if (!intersectSchema) {
+          return null;
+        }
+        return new OrderedListField(intersectSchema);
+      }
+      default:
+        return Schema.typesEqual(field1, field2) ? field1 : null;
+    }
+  }
+
   static intersect(schema1: Schema, schema2: Schema): Schema {
     const names = [...schema1.names].filter(name => schema2.names.includes(name));
     const fields = {};
 
-    for (const [field, type] of Object.entries(schema1.fields)) {
+    const fieldNames = new Set([...Object.keys(schema1.fields), ...Object.keys(schema2.fields)]);
+    for (const field of fieldNames) {
+      const type = schema1.fields[field];
       const otherType = schema2.fields[field];
-      if (otherType && Schema.typesEqual(type, otherType)) {
-        fields[field] = {...type};
-        fields[field].refinement = Refinement.unionOf(type.refinement, otherType.refinement);
+      const intersectionType = Schema.fieldTypeIntersection(type, otherType);
+      if (intersectionType) {
+        fields[field] = intersectionType;
+        fields[field].refinement = Refinement.unionOf(type && type.refinement, otherType && otherType.refinement);
         fields[field].annotations = (type.annotations || []).filter(a => (otherType.annotations || []).includes(a));
       }
     }
