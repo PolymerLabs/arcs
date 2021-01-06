0a27f7bc4cbcac2bd247aadc800e2cda1ab14601
Alex Rosengarten <alxr@google.com>
2021-01-06 14:57:54 -0800

    Added stubs for tests.

---
+++ javatests/arcs/android/crdt/CrdtEntityProtoTest.kt
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2021 Google LLC.
+ *
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ *
+ * Code distributed by Google as part of this project is also subject to an additional IP rights
+ * grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+package arcs.android.crdt
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CrdtEntityProtoTest {
+
+  @Test
+  fun data_toData_dataPropagated() {
+
+  }
+
+  @Test
+  fun data_toProto_dataPropagated() {
+
+  }
+
+  @Test
+  fun data_roundTrip_dataToProto() {
+
+  }
+
+  @Test
+  fun data_roundTrip_protoToData() {
+
+  }
+
+  @Test
+  fun operation_toOperation_setSingleton() {
+
+  }
+
+  @Test
+  fun operation_toOperation_clearSingleton() {
+
+  }
+
+  @Test
+  fun operation_toOperation_addToSet() {
+
+  }
+
+  @Test
+  fun operation_toOperation_removeFromSet() {
+
+  }
+
+  @Test
+  fun operation_toOperation_clearAll() {
+
+  }
+
+  @Test
+  fun operation_toOperation_operationNotSet() {
+
+  }
+
+  @Test
+  fun operation_toProto_setSingleton() {
+
+  }
+
+  @Test
+  fun operation_toProto_clearSingleton() {
+
+  }
+
+  @Test
+  fun operation_toProto_addToSet() {
+
+  }
+
+  @Test
+  fun operation_toProto_removeFromSet() {
+
+  }
+
+  @Test
+  fun operation_toProto_clearAll() {
+
+  }
+
+  @Test
+  fun operation_toProto_operationNotSet() {
+
+  }
+
+  @Test
+  fun operation_roundTrip_operationToProto() {
+
+  }
+
+  @Test
+  fun operation_roundTrip_protoToOperation() {
+
+  }
+}
