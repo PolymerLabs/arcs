101fd27d0e9c1bdd992e1883822d532e2fd2e6ab
Alex Rosengarten <alxr@google.com>
2021-01-25 15:43:07 -0800

    CrdtEntityProto Tests (#6825)
    - Removed dead code + added a license comment
    - Added all tests except for `Unset Operation --> Proto` test, because this code is currently unreachable
    
    Closes #6825
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/polymerlabs/arcs/pull/6825 from alxrsngrtn:crdt-ent-proto-test 923e8b5d201a4602994bec2e77da160e95e80eba
    PiperOrigin-RevId: 353746668

--- java/arcs/android/crdt/CrdtEntityProto.kt
+++ java/arcs/android/crdt/CrdtEntityProto.kt
@@ -1,7 +1,15 @@
+/*
+ * Copyright 2021 Google LLC.
+ *
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ *
+ * Code distributed by Google as part of this project is also subject to an additional IP rights
+ * grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
 package arcs.android.crdt
 
-import android.os.Parcel
-import arcs.android.util.readProto
 import arcs.core.crdt.CrdtEntity
 import arcs.core.crdt.CrdtSet
 import arcs.core.crdt.CrdtSingleton
@@ -124,11 +132,3 @@ fun CrdtEntity.Operation.toProto(): CrdtEntityProto.Operation {
   }
   return proto.build()
 }
-
-/** Reads a [CrdtEntity.Data] out of a [Parcel]. */
-fun Parcel.readCrdtEntityData(): CrdtEntity.Data? =
-  readProto(CrdtEntityProto.Data.getDefaultInstance())?.toData()
-
-/** Reads a [CrdtEntity.Operation] out of a [Parcel]. */
-fun Parcel.readCrdtEntityOperation(): CrdtEntity.Operation? =
-  readProto(CrdtEntityProto.Operation.getDefaultInstance())?.toOperation()
