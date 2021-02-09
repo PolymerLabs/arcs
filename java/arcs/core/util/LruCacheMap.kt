8a5fb9892af2ed75c2253b8c43a5df391d0a0fee
Ray Cromwell <cromwellian@google.com>
2021-02-08 16:44:12 -0800

    Add Ttl based eviction to LruCacheMap
    To use, pass a TtlConfig() to LruCacheMap with a TTL > 0. You must also pass a lambda that returns the current time.  Entries are cleaned up over time whenever a cache fetch is initiated.
    
    PiperOrigin-RevId: 356382716

--- java/arcs/core/util/LruCacheMap.kt
+++ java/arcs/core/util/LruCacheMap.kt
@@ -25,40 +25,89 @@ package arcs.core.util
 open class LruCacheMap<K, V>(
   val capacity: Int = 100,
   private val backingMap: LinkedHashMap<K, V> = linkedMapOf(),
+  val ttlConfig: TtlConfig = TtlConfig(),
   val livenessPredicate: ((K, V) -> Boolean) = { _, _ -> true },
   val onEvict: ((K, V) -> Unit)? = null
 ) : MutableMap<K, V> by backingMap {
+  /**
+   * Specifies the ttl for a cache entry in millis, or 0 for never, and a function that returns
+   * the current time in millis.
+   */
+  data class TtlConfig(val ttlMilliseconds: Long, val currentTimeMillis: () -> Long) {
+    constructor() : this(0, { -1L })
+  }
+
+  private val ttlMap: MutableMap<K, Long> = mutableMapOf()
+
+  private val ttlEnabled get() = ttlConfig.ttlMilliseconds > 0
+
+  private fun now() = ttlConfig.currentTimeMillis()
+
+  private fun expired(key: K, now: Long): Boolean {
+    val ttl = requireNotNull(ttlMap[key]) {
+      "Missing ttl entry for $key"
+    }
+    return ttlEnabled && now - ttl > ttlConfig.ttlMilliseconds
+  }
+
+  private fun updateEntry(key: K, value: V, now: Long) {
+    backingMap[key] = value
+    ttlMap[key] = now
+  }
+
+  private fun postRemoveEntry(key: K, value: V) {
+    ttlMap.remove(key)
+    onEvict?.let { it(key, value) }
+  }
+
+  private fun expireEntries() {
+    if (ttlEnabled) {
+      val now = now()
+      val iterator = backingMap.entries.iterator()
+      while (iterator.hasNext()) {
+        val entry = iterator.next()
+        if (expired(entry.key, now)) {
+          iterator.remove()
+          postRemoveEntry(entry.key, entry.value)
+        } else {
+          break
+        }
+      }
+    }
+  }
 
   override fun put(key: K, value: V): V? {
-    val previousValue = backingMap.get(key)
+    val previousValue = backingMap[key]
     if (backingMap.size >= capacity) {
       val iterator = backingMap.entries.iterator()
       repeat(backingMap.size - capacity + 1) {
         if (iterator.hasNext()) {
           val entry = iterator.next()
           iterator.remove()
-          onEvict?.let { it(entry.key, entry.value) }
+          postRemoveEntry(entry.key, entry.value)
         } else {
           return@repeat
         }
       }
     }
-    backingMap.put(key, value)
+    updateEntry(key, value, now())
     return previousValue
   }
 
   override fun get(key: K): V? {
-    val value = backingMap.get(key)?.let {
+    return backingMap[key]?.let {
       // remove and put moves entry to end of internal linked list
       backingMap.remove(key)
-      if (livenessPredicate(key, it)) {
-        backingMap.put(key, it)
-        return it
+      val now = now()
+      val value = if (this.livenessPredicate(key, it) && !expired(key, now)) {
+        updateEntry(key, it, now)
+        it
       } else {
-        onEvict?.invoke(key, it)
-        return null
+        postRemoveEntry(key, it)
+        null
       }
+      expireEntries()
+      return value
     }
-    return value
   }
 }
