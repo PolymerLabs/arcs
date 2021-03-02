e29762b63c1ff2c4f0f3159907d23210625ab414
J Pratt <jp10010101010000@gmail.com>
2021-03-02 15:03:29 +1100

    Lints

--- javatests/arcs/showcase/nullable/Invited.kt
+++ javatests/arcs/showcase/nullable/Invited.kt
@@ -5,6 +5,7 @@ package arcs.showcase.nullable
 import arcs.jvm.host.TargetHost
 
 typealias Guest = AbstractInvited.Guest
+typealias Name = AbstractInvited.Guest.Name
 
 @TargetHost(arcs.android.integration.IntegrationHost::class)
 class Invited : AbstractInvited() {
@@ -14,39 +15,39 @@ class Invited : AbstractInvited() {
         Guest(
           name = Name {
             legal = "Ms. Jane Smith",
-            first = "Jane",
-            last = "Smith"
+            first_name = "Jane",
+            last_name = "Smith"
           }
           employee_id = 30125,
-          rsvp = true,
+          rsvp = true
         ),
         Guest(
           name = Name {
             legal = "Mr. John Smith",
-            first = "John",
-            last = "Smith"
+            first_name = "John",
+            last_name = "Smith"
           }
           employee_id = null,
-          rsvp = null,
+          rsvp = null
         ),
         Guest(
           name = Name {
             legal = "Walter Bruce Willis",
-            first = "Bruce"
+            first_name = "Bruce"
           }
           employee_id = null,
-          rsvp = false,
+          rsvp = false
         ),
         Guest(
           name = null,
           employee_id = 1,
-          rsvp = true,
+          rsvp = true
         ),
         Guest(
           name = null,
           employee_id = 2,
-          rsvp = false,
-        ),
+          rsvp = false
+        )
       )
     )
   }
