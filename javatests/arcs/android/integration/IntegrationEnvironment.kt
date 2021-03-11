d08bd3e771d686fe2d966fba959d887582022cf0
Alex Rosengarten <alxr@google.com>
2021-03-10 16:30:05 -0800

    Reverted code that was more suited for unit tests, not integration.

--- javatests/arcs/android/integration/IntegrationEnvironment.kt
+++ javatests/arcs/android/integration/IntegrationEnvironment.kt
@@ -12,32 +12,20 @@ import arcs.android.storage.database.DatabaseGarbageCollectionPeriodicTask
 import arcs.android.storage.ttl.PeriodicCleanupTask
 import arcs.core.allocator.Allocator
 import arcs.core.allocator.Arc
-import arcs.core.data.Capability
 import arcs.core.data.Plan
 import arcs.core.data.Schema
 import arcs.core.entity.ForeignReferenceChecker
 import arcs.core.entity.ForeignReferenceCheckerImpl
-import arcs.core.entity.Handle
-import arcs.core.entity.HandleSpec
 import arcs.core.host.AbstractArcHost
 import arcs.core.host.ArcHost
-import arcs.core.host.HandleManager
 import arcs.core.host.HandleManagerFactory
-import arcs.core.host.HandleManagerImpl
 import arcs.core.host.NoOpArcHostContextSerializer
 import arcs.core.host.ParticleRegistration
 import arcs.core.host.ParticleState
-import arcs.core.host.SchedulerProvider
 import arcs.core.host.SimpleSchedulerProvider
-import arcs.core.storage.StorageEndpointManager
-import arcs.core.storage.StorageKey
-import arcs.core.storage.StorageProxy
 import arcs.core.storage.api.DriverAndKeyConfigurator
-import arcs.core.storage.database.DatabaseData
 import arcs.core.storage.driver.RamDisk
-import arcs.core.util.Scheduler
 import arcs.core.util.TaggedLog
-import arcs.core.util.Time
 import arcs.jvm.host.ExplicitHostRegistry
 import arcs.jvm.util.JvmTime
 import arcs.sdk.Particle
@@ -136,7 +124,6 @@ class IntegrationEnvironment(
   private lateinit var allocator: Allocator
   lateinit var hostRegistry: ExplicitHostRegistry
   private lateinit var dbManager: AndroidSqliteDatabaseManager
-  private lateinit var handleManagerFactory: HandleManagerFactory
 
   private val startedArcs = mutableListOf<Arc>()
   private val testScope = TestCoroutineScope()
@@ -300,7 +287,7 @@ class IntegrationEnvironment(
       periodicWorkConfig
     )
 
-    handleManagerFactory = CachedHandleManagerFactory(
+    val handleManagerFactory = HandleManagerFactory(
       schedulerProvider = schedulerProvider,
       storageEndpointManager = storageEndpointManager,
       platformTime = JvmTime,
@@ -405,98 +392,6 @@ class IntegrationEnvironment(
 
   class IntegrationStorageService : StorageService() {
     fun changeConfig(config: StorageServiceConfig) = schedulePeriodicJobs(config)
-  }
-
-  /** Returns a collection of [CreateHandleArgs] that arose from starting the [Arc]. */
-  fun getCreateHandleArgs(): List<CreateHandleArgs> {
-    val handleManager = requireNotNull(
-      (handleManagerFactory as CachedHandleManagerFactory).handleManager
-    ) {
-      "HandleManagerImpl has not yet been initialized! Make sure startArc() has been called."
-    }
-    return handleManager.createHandleArguments.toList()
-  }
-
-  /** Get [DatabaseData.Entity]s from a Collection in storage. */
-  suspend fun getDatabaseEntities(
-    startingKey: StorageKey,
-    fullSchema: Schema
-  ): List<DatabaseData.Entity> {
-    return dbManager.registry.fetchAll()
-      .map { dbManager.getDatabase(it.name, it.isPersistent) }
-      .flatMap { db ->
-        val collection = db.get(startingKey, DatabaseData.Collection::class, fullSchema)
-          as DatabaseData.Collection?
-
-        // Return associated entities with the collection.
-        collection?.values?.mapNotNull {
-          db.get(it.rawReference.referencedStorageKey(), DatabaseData.Entity::class, fullSchema)
-            as DatabaseData.Entity?
-        }
-          ?: emptyList()
-      }
-  }
-}
-
-/** A [HandleManager] that exposes data about created [Handle]s. */
-class IntegrationHandleManager(private val handleManagerImpl: HandleManagerImpl) : HandleManager {
-  val createHandleArguments = mutableListOf<CreateHandleArgs>()
-
-  override suspend fun createHandle(
-    spec: HandleSpec,
-    storageKey: StorageKey,
-    ttl: Capability.Ttl,
-    particleId: String,
-    immediateSync: Boolean,
-    storeSchema: Schema?,
-    actor: String?
-  ): Handle {
-    createHandleArguments.add(
-      CreateHandleArgs(spec, storageKey, ttl, particleId, storeSchema, actor)
-    )
-    return handleManagerImpl.createHandle(
-      spec, storageKey, ttl, particleId, immediateSync, storeSchema, actor
-    )
-  }
-
-  override fun scheduler(): Scheduler = handleManagerImpl.scheduler()
-
-  override suspend fun close() = handleManagerImpl.close()
-
-  override suspend fun allStorageProxies(): List<StorageProxy<*, *, *>> =
-    handleManagerImpl.allStorageProxies()
-}
-
-/** A record of arguments passed to [HandleManager.createHandle]. */
-data class CreateHandleArgs(
-  val spec: HandleSpec,
-  val storageKey: StorageKey,
-  val ttl: Capability.Ttl,
-  val particleId: String,
-  val storeSchema: Schema?,
-  val actor: String?
-)
-
-/** A [HandleManagerFactory] that caches and exposes the [HandleManagerImpl]. */
-class CachedHandleManagerFactory(
-  schedulerProvider: SchedulerProvider,
-  storageEndpointManager: StorageEndpointManager,
-  platformTime: Time,
-  foreignReferenceChecker: ForeignReferenceChecker = ForeignReferenceCheckerImpl(
-    emptyMap()
-  )
-) : HandleManagerFactory(
-  schedulerProvider,
-  storageEndpointManager,
-  platformTime,
-  foreignReferenceChecker = foreignReferenceChecker
-) {
-  lateinit var handleManager: IntegrationHandleManager
-
-  @Suppress("UNCHECKED_CAST")
-  override fun build(arcId: String, hostId: String): HandleManager {
-    handleManager = IntegrationHandleManager(super.build(arcId, hostId) as HandleManagerImpl)
-    return handleManager
   }
 }
 
