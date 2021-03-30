82f98562af7090eb7f1bdb455142eea823a02aac
Josh Pratt <jopra@google.com>
2021-03-29 23:15:42 -0700

    Enforce type restrictions for ReferencablePrimitive
    PiperOrigin-RevId: 365744584

--- java/arcs/core/crdt/testutil/CrdtEntityTestExtensions.kt
+++ java/arcs/core/crdt/testutil/CrdtEntityTestExtensions.kt
@@ -16,11 +16,11 @@ import arcs.core.data.RawEntity
 import arcs.core.data.util.ReferencableList
 import arcs.core.data.util.ReferencablePrimitive
 
-fun <T> CrdtEntity.Data.primitiveSingletonValue(field: String): T {
+fun <T : Any> CrdtEntity.Data.primitiveSingletonValue(field: String): T {
   return (singletons[field]!!.consumerView!!.unwrap() as ReferencablePrimitive<T>).value
 }
 
-fun <T> CrdtEntity.Data.primitiveSingletonListValue(field: String): List<T> {
+fun <T : Any> CrdtEntity.Data.primitiveSingletonListValue(field: String): List<T> {
   return (singletons[field]!!.consumerView!!.unwrap() as ReferencableList<ReferencablePrimitive<T>>)
     .value.map { it.value }
 }
