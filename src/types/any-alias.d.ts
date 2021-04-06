e9e3881c7f4a1bc7f6224b6e275cc2f8ac3fbb92
Alex Rosengarten <alxr@google.com>
2021-04-06 15:51:02 -0700

    Added type alias to support internal migration.

---
+++ src/types/any-alias.d.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2021 Google LLC.
+ *
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ *
+ * Code distributed by Google as part of this project is also subject to an additional IP rights
+ * grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+/** TODO(b/181591179) Remove after migration is resolved. */
+type AnyDuringTs42Migration = any;
